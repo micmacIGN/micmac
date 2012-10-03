457ce5c0c9bcc43cc82baeae660ad9676874cf5a
gchoqueux <gchoqueux@localhost>
2012-10-03 16:56:07 +0200

    ajout 1 fichier BAT pour effectuer des tests sur Windows

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ cd build
 cmake ..
 make install
 cd ..
-mkdir data
-cd data
-svn co http://www.micmac.ign.fr/svn/micmac_data/trunk/ExempleDoc/Boudha/
-cd ..
 chmod +x ./binaire-aux/siftpp_tgi.LINUX
 chmod +x ./binaire-aux/ann_mec_filtre.LINUX
+
