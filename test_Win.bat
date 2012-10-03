f2407c6e154dc837ba998592a28edc63e2aca979
gchoqueux <gchoqueux@localhost>
2012-10-03 17:17:33 +0200

    Modification fichier de test Windows

--- test_Win.bat
+++ test_Win.bat
@@ -1,8 +1,8 @@
 mkdir data
 cd data
 svn co http://www.micmac.ign.fr/svn/micmac_data/trunk/ExempleDoc/Boudha/
-cd ../bin
-Tapioca MulScale ../data/Boudha/IMG_[0-9]{4}.tif 300 -1 ExpTxt=1
-Apero  ../data/Boudha/Apero-5.xml
-MICMAC  ../data/Boudha/Param-6-Ter.xml
+cd ..\bin
+Tapioca MulScale ..\data\Boudha\IMG_[0-9]{4}.tif 300 -1 ExpTxt=1
+Apero  ..\data\Boudha\Apero-5.xml
+MICMAC  ..\data\Boudha\Param-6-Ter.xml
 
