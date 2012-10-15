d60fc9b371a9f6d179537c5797fa9610e8b140b7
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2012-10-15 15:42:38 +0200

    Choix du make clean dans le build_Unix.sh

--- build_Unix.sh
+++ build_Unix.sh
@@ -1,10 +1,23 @@
-rm -rf bin
-mkdir build
+#rm -rf bin
+#mkdir build
 cd build
 cmake ..
-make clean
+#read -p "Effectuer un make clean o/n " reponse
+echo "Voulez-vous effectuer MAKE CLEAN [o/n] ?"
+read REP
+
+case $REP in 	
+	O|o)
+        	echo "Make CLEAN"
+		make clean
+        	;;
+	N|n|*)
+                echo "Pas de Make CLEAN"
+                ;;
+esac
+
 NBRP=$(cat /proc/cpuinfo | grep processor | wc -l)
-echo "Nbre de coeurs : " $NBRP
+echo "Nbre de coeurs à la compilation : " $NBRP
 make install -j$NBRP
 cd ..
 chmod +x ./binaire-aux/siftpp_tgi.LINUX
