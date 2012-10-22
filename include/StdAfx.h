b5a0ce0e1d7881e337773e4cd450b2fcbe9ae5b4
deseilligny <deseilligny@users.noreply.github.com>
2012-10-22 19:43:30 +0200

    iiii

--- include/StdAfx.h
+++ include/StdAfx.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "general/all.h"
+#include "private/all.h"
+
 #include <algorithm>
 #include <cfloat>
 #include <cmath>
@@ -10,123 +13,28 @@
 #include <fstream>
 #include <iomanip>
 #include <iterator>
-#include <list>
 #include <map>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <set>
-#include <vector>
-#include <strstream>
-#include <iostream>
-#include <sstream>
 
-extern bool BugDG;
 
-#define ELISE_INSERT_CODE_GEN 1
-
-#ifdef _WIN32
-	#define USE_NOYAU 1
-	#define ELISE_unix 0
-	#define ELISE_windows 1
-        #define ELISE_MacOs 0
-        #define ELISE_Cygwin 0
-#elif __APPLE__
-	#define USE_NOYAU 0
-	#define ELISE_unix 0
-	#define ELISE_MacOs 1
-	#define ELISE_windows 0
-        #define ELISE_Cygwin 0
-#elif __CYGWIN__
-       #define USE_NOYAU 0
-       #define ELISE_unix 0
-       #define ELISE_MacOs 0
-       #define ELISE_windows 0
-       #define ELISE_Cygwin 1
-#else
-       #define USE_NOYAU 0
-       #define ELISE_unix 1
-       #define ELISE_MacOs 0
-       #define ELISE_windows 0
-       #define ELISE_Cygwin 0
+#if (ELISE_windows)
+	#ifdef INT
+		#undef INT
+	#endif
+	#include "Windows.h"
+	#include "winbase.h"
+	#include "direct.h"
 #endif
-#include "general/all.h"
-#include "private/all.h"
 
-//#include "general/MM_InstalDir.h"
-//#include "general/sys_dep.h"
-//#include "general/opt_debug.h"
-//#include "general/util.h"
-//#include "general/ptxd.h"
-
-//#include "api/el_regex.h"
-
-//#include "general/allocation.h"
-//#include "general/garb_coll_pub.h"
-//#include "general/abstract_types.h"
-//#include "general/bitm.h"
-//#include "general/tabulation.h"
-
-
-
-/*
-#include "private/util.h"
-#include "private/garb_coll_private.h"
-#include "private/colour.h"
-#include "private/garb_coll_tpl.h"
-#include "private/flux_pts.h"
-#include "private/flux_pts_tpl.h"
-#include "private/fonc_num.h"
-#include "private/fonc_num_tpl.h"
-#include "private/output.h"
-#include "private/bitm_def.h"
-#include "private/bitm_tpl.h"
-#include "private/bitm_bits.h"
-#include "private/bitm_comp.h"
-#include "private/neihgbour_rel.h"
-#include "private/graphics.h"
-#include "private/gen_window.h"
-#include "private/plot1d.h"
-#include "private/files.h"
-#include "private/tiff.h"
-#include "private/func_opbinmix_tpl.h"
-#include "private/op_buf.h"
-#include "private/hassan.h"
-#include "private/orilib.h"
-#include "private/morpho.h"
-#include "private/post_script.h"
-#include "private/compr_im.h"
-#include "private/recipes.h"
-#include "private/cElNuage3DMaille.h"
-#include "private/TraitRadiom.h"
-*/
-
-
-// #include "private/garb_coll_tpl.h"
-// #include "private/flux_pts.h"
-// #include "private/flux_pts_tpl.h"
-
-#include "im_tpl/flux.h"
-
-// #include "private/bitm_def.h"
-// #include "private/bitm_bits.h"
-
-#include "ext_stl/fixed.h"
-
-#include "im_tpl/image.h"
 
 #include "XML_GEN/all.h"
+#include "XML_GEN/SuperposImage.h"
+#include "XML_GEN/ParamChantierPhotogram.h"
 
 #include "api/vecto.h"
-
-// TODO : these global functions and classes should be placed somewhere else
-// see all.cpp for definitions
-
-extern std::string NoInit;
-extern Pt2dr	   aNoPt;
-Im2DGen AllocImGen(Pt2di aSz,const std::string & aName);
-
-// ---------
 
 #if(ELISE_unix)
 	#include <cstring>
@@ -142,47 +50,33 @@ Im2DGen AllocImGen(Pt2di aSz,const std::string & aName);
 #include "general/optim.h"
 #include "general/abstract_types.h"
 #include "graphes/graphe.h"
-#include "general/phgr_formel.h"
+
+#include "algo_geom/qdt.h"
+#include "algo_geom/qdt_implem.h"
+#include "algo_geom/rvois.h"
+#include "algo_geom/delaunay_mediatrice.h"
+#include "algo_geom/qdt_insertobj.h"
+#include "algo_geom/Shewchuk.h"
 
 #include "api/vecto.h"
 #include "api/cox_roy.h"
 #include "api/el_regex.h"
 
-#include "graphes/algo_pcc.h"
-
-#include "im_tpl/correl_imget.h"
-#include "im_tpl/cPtOfCorrel.h"
-#include "im_tpl/max_loc.h"
-#include "im_tpl/correl_imget_ptr.h"
-#include "im_tpl/algo_filter_exp.h"
-#include "im_tpl/oper_assoc_exter.h"
-#include "im_tpl/algo_dist32.h"
-#include "im_tpl/ex_oper_assoc_exter.h"
-#include "im_tpl/elise_ex_oper_assoc_exter.h"
-#include "ext_stl/Nappes.h"
-#include "im_tpl/ProgDyn2D.h"
-#include "im_tpl/algo_cc.h"
-
 #include "ext_stl/fifo.h"
+#include "ext_stl/fixed.h"
 #include "ext_stl/intheap.h"
 #include "ext_stl/pack_list.h"
 #include "ext_stl/numeric.h"
-#include "ext_stl/cluster.h"
-#include "ext_stl/tab2D_dyn.h"
-#include "ext_stl/appli_tab.h"
-#include "ext_stl/elslist.h"
 
 #include "graphes/graphe.h"
 #include "graphes/graphe_implem.h"
 #include "graphes/algo_planarite.h"
+#include "graphes/algo_pcc.h"
 
-#include "algo_geom/qdt.h"
-#include "algo_geom/qdt_implem.h"
-#include "algo_geom/rvois.h"
-#include "algo_geom/integer_delaunay_mediatrice.h"
-#include "algo_geom/delaunay_mediatrice.h"
-#include "algo_geom/qdt_insertobj.h"
-#include "algo_geom/Shewchuk.h"
+#include "im_tpl/correl_imget.h"
+#include "im_tpl/cPtOfCorrel.h"
+#include "im_tpl/image.h"
+#include "im_tpl/max_loc.h"
 
 #include "im_special/hough.h"
 
@@ -195,10 +89,15 @@ Im2DGen AllocImGen(Pt2di aSz,const std::string & aName);
 #include "../src/HassanArrangt/cElHJa_all.h"
 #include "../src/hough/hough_include.h"
 
+
 #include "../src/uti_image/MpDcraw/MpDcraw.h"
+
+
 
 #include "../src/uti_phgrm/ReducHom/ReducHom.h"
 #include "../src/uti_phgrm/Apero/cParamApero.h"
 #include "../src/uti_phgrm/Apero/Apero.h"
 #include "../src/uti_phgrm/MICMAC/MICMAC.h"
 #include "../src/uti_phgrm/Porto/Porto.h"
+
+
