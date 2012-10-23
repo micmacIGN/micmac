afd86ca819d8b97e9d6e96c94deb8dd43807b581
gmaillet <gmaillet@users.noreply.github.com>
2012-10-23 16:23:10 +0200

    correction d un dble include StdAfx.h

---
+++ src/uti_files/Sources.cmake
@@ -0,0 +1,11 @@
+set(uti_files_Src_Files
+    ${UTI_FILES_DIR}/CPP_MapCmd.cpp
+    ${UTI_FILES_DIR}/CPP_BatchFDC.cpp
+)
+
+SOURCE_GROUP(uti_files FILES ${uti_files_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${uti_files_Src_Files}
+)
