f1255ebe7ecd6f79b9ecf8fd1315f0f3f902fc37
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2012-10-24 09:55:01 +0200

    Ajout de la compilation multi-CPU dans le script Jenkins

--- script_jenkins_unix.sh
+++ script_jenkins_unix.sh
@@ -2,6 +2,10 @@ rm -rf build
 mkdir build
 cd build
 cmake ..
-make 
+NBRP=$(cat /proc/cpuinfo | grep processor | wc -l)
+make -j$NBRP
 make install
+cd ..
+chmod +x ./binaire-aux/siftpp_tgi.LINUX
+chmod +x ./binaire-aux/ann_mec_filtre.LINUX
 
