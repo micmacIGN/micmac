b127ad19b856bd6e9690d978c7370e65f7dfae99
JBelvaux <JBelvaux@ASU1210P002.ign.fr>
2012-12-04 11:36:06 +0100

    - ajoute l'interface graphique (pas encore op?rationnelle sous windows)

---
+++ src/ras_vect/seg_dr.cpp
@@ -0,0 +1,556 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+   
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in 
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte 
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#include "StdAfx.h"
+
+
+
+
+        //==================================================
+
+
+
+
+
+
+
+// static const INT NBB = 8;
+#define NBB 8
+
+/**********************************************************/
+/*                                                        */
+/*       SCORES                                           */
+/*                                                        */
+/**********************************************************/
+
+/*
+    ????????
+
+    Ces lignes ont le pouvoir "magique" de supprimer un
+    internal bug de G++ 
+    (gcc version egcs-2.90.29 980515 (egcs-1.0.3 release)     )
+*/
+
+template <class Type,class TypeBase>  void f()
+{
+    Pt2dr p1;
+    Pt2dr p2;
+    ElPFixed<NBB> pf1 (p1);
+    ElPFixed<NBB> pf2 (p2);    
+    ElSegIter<NBB>  Seg (pf1,pf2,44); 
+    Seg.next(pf1);
+}
+void g()
+{
+    f<U_INT1,INT>();
+}
+
+/* fin ???????? */
+
+
+
+
+template <class Type,class TypeBase> 
+REAL FixedSomSegDr
+     (
+             TIm2D<Type,TypeBase>& Tim,
+             Pt2dr p1,
+             Pt2dr p2,
+             INT NBPts,
+             REAL DefOut
+     )
+{
+    ElPFixed<NBB> pf1 (p1);
+    ElPFixed<NBB> pf2 (p2);    
+    if ( 
+               (! pf1.inside(Pt2di(0,0),Tim.sz()-Pt2di(1,1)))
+           ||  (! pf2.inside(Pt2di(0,0),Tim.sz()-Pt2di(1,1)))
+       )
+       return DefOut;
+
+    ElSegIter<NBB>  Seg (pf1,pf2,NBPts); 
+
+    INT res = 0;
+    ElPFixed<NBB> pcur; 
+    while(Seg.next(pcur))
+    {                      
+		res += (TImGet<Type,TypeBase,NBB>::getb2(Tim,pcur)>>NBB);
+    }
+    return res / (REAL) (1<<NBB);
+}
+
+
+template <class Type,class TypeBase> 
+REAL FixedSomSegDr
+     (
+         Im2D<Type,TypeBase> im,
+         Pt2dr p1,
+         Pt2dr p2,
+         INT NBPts,
+         REAL DefOut
+    )
+{
+     TIm2D<Type,TypeBase> Tim(im);
+
+     return FixedSomSegDr(Tim,p1,p2,NBPts,DefOut);
+}
+
+
+/*
+template <class Type,class TypeBase> 
+REAL FixedSomScalSegDr
+     (
+             TIm2D<Type,TypeBase>& TimX,
+             TIm2D<Type,TypeBase>& TimY,
+             Pt2dr p1,
+             Pt2dr p2,
+             INT NBPts,
+             REAL DefOut
+     )
+{
+    Pt2di aSzIm = Inf(TimX.sz(),TimY.sz());
+    ElPFixed<NBB> pf1 (p1);
+    ElPFixed<NBB> pf2 (p2);    
+    if ( 
+               (! pf1.inside(Pt2di(0,0),aSzIm-Pt2di(1,1)))
+           ||  (! pf2.inside(Pt2di(0,0),aSzIm-Pt2di(1,1)))
+       )
+       return DefOut;
+
+    ElSegIter<NBB>  Seg (pf1,pf2,NBPts); 
+
+    INT res = 0;
+    ElPFixed<NBB> pcur; 
+    while(Seg.next(pcur))
+    {                      
+		res += (TImGet<Type,TypeBase,NBB>::getb2(Tim,pcur)>>NBB);
+    }
+    return res / (REAL) (1<<NBB);
+}
+
+
+template <class Type,class TypeBase> 
+REAL FixedSomSegDr
+     (
+         Im2D<Type,TypeBase> im,
+         Pt2dr p1,
+         Pt2dr p2,
+         INT NBPts,
+         REAL DefOut
+    )
+{
+     TIm2D<Type,TypeBase> Tim(im);
+
+     return FixedSomSegDr(Tim,p1,p2,NBPts,DefOut);
+}
+*/
+
+/**********************************************************/
+/*                                                        */
+/*        PRIMITIVES                                      */
+/*                                                        */
+/**********************************************************/
+
+class SegTournant
+{
+      public :
+         SegTournant(Pt2dr p1,Pt2dr p2,REAL step);
+
+         Seg2d  SegParam(REAL Pabs,REAL Pteta) 
+         {
+              Pt2dr vec = _vec*Pt2dr::FromPolar(1.0,Pteta*_step_teta);
+              Pt2dr milieu = _milieu + _norm * (Pabs*_step_abs);
+              return Seg2d(milieu-vec,milieu+vec);
+         }
+
+      private :
+
+          REAL  _step_teta;
+          REAL  _step_abs;
+          Pt2dr _milieu;
+          Pt2dr _vec;
+          Pt2dr _norm;
+};
+
+SegTournant::SegTournant(Pt2dr p1,Pt2dr p2,REAL step) :
+     _step_teta  (step*2.0/ euclid(p1,p2)),
+     _step_abs   (step),
+     _milieu     ((p1+p2)/2.0),
+     _vec        ((p2-p1)/2.0),
+     _norm       (rot90(vunit(_vec)))
+{
+}
+
+
+/**********************************************************/
+/*                                                        */
+/*        CLASSE D'OPTIMISATION GENERALE                  */
+/*                                                        */
+/**********************************************************/
+
+class ElMEM_K_POSSIBLE
+{
+   public :
+      friend class ElTAB_MEM_K_POSSIBLE;
+
+      INT nbk() {return _nbk;}
+      INT JiemeK(INT j) {return _kpos[j];}
+
+   private :
+      void  init(Pt2di p);
+      INT   _nbk;
+      INT   _kpos[9];
+};
+
+class  ElTAB_MEM_K_POSSIBLE
+{
+       public :
+            static INT k0()   {return 9;}
+            static INT no_k() {return -1;}
+            static ElTAB_MEM_K_POSSIBLE  THE_ONE;
+
+            static ElMEM_K_POSSIBLE & kpos(INT k)
+            {return THE_ONE._mem_kpos[k];}
+
+       private :
+
+             ElTAB_MEM_K_POSSIBLE();
+             ElMEM_K_POSSIBLE _mem_kpos[10];
+};
+
+
+
+
+
+void ElMEM_K_POSSIBLE::init(Pt2di p)
+{
+    _nbk = 0;
+    for (INT k =0; k<9; k++)
+       if ( dist8(VOIS_9[k]+p) > 1)
+          _kpos[_nbk++] = k;
+}                         
+
+ElTAB_MEM_K_POSSIBLE  ElTAB_MEM_K_POSSIBLE::THE_ONE;
+
+ElTAB_MEM_K_POSSIBLE::ElTAB_MEM_K_POSSIBLE()
+{
+    for (INT k =0 ; k<9 ; k++)
+        _mem_kpos[k].init(VOIS_9[k]);
+      _mem_kpos[k0()].init(Pt2di(1000,3000));
+}               
+
+        //==================================================
+
+
+
+Optim2DParam::Optim2DParam
+(
+       REAL     step_lim,
+       REAL     def_out,
+       REAL     epsilon,
+       bool     Maxim,
+       REAL     lambda,
+       bool     optim_p1,
+       bool     optim_p2
+) :
+    _step_lim     (step_lim),
+    _step_cur     (1.0),
+    _param        (0,0),
+    _def_out      (def_out),
+    _epsilon      (epsilon),
+    _Maxim        (Maxim),
+    _lambda       (lambda),
+    _optim_p1     (optim_p1),
+    _optim_p2     (optim_p2),
+    mFreelyOpt    (true),
+    mStepInit     (1.0),
+    mSzVoisInit   (1)
+{
+}
+
+void Optim2DParam::set(REAL aStepInit,INT aSzVoisInit) 
+{
+     mStepInit   = aStepInit;
+     mSzVoisInit = aSzVoisInit;
+}
+
+void Optim2DParam::reset()
+{
+    _step_cur = mStepInit;
+    _param  =  Pt2dr(0,0);
+    mFreelyOpt =true;
+    mScOpt = _def_out;
+}
+
+
+void  Optim2DParam::optim_step_fixed ()
+{
+     REAL score_opt = _def_out;
+     INT last_k = ElTAB_MEM_K_POSSIBLE::k0();
+
+     for(;;)
+     {
+          ElMEM_K_POSSIBLE & kpos = ElTAB_MEM_K_POSSIBLE::kpos(last_k);
+          last_k                  = ElTAB_MEM_K_POSSIBLE::no_k();
+
+          INT j,k;     REAL score_cur;
+          for (j=0; j<kpos.nbk(); j++)
+          {
+              k = kpos.JiemeK(j);
+              Pt2di v9 = VOIS_9[k];
+              if (
+                        (_optim_p1 || (v9.x == 0))
+                    &&  (_optim_p2 || (v9.y == 0))
+                 )
+              {
+                   Pt2dr p = _param+v9*_step_cur;
+                   score_cur = Op2DParam_ComputeScore(p.x,p.y);
+                   if (  _Maxim                               ?  
+                         (score_cur > score_opt + _epsilon)   :
+                         (score_cur < score_opt - _epsilon)
+                      )
+                   {
+                        score_opt  = score_cur;
+                        last_k = k;
+                   }
+                   if (score_cur == _def_out)
+                      mFreelyOpt = false;
+              }
+          }
+          if (last_k == ElTAB_MEM_K_POSSIBLE::no_k())
+          {
+             mScOpt = score_opt;
+             return ;
+          }
+          else
+             _param += VOIS_9[last_k] * _step_cur;
+    }                           
+}
+
+void  Optim2DParam::optim()
+{
+    optim(Pt2dr(0,0));
+}
+
+void  Optim2DParam::optim(Pt2dr aPInit)
+{
+
+     reset();
+    _param = aPInit;
+
+     if (mSzVoisInit != 1)
+     {
+         mScOpt = _def_out;
+         Pt2dr pOpt =aPInit;
+         for (INT kX=-mSzVoisInit ; kX<=mSzVoisInit ; kX++)
+         {
+             for (INT kY=-mSzVoisInit ; kY<=mSzVoisInit ; kY++)
+             {
+                   Pt2dr aP = aPInit + Pt2dr(kX,kY) * _step_cur;
+                   REAL Score = Op2DParam_ComputeScore ( aP.x,aP.y);
+                   if (Score == _def_out)
+                      mFreelyOpt = false;
+                   if (  _Maxim                               ?  
+                         (Score > mScOpt + _epsilon)   :
+                         (Score < mScOpt - _epsilon)
+                      )
+                   {
+                        mScOpt  = Score;
+                        pOpt = Pt2di(kX,kY)*_step_cur;
+                   }
+             }
+         }
+         _param = pOpt;
+     }
+
+     while (_step_cur > _step_lim)
+     {
+           optim_step_fixed ();
+          _step_cur *=  _lambda;
+     }
+}
+
+
+/**********************************************************/
+/*                                                        */
+/*        CLASSE DEDIEE AU SEGMENT DE DROITE              */
+/*                                                        */
+/**********************************************************/
+
+template <class Type,class TypeBase> 
+class OptimSegTournantSom : public Optim2DParam
+{
+      public :
+          OptimSegTournantSom
+          (
+               Im2D<Type,TypeBase>    im,
+               Seg2d        seg,
+               INT          NbPts,
+               REAL         step_init,
+               REAL         step_limite,
+               bool         optim_absc = true,
+               bool         optim_teta = true
+          )    :
+               Optim2DParam(step_limite/step_init,-1e20,1e-2,true,0.5,optim_absc,optim_teta),
+               _im   (im),
+               _Tim  (im),
+               _segT (seg.p0(),seg.p1(),step_init),
+               _NbPts (NbPts)
+          {
+          }
+
+          Seg2d seg_cur(REAL & Score);
+
+
+      private :
+           REAL Op2DParam_ComputeScore(REAL,REAL);
+
+           Im2D<Type,TypeBase>      _im;
+           TIm2D<Type,TypeBase>     _Tim;
+           SegTournant              _segT;
+           INT                      _NbPts;
+           
+};
+
+
+
+template <class Type,class TypeBase>
+REAL OptimSegTournantSom<Type,TypeBase>::Op2DParam_ComputeScore(REAL PAbs,REAL PTeta)
+{
+     Seg2d  seg = _segT.SegParam(PAbs,PTeta);
+     return FixedSomSegDr(_Tim,seg.p0(),seg.p1(),_NbPts,def_out());
+}
+
+
+template <class Type,class TypeBase>
+Seg2d OptimSegTournantSom<Type,TypeBase>::seg_cur(REAL & Score)
+{
+     Pt2dr p = param();
+     Score = Op2DParam_ComputeScore(p.x,p.y);
+     return _segT.SegParam(p.x,p.y);
+}
+
+
+
+template <class Type,class TypeBase>
+Seg2d   OptimizeSegTournantSomIm
+        (
+             REAL &                  score,
+             Im2D<Type,TypeBase>    im,
+             Seg2d                  seg,
+             INT                    NbPts,
+             REAL                   step_init,
+             REAL                   step_limite,
+             bool                   optim_absc,
+             bool                   optim_teta ,
+             bool  *                FreelyOpt
+        )
+{
+      OptimSegTournantSom<Type,TypeBase> 
+               OSTSI1
+               (
+                  im,
+                  seg,
+                  NbPts,
+                  step_init,
+                  step_limite,
+                  optim_absc,
+                  optim_teta 
+               );
+      OSTSI1.optim();
+
+      if (FreelyOpt)
+         *FreelyOpt =  OSTSI1.FreelyOptimized();
+      return OSTSI1.seg_cur(score);
+}
+
+
+#define INSTANTIATE_OptimizeSegTournantSomIm(Type,TypeBase)\
+template Seg2d   OptimizeSegTournantSomIm\
+                 (\
+                      REAL &                 score,\
+                      Im2D<Type,TypeBase>    im,\
+                      Seg2d                  seg,\
+                      INT                    NbPts,\
+                      REAL                   step_init,\
+                      REAL                   step_limite,\
+                      bool                   optim_absc,\
+                      bool                   optim_teta, \
+					  bool *                  FreelyOpt\
+                 );
+
+
+
+INSTANTIATE_OptimizeSegTournantSomIm(INT1,INT)
+INSTANTIATE_OptimizeSegTournantSomIm(U_INT1,INT)
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
---
+++ src/uti_phgrm/MICMAC/Cuda/cudaAppliMicMac.cu
@@ -0,0 +1,332 @@
+#include <cuda_runtime.h>
+#include <helper_functions.h>
+#include <helper_cuda.h>
+
+texture<float, cudaTextureType2D, cudaReadModeNormalizedFloat> refTex_Image;
+texture<float2, cudaTextureType2D, cudaReadModeNormalizedFloat> refTex_Project;
+
+
+// ATTENTION : erreur de compilation avec l'option cudaReadModeNormalizedFloat et l'utilisation de la fonction tex2DLayered
+texture<float2,	cudaTextureType2DLayered>	refTex_ProjectsLayered;
+texture<float,	cudaTextureType2DLayered>	refTex_ImagesLayered;
+
+cudaArray* dev_Img;				// Tableau des valeurs de l'image
+cudaArray* dev_CubeProjImg;		// Declaration du cube de projection pour le device
+cudaArray* dev_ArrayProjImg;	// Declaration du tableau de projection pour le device
+
+cudaArray* dev_ImagesLayered;	//
+cudaArray* dev_ProjLayered; //
+
+__constant__ int dev_ListImgs[32];
+
+extern "C" void imagesToLayers(float *fdataImg1D, int sx, int sy, int sz)
+{
+
+		cudaExtent sizeImgsLay = make_cudaExtent( sx, sy, sz );
+
+		// Définition du format des canaux d'images
+		cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
+
+		// Allocation memoire GPU du tableau des calques d'images
+		checkCudaErrors( cudaMalloc3DArray(&dev_ImagesLayered,&channelDesc,sizeImgsLay,cudaArrayLayered) );
+
+		// Déclaration des parametres de copie 3D
+		cudaMemcpy3DParms p = { 0 };
+
+		p.dstArray	= dev_ImagesLayered;		// Pointeur du tableau de destination
+		p.srcPtr	= make_cudaPitchedPtr(fdataImg1D, sizeImgsLay.width * sizeof(float), sizeImgsLay.width, sizeImgsLay.height);	
+		p.extent	= sizeImgsLay;				// Taille du cube
+		p.kind		= cudaMemcpyHostToDevice;	// Type de copie
+
+		// Copie des images du Host vers le Device
+		checkCudaErrors( cudaMemcpy3D(&p) );
+
+		// Lié à la texture
+		checkCudaErrors( cudaBindTextureToArray(refTex_ImagesLayered,dev_ImagesLayered) );
+
+
+};
+#define   BLOCKDIM 32
+
+
+
+extern "C" void  projectionsToLayers(float *h_TabProj, int sx, int sy, int sz)
+{
+	// Définition du format des canaux d'images
+	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float2>();
+
+	// Taille du tableau des calques 
+	cudaExtent siz_PL = make_cudaExtent( sx, sy, sz);
+
+	// Allocation memoire GPU du tableau des calques d'images
+	checkCudaErrors( cudaMalloc3DArray(&dev_ProjLayered,&channelDesc,siz_PL,cudaArrayLayered ));
+				
+	// Déclaration des parametres de copie 3D
+	cudaMemcpy3DParms p = { 0 };
+
+	p.dstArray	= dev_ProjLayered;			// Pointeur du tableau de destination
+	p.srcPtr	= make_cudaPitchedPtr(h_TabProj, siz_PL.width * sizeof(float2), siz_PL.width, siz_PL.height);
+	p.extent	= siz_PL;
+	p.kind		= cudaMemcpyHostToDevice;	// Type de copie
+
+	// Copie des projections du Host vers le Device
+	checkCudaErrors( cudaMemcpy3D(&p) );
+
+};
+
+extern __shared__ float cacheCorrel[];
+extern __shared__ float cache__aSV[];
+extern __shared__ float cache_aSVV[];
+extern __shared__ float anEC2[];
+
+__global__ void correlationKernel(float *dest, int sx, int sy, int winX, int winY, int sXI, int sYI, float mAhEpsilon )
+{
+
+
+	__shared__ int aNbImOk;
+
+	// Se placer dans l'espace terrain
+	const int X		= blockIdx.x * blockDim.x  + threadIdx.x;
+	const int Y		= blockIdx.y * blockDim.y  + threadIdx.y;
+	const int Z		= blockIdx.z * blockDim.z  + threadIdx.z;
+	const int idL	= dev_ListImgs[Z];
+
+	// Definir la zone de la fenetre/vignette
+	const int x0	= X - winX;
+	const int x1	= X + winX;
+	const int y0	= Y - winY;
+	const int y1	= Y + winY;
+
+	// Intialisation des valeurs de calcul 
+	float aSV	= 0.0f;
+	float aSVV	= 0.0f;
+
+	// nombre de pixel dans la vignette
+	int dimVign	=	(winX * 2 + 1 ) * ( winY * 2 + 1 );
+	
+	// Decalage dans la memoire partagée de la vignette
+	int piX		= dimVign * threadIdx.x;
+	int piY		= dimVign * threadIdx.y * blockDim.x;
+	int	piZ		= dimVign * threadIdx.z * blockDim.y *  blockDim.x;
+	int pitchCo	= dimVign * ( piX + piY + piZ );
+
+	// Balayage des points de la vignettes
+	#pragma unroll
+	for (int x = x0 ; x <= x1; x++)
+	{
+		#pragma unroll
+		for (int y = y0 ; y <= y1; y++)
+		{
+			const float  u		= (X / (float) sx)*2.0f-1.0f;
+			const float	 v		= (Y / (float) sy)*2.0f-1.0f;
+
+			// Projection dans l'image
+			const float2 pTProj	= tex2DLayered( refTex_ProjectsLayered, u, v, Z);
+
+			// Sortir si la projection est hors de l'image
+			if ((pTProj.x <0.0f)|(pTProj.y <0.0f)) return;
+
+			// Projection dans l'image en coordonnées de la texture GPU 
+			const float ui = (pTProj.x / (float) sXI)*2.0f-1.0f;
+			const float vi = (pTProj.y / (float) sYI)*2.0f-1.0f;
+			
+			// Valeur de l'image
+			float val = tex2DLayered( refTex_ImagesLayered, ui, vi, idL);
+
+			// Calcul du décalage dans la vignette
+			int pi	= pitchCo +  x * (winX * 2 + 1 ) + y;
+			 
+			cacheCorrel[ pi ] = val;	// Mis en cache de la valeur de l'image
+			aSV  += val;				// Somme des valeurs de l'image cte 
+			aSVV += val*val;			// Somme des carrés des vals image cte
+
+		}
+	}
+	
+	// Je dois recuperer le nombre de vignettes OK
+	
+
+	aSV 		/=	dimVign;
+	aSVV 		/=	dimVign;
+	aSVV		-=	aSV * aSV;
+	float saSVV	 =	sqrt(aSVV);
+
+	if ( aSVV <= mAhEpsilon ) return;
+	
+	int cI = atomicAdd( &aNbImOk, 1);
+
+	__syncthreads();
+
+	#pragma unroll
+	for (int i = 0 ; i <= dimVign; i++)
+		cacheCorrel[pitchCo + i] = (cacheCorrel[pitchCo + i]-aSV)/saSVV;
+
+	
+
+	// Si plus 1 image correcte
+	// Calcul "rapide"  de la multi-correlation en utilisant la formule de Huygens
+
+	if (aNbImOk < 2 ) return;
+
+#pragma unroll
+	for (int i = 0 ; i <= dimVign; i++)
+	{
+		 
+		float aSV = atomicAdd( &cache__aSV[i], cacheCorrel[pitchCo + i] );
+		atomicAdd( &cache_aSVV[i] , aSV * aSV );
+
+	}	
+	
+	int id = blockDim.x * threadIdx.y + threadIdx.x;
+	anEC2[id] = threadIdx.z;
+
+	__syncthreads();
+
+	if ( threadIdx.z != anEC2[id])
+		return;
+/*
+	anEC2[id] = 0;
+
+#pragma unroll	
+	for (int i = 0 ; i <= dimVign; i++)
+		anEC2[id] += (cache_aSVV[i] - cache__aSV[i] * cache__aSV[i] /aNbImOk); // Additionner l'ecart type inter imagettes
+	
+
+	// Normalisation pour le ramener a un equivalent de 1-Correl 
+	float aCost			= anEC2[id] / (( aNbImOk-1) * dimVign);
+	dest[ Y * sx + X ]	= 1 - max(-1.0,min(1.0,1-aCost));
+*/
+	dest[ Y * sx + X ]	= anEC2[id];
+	
+};
+
+extern "C" void correlation( float* h_TabCorre, int* listImgProj, int sx, int sy, int sz , int winX, int winY , int sXI, int sYI, float mAhEpsilon ){
+
+	int mem_size = sx * sy;
+	
+	float* host_Correl_Out = (float *) malloc(mem_size);
+	float* dev_Correl_Out;
+
+	checkCudaErrors(  cudaMalloc((void **) &dev_Correl_Out, mem_size) );
+
+	// liste des images projetées copies de Host vers device
+	checkCudaErrors( cudaMemcpyToSymbol(dev_ListImgs, listImgProj, sizeof(int)*sz));
+
+	dim3 threads(BLOCKDIM / winX , BLOCKDIM / winY, sz);
+	dim3 blocks(sx / threads.x , sy /  threads.y, sz / threads.y);
+
+	// set texture parameters
+    refTex_ProjectsLayered.filterMode		= cudaFilterModePoint;
+    refTex_ProjectsLayered.normalized		= true;  // access with normalized texture coordinates
+
+	// Lié à la texture
+	checkCudaErrors( cudaBindTextureToArray(refTex_ProjectsLayered,dev_ProjLayered) );
+
+	// Lancer la fonction de Kernel GPU pour calculer la correlation
+	correlationKernel<<<blocks, threads>>>( dev_Correl_Out, sx, sy, winX, winY, sXI, sYI, mAhEpsilon );
+
+    checkCudaErrors( cudaUnbindTexture(refTex_ProjectsLayered) );
+	checkCudaErrors( cudaMemcpy(host_Correl_Out, dev_Correl_Out, mem_size, cudaMemcpyDeviceToHost) );
+	
+	//if(0)
+	{
+		float result = 0.0f;
+
+			for ( int i = 0 ; i < sx; i++)
+			{
+				for ( int j = 0 ; j < sy; j++)
+				{
+					result = floor(host_Correl_Out[ i * sy + j ]);
+					if (result > 0.0f)
+						std::cout <<  result << " ";
+				}
+				if (result != 0.0f) std::cout << std::endl;
+
+			//std::cout << "---------------------------------------------------------" << std::endl;
+		}
+	}
+	
+	cudaFree(dev_Correl_Out);
+	free(host_Correl_Out);
+}
+
+
+
+extern "C" void freeTexture()
+{
+	checkCudaErrors( cudaUnbindTexture(refTex_Image) );
+	checkCudaErrors( cudaUnbindTexture(refTex_Project) );
+	checkCudaErrors( cudaUnbindTexture(refTex_ImagesLayered) );
+	checkCudaErrors( cudaFreeArray(dev_Img) );
+	checkCudaErrors( cudaFreeArray(dev_CubeProjImg) );
+	checkCudaErrors( cudaFreeArray(dev_ProjLayered) );
+}
+
+extern "C" void  FreeLayers()
+{
+	checkCudaErrors( cudaFreeArray(dev_ImagesLayered));
+
+};
+
+extern "C" void  projToDevice(float* aProj,  int sXImg, int sYImg)
+{
+
+	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float2>();
+
+	// Allocation mémoire du tableau cuda
+	checkCudaErrors( cudaMallocArray(&dev_ArrayProjImg,&channelDesc,sYImg,sXImg) );
+
+	// Copie des données du Host dans le tableau Cuda
+	checkCudaErrors( cudaMemcpy2DToArray(dev_ArrayProjImg,0,0,aProj, sYImg*sizeof(float2),sYImg*sizeof(float2), sXImg, cudaMemcpyHostToDevice) );
+
+	// Lier la texture au tableau Cuda
+	checkCudaErrors( cudaBindTextureToArray(refTex_Project,dev_ArrayProjImg) );
+
+}
+
+extern "C" void cubeProjToDevice(float* cubeProjPIm, cudaExtent dimCube)
+{
+
+	// Format des canaux 
+	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(32, 32, 0, 0, cudaChannelFormatKindFloat);
+			
+	// Taille du cube
+	cudaExtent sizeCube = dimCube;
+			
+	// Allocation memoire GPU du cube de projection
+	checkCudaErrors( cudaMalloc3DArray(&dev_CubeProjImg,&channelDesc,sizeCube) );
+
+	// Déclaration des parametres de copie 3D
+	cudaMemcpy3DParms p = { 0 };
+			
+	p.dstArray	= dev_CubeProjImg;			// Pointeur du tableau de destination
+	p.srcPtr	= make_cudaPitchedPtr(cubeProjPIm, dimCube.width * 2 * sizeof(float), dimCube.width, dimCube.height);
+	p.extent	= dimCube;					// Taille du cube
+	p.kind		= cudaMemcpyHostToDevice;	// Type de copie
+
+	// Copie du cube de projection du Host vers le Device
+	checkCudaErrors( cudaMemcpy3D(&p) );
+		
+}
+
+extern "C" void  imageToDevice(float** aDataIm,  int sXImg, int sYImg)
+{
+	float *dataImg1D	= new float[sXImg*sYImg];
+	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float>();
+
+	// TACHE : changer la structuration des donnees pour le stockage des images 
+	// Tableau 2D  --->> tableau linéaire
+	for (int i = 0; i < sXImg ; i++)
+		for (int j = 0; j < sYImg ; j++)
+			dataImg1D[i*sYImg+j] = aDataIm[j][i];
+
+	// Allocation mémoire du tableau cuda
+	checkCudaErrors( cudaMallocArray(&dev_Img,&channelDesc,sYImg,sXImg) );
+
+	// Copie des données du Host dans le tableau Cuda
+	checkCudaErrors( cudaMemcpy2DToArray(dev_Img,0,0,dataImg1D, sYImg*sizeof(float),sYImg*sizeof(float), sXImg, cudaMemcpyHostToDevice) );
+
+	// Lier la texture au tableau Cuda
+	checkCudaErrors( cudaBindTextureToArray(refTex_Image,dev_Img) );
+
+}
