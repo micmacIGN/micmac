f6de2e2168db3f733baabf826180e9f498900877
gmaillet <gmaillet@users.noreply.github.com>
2012-12-04 18:10:40 +0100

    compilation clang

---
+++ src/uti_files/Sources.cmake
@@ -0,0 +1,12 @@
+set(uti_files_Src_Files
+    ${UTI_FILES_DIR}/CPP_MapCmd.cpp
+    ${UTI_FILES_DIR}/CPP_BatchFDC.cpp
+    ${UTI_FILES_DIR}/CPP_MyRename.cpp
+)
+
+SOURCE_GROUP(uti_files FILES ${uti_files_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${uti_files_Src_Files}
+)
