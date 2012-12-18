9abbc066562566677845ce67b769d01a348c4c43
deseilligny <deseilligny@localhost>
2012-12-18 19:51:40 +0100

    Ajout Doc et cod

--- src/CBinaires/MapCmd.c
+++ src/CBinaires/MapCmd.c
@@ -2,6 +2,9 @@
 
 int main(int argc,char ** argv)
 {
+    int aK;
+    for (aK=0 ; aK< 10; aK++)
+        printf("!!!!!  USE   mm3d MapCmd  : $ do not cross correctly shell \n"); exit(-1);
     return BinaireUnique("MapCmd",argc,argv);
 }
 
