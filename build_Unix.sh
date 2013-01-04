f62d2b782e573aee3a7e804dfbc9c5ba5fcd2d04
mdeveau <mattdeveau@users.noreply.github.com>
2013-01-04 16:43:19 +0100

    Modifs mineures dans le graph-cut

---
+++ build_Unix.sh
@@ -0,0 +1,23 @@
+#rm -rf bin
+#mkdir build
+cd build
+cmake ..
+echo "Voulez-vous effectuer MAKE CLEAN [o/n] ?"
+read REP
+case $REP in 	
+	O|o)
+        echo "Make CLEAN"
+		make clean
+        ;;
+	N|n|*)
+        echo "Pas de Make CLEAN"
+        ;;
+esac
+
+NBRP=$(cat /proc/cpuinfo | grep processor | wc -l)
+echo "Nbre de coeurs à la compilation : " $NBRP
+make install -j$NBRP
+cd ..
+chmod +x ./binaire-aux/siftpp_tgi.LINUX
+chmod +x ./binaire-aux/ann_mec_filtre.LINUX
+
