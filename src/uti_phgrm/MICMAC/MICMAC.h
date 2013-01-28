24dff908bd5afbede712dd3dbdbeaa156587f03e
GChoqueux <GChoqueux@ASU1210P001.ign.fr>
2013-01-28 15:59:36 +0100

    GpGpu : Tab proj en float2

--- src/uti_phgrm/MICMAC/MICMAC.h
+++ src/uti_phgrm/MICMAC/MICMAC.h
@@ -2712,7 +2712,7 @@ class cAppliMICMAC  : public   cParamMICMAC,
 		void DoGPU_Correl_Basik (const Box2di & aBoxInterne); 
 
 #ifdef  CUDA_ENABLED
-		void Tabul_Projection( float* TabProj, int Z, int2 Ter0, int2 Ter1, uint sample);
+		void Tabul_Projection( float2* TabProj, int Z, int2 Ter0, int2 Ter1, uint sample);
 #endif
 		void Correl_MNE_ZPredic (const Box2di & aBoxInterne,const cCorrel_Correl_MNE_ZPredic &);  
 		void DoCorrelPonctuelle2ImGeomI(const Box2di&aBoxInterne,const cCorrel_Ponctuel2ImGeomI&);  
