8b37a066527af822680891b61f0d7d4be1ffcc32
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-02-05 17:34:06 +0100

    Suppression Warning Windows

---
+++ src/uti_phgrm/GPGPU/GpGpuTools.cpp
@@ -0,0 +1,55 @@
+#include "GpGpu/GpGpuTools.h"
+
+std::string GpGpuTools::GetImagesFolder()
+{
+
+#ifdef _WIN32
+
+	TCHAR name [ UNLEN + 1 ];
+	DWORD size = UNLEN + 1;
+	GetUserName( (TCHAR*)name, &size );
+
+	std::string suname = name;
+	std::string ImagesFolder = "C:\\Users\\" + suname + "\\Pictures\\";
+#else
+	struct passwd *pw = getpwuid(getuid());
+
+	const char *homedir = pw->pw_dir;
+
+	std::string ImagesFolder = std::string(homedir) + "/Images/";
+
+#endif
+
+	return ImagesFolder;
+}
+
+
+void GpGpuTools::OutputReturn( char * out )
+{
+
+#ifndef DISPLAYOUTPUT
+	return;
+#else
+	
+	std::cout << std::string(out) << "\n";
+
+#endif
+
+}
+
+void GpGpuTools::OutputGpu()
+{
+
+	//std::cout << '-' << std::flush;
+	
+	Sleep(10);
+	std::cout << "\b\\" << std::flush;
+	Sleep(10);
+	std::cout << "\b|" << std::flush;
+	Sleep(10);
+	std::cout << "\b/" << std::flush;
+	Sleep(10);
+	std::cout << "\b-" << std::flush;
+	
+}
+
---
+++ src/uti_phgrm/GPGPU/Sources.cmake
@@ -0,0 +1,6 @@
+set(uti_phgrm_GpGpu_Src_Files
+
+    ${UTI_PHGRM_GPGPU_DIR}/GpGpuTools.cpp
+	${UTI_PHGRM_GPGPU_DIR}/cudaAppliMicMac.cu
+	#${UTI_PHGRM_GPGPU_DIR}/cudaTextureTools.cu
+)
