2eaa8c948ad8cd6ab50a8d467fe056e33faa0978
jbelvaux <jbelvaux@localhost>
2013-02-07 17:08:27 +0100

    - modifie les scripts de cr?ation de package - enl?ve les accents dans le README.fr

--- make_interface_source_archive.sh
+++ make_interface_source_archive.sh
@@ -1,23 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 MICMAC_REPOSITORY=`basename $PWD`
 REV_NUMBER=`hg log -r tip --template "{rev}"`
-TEMP_DIRECTORY=micmac
+OUT_MICMAC_DIR=micmac
 ARCHIVE_NAME=micmac_interface_source_rev$REV_NUMBER
 if [ -f $ARCHIVE_NAME.tar.gz ]
 then
 	echo l\'archive $ARCHIVE_NAME.tar.gz existe déjà
 	exit 1
 fi
-if [ -d $TEMP_DIRECTORY ]
+if [ -d $OUT_MICMAC_DIR ]
 then
-	echo le repertoire $TEMP_DIRECTORY existe déjà
+	echo le repertoire $OUT_MICMAC_DIR existe déjà
 	exit 1
 fi
-mkdir $TEMP_DIRECTORY
-mkdir $TEMP_DIRECTORY/src
-cp -R interface $TEMP_DIRECTORY
-cp -R src/interface $TEMP_DIRECTORY/src
+mkdir $OUT_MICMAC_DIR
+mkdir ${OUT_MICMAC_DIR}/src
+cp -R interface $OUT_MICMAC_DIR
+cp -R src/interface ${OUT_MICMAC_DIR}/src
+rm -fR ${OUT_MICMAC_DIR}/interface/documentation
 
-tar czf $ARCHIVE_NAME.tar.gz $TEMP_DIRECTORY
-rm -fr $TEMP_DIRECTORY
+tar czf ${ARCHIVE_NAME}.tar.gz $OUT_MICMAC_DIR
+rm -fr $OUT_MICMAC_DIR
