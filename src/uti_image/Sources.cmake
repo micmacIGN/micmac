d8a28f97adbd90a4ee2b0efff6c52254ecf37427
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-03-05 15:23:37 +0100

    GpGpu : class CGObject

---
+++ src/uti_image/Sources.cmake
@@ -0,0 +1,25 @@
+set(UTI_IMAGE_MPDCRAW_DIR ${UTI_IMAGE_DIR}/MpDcraw)
+set(SrcGrp_Uti_Image uti_image)
+
+INCLUDE (${UTI_IMAGE_MPDCRAW_DIR}/Sources.cmake)
+
+list( APPEND uti_image_Src_Files
+    ${UTI_IMAGE_DIR}/CPP_Undist.cpp
+    ${UTI_IMAGE_DIR}/CPP_CmpIm.cpp
+    ${UTI_IMAGE_DIR}/CPP_Dequant.cpp
+    ${UTI_IMAGE_DIR}/CPP_Devlop.cpp
+    ${UTI_IMAGE_DIR}/CPP_ElDcraw.cpp
+    ${UTI_IMAGE_DIR}/CPP_GenXML2Cpp.cpp
+    ${UTI_IMAGE_DIR}/CPP_GrShade.cpp
+    ${UTI_IMAGE_DIR}/CPP_MpDcraw.cpp
+    ${UTI_IMAGE_DIR}/CPP_PastDevlop.cpp
+    ${UTI_IMAGE_DIR}/CPP_Reduc2MM.cpp
+    ${UTI_IMAGE_DIR}/CPP_ScaleIm.cpp
+    ${UTI_IMAGE_DIR}/CPP_tiff_info.cpp
+    ${UTI_IMAGE_DIR}/CPP_to8Bits.cpp 
+	${UTI_IMAGE_DIR}/CPP_Drunk.cpp 
+    ${UTI_IMAGE_DIR}/CPP_MPDtest.cpp )
+
+SOURCE_GROUP(${SrcGrp_Uti_Image}\\outils FILES ${uti_image_Src_Files})
+
+list( APPEND Elise_Src_Files ${uti_image_Src_Files} )
---
+++ src/uti_phgrm/GPGPU/GpGpuTools.cpp
@@ -0,0 +1,146 @@
+#include "GpGpu/GpGpuTools.h"
+
+std::string GpGpuTools::GetImagesFolder()
+{
+
+#ifdef _WIN32
+
+	TCHAR name [ UNLEN + 1 ];
+	DWORD size = UNLEN + 1;
+	GetUserName( (TCHAR*)name, &size );
+
+	std::string suname = name;
+	std::string ImagesFolder = "C:\\Users\\" + suname + "\\Pictures\\";
+#else
+	struct passwd *pw = getpwuid(getuid());
+
+	const char *homedir = pw->pw_dir;
+
+	std::string ImagesFolder = std::string(homedir) + "/Images/";
+
+#endif
+
+	return ImagesFolder;
+}
+
+
+void GpGpuTools::OutputReturn( char * out )
+{
+
+#ifndef DISPLAYOUTPUT
+	return;
+#else
+	
+	std::cout << std::string(out) << "\n";
+
+#endif
+
+}
+
+void GpGpuTools::OutputGpu()
+{
+#if (ELISE_windows)
+	Sleep(10);
+	std::cout << "\b\\" << std::flush;
+	Sleep(10);
+	std::cout << "\b|" << std::flush;
+	Sleep(10);
+	std::cout << "\b/" << std::flush;
+	Sleep(10);
+	std::cout << "\b-" << std::flush;
+#endif
+}
+
+float GpGpuTools::fValue( float value,float factor )
+{
+	return value * factor;
+}
+
+float GpGpuTools::fValue( float2 value,float factor )
+{
+	return (float)value.x * factor;
+}
+
+std::string CGObject::CGObjectName()
+{
+  return _name;
+}
+
+uint2 struct2D::GetDimension()
+{
+	return _dimension;
+}
+
+uint2 struct2D::SetDimension( uint2 dimension )
+{
+	_dimension = dimension;
+	return _dimension;
+}
+
+uint2 struct2D::SetDimension( uint dimX,uint dimY )
+{
+	return SetDimension(make_uint2(dimX,dimY));
+}
+
+uint2 struct2D::SetDimension( int dimX,int dimY )
+{
+	return SetDimension((uint)dimX,(uint)dimY);
+}
+
+uint struct2D::GetSize()
+{
+	return size(_dimension);
+}
+
+void struct2DLayered::SetDimension( uint3 dimension )
+{
+	SetDimension(make_uint2(dimension),dimension.z);
+}
+
+void struct2DLayered::SetDimension( uint2 dimension, uint nbLayer )
+{
+	struct2D::SetDimension(dimension);
+	SetNbLayer(nbLayer);
+}
+
+void struct2DLayered::SetNbLayer( uint nbLayer )
+{
+	_nbLayers = nbLayer;
+}
+
+uint struct2DLayered::GetNbLayer()
+{
+	return _nbLayers;
+
+}
+
+uint struct2DLayered::GetSize()
+{
+	return struct2D::GetSize() * GetNbLayer();
+}
+
+void AImageCuda::bindTexture( textureReference& texRef )
+{
+	cudaChannelFormatDesc desc;
+	checkCudaErrors(cudaGetChannelDesc(&desc, GetCudaArray()));
+	checkCudaErrors(cudaBindTextureToArray(&texRef,GetCudaArray(),&desc));
+
+}
+
+cudaArray* AImageCuda::GetCudaArray()
+{
+	return CData<cudaArray>::pData();
+}
+
+void AImageCuda::Dealloc()
+{
+
+	if (CData<cudaArray>::isNULL()) checkCudaErrors( cudaFreeArray( CData<cudaArray>::pData()) );
+	CData<cudaArray>::dataNULL();
+
+}
+
+void AImageCuda::Memset( int val )
+{
+	std::cout << "PAS DE MEMSET POUR CUDA ARRAY" << "\n";
+}
