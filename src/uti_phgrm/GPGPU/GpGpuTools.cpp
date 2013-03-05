61e5ade049f4539fe6c91f7053e6d6d040ddcfcf
GChoqueux <GChoqueux@ASU1210P001.ign.fr>
2013-03-05 19:38:20 +0100

    GpGpu : Outils de Debug

--- src/uti_phgrm/GPGPU/GpGpuTools.cpp
+++ src/uti_phgrm/GPGPU/GpGpuTools.cpp
@@ -61,9 +61,60 @@ float GpGpuTools::fValue( float2 value,float factor )
 	return (float)value.x * factor;
 }
 
-std::string CGObject::CGObjectName()
+std::string GpGpuTools::toStr( uint2 tt )
+{
+	stringstream sValS (stringstream::in | stringstream::out);
+
+	sValS << "(" << tt.x << "," << tt.y << ")";
+
+	return sValS.str();
+}
+
+std::string CGObject::Name()
 {
   return _name;
+}
+
+void CGObject::Name( std::string name )
+{
+	_name = name;
+}
+
+std::string CGObject::Type()
+{
+	return _type;
+}
+
+void CGObject::SetType( std::string type )
+{
+	_type = type;
+}
+
+CGObject::CGObject()
+{
+	Name("NO_NAME");
+	SetType("NO_TYPE");
+	ClassTemplate("NO_CLASS_TEMPLATE");
+}
+
+CGObject::~CGObject()
+{
+
+}
+
+std::string CGObject::ClassTemplate()
+{
+	return _classTemplate;
+}
+
+void CGObject::ClassTemplate( std::string classTemplate )
+{
+	_classTemplate = classTemplate;
+}
+
+std::string CGObject::Id()
+{
+	return " NAME : " + Name() + ", " + "TYPE : " + Type() + "/" + ClassTemplate() + "\n";
 }
 
 uint2 struct2D::GetDimension()
@@ -90,6 +141,11 @@ uint2 struct2D::SetDimension( int dimX,int dimY )
 uint struct2D::GetSize()
 {
 	return size(_dimension);
+}
+
+void struct2D::Output()
+{
+	std::cout << "Dimension : " << GpGpuTools::toStr(_dimension) << "\n";
 }
 
 void struct2DLayered::SetDimension( uint3 dimension )
@@ -119,12 +175,17 @@ uint struct2DLayered::GetSize()
 	return struct2D::GetSize() * GetNbLayer();
 }
 
-void AImageCuda::bindTexture( textureReference& texRef )
+void struct2DLayered::Output()
+{
+	std::cout << "Nombre de calques : " << GetNbLayer() << "\n";
+}
+
+bool  AImageCuda::bindTexture( textureReference& texRef )
 {
 	cudaChannelFormatDesc desc;
 	checkCudaErrors(cudaGetChannelDesc(&desc, GetCudaArray()));
 	checkCudaErrors(cudaBindTextureToArray(&texRef,GetCudaArray(),&desc));
-
+	return true;
 }
 
 cudaArray* AImageCuda::GetCudaArray()
@@ -132,15 +193,86 @@ cudaArray* AImageCuda::GetCudaArray()
 	return CData<cudaArray>::pData();
 }
 
-void AImageCuda::Dealloc()
+bool AImageCuda::Dealloc()
 {
 
 	if (CData<cudaArray>::isNULL()) checkCudaErrors( cudaFreeArray( CData<cudaArray>::pData()) );
 	CData<cudaArray>::dataNULL();
-
+	return true;
 }
 
-void AImageCuda::Memset( int val )
+bool AImageCuda::Memset( int val )
 {
 	std::cout << "PAS DE MEMSET POUR CUDA ARRAY" << "\n";
+	return true;
 }
+
+
+/*
+template<class T>
+const char* CGObject::StringClass( T* tt )
+{
+	return "*T";
+}
+
+
+template<>
+const char* CGObject::StringClass<float>( float* tt )
+{
+	return "*float";
+}
+*//*
+template<>
+const char* CGObject::StringClass<float*>( float* tt )
+{
+	return "*float";
+}
+
+template<>
+const char* CGObject::StringClass<struct  float2*>(struct  float2* tt )
+{
+	return "*float2";
+}
+
+template<>
+const char* CGObject::StringClass<uint*>( uint* tt )
+{
+	return "*uint";
+}
+
+template<>
+const char* CGObject::StringClass<pixel*>( pixel* tt )
+{
+	return "*uint";
+}
+
+
+template<class T>
+const char* CGObject::StringClass( T tt )
+{
+	return "T";
+}
+
+template<>
+const char* CGObject::StringClass<float>( float tt )
+{
+	return "float";
+}
+
+template<>
+const char* CGObject::StringClass<struct float2>(struct float2 tt )
+{
+	return "float2";
+}
+
+template<>
+const char* CGObject::StringClass<uint>( uint tt )
+{
+	return "uint";
+}
+
+template<>
+const char* CGObject::StringClass<pixel>( pixel tt )
+{
+	return "uint";
+}*/
