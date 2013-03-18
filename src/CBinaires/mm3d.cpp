4b7729b6d3d1c9c5e78bb47a4ef5eb8c1cb68d26
deseilligny <deseilligny@localhost>
2013-03-18 22:26:56 +0100

    mmxv + correc (a 2 balles : nombre de point) dans MM masq Tie P

--- src/CBinaires/mm3d.cpp
+++ src/CBinaires/mm3d.cpp
@@ -223,7 +223,7 @@ const std::vector<cMMCom> & getAvailableCommands()
 
        aRes.push_back(cMMCom("tiff_info",tiff_info_main," Tool for giving information about a tiff file"));
        aRes.push_back(cMMCom("to8Bits",to8Bits_main," Tool for converting 16 or 32 bit image in a 8 bit image."));
-       // aRes.push_back(cMMCom("mmxv",mmxv_main," Interface to xv (due to problem in tiff lib)"));
+       aRes.push_back(cMMCom("mmxv",mmxv_main," Interface to xv (due to problem in tiff lib)"));
        aRes.push_back(cMMCom("CmpIm",CmpIm_main," Tool basic for comparison of images"));
        aRes.push_back(cMMCom("Undist",Undist_main," Tool for removing images distortion"));
 
