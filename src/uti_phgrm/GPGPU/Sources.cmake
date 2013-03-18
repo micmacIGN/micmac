bb786fcff40e6d2630d14076eb7f424818d5ca0c
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-03-18 14:52:16 +0000

    GpGpu : Lib gpgpu

---
+++ src/uti_phgrm/GPGPU/Sources.cmake
@@ -0,0 +1,10 @@
+set(GpGpuTools_Src_Files
+   ${UTI_PHGRM_GPGPU_DIR}/GpGpuTools.cpp
+)
+
+set(uti_phgrm_GpGpu_Src_Files
+    ${UTI_PHGRM_GPGPU_DIR}/InterfaceMicMacGpGpu.cpp
+    ${UTI_PHGRM_GPGPU_DIR}/cudaAppliMicMac.cu
+    ${UTI_PHGRM_GPGPU_DIR}/cudaFilters.cu
+    #${UTI_PHGRM_GPGPU_DIR}/cudaTextureTools.cu
+)
