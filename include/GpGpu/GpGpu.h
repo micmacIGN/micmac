2108637b59600496b1fadafc4529543212f28b67
deseilligny <deseilligny@users.noreply.github.com>
2013-04-03 22:34:07 +0200

    GPU Prog Dyn : compile

---
+++ include/GpGpu/GpGpu.h
@@ -0,0 +1,18 @@
+#ifndef __GPGPU_H__
+#define __GPGPU_H__
+
+#if defined __GNUC__
+#pragma GCC system_header
+#elif defined __SUNPRO_CC
+#pragma disable_warn
+#elif defined _MSC_VER
+#pragma warning(push, 1)
+#endif
+
+#include "GpGpu/GpGpuDefines.h"
+
+#ifdef CUDA_ENABLED
+#include "GpGpu/InterfaceMicMacGpGpu.h"
+#endif
+
+#endif // __GPGPU_H__
