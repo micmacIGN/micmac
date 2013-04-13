2e6fb856a0436265ed3c0ff00eed65b9de69fd63
deseilligny <deseilligny@users.noreply.github.com>
2013-04-13 12:22:34 +0200

    iii

--- applis/Digeo/Digeo.h
+++ applis/Digeo/Digeo.h
@@ -101,6 +101,54 @@ typedef enum
 } eTypeExtreSift;
 
 
+/*****************************************************************/
+/*                                                               */
+/*   Fonctions elementaires de noyaux 1D faits notamment pour    */
+/* permettre le calcul de noyau enchaine                         */
+/*                                                               */
+/*****************************************************************/
+
+//  Resout l'equation  aI o I2 = aI3 , au sens des moindres carres
+Im1D_REAL8 DeConvol
+(
+     int aC2,   // Indexe 0 dans I2
+     int aSz2,  // Taille I2
+     Im1D_REAL8 aI1,   // Kernel 1
+     int aC1,          // Indexe 0 dans I1
+     Im1D_REAL8 aI3,   // Kernel 3
+     int aC3           // Indexe 0 dans I3
+);
+// Paramametrage standardA  0 en centre image
+Im1D_REAL8 DeConvol(int aDemISz2,Im1D_REAL8 aI1,Im1D_REAL8 aI3);
+
+
+
+// Convolution C1 et C2 = indexe 0,  lent ; pour verif DeConvol
+Im1D_REAL8 Convol(Im1D_REAL8 aI1,int aC1,Im1D_REAL8 aI2,int aC2);
+// Parametrage stantdard 
+Im1D_REAL8 Convol(Im1D_REAL8 aI1,Im1D_REAL8 aI2);
+
+// Force l'image à une integrale donnee
+Im1D_REAL8 MakeSom(Im1D_REAL8 aIm,double aSomCible);
+Im1D_REAL8 MakeSom1(Im1D_REAL8 aIm);
+
+//  Calcul un noyau gaussien en faisant pour chaque pixel la valeur integrale.
+Im1D_REAL8  GaussianKernel(double aSigma,int aNb,int aSurEch);
+
+//  Calcul le nombre d'element pour que la gaussiennne puisse etre tronquee a Residu pres
+int NbElemForGausKern(double aSigma,double aResidu);
+
+//  Calcule un noyau gaussien
+Im1D_REAL8  GaussianKernelFromResidu(double aSigma,double aResidu,int aSurEch);
+
+
+// Conversion d'un noyau double (de somme 1) en entier, en conservant la somme
+// (Pour une image entiere qui sera interpretee comme rationnele de quotient aMul)
+Im1D_INT4 ToIntegerKernel(Im1D_REAL8 aRK,int aMul,bool aForceSym);
+
+
+
+
 
      //     Pour representer une image
 
@@ -208,6 +256,30 @@ template <class Type> class cTplImInMem : public cImInMem
 
         eTypeExtreSift CalculateDiff(Type***aC,int anX,int anY,int aNiv);
 
+/*
+void  cTplImInMem<Type>::SetConvolBordX :
+ 
+          Pour la "colonne" X, calcul dans ImOut toute les convolution en gerant 
+       les effets de bord :
+
+       aDebX  , aFinX : borne du filtre, typiquement de -SzKer , + SzKer (inclus)
+
+  Utilise 
+  
+template <class tBase> tBase ClipForConvol(int aSz,int aKXY,tBase * aData,int & aDeb,int & aFin) :
+
+    Clip l'intervalle (genre [-SzKe,+SzK] au depart) pour que la convol ne deborde 
+    pas de [0,aSz[
+
+
+inline tBase CorrelLine(tBase aSom,const Type * aData1,const tBase *  aData2,const int & aDeb,const int & aFin)
+
+    Produit scalaire tout a fait basique, utilise pour correler les bord
+
+
+*/
+
+
         static void SetConvolBordX
              (
                   Im2D<Type,tBase> aImOut,
@@ -224,6 +296,8 @@ template <class Type> class cTplImInMem : public cImInMem
                   int  aNbShitX,
                   cConvolSpec<Type> *
              );
+
+
 
 
         void SetConvolSepX
--- include/general/gauss_kernel.h
+++ include/general/gauss_kernel.h
@@ -44,51 +44,6 @@ Header-MicMac-eLiSe-25/06/2007*/
 
 
 
-/*****************************************************************/
-/*                                                               */
-/*   Fonctions elementaires de noyaux 1D faits notamment pour    */
-/* permettre le calcul de noyau enchaine                         */
-/*                                                               */
-/*****************************************************************/
-
-//  Resout l'equation  aI o I2 = aI3 , au sens des moindres carres
-Im1D_REAL8 DeConvol
-(
-     int aC2,   // Indexe 0 dans I2
-     int aSz2,  // Taille I2
-     Im1D_REAL8 aI1,   // Kernel 1
-     int aC1,          // Indexe 0 dans I1
-     Im1D_REAL8 aI3,   // Kernel 3
-     int aC3           // Indexe 0 dans I3
-);
-// Paramametrage standardA  0 en centre image
-Im1D_REAL8 DeConvol(int aDemISz2,Im1D_REAL8 aI1,Im1D_REAL8 aI3);
-
-
-
-// Convolution C1 et C2 = indexe 0,  lent ; pour verif DeConvol
-Im1D_REAL8 Convol(Im1D_REAL8 aI1,int aC1,Im1D_REAL8 aI2,int aC2);
-// Parametrage stantdard 
-Im1D_REAL8 Convol(Im1D_REAL8 aI1,Im1D_REAL8 aI2);
-
-// Force l'image à une integrale donnee
-Im1D_REAL8 MakeSom(Im1D_REAL8 aIm,double aSomCible);
-Im1D_REAL8 MakeSom1(Im1D_REAL8 aIm);
-
-//  Calcul un noyau gaussien en faisant pour chaque pixel la valeur integrale.
-Im1D_REAL8  GaussianKernel(double aSigma,int aNb,int aSurEch);
-
-//  Calcul le nombre d'element pour que la gaussiennne puisse etre tronquee a Residu pres
-int NbElemForGausKern(double aSigma,double aResidu);
-
-//  Calcule un noyau gaussien
-Im1D_REAL8  GaussianKernelFromResidu(double aSigma,double aResidu,int aSurEch);
-
-
-// Conversion d'un noyau double (de somme 1) en entier, en conservant la somme
-// (Pour une image entiere qui sera interpretee comme rationnele de quotient aMul)
-Im1D_INT4 ToIntegerKernel(Im1D_REAL8 aRK,int aMul,bool aForceSym);
-
 
 
 
