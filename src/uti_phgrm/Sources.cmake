4262452ccfb3f70b0bc9f696a46ce837f6f25428
jbelvaux <jbelvaux@localhost>
2013-04-23 19:05:24 +0200

    - probl?me de fusion

--- src/uti_phgrm/Sources.cmake
+++ src/uti_phgrm/Sources.cmake
@@ -75,6 +75,7 @@ set( Applis_phgrm_Src_Files
     ${UTI_PHGRM_DIR}/CPP_SaisieBasc.cpp
     ${UTI_PHGRM_DIR}/CPP_ChgSysCo.cpp
     ${UTI_PHGRM_DIR}/CPP_XYZ2Im.cpp
+    ${UTI_PHGRM_DIR}/CPP_GrapheHom.cpp
 )
 
 SOURCE_GROUP(${SrcGrp_Uti_PHGRM} FILES ${uti_phgrm_Src_Files})
