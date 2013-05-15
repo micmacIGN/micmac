661bf3f2dedc737f5fef3046cd6a960a7d2ce2fa
deseilligny <deseilligny@localhost>
2013-05-15 17:10:25 +0200

    ???? Pb Pt2dr ???

--- src/SamplesLibElise/Sources.cmake
+++ src/SamplesLibElise/Sources.cmake
@@ -2,6 +2,7 @@ set(SamplesLibElise_Src_Files
     ${SAMPLESLIBELISE_DIR}/CPP_Windows0Main.cpp
     ${SAMPLESLIBELISE_DIR}/CPP_LSQMain.cpp
     ${SAMPLESLIBELISE_DIR}/CPP_TestAbdou.cpp
+    ${SAMPLESLIBELISE_DIR}/CPP_Test0LucGirod.cpp
 )
 
 # JE NE SAIS PAS SI CA SERT ???
