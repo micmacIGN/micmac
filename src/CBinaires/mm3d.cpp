1a9c0e929e74184f0572413b9fe4d753a1ce75c1
deseilligny <deseilligny@users.noreply.github.com>
2013-05-15 17:41:16 +0200

    Ex Pts Hom pour Luc

--- src/CBinaires/mm3d.cpp
+++ src/CBinaires/mm3d.cpp
@@ -312,6 +312,7 @@ int GenMain(int argc,char ** argv, const std::vector<cMMCom> & aVComs);
 extern int  Sample_W0_main(int argc,char ** argv);
 extern int  Sample_LSQ0_main(int argc,char ** argv);
 extern int  Abdou_main(int argc,char ** argv);
+extern int  PtsHom_main(int argc,char ** argv);
 
 const std::vector<cMMCom> & TestLibAvailableCommands()
 {
@@ -319,6 +320,7 @@ const std::vector<cMMCom> & TestLibAvailableCommands()
    aRes.push_back(cMMCom("W0",Sample_W0_main,"Test on Graphic Windows "));
    aRes.push_back(cMMCom("LSQ0",Sample_LSQ0_main,"Basic Test on Least Square libray "));
    aRes.push_back(cMMCom("Abdou",Abdou_main,"Exemples fonctions abdou "));
+   aRes.push_back(cMMCom("PtsHom",PtsHom_main,"Exemples for homologous point manipulation "));
    return aRes;
 }
 
