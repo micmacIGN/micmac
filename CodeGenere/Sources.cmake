f6ac1d9705bb1dc20d576d2d08eb8193dd5cdc82
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-05-16 11:14:18 +0200

    GpuGpu : clean code

---
+++ CodeGenere/Sources.cmake
@@ -0,0 +1,17 @@
+set(code_genere_file2string_dir ${code_genere_dir}/File2String)
+#set(code_genere_photogram_dir ${code_genere_dir}/photogram)
+
+list( APPEND code_genere_file2string_Src_Files
+    ${code_genere_file2string_dir}/Str_SuperposImage.cpp
+    ${code_genere_file2string_dir}/Str_ParamMICMAC.cpp
+    ${code_genere_file2string_dir}/Str_ParamChantierPhotogram.cpp
+    ${code_genere_file2string_dir}/Str_ParamApero.cpp
+    ${code_genere_file2string_dir}/Str_ParamDigeo.cpp
+    ${code_genere_file2string_dir}/Str_DefautChantierDescripteur.cpp
+)
+
+SOURCE_GROUP(CodeGenere FILES ${code_genere_file2string_Src_Files})
+
+list( APPEND Elise_Src_Files
+	${code_genere_file2string_Src_Files}
+)
