8f9756f6b9b0a1eb082741b7f65dc156663e66ac
luc-girod <luc-girod@localhost>
2013-05-24 15:26:01 +0200

    supp de vignette.h

---
+++ src/flux_pts/Sources.cmake
@@ -0,0 +1,18 @@
+set(Flux_pts_Src_Files
+	${FLUX_PTS_DIR}/curser_pack_pts.cpp
+	${FLUX_PTS_DIR}/flx_gen_tpl.cpp
+	${FLUX_PTS_DIR}/flx_general.cpp
+	${FLUX_PTS_DIR}/flx_oper.cpp
+	${FLUX_PTS_DIR}/front_to_surf.cpp
+	${FLUX_PTS_DIR}/lineique_2d.cpp
+	${FLUX_PTS_DIR}/rle_pack.cpp
+	${FLUX_PTS_DIR}/select.cpp
+	${FLUX_PTS_DIR}/surf_geom_2d.cpp
+)
+
+SOURCE_GROUP(Flux_pts FILES ${Flux_pts_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${Flux_pts_Src_Files}
+)
