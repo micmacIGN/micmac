14b917ecd0c3e3f9698174ad85690249adc5a186
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-05-27 11:35:55 +0200

    GpGpu : ajout de methodes maths

---
+++ src/uti_image/CPP_Vignette.cpp
@@ -0,0 +1,414 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMa cis an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+#include "../../include/StdAfx.h"
+#include "hassan/reechantillonnage.h"
+#include <algorithm>
+
+void Vodka_Banniere()
+{
+    std::cout <<  "\n";
+    std::cout <<  " *********************************\n";
+    std::cout <<  " *     V-ignette                 *\n";
+    std::cout <<  " *     O-f                       *\n";
+    std::cout <<  " *     D-igital                  *\n";
+    std::cout <<  " *     K-amera                   *\n";
+    std::cout <<  " *     A-nalysis                 *\n";
+    std::cout <<  " *********************************\n\n";
+}
+
+vector<vector<double> > ReadPtsHom(string aDir,std::vector<std::string> * aSetIm,string Extension)
+{
+
+	vector<double> D1,D2,G1,G2;//X1,Y1,X2,Y2,
+	Pt2di aSz;
+    // Permet de manipuler les ensemble de nom de fichier
+    cInterfChantierNameManipulateur * aICNM = cInterfChantierNameManipulateur::BasicAlloc(aDir);
+
+//On parcours toutes les paires d'images différentes (->testé dans le if)
+	int cpt=0;
+    for (int aK1=0 ; aK1<int(aSetIm->size()) ; aK1++)
+    {
+		std::cout<<"Getting homologous points from: "<<(*aSetIm)[aK1]<<endl;
+		//
+		    
+		//Reading the image and creating the objects to be manipulated
+			Tiff_Im aTF1= Tiff_Im::StdConvGen(aDir + (*aSetIm)[aK1],1,false);
+			aSz = aTF1.sz();
+			Im2D_U_INT1  aIm1(aSz.x,aSz.y);
+			ELISE_COPY
+				(
+				   aTF1.all_pts(),
+				   aTF1.in(),
+				   aIm1.out()
+				);
+
+			U_INT1 ** aData1 = aIm1.data();
+
+
+        for (int aK2=0 ; aK2<int(aSetIm->size()) ; aK2++)
+        {
+			Tiff_Im aTF2= Tiff_Im::StdConvGen(aDir + (*aSetIm)[aK2],1,false);
+			Im2D_U_INT1  aIm2(aSz.x,aSz.y);
+			ELISE_COPY
+				(
+				   aTF2.all_pts(),
+				   aTF2.in(),
+				   aIm2.out()
+				);
+
+			U_INT1 ** aData2 = aIm2.data();
+
+			string prefixe="";
+            if (aK1!=aK2)
+            {
+               std::string aNamePack =  aDir +  aICNM->Assoc1To2
+                                        (
+                                           "NKS-Assoc-CplIm2Hom@"+prefixe + "@" + Extension,
+                                           (*aSetIm)[aK1],
+                                           (*aSetIm)[aK2],
+                                           true
+                                        );
+
+				   bool Exist = ELISE_fp::exist_file(aNamePack);
+                   if (Exist)
+                   {
+                      ElPackHomologue aPack = ElPackHomologue::FromFile(aNamePack);
+
+                           for 
+                           (
+                               ElPackHomologue::const_iterator itP=aPack.begin();
+                               itP!=aPack.end();
+                               itP++
+                           )
+                           {
+							   cpt++;
+							   //Compute the distance between the point and the center of the image
+							   double x0=aSz.x/2;
+							   double y0=aSz.y/2;
+							   double Dist1=sqrt(pow(itP->P1().x-x0,2)+pow(itP->P1().y-y0,2));
+							   double Dist2=sqrt(pow(itP->P2().x-x0,2)+pow(itP->P2().y-y0,2));
+							   //Check that the distances are different-> point relevent
+                               //double rap=Dist1/Dist2;
+
+							   if(1){//(Dist1>aSz.x/3 || Dist2>aSz.x/3)){// && (rap<0.75 || rap>1.33)){Filtre à mettre en place?
+								   D1.push_back(Dist1);
+								   D2.push_back(Dist2);
+								   //X1.push_back(itP->P1().x);Unused, was for possible linear shift in vignette
+								   //Y1.push_back(itP->P1().y);
+								   //X2.push_back(itP->P2().x);
+								   //Y2.push_back(itP->P2().y);
+								   //Go looking for grey value of the point
+								   double G = Reechantillonnage::biline(aData1, aSz.x, aSz.y, itP->P1());
+								   G1.push_back(G);
+								   G = Reechantillonnage::biline(aData2, aSz.x, aSz.y, itP->P2());
+								   G2.push_back(G);
+							   }
+                   }
+				   }
+                   else
+                      std::cout  << "     # NO PACK FOR  : " << aNamePack  << "\n";
+            }
+        }
+    }
+	int nbpts=G1.size();
+	std::cout<<"Total number of points used in least square : "<<nbpts<<" out of "<<cpt<<endl;
+	vector<vector<double> > aPtsHomol;
+	vector<double> SZ;
+	SZ.push_back(aSz.x);SZ.push_back(aSz.y);
+	aPtsHomol.push_back(D1);
+	aPtsHomol.push_back(D2);
+	aPtsHomol.push_back(G1);
+	aPtsHomol.push_back(G2);
+	aPtsHomol.push_back(SZ);
+	//aPtsHomol.push_back(X1);
+	//aPtsHomol.push_back(Y1);
+	//aPtsHomol.push_back(X2);
+	//aPtsHomol.push_back(Y2);
+   return aPtsHomol;
+}
+
+void Vignette_correct(string aDir,std::vector<std::string> * aSetIm,vector<double> aParam,string aDirOut){
+
+	//Bulding the output file system
+    ELISE_fp::MkDirRec(aDir + aDirOut);
+	//Reading input files
+    int nbIm=(aSetIm)->size();
+    for(int i=0;i<nbIm;i++)
+	{
+	    string aNameIm=(*aSetIm)[i];
+		cout<<"Correcting "<<aNameIm<<endl;
+		string aNameOut=aDir + aDirOut + aNameIm +"_vodka.tif";
+
+		//Reading the image and creating the objects to be manipulated
+		Tiff_Im aTF= Tiff_Im::StdConvGen(aDir + aNameIm,3,false);
+		Pt2di aSz = aTF.sz();
+
+		Im2D_U_INT1  aImR(aSz.x,aSz.y);
+		Im2D_U_INT1  aImG(aSz.x,aSz.y);
+		Im2D_U_INT1  aImB(aSz.x,aSz.y);
+
+		ELISE_COPY
+		(
+		   aTF.all_pts(),
+		   aTF.in(),
+		   Virgule(aImR.out(),aImG.out(),aImB.out())
+		);
+
+		U_INT1 ** aDataR = aImR.data();
+		U_INT1 ** aDataG = aImG.data();
+		U_INT1 ** aDataB = aImB.data();
+
+		for (int aY=0 ; aY<aSz.y  ; aY++)
+			{
+				for (int aX=0 ; aX<aSz.x  ; aX++)
+				{
+					double x0=aSz.x/2;
+					double y0=aSz.y/2;
+					double D=pow(aX-x0,2)+pow(aY-y0,2);
+					double R = aDataR[aY][aX] + 255*(aParam[0]*D+aParam[1]*pow(D,2)+aParam[2]*pow(D,3));
+					double G = aDataG[aY][aX] + 255*(aParam[0]*D+aParam[1]*pow(D,2)+aParam[2]*pow(D,3));
+					double B = aDataB[aY][aX] + 255*(aParam[0]*D+aParam[1]*pow(D,2)+aParam[2]*pow(D,3));
+					if(R>255){aDataR[aY][aX]=255;}else{aDataR[aY][aX]=R;}
+					if(G>255){aDataG[aY][aX]=255;}else{aDataG[aY][aX]=G;}
+					if(B>255){aDataB[aY][aX]=255;}else{aDataB[aY][aX]=B;}
+				}
+		}
+
+		 Tiff_Im  aTOut
+			(
+				aNameOut.c_str(),
+				aSz,
+				GenIm::u_int1,
+				Tiff_Im::No_Compr,
+				Tiff_Im::RGB
+			);
+
+
+		 ELISE_COPY
+			 (
+				 aTOut.all_pts(),
+				 Virgule(aImR.in(),aImG.in(),aImB.in()),
+				 aTOut.out()
+			 );
+	  
+	}
+}
+
+vector<double> Vignette_Solve(vector<vector<double> > aPtsHomol)
+{
+
+	   // Create L2SysSurResol to solve least square equation with 3 unknown
+	L2SysSurResol aSys(3);
+
+  	//For Each SIFT point
+	for(int i=0;i<int(aPtsHomol[0].size());i++){
+				 double aPds[3]={(aPtsHomol[3][i]*pow(aPtsHomol[1][i],2)-aPtsHomol[2][i]*pow(aPtsHomol[0][i],2)),
+								 (aPtsHomol[3][i]*pow(aPtsHomol[1][i],4)-aPtsHomol[2][i]*pow(aPtsHomol[0][i],4)),
+								 (aPtsHomol[3][i]*pow(aPtsHomol[1][i],6)-aPtsHomol[2][i]*pow(aPtsHomol[0][i],6)),
+								 //(aPtsHomol[7][i]*aPtsHomol[4][i]-aPtsHomol[6][i]*aPtsHomol[1][i]),
+								 //(aPtsHomol[7][i]*aPtsHomol[5][i]-aPtsHomol[6][i]*aPtsHomol[2][i])
+								};
+				 aSys.AddEquation(1,aPds,aPtsHomol[2][i]-aPtsHomol[3][i]);
+	}
+
+	//System has 3 unknowns and nbPtsSIFT equations (significantly more than enough)
+
+    bool Ok;
+    Im1D_REAL8 aSol = aSys.GSSR_Solve(&Ok);
+
+	vector<double> aParam;
+    if (Ok)
+    {
+        double* aData = aSol.data();
+        std::cout << "Vignette parameters : " << aData[0] << " " << aData[1] << " " << aData[2] << "\n";
+		aParam.push_back(aData[0]);
+		aParam.push_back(aData[1]);
+		aParam.push_back(aData[2]);
+    }
+		return aParam;
+}
+
+int  Vignette_main(int argc,char ** argv)
+{
+
+	std::string aFullPattern,aDirOut="Vignette/";
+	bool InTxt=false,DoCor=false;
+	  //Reading the arguments
+        ElInitArgMain
+        (
+            argc,argv,
+            LArgMain()  << EAMC(aFullPattern,"Images Pattern"),
+            LArgMain()  << EAM(aDirOut,"Out",true,"Output folder (end with /) and/or prefix (end with another char)")
+						<< EAM(InTxt,"InTxt",true,"True if homologous points have been exported in txt (Defaut=false)")
+						<< EAM(DoCor,"DoCor",true,"Use the computed parameters to correct the images (Defaut=false)")
+        );
+		std::string aDir,aPatIm;
+		SplitDirAndFile(aDir,aPatIm,aFullPattern);
+
+		std::string Extension = "dat";
+		if (InTxt){Extension="txt";}
+
+		cInterfChantierNameManipulateur * aICNM = cInterfChantierNameManipulateur::BasicAlloc(aDir);
+		const std::vector<std::string> * aSetIm = aICNM->Get(aPatIm);
+
+		vector<vector<string> > listOfListIm;
+		vector<vector<double> > vectOfDiaphFoc;
+        for (int j=0;j<(int)aSetIm->size();j++){
+			std::string aFullName=(*aSetIm)[j];
+			const cMetaDataPhoto & infoIm = cMetaDataPhoto::CreateExiv2(aFullName);
+			vector<double> diaphFoc;diaphFoc.push_back(infoIm.Diaph());diaphFoc.push_back(infoIm.FocMm());
+			//Creating a new list of images for each combination of Diaph & Foc
+			cout<<"Getting Diaph and Focal from "<<aFullName<<endl;
+			if (vectOfDiaphFoc.size()==0){
+				vectOfDiaphFoc.push_back(diaphFoc);
+				vector<string>newSetOfIm;
+				newSetOfIm.push_back(aFullName);
+				listOfListIm.push_back(newSetOfIm);
+			}else{
+                for (int i=0;i<(int)vectOfDiaphFoc.size();i++){
+					if (diaphFoc==vectOfDiaphFoc[i]){
+						listOfListIm[i].push_back(aFullName); 
+						break;
+                        }else{if(i==(int)vectOfDiaphFoc.size()-1){
+								vectOfDiaphFoc.push_back(diaphFoc);
+								vector<string>newSetOfIm;
+								newSetOfIm.push_back(aFullName);
+								listOfListIm.push_back(newSetOfIm);
+								break;
+							}else{continue;}}
+					}
+			}
+		}
+		cout<<"Number of different sets of images with the same Diaph-Focal combination : "<<listOfListIm.size()<<endl;
+        for(int i=0;i<(int)listOfListIm.size();i++){
+			std::cout << "Computing the parameters of the vignette effect for the set of "<<listOfListIm[i].size()<<" images with Diaph="<<vectOfDiaphFoc[i][0]<<" and Foc="<<vectOfDiaphFoc[i][1]<<endl;
+
+		//Avec Points homol
+			vector<vector<double> > aPtsHomol=ReadPtsHom(aDir, & listOfListIm[i],Extension);
+			//aPtsHomol est l'ensemble des vecteurs D1,D2,G1,G2,SZ;
+			vector<double> aParam = Vignette_Solve(aPtsHomol);
+			
+		//Avec Stack
+				/*
+				string cmdStack="mm3d StackFlatField " + aFullPattern + " 16";
+				system_call(cmdStack.c_str());
+
+				Tiff_Im aTF= Tiff_Im::StdConvGen(aDir + "FlatField.tif",1,true);
+					Pt2di aSz = aTF.sz();
+					Im2D_U_INT1  aIm(aSz.x,aSz.y);
+					ELISE_COPY
+						(
+						   aTF.all_pts(),
+						   aTF.in(),
+						   aIm.out()
+						);
+
+					U_INT1 ** aData = aIm.data();
+		
+				L2SysSurResol aSys(3);
+				int x0=aSz.x/2;
+				int y0=aSz.y/2;
+				cout<<"Size=["<<aSz.x<<" - "<<aSz.y<<"]"<<endl;
+				int cpt=0;
+				for (int aY=0 ; aY<aSz.y  ; aY=aY+8)
+					{
+						for (int aX=0 ; aX<aSz.x  ; aX=aX+8)
+						{
+							double Dist=sqrt(pow(aX-x0,2)+pow(aY-y0,2));
+							double aPds[3]={pow(Dist,2),pow(Dist,4),pow(Dist,6)};
+							aSys.AddEquation(1,aPds,aData[aY][aX]);
+		cpt++;
+						}
+					}
+		cout<<cpt<<" points"<<endl;
+		double* aParam = Vignette_Solve(aSys);
+		*/
+
+
+		   if (aParam.size()==0){
+			   cout<<"Could'nt compute vignette parameters"<<endl;
+		   }else{ 
+			   if (DoCor){
+			   //Correction des images avec les params calculés
+			   cout<<"Correcting the images"<<endl;
+			   Vignette_correct(aDir, & listOfListIm[i],aParam,aDirOut);
+	   
+						}
+				}
+		}
+   Vodka_Banniere();
+   return 0;
+}
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
+
---
+++ src/uti_phgrm/GpGpu/GpGpuOptimisation.cu
@@ -0,0 +1,285 @@
+#ifndef _OPTIMISATION_KERNEL_H_
+/// \brief ....
+#define _OPTIMISATION_KERNEL_H_
+
+/// \file       GpGpuOptimisation.cu
+/// \brief      Kernel optimisation
+/// \author     GC
+/// \version    0.01
+/// \date       Avril 2013
+
+#include "GpGpu/GpGpuStreamData.cuh"
+
+/// brief Calcul le Z min et max.
+__device__ void ComputeIntervaleDelta(short2 & aDz, int aZ, int MaxDeltaZ, short2 aZ_Next, short2 aZ_Prev)
+{
+    aDz.x =   aZ_Prev.x-aZ;
+    if (aZ != aZ_Next.x)
+        aDz.x = max(aDz.x,-MaxDeltaZ);
+
+    aDz.y = aZ_Prev.y-1-aZ;
+    if (aZ != aZ_Next.y-1)
+        aDz.y = min(aDz.y,MaxDeltaZ);
+
+    if (aDz.x > aDz.y)
+        if (aDz.y <0)
+            aDz.x = aDz.y;
+        else
+            aDz.y = aDz.x;
+}
+
+template<class T, bool sens > __device__ void ReadOneSens(CDeviceDataStream<T> &costStream, uint lenghtLine, T pData[][NAPPEMAX], bool& idBuffer, T* gData, ushort penteMax, uint3 dimBlockTer)
+{
+    const ushort    tid     = threadIdx.x;
+
+    for(int idParLine = 0; idParLine < lenghtLine;idParLine++)
+    {
+        const short2 uZ = costStream.read(pData[0],tid,sens,0);
+        short z = uZ.x;
+
+        while( z < uZ.y )
+        {
+            int Z       = z + tid - uZ.x;
+            if(Z < NAPPEMAX )
+                gData[idParLine * dimBlockTer.z + Z]    = pData[0][Z];
+            z          += min(uZ.y - z,WARPSIZE);
+        }
+    }
+}
+
+template<class T, bool sens > __device__ void ScanOneSens(CDeviceDataStream<T> &costStream, uint lenghtLine, T pData[][NAPPEMAX], bool& idBuffer, T* g_ForceCostVol, ushort penteMax, uint& idStreamOut )
+{
+    const ushort    tid     = threadIdx.x;
+    short2          uZ_Prev = costStream.read(pData[idBuffer],tid, sens,0);
+    short           z       = uZ_Prev.x;
+    __shared__ T    minCost;
+
+    if(sens)
+        while( z < uZ_Prev.y )
+        {
+            int Z       = z + tid - uZ_Prev.x;
+            g_ForceCostVol[Z]    = pData[idBuffer][Z];
+            z += min(uZ_Prev.y - z,WARPSIZE);
+        }
+
+    for(int idParLine = 1; idParLine < lenghtLine;idParLine++)
+    {
+
+        idStreamOut += (sens? 1 : -1) * count(uZ_Prev);
+
+        const short2 uZ_Next = costStream.read(pData[2],tid,sens,0);
+        short2 aDz;
+        short z = uZ_Next.x;
+
+        if(!tid) minCost = 1e9;
+
+        while( z < uZ_Next.y )
+        {
+            int Z = z + tid;
+
+            if( Z < uZ_Next.y)
+            {
+                ComputeIntervaleDelta(aDz,Z,penteMax,uZ_Next,uZ_Prev);
+                T costMin   = 1e9;
+
+                short ZId = Z - uZ_Next.x;
+
+                T costInit  = ZId < NAPPEMAX ? pData[2][Z - uZ_Next.x] : 0;
+
+                for(short i = aDz.x ; i <= aDz.y; i++)
+                {
+                    short idZprev = Z - uZ_Prev.x + i;
+                    if(idZprev < NAPPEMAX)
+                        costMin = min(costMin, costInit + pData[idBuffer][Z - uZ_Prev.x + i]);
+                }
+
+                if(ZId < NAPPEMAX)
+                    pData[!idBuffer][Z - uZ_Next.x] = costMin;
+
+                //int idGData     = (sens ? idParLine : lenghtLine -  idParLine - 1) * dimBlockTer.z + Z - uZ_Next.x;
+                int idGData     = idStreamOut + Z - uZ_Next.x;
+                int cost        = sens ? costMin : costMin + g_ForceCostVol[idGData] - costInit;
+
+                g_ForceCostVol[idGData]  = cost;
+
+                if(!sens)
+                    atomicMin(&minCost,cost);
+
+            }
+
+            z += min(uZ_Next.y - z,WARPSIZE);
+        }
+
+        if(!sens)
+        {
+            z = uZ_Next.x;
+            while( z < uZ_Next.y )
+            {
+                int idGData  = idStreamOut + z - uZ_Next.x + tid;
+                g_ForceCostVol[idGData] -= minCost;
+                z += min(uZ_Next.y - z,WARPSIZE);
+            }
+        }
+
+        idBuffer    = !idBuffer;
+        uZ_Prev     = uZ_Next;
+    }
+
+    idStreamOut += count(uZ_Prev);
+}
+
+template<class T> __global__ void kernelOptiOneDirection(T* g_StrCostVol, short2* g_StrId, T* g_ForceCostVol, uint3* g_RecStrParam, uint penteMax)
+{
+    __shared__ T        bufferData[WARPSIZE];
+    __shared__ short2   bufferIndex[WARPSIZE];
+    __shared__ T        pdata[3][NAPPEMAX];
+    __shared__ uint     pit_Id;
+    __shared__ uint     pit_Stream;
+    __shared__ uint     sizeLine;
+
+    uint                idStreamOut = 0;
+    bool                idBuf        = false;
+
+    if(!threadIdx.x)
+    {
+        uint3 recStrParam   = g_RecStrParam[blockIdx.x];
+        pit_Stream          = recStrParam.x;
+        pit_Id              = recStrParam.y;
+        sizeLine            = recStrParam.z;
+        //printf(" %d ] sizeLine : %d | pit_Stream : %d | pit_Id : %d \n",blockIdx.x,sizeLine,pit_Stream ,pit_Id);
+    }
+
+    __syncthreads();
+
+    CDeviceDataStream<T> costStream(bufferData, g_StrCostVol + pit_Stream,bufferIndex, g_StrId + pit_Id, sizeLine * NAPPEMAX, sizeLine);
+
+    ScanOneSens<T,eAVANT>   (costStream, sizeLine, pdata,idBuf,g_ForceCostVol + pit_Stream,penteMax, idStreamOut);
+    ScanOneSens<T,eARRIERE> (costStream, sizeLine, pdata,idBuf,g_ForceCostVol + pit_Stream,penteMax, idStreamOut);
+
+}
+
+/// \brief Lance le kernel d optimisation pour une direction
+template <class T> void LaunchKernelOptOneDirection(CuHostData3D<T> &hInputStream, CuHostData3D<short2> &hInputindex, uint nBLine, CuHostData3D<T> &h_ForceCostVol, CuHostData3D<uint3>  rStrPar)
+{
+
+    uint    deltaMax    =   3;
+    uint    dimDeltaMax =   deltaMax * 2 + 1;
+    dim3    Threads(32,1,1);
+    dim3    Blocks(nBLine,1,1);
+
+    float   hPen[PENALITE];
+    ushort  hMapIndex[WARPSIZE];
+
+    for(int i=0 ; i < WARPSIZE; i++)
+        hMapIndex[i] = i / dimDeltaMax;
+
+    for(int i=0;i<PENALITE;i++)
+        hPen[i] = ((float)(1 / 10.0f));
+
+
+    //      Copie des penalites dans le device                              ---------------
+
+
+    checkCudaErrors(cudaMemcpyToSymbol(penalite,    hPen,       sizeof(float)   * PENALITE));
+    checkCudaErrors(cudaMemcpyToSymbol(dMapIndex,   hMapIndex,  sizeof(ushort)  * WARPSIZE));
+
+
+    //      Declaration et allocation memoire des variables Device          ---------------
+
+
+    CuDeviceData3D<T>       d_InputStream    ( hInputStream .GetSize(), "d_InputStream"  );
+    CuDeviceData3D<short2>  d_InputIndex     ( hInputindex  .GetSize(), "d_InputIndex"   );
+    CuDeviceData3D<uint3>   d_RecStrParam    ( rStrPar      .GetSize(), "d_InputStream"  );
+    CuDeviceData3D<T>       d_ForceCostVol   ( hInputStream .GetSize(), "d_ForceCostVol" );
+
+
+    //      Copie du volume de couts dans le device                         ---------------
+
+
+    d_InputStream.CopyHostToDevice(  hInputStream.pData());
+    d_InputIndex .CopyHostToDevice(  hInputindex .pData());
+    d_RecStrParam.CopyHostToDevice(  rStrPar     .pData());
+
+    //                                                                      ---------------
+
+    kernelOptiOneDirection<T><<<Blocks,Threads>>>
+                                                (
+                                                    d_InputStream    .pData(),
+                                                    d_InputIndex     .pData(),
+                                                    d_ForceCostVol   .pData(),
+                                                    d_RecStrParam    .pData(),
+                                                    deltaMax
+                                                    );
+
+    getLastCudaError("kernelOptiOneDirection failed");
+
+    //                                                                      ---------------
+
+    d_ForceCostVol.CopyDevicetoHost(h_ForceCostVol.pData());
+
+
+    //      Declaration et allocation memoire des variables Device          ---------------
+
+    d_ForceCostVol  .Dealloc();
+    d_InputStream   .Dealloc();
+    d_InputIndex    .Dealloc();
+    d_RecStrParam   .Dealloc();
+}
+
+/// \brief Appel exterieur du kernel d optimisation
+extern "C" void OptimisationOneDirection(CuHostData3D<uint> &data,CuHostData3D<short2> &index, uint nBLine, CuHostData3D<uint> & h_ForceCostVol, CuHostData3D<uint3>  rStrPar)
+{
+    LaunchKernelOptOneDirection(data,index,nBLine, h_ForceCostVol, rStrPar);
+}
+
+/// \brief Appel exterieur du kernel
+extern "C" void Launch()
+{
+    uint    prof        = 40;
+    uint3   dimVolCost  = make_uint3(80,4,prof );
+
+    CuHostData3D<uint>      H_StreamCost      ( make_uint3( dimVolCost.z, dimVolCost.y, dimVolCost.x) );
+    CuHostData3D<uint>      H_ForceCostVol    ( make_uint3( dimVolCost.z, dimVolCost.y, dimVolCost.x) );
+    CuHostData3D<short2>    H_StreamIndex     ( make_uint2( dimVolCost.y, dimVolCost.x ));
+
+    H_StreamCost  .SetName("streamCost");
+    H_StreamIndex .SetName("streamIndex");
+
+    uint si = 0 , sizeStreamCost = 0;
+
+    srand (time(NULL));
+
+    for(int i = 0 ; i < dimVolCost.x ; i++)
+    {
+        int pit         = i     * dimVolCost.y;
+        int pitLine     = pit   * dimVolCost.z;
+        si =  sizeStreamCost = 0;
+
+        while (si < dimVolCost.y){
+
+            int min                         =  -CData<int>::GetRandomValue(prof / 4,prof / 2 -1);
+            int max                         =   CData<int>::GetRandomValue(prof / 4,prof / 2 -1);
+            int dim                         =   max - min + 1;
+            H_StreamIndex[pit + si]         =   make_short2(min,max);
+
+            for(int i = 0 ; i < dim; i++)
+                H_StreamCost[pitLine + sizeStreamCost + i] = i+1;//CData<uint>::GetRandomValue(16,128);
+
+            si++;
+            sizeStreamCost += dim;
+
+        }
+    }
+
+    int id = 0;
+    H_StreamCost.OutputValues(id);
+
+    //LaunchKernelOptOneDirection(H_StreamCost,H_StreamIndex,H_ForceCostVol);
+
+    H_ForceCostVol.OutputValues(id);
+
+    H_StreamCost.Dealloc();
+    H_StreamIndex.Dealloc();
+}
+
+#endif
