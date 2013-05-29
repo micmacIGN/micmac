17cf18aa74c9c7dac84f9737e02a9b39d46d78cf
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-05-29 15:05:07 +0200

    Saisie 3d : Compilation Qt5 ubuntu

---
+++ include/GpGpu/GpGpu.h
@@ -0,0 +1,11 @@
+#ifndef __GPGPU_H__
+#define __GPGPU_H__
+
+#include "GpGpu/GpGpuDefines.h"
+
+#ifdef CUDA_ENABLED
+#include "GpGpu/InterfaceMicMacGpGpu.h"
+#include "GpGpu/GpGpuOptimisation.h"
+#endif
+
+#endif // __GPGPU_H__
