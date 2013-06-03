10ad8ca361374e7127518b03f0578c2ef2a5a96f
G?rald Choqueux <G?rald Choqueux@localhost>
2013-06-03 15:23:10 +0200

    GpGpu : teamplate Data2Optimiz

--- include/GpGpu/GpGpuOptimisation.h
+++ include/GpGpu/GpGpuOptimisation.h
@@ -8,44 +8,13 @@ void LaunchKernel();
 
 
 template<template<class T> class U>
-struct Data2Optimiz2
+struct Data2Optimiz
 {
     U<uint3>     h__Param;
     U<uint>      hS_InitCostVol;
     U<uint>      hS_ForceCostVol;
     U<short2>    hS_Index;
     uint         nbLines;
-
-    Data2Optimiz2():
-        h__Param((uint)1),
-        hS_InitCostVol((uint)1),
-        hS_ForceCostVol((uint)1),
-        hS_Index((uint)1)
-    {}
-
-    void Dealloc()
-    {
-        hS_InitCostVol  .Dealloc();
-        hS_ForceCostVol .Dealloc();
-        hS_Index        .Dealloc();
-        h__Param        .Dealloc();
-    }
-
-    void ReallocIf(uint pStr,uint pIdStr)
-    {
-        hS_InitCostVol  .ReallocIf(pStr);
-        hS_ForceCostVol .ReallocIf(pStr);
-        hS_Index        .ReallocIf(pIdStr);
-    }
-};
-
-struct Data2Optimiz
-{
-    CuHostData3D<uint3>     h__Param;
-    CuHostData3D<uint>      hS_InitCostVol;
-    CuHostData3D<uint>      hS_ForceCostVol;
-    CuHostData3D<short2>    hS_Index;
-    uint                    nbLines;
 
     Data2Optimiz():
         h__Param((uint)1),
@@ -82,10 +51,56 @@ struct Data2Optimiz
     {
         nbLines = nbl;
     }
+
 };
 
+//struct Data2Optimiz
+//{
+//    CuHostData3D<uint3>     h__Param;
+//    CuHostData3D<uint>      hS_InitCostVol;
+//    CuHostData3D<uint>      hS_ForceCostVol;
+//    CuHostData3D<short2>    hS_Index;
+//    uint                    nbLines;
+
+//    Data2Optimiz():
+//        h__Param((uint)1),
+//        hS_InitCostVol((uint)1),
+//        hS_ForceCostVol((uint)1),
+//        hS_Index((uint)1)
+//    {}
+
+//    void Dealloc()
+//    {
+//        hS_InitCostVol  .Dealloc();
+//        hS_ForceCostVol .Dealloc();
+//        hS_Index        .Dealloc();
+//        h__Param        .Dealloc();
+//    }
+
+//    void ReallocParam(uint size)
+//    {
+//        h__Param.Realloc(size);
+//    }
+
+//    void SetParamLine(uint id, uint pStr,uint pIdStr, uint lLine)
+//    {
+//        h__Param[id] = make_uint3(pStr,pIdStr,lLine);
+//    }
+
+//    void ReallocIf(uint pStr,uint pIdStr)
+//    {
+//        hS_InitCostVol  .ReallocIf(pStr);
+//        hS_ForceCostVol .ReallocIf(pStr);
+//        hS_Index        .ReallocIf(pIdStr);
+//    }
+//    void SetNbLine(uint nbl)
+//    {
+//        nbLines = nbl;
+//    }
+//};
+
 extern "C" void Launch();
-extern "C" void OptimisationOneDirection(Data2Optimiz &d2O);
+extern "C" void OptimisationOneDirection(Data2Optimiz<CuHostData3D> &d2O);
 
 /// \class InterfMicMacOptGpGpu
 /// \brief Class qui permet a micmac de lancer les calculs d optimisations sur le Gpu
