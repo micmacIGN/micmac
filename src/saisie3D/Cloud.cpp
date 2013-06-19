95ea6a0a6c15721bc5c603f4e1f3aaaf853eb591
jbelvaux <jbelvaux@localhost>
2013-06-19 14:49:58 +0200

    - modif commentaire bis

--- src/saisie3D/Cloud.cpp
+++ src/saisie3D/Cloud.cpp
@@ -54,7 +54,7 @@ Vertex::Vertex(Pt3dr pos, QColor col)
 }
 
 /*!
-    Read a ply file, store the point cloud and returns true if success.
+    Read a ply file, store the point cloud
 */
 Cloud* Cloud::loadPly( string i_filename )
 {
