367df58c4c1f55c032f4f0b5f91fe581a5b23018
gmaillet <gmaillet@users.noreply.github.com>
2013-06-21 12:25:06 +0200

    correction mm3d sous MacOS

---
+++ src/saisie3D/Data.h
@@ -0,0 +1,41 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include "StdAfx.h"
+#include "general/ptxd.h"
+#include "Cloud.h"
+
+using namespace Cloud_;
+
+class cData
+{
+    public:
+
+        cData();
+        ~cData();
+
+        void addCamera(CamStenope *);
+        void addCloud(Cloud *);
+        void centerCloud(Cloud *);
+
+        void addCameras(vector <CamStenope *>);
+        void addClouds(vector <Cloud *>);
+
+        int NbCameras(){return m_Cameras.size();}
+        int NbClouds(){return m_Clouds.size();}
+
+        CamStenope * & getCamera(int aK) {return m_Cameras[aK];}
+        Cloud * & getCloud(int aK) {return m_Clouds[aK];}
+        Cloud * & getOriginalCloud(int aK) {return m_oClouds[aK];}
+
+        //Bounding box, center and diameter of all clouds
+        double m_minX, m_maxX, m_minY, m_maxY, m_minZ, m_maxZ, m_cX, m_cY, m_cZ, m_diam;
+
+   private:
+
+        vector <CamStenope *> m_Cameras;
+        vector <Cloud *>    m_Clouds;  //centered and scaled clouds
+        vector <Cloud *>    m_oClouds; //original clouds
+};
+
+#endif // DATA_H
---
+++ src/uti_phgrm/GpGpu/GpGpuMultiThreadingCpu.cpp
@@ -0,0 +1,22 @@
+#include "GpGpu/GpGpuMultiThreadingCpu.h"
+
+void JobCpuGpuTest::InitPrecompute()
+{
+}
+
+void JobCpuGpuTest::Precompute(uint idBuffer)
+{
+}
+
+
+//void JobCpuGpuTest::GpuCompute(UINT3D* host_In,UINT3D* host_Out)
+//{
+
+//   GetDataDevice().CopyHostToDevice(host_In->pData());
+
+//   Launch((uint*)GetDataDevice().pData());
+
+//   GetDataDevice().CopyDevicetoHost(host_Out->pData());
+
+//}
+
