49691b6fb217f1c6888d64c73bfe9ed8dc956c32
mdeveau <mattdeveau@users.noreply.github.com>
2013-06-25 15:28:29 +0200

    Translation de la boule

--- src/saisie3D/GLWidget.cpp
+++ src/saisie3D/GLWidget.cpp
@@ -1034,6 +1034,9 @@ void GLWidget::drawAxis()
 
         glEndList();
     }
+
+    glTranslatef(m_Data->m_cX,m_Data->m_cY,m_Data->m_cZ);
+
     glCallList(m_trihedronGLList);
 
     glPopMatrix();
@@ -1113,6 +1116,7 @@ void GLWidget::drawBall()
         glEndList();
     }
 
+    glTranslatef(m_Data->m_cX,m_Data->m_cY,m_Data->m_cZ);
     glScalef(scale,scale,scale);
 
     glCallList(m_ballGLList);
