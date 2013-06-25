b818cf2f07f5fba04d502869a9f73570113d2ef3
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-06-25 13:42:36 +0200

    Saisie3D : optimisation

---
+++ src/saisie3D/Data.h
@@ -0,0 +1,76 @@
+#ifndef DATA_H
+#define DATA_H
+
+
+#include "StdAfx.h"
+#include "Cloud.h"
+#include <QVector>
+#include <QPoint>
+
+using namespace Cloud_;
+
+class cData
+{
+    public:
+
+        cData();
+        ~cData();
+
+        void addCamera(CamStenope *);
+        void addCloud(Cloud *);
+        void centerCloud(Cloud *);
+
+        void addCameras(vector <CamStenope *>);
+        void addClouds(vector <Cloud *>);
+
+        void clearCameras();
+        void clearClouds();
+
+        int NbCameras(){return m_Cameras.size();}
+        int NbClouds(){return m_Clouds.size();}
+
+        CamStenope * & getCamera(int aK) {return m_Cameras[aK];}
+        Cloud * & getCloud(int aK) {return m_Clouds[aK];}
+        Cloud * & getOriginalCloud(int aK) {return m_oClouds[aK];}
+
+        int     GetSizeClouds();
+
+        //Bounding box, center and diameter of all clouds
+        double m_minX, m_maxX, m_minY, m_maxY, m_minZ, m_maxZ, m_cX, m_cY, m_cZ, m_diam;
+
+   private:
+
+        vector <CamStenope *> m_Cameras;
+        vector <Cloud *>      m_Clouds;  //centered and scaled clouds
+        vector <Cloud *>      m_oClouds; //original clouds
+};
+
+class cSaisieInfos
+{
+    public:
+
+        cSaisieInfos();
+        ~cSaisieInfos();
+
+        //! Selection mode
+        enum SELECTION_MODE { INSIDE,
+                              OUTSIDE,
+                              ADD,
+                              NONE
+        };
+
+        cSaisieInfos(float rotX, float rotY, float translation[3], float scale, QVector <QPoint> polyline, SELECTION_MODE);
+
+    private:
+         //Ortho camera infos
+         float               m_rotationX;
+         float               m_rotationY;
+         float*              m_translation;
+         float               m_scale;
+
+         //polyline infos
+         QVector <QPoint>    m_poly;
+         SELECTION_MODE      m_selection_mode;
+};
+
+#endif // DATA_H
