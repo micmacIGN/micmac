754e370f66572009a2ff6f0cdeae70bec27cb926
gmaillet <gmaillet@users.noreply.github.com>
2013-06-28 09:46:40 +0200

    correction warning MacOS

--- src/uti_phgrm/MICMAC/cModuleOrientation.h
+++ src/uti_phgrm/MICMAC/cModuleOrientation.h
@@ -45,14 +45,14 @@ Header-MicMac-eLiSe-25/06/2007*/
 class ModuleOrientation
 {
 public:
-  ModuleOrientation(std::string const &nom){;}
+  ModuleOrientation(std::string const &/*nom*/){;}
   virtual ~ModuleOrientation(){;}
-  virtual void ImageAndPx2Obj(double c, double l, const double *z,
-			      double &x, double &y)const{;};
-  virtual void Objet2ImageInit(double x, double y, const double *z,
-			       double &c, double &l)const{;};
+  virtual void ImageAndPx2Obj(double /*c*/, double /*l*/, const double */*z*/,
+                              double &/*x*/, double &/*y*/)const{;};
+  virtual void Objet2ImageInit(double /*x*/, double /*y*/, const double */*z*/,
+                               double &/*c*/, double &/*l*/)const{;};
   virtual double GetResolMoyenne() const {return 1.;}
-  virtual bool GetPxMoyenne(double * aPxMoy) const {return false;}
+  virtual bool GetPxMoyenne(double * /*aPxMoy*/) const {return false;}
 };
 
 #endif
