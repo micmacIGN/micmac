40fed26279656156c93fa34a7170a56588a776b5
G?rald Choqueux <G?rald Choqueux@localhost>
2013-07-05 16:57:09 +0200

    GpGpu : CSimpleJobCpuGpu

--- src/uti_phgrm/GpGpu/GpGpuMultiThreadingCpu.cpp
+++ src/uti_phgrm/GpGpu/GpGpuMultiThreadingCpu.cpp
@@ -1,15 +1,74 @@
 #include "GpGpu/GpGpuMultiThreadingCpu.h"
 
+template< class T >
+CSimpleJobCpuGpu<T>::CSimpleJobCpuGpu(bool useMultiThreading):
+    _useMultiThreading(useMultiThreading)
+{
+    if(UseMultiThreading())
+        _gpGpuThread = new boost::thread(&CSimpleJobCpuGpu::threadCompute,this);
+}
 
+template< class T >
+CSimpleJobCpuGpu<T>::~CSimpleJobCpuGpu()
+{
+    if(UseMultiThreading())
+    {
+        _gpGpuThread->interrupt();
+        //_gpGpuThread->join();
+        delete _gpGpuThread;
+    }
+    _mutexCompu.unlock();
+    _mutexCopy.unlock();
+    _mutexPreCompute.unlock();
+}
 
-//void JobCpuGpuTest::GpuCompute(UINT3D* host_In,UINT3D* host_Out)
-//{
+template< class T >
+void CSimpleJobCpuGpu<T>::SetCompute(T toBeComputed)
+{
+    boost::lock_guard<boost::mutex> guard(_mutexCompu);
+    _compute = toBeComputed;
+}
 
-//   GetDataDevice().CopyHostToDevice(host_In->pData());
+template< class T >
+bool CSimpleJobCpuGpu<T>::GetCompute()
+{
+    boost::lock_guard<boost::mutex> guard(_mutexCompu);
+    return _compute;
+}
 
-//   Launch((uint*)GetDataDevice().pData());
+template< class T >
+void CSimpleJobCpuGpu<T>::SetDataToCopy(T toBeCopy)
+{
+    boost::lock_guard<boost::mutex> guard(_mutexCopy);
+    _copy = toBeCopy;
 
-//   GetDataDevice().CopyDevicetoHost(host_Out->pData());
+}
 
-//}
+template< class T >
+bool CSimpleJobCpuGpu<T>::GetDataToCopy()
+{
+    boost::lock_guard<boost::mutex> guard(_mutexCopy);
+    return _copy;
+}
+
+template< class T >
+void CSimpleJobCpuGpu<T>::SetPreComp(bool canBePreCompute)
+{
+    boost::lock_guard<boost::mutex> guard(_mutexPreCompute);
+    _precompute = canBePreCompute;
+}
+
+template< class T >
+bool CSimpleJobCpuGpu<T>::GetPreComp()
+{
+    boost::lock_guard<boost::mutex> guard(_mutexPreCompute);
+    return _precompute;
+
+}
+
+template< class T >
+bool CSimpleJobCpuGpu<T>::UseMultiThreading()
+{
+    return _useMultiThreading;
+}
 
