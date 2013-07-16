57ea844c4106de6fe059b7b22ffb3d21a632f220
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-07-16 17:32:26 +0200

    GpGpu : clean code

---
+++ src/saisie3D/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication::setStyle("fusion");
+
+    QApplication app(argc, argv);
+	
+    app.setOrganizationName("IGN");
+    app.setApplicationName("Saisie3D");
+
+    MainWindow w;
+    w.show();
+
+    QStringList cmdline_args = QCoreApplication::arguments();
+
+    if (cmdline_args.size() > 1)
+    {
+        cmdline_args.pop_front();
+        w.addFiles(cmdline_args);
+    }
+
+    w.checkForLoadedData();
+
+    return app.exec();
+}
+
