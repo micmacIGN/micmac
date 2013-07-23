cc57a7a3764c0b48939cf38746a4235578a1eefe
jbelvaux <jbelvaux@localhost>
2013-07-23 13:59:00 +0200

    - bug appel de Tapas en dehors du r?pertoire chantier

---
+++ src/recipes/Sources.cmake
@@ -0,0 +1,13 @@
+set(Recipes_Src_Files
+	${RECIPES_DIR}/refft.cpp
+	${RECIPES_DIR}/regaussj.cpp
+	${RECIPES_DIR}/rejacobi.cpp
+	${RECIPES_DIR}/resparse.cpp
+)
+
+SOURCE_GROUP(Recipes FILES ${Recipes_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${Recipes_Src_Files}
+)
---
+++ src/uti_phgrm/GpGpu/GpGpuOptimisation.cpp
@@ -0,0 +1,78 @@
+#include "GpGpu/GpGpuOptimisation.h"
+
+InterfOptimizGpGpu::InterfOptimizGpGpu()
+{
+    CreateJob();
+}
+
+InterfOptimizGpGpu::~InterfOptimizGpGpu(){}
+
+void InterfOptimizGpGpu::Dealloc()
+{
+    _H_data2Opt.Dealloc();
+    _D_data2Opt.Dealloc();
+}
+
+void InterfOptimizGpGpu::oneDirOptGpGpu()
+{
+    _D_data2Opt.SetNbLine(_H_data2Opt._nbLines);
+    _H_data2Opt.ReallocOutputIf(_H_data2Opt._s_InitCostVol.GetSize());
+    _D_data2Opt.ReallocIf(_H_data2Opt);
+
+    //      Transfert des données vers le device                            ---------------		-
+    _D_data2Opt.CopyHostToDevice(_H_data2Opt);
+
+    //      Kernel optimisation                                             ---------------     -
+    OptimisationOneDirection(_D_data2Opt);
+    getLastCudaError("kernelOptiOneDirection failed");
+
+    //      Copie des couts de passage forcé du device vers le host         ---------------     -
+    _D_data2Opt.CopyDevicetoHost(_H_data2Opt);
+
+}
+
+void InterfOptimizGpGpu::ReallocParam(uint size)
+{
+    ResetIdBuffer();
+    _idDir  = 0;
+    _H_data2Opt.ReallocParam(size);
+    _D_data2Opt.ReallocParam(size);
+}
+
+void InterfOptimizGpGpu::threadCompute()
+{
+
+    while(true)
+    {
+        boost::this_thread::sleep(boost::posix_time::microsec(1));
+        if(GetCompute())
+        {
+            SetCompute(false);
+            _D_data2Opt.SetNbLine(_H_data2Opt._nbLines);
+            _H_data2Opt.ReallocOutputIf(_H_data2Opt._s_InitCostVol.GetSize(),GetIdBuf());
+            _D_data2Opt.ReallocIf(_H_data2Opt);
+
+            //      Transfert des données vers le device                            ---------------		-
+            _D_data2Opt.CopyHostToDevice(_H_data2Opt,GetIdBuf());
+
+            SetPreComp(true);
+
+            //      Kernel optimisation                                             ---------------     -
+            OptimisationOneDirection(_D_data2Opt);
+
+            //      Copie des couts de passage forcé du device vers le host         ---------------     -
+            _D_data2Opt.CopyDevicetoHost(_H_data2Opt,GetIdBuf());
+            SwitchIdBuffer();
+            while(GetDataToCopy());
+            SetDataToCopy(true);        
+        }
+    }
+}
+
+void InterfOptimizGpGpu::freezeCompute()
+{
+    SetDataToCopy(false);
+    SetCompute(false);
+    SetPreComp(false);
+}
+
