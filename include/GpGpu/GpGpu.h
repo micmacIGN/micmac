52e25ef136f2103f96907532c9bea19521407547
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-07-26 17:23:35 +0200

    GpGpu : encap

---
+++ include/GpGpu/GpGpu.h
@@ -0,0 +1,11 @@
+#ifndef __GPGPU_H__
+#define __GPGPU_H__
+
+#include "GpGpu/GpGpuDefines.h"
+
+#if CUDA_ENABLED
+#include "GpGpu/InterfaceMicMacGpGpu.h"
+#include "GpGpu/GpGpuOptimisation.h"
+#endif
+
+#endif // __GPGPU_H__
