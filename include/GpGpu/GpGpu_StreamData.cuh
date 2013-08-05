677393b336e40f73899617761783ecea5864ff81
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2013-08-05 17:20:06 +0200

    GpGpu : Optimisation nouvelle approche 5

---
+++ include/GpGpu/GpGpu_StreamData.cuh
@@ -0,0 +1,209 @@
+#ifndef _GPGPUSTREAMDATA_H_
+#define _GPGPUSTREAMDATA_H_
+
+#include "GpGpu/GpGpu_Data.h"
+
+using namespace std;
+
+static __constant__ ushort  dMapIndex[WARPSIZE];
+
+/// \class CDeviceStream
+/// \brief Classe gerant un flux de données en memoire video
+template< class T >
+class CDeviceStream
+{
+public:
+
+    __device__ CDeviceStream(T* buf,T* stream, uint sizeStream):
+        _bufferData(buf),
+        _streamData(stream),
+        _curStreamId(0),
+        _curBufferId(WARPSIZE),
+        _sizeStream(sizeStream)
+    {}
+
+    __device__ virtual short getLen2ReadAV(short2 &index)
+    {
+        index = make_short2(0,0);
+        return 1;
+    }
+
+    __device__ virtual short getLen2ReadAR(short2 &index)
+    {
+        index = make_short2(0,0);
+        return 1;
+    }
+
+
+    template<bool sens, class D> __device__ short2 read(D* destData, ushort tid, T def);
+
+private:
+
+    template<bool sens> __device__ short vec(){ return 1 - 2 * !sens; }
+
+    template<bool sens> __device__ short GetNbToCopy(ushort nTotal,ushort nCopied)
+    {
+        return min(nTotal - nCopied , MaxReadBuffer<sens>());
+    }
+
+    template<bool sens> __device__ ushort MaxReadBuffer()
+    {
+        return sens ? ((ushort)WARPSIZE - _curBufferId) : _curBufferId;
+    }
+
+
+    T*                          _bufferData;
+    T*                          _streamData;
+    uint                        _curStreamId;
+    ushort                      _curBufferId;
+    uint                        _sizeStream;
+};
+
+template< class T > template<bool sens, class D>  __device__
+short2 CDeviceStream<T>::read(D *destData, ushort tid, T def)
+{
+    short2  index;
+    ushort  NbCopied    = 0 , NbTotalToCopy = sens ? getLen2ReadAV(index) : getLen2ReadAR(index);
+    short   PitSens     = !sens * WARPSIZE;
+
+    while(NbCopied < NbTotalToCopy)
+    {
+        ushort NbToCopy = GetNbToCopy<sens>(NbTotalToCopy,NbCopied);
+
+        if(!NbToCopy)
+        {
+            uint idStream =_curStreamId + threadIdx.x - 2 * PitSens;
+            if(idStream < _sizeStream)
+                _bufferData[threadIdx.x] = _streamData[idStream];
+            _curBufferId   = PitSens;            
+            _curStreamId   = _curStreamId  + (vec<sens>()<<5); // * 32
+
+            NbToCopy = GetNbToCopy<sens>(NbTotalToCopy,NbCopied);
+        }
+
+        ushort idDest = tid + (sens ? NbCopied : NbTotalToCopy - NbCopied - NbToCopy);
+
+        if(!(idDest>>8)) // < 256
+        {
+            if (tid < NbToCopy && idDest < NbTotalToCopy)
+                destData[idDest] = _bufferData[_curBufferId + tid - !sens * NbToCopy];
+            else if (idDest >= NbTotalToCopy)
+                destData[idDest] = def;
+
+        }
+        _curBufferId  = _curBufferId + vec<sens>() * NbToCopy;
+        NbCopied     += NbToCopy;
+   }
+
+   return index;
+}
+
+
+template< class T >
+class CDeviceDataStream : public CDeviceStream<T>
+{
+public:
+
+    __device__ CDeviceDataStream(T* buf,T* stream,short2* bufId,short2* streamId, uint sizeStream, uint sizeStreamId):
+        CDeviceStream<T>(buf,stream,sizeStream),
+        _streamIndex(bufId,streamId,sizeStreamId)
+    {}
+
+
+    __device__ short getLen2ReadAV(short2 &index)
+    {       
+
+        _streamIndex.read<eAVANT>(&index,0,make_short2(0,0));
+
+        return diffYX(index) + 1;
+    }
+
+    __device__ short getLen2ReadAR(short2 &index)
+    {
+
+        _streamIndex.read<eARRIERE>(&index,0,make_short2(0,0));
+
+        return diffYX(index) + 1;
+    }
+
+private:
+
+    CDeviceStream<short2>       _streamIndex;
+
+};
+
+#define sgn s<sens>
+#define max_cost 1e9
+
+template< bool sens,class T>
+__device__ inline T s(T v)
+{
+    return sens ? v : -v;
+}
+
+
+template<class T>
+class SimpleStream
+{
+public:
+
+    SimpleStream(   T*      globalStream,
+                    ushort  sizeBuffer);
+
+    template<bool sens> void read(T* sharedBuffer);
+
+    template<bool sens> void incremt();
+
+    T                   GetValue(uint id);
+    void                SetValue(uint id,T value);
+
+private:
+
+
+    ushort  _idS;
+    T*      _globalStream;
+    uint    _idG;
+    ushort  _sizeBuffer;
+};
+
+template<class T>
+SimpleStream<T>::SimpleStream( T *globalStream, ushort sizeBuffer):
+    _idS(0),
+    _globalStream(globalStream + threadIdx.x),
+    _idG(0),
+    _sizeBuffer(sizeBuffer)
+{}
+
+template<class T>
+T SimpleStream<T>::GetValue(uint id)
+{
+    return _globalStream[_idG + id];
+}
+
+template<class T>
+void SimpleStream<T>::SetValue(uint id, T value)
+{
+    _globalStream[_idG + id] = value;
+}
+
+template<class T> template<bool sens>
+void SimpleStream<T>::read(T *sharedBuffer)
+{
+    for(ushort i = 0; i < sgn(_sizeBuffer); i+= sgn(WARPSIZE))
+        *(sharedBuffer + i) = *(_globalStream + i);
+
+    if(!threadIdx.x)
+    {
+        _idS  = 0;
+        _idG += sgn(_sizeBuffer);
+    }
+}
+
+template<class T> template<bool sens>
+void SimpleStream<T>::incremt()
+{
+    _idG += sgn(_sizeBuffer);
+}
+#endif
+
+
---
+++ src/uti_phgrm/GpGpu/GpGpu_Cuda_Optimisation_Z.cu
@@ -0,0 +1,154 @@
+#ifndef _OPTIMISATION_KERNEL_Z_H_
+#define _OPTIMISATION_KERNEL_Z_H_
+
+#include "GpGpu/GpGpu_StreamData.cuh"
+#include "GpGpu/SData2Optimize.h"
+
+// On pourrait imaginer un buffer des tailles calculer en parallel
+// SIZEBUFFER[threadIdx.x] = count(lI[threadIdx.x]);
+
+
+__device__ void GetConeZ(short2 & aDz, int aZ, int MaxDeltaZ, short2 aZ_Next, short2 aZ_Prev)
+{
+    aDz.x =   aZ_Prev.x-aZ;
+    if (aZ != aZ_Next.x)
+        aDz.x = max(aDz.x,-MaxDeltaZ);
+
+    aDz.y = aZ_Prev.y-1-aZ;
+    if (aZ != aZ_Next.y-1)
+        aDz.y = min(aDz.y,MaxDeltaZ);
+
+    if (aDz.x > aDz.y)
+        if (aDz.y <0)
+            aDz.x = aDz.y;
+        else
+            aDz.y = aDz.x;
+}
+
+template<bool sens>
+__device__ inline void ReadIndex(short2 *g_BuffIdex, short2 *s_BuffIdex, uint& g_id,ushort* dZ)
+{
+    *(s_BuffIdex) = *(g_BuffIdex + g_id);
+    *(dZ) = count(*(s_BuffIdex));
+    g_id += sgn(WARPSIZE);
+}
+
+template<bool sens>
+__device__ inline void ReadInitCost(ushort *g__ICst, ushort* s__ICst, ushort& s_id, uint& g_id)
+{    
+    for(ushort i = 0; i < sgn(NAPPEMAX); i+= sgn(WARPSIZE))
+        *(s__ICst + i) = *(g__ICst + i);
+
+    s_id  = 0;
+    g_id += sgn(NAPPEMAX);
+}
+
+template<class T, bool sens> __device__
+void RunLine(SimpleStream<short2> &streamIndex, SimpleStream<uint> streamFCost, SimpleStream<ushort> &streamICost,short2* S_Bf_Index,ushort *S_Bf_ICost, uint* G_Stream_FCost, uint  &g_id_Index, uint   &g_id_ICost,ushort &s_id_ICost,uint lenghtLine)
+{
+    const ushort  tid       = threadIdx.x;
+    const ushort penteMax   = 3;
+    uint  id_Line           = 0;
+
+    short2* ivS_Bf_Index    = S_Bf_Index     + WARPSIZE;
+    ushort* ST_Bf_ICost     = S_Bf_ICost     + tid;
+
+
+    __shared__ ushort s_dZ[WARPSIZE];
+
+    short2 ConeZ;
+
+    __shared__ uint S_FCost[2][NAPPEMAX];
+
+    short2  prevIndex;
+
+    bool sB = false;
+
+    if(!sens) streamICost.read<sens>(S_Bf_ICost);
+
+    while(id_Line < lenghtLine)
+    {
+        streamIndex.read<sens>(S_Bf_Index);
+
+        uint segment = min(lenghtLine-id_Line,WARPSIZE);
+
+        while(segment)
+        {
+            const short2 index  = *(ivS_Bf_Index + sgn(segment));
+            const ushort dZ     = *(s_dZ + WARPSIZE  + sgn(segment));
+            ushort       z      = 0;
+            uint globMinFCost   = max_cost;
+            ushort s_idCur_ICost;
+
+            while(z < dZ)
+            {
+                s_idCur_ICost = s_idCur_ICost + z;
+
+                if(s_idCur_ICost > NAPPEMAX)
+                {
+                    streamICost.read<sens>(S_Bf_ICost);
+                    streamFCost.incremt<sens>();
+                    //GT_Stream_FCost  += sgn(NAPPEMAX);
+                    s_idCur_ICost = z;
+                }
+
+                uint fCostMin           = max_cost;
+
+                const ushort costInit   = ST_Bf_ICost[s_idCur_ICost];
+                const ushort tZ         = z + tid;
+                const short  Z          = index.x + tZ;
+
+                GetConeZ(ConeZ,Z,penteMax,index,prevIndex);
+
+                uint* prevFCost = S_FCost[sB] + Z - prevIndex.x;
+
+                #pragma unroll
+                for (int i = ConeZ.x; i < ConeZ.y; ++i)
+                    fCostMin = min(fCostMin, costInit + *(prevFCost+i));
+
+                const uint fcost                = fCostMin + sens * (streamFCost.GetValue(s_idCur_ICost) - costInit);
+                S_FCost[!sB][tZ]                = fcost;
+
+                streamFCost.SetValue(s_idCur_ICost, fcost);
+
+                if(!sens) atomicMin(&globMinFCost,fcost);
+
+                z += WARPSIZE;
+            }
+
+            if(!sens)
+            {
+                // retrancher le globMinFCost
+            }
+
+            prevIndex = index;
+
+            segment--;
+        }
+
+        id_Line += WARPSIZE;
+    }
+}
+
+template<class T> __device__
+void Run(short2 *g_Index,ushort *g_ICost, uint* g_FCost )
+{
+    __shared__ short2 S_BuffIndex[WARPSIZE];
+    __shared__ ushort S_BuffICost[NAPPEMAX];
+    __shared__ ushort S_BuffFCost[2][NAPPEMAX];
+
+    SimpleStream<ushort>    streamICost(g_ICost,NAPPEMAX);
+    SimpleStream<uint>      streamFCost(g_FCost,NAPPEMAX);
+    SimpleStream<short2>    streamIndex(g_Index,WARPSIZE);
+
+    RunLine<T,true>(streamICost,streamFCost,streamIndex,S_BuffIndex,S_BuffICost,S_BuffFCost,777);
+
+//    g_idIX -= WARPSIZE;
+//    g_idICO-= NAPPEMAX;
+
+    RunLine<T,false>(streamICost,streamFCost,streamIndex,S_BuffIndex,S_BuffICost,S_BuffFCost,777);
+}
+
+
+#endif //_OPTIMISATION_KERNEL_Z_H_
+
