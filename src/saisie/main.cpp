f00fbba0c5b6969a5258da6cc4d4f175afd3d72a
mdeveau <mattdeveau@users.noreply.github.com>
2013-08-27 15:43:11 +0200

    saisie: Erreur d'index

---
+++ src/saisie/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <QtGui>
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication::setStyle("fusion");
+
+    QApplication app(argc, argv);
+	
+    app.setOrganizationName("IGN");
+    app.setApplicationName("saisie");
+
+    const QString locale = QLocale::system().name().section('_', 0, 0);
+
+    // qt translations
+    QTranslator qtTranslator;
+    qtTranslator.load(app.applicationName() + "_" + locale);
+    app.installTranslator(&qtTranslator);
+
+    MainWindow w;
+
+    QStringList cmdline_args = QCoreApplication::arguments();
+
+    if (cmdline_args.size() > 1)
+    {
+        cmdline_args.pop_front();
+
+        for (int i=0; i< cmdline_args.size(); ++i)
+        {
+            if (cmdline_args[i] == "mode2D")
+            {
+                w.setMode2D(true);
+                break;
+            }
+        }
+    }
+
+    w.show();
+
+    if (cmdline_args.size() > 1)
+        w.addFiles(cmdline_args);
+
+    w.checkForLoadedData();
+
+    return app.exec();
+}
+
