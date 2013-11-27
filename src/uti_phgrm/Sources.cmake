2c1a9e9c87f2f981740c2338a08fb68b629dff07
deseilligny <deseilligny@localhost>
2013-11-27 22:16:09 +0100

    iii

--- src/uti_phgrm/Sources.cmake
+++ src/uti_phgrm/Sources.cmake
@@ -99,6 +99,7 @@ set( Applis_phgrm_Src_Files
     ${UTI_PHGRM_DIR}/CPP_XYZ2Im.cpp
     ${UTI_PHGRM_DIR}/CPP_GrapheHom.cpp
     ${UTI_PHGRM_DIR}/CPP_MMOnePair.cpp
+    ${UTI_PHGRM_DIR}/CPP_VisuCoupeEpip.cpp
 )
 
 SOURCE_GROUP(${SrcGrp_Uti_PHGRM} FILES ${uti_phgrm_Src_Files})
