c2f0d4bcd887658fcaeb44e059119369eb191f76
deseilligny <deseilligny@users.noreply.github.com>
2013-12-06 08:41:33 +0100

    Coher epip; importe point homologues, mais pas utilis?s.

---
+++ src/uti_image/Digeo/Sources.cmake
@@ -0,0 +1,20 @@
+set(uti_image_Digeo_Src_Files
+    ${UTI_IMAGE_DIGEO_DIR}/cParamDigeo.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/cAppliDigeo.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/cConvolSpec.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/cDigeo_Topo.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/cImDigeo.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/cImInMem.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/cOctaveDigeo.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/cVisuCarac.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/Digeo.cpp
+#    ${UTI_IMAGE_DIGEO_DIR}/Digeo_Detecteurs.cpp
+#    ${UTI_IMAGE_DIGEO_DIR}/Digeo_GaussFilter.cpp
+#    ${UTI_IMAGE_DIGEO_DIR}/Digeo_Pyram.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/GenConvolSpec.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/TestDigeoExt.cpp
+    ${UTI_IMAGE_DIGEO_DIR}/DigeoPoint.cpp
+)
+
+
+list( APPEND uti_image_Src_Files ${uti_image_Digeo_Src_Files} )
