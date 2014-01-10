817b93351a1b3eb2279fa66cf179e4ae679fa99f
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2014-01-10 16:14:56 +0100

    Saisie : flag option debug

---
+++ src/saisieQT/Cloud.h
@@ -0,0 +1,46 @@
+﻿#ifndef __CLOUD__
+#define __CLOUD__
+
+#include <vector>
+#include <string>
+
+#include <fstream>
+#include <iostream>
+
+#ifdef Int
+    #undef Int
+#endif
+
+#include <QtOpenGL/QGLBuffer>
+
+#include "3DObject.h"
+
+#define GlVertex cObject
+
+class GlCloud : public cObjectGL
+{
+public:
+    GlCloud(){}
+    GlCloud(vector<GlVertex> const &);
+
+    static GlCloud* loadPly(string,  int *incre = NULL);
+
+    void    addVertex( const GlVertex & );
+    GlVertex& getVertex( uint );
+    int     size();
+
+    void    clear();
+
+    void    setBufferGl(bool onlyColor=false);
+
+    void    draw();
+
+private:
+    vector<GlVertex> _vertices;
+
+    QGLBuffer   _vertexbuffer;
+    QGLBuffer   _vertexColor;
+};
+
+
+#endif
---
+++ src/saisieQT/Engine.h
@@ -0,0 +1,180 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "qiodevice.h"
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QDir>
+#include <QDomDocument>
+#include <QTextStream>
+
+#include "Cloud.h"
+#include "Data.h"
+#include "general/bitm.h"
+
+class ViewportParameters
+{
+public:
+    //! Default constructor
+    ViewportParameters();
+
+    //! Copy constructor
+    ViewportParameters(const ViewportParameters& params);
+
+    //! Destructor
+    ~ViewportParameters();
+
+    //!
+    ViewportParameters & operator = (const ViewportParameters &);
+
+    void    reset();
+
+    void    ptSizeUp(bool up);
+
+    //! Current zoom
+    float m_zoom;
+
+    //! Point size
+    int m_PointSize;
+
+    //! Line width
+    float m_LineWidth;
+
+    //! Rotation and translation speed
+	float m_speed;
+};
+
+struct selectInfos
+{
+    //! polyline infos
+    QVector <QPointF> poly;
+
+    //! selection mode
+    int         selection_mode;
+
+    GLdouble    mvmatrix[16];
+    GLdouble    projmatrix[16];
+    GLint       glViewport[4];
+};
+
+//! Selection mode
+enum SELECTION_MODE { SUB,
+                      ADD,
+                      INVERT,
+                      ALL,
+                      NONE
+                    };
+
+class cLoader
+{
+
+public:
+
+    cLoader();
+
+    CamStenope* loadCamera(QString aNameFile);
+
+    GlCloud*      loadCloud(string i_ply_file , int *incre = NULL);
+
+    void        loadImage(QString aNameFile, QMaskedImage &maskedImg);
+
+    void        setDir(QDir aDir){_Dir = aDir;}
+    QDir        getDir(){return _Dir;}
+
+    void        setFilenamesIn(QStringList const &strl){_FilenamesIn = strl;}
+    void        setFilenamesOut();
+    void        setFilenameOut(QString str);
+    void        setSelectionFilename();
+
+    QStringList getFilenamesOut() {return _FilenamesOut;}
+    QString     getSelectionFilename() {return _SelectionOut;}
+
+    void        setPostFix(QString str);
+
+private:
+    QStringList _FilenamesIn;
+    QStringList _FilenamesOut; //binary masks
+    QString     _SelectionOut; //selection infos
+    QString     _postFix;
+
+    //! Working directory
+    QDir        _Dir;
+};
+
+class cGLData;
+
+class cEngine
+{    
+public:
+
+    cEngine();
+    ~cEngine();
+
+    //! Set working directory
+    void    setDir(QDir aDir){_Loader->setDir(aDir);}
+
+    //! Set working directory
+    void    setFilename(){_Loader->setSelectionFilename();}
+
+    //! Set input filenames
+    void    setFilenamesIn(QStringList const &strl){_Loader->setFilenamesIn(strl);}
+
+    //! Set output filenames
+    void    setFilenamesOut(){_Loader->setFilenamesOut();}
+
+    //! Set output filename
+    void    setFilenameOut(QString filename){_Loader->setFilenameOut(filename);}
+
+    //! Set postfix
+    void    setPostFix(QString filename){_Loader->setPostFix(filename);}
+
+    //! Load point cloud .ply files
+    void    loadClouds(QStringList, int *incre = NULL);
+
+    //! Load cameras .xml files
+    void    loadCameras(QStringList);
+
+    //! Load images  files
+    void    loadImages(QStringList);
+
+    //! Load image (and mask) file
+    void    loadImage(QString imgName);
+
+    void    unloadAll();
+
+    //! Compute mask binary images: projection of visible points into loaded cameras
+    void    do3DMasks();
+
+    //! Creates binary image from selection and saves
+    void    doMaskImage(ushort idCur);
+
+    void    saveMask(ushort idCur);
+
+    void    saveSelectInfos(QVector <selectInfos> const &Infos);
+
+    cData*  getData()  {return _Data;}
+
+    //!looks for data and creates GLobjects
+    void    AllocAndSetGLData();
+
+    //!sends GLObjects to GLWidget
+    cGLData* getGLData(int WidgetIndex);
+
+    void     setGamma(float aGamma) {_Gamma = aGamma;}
+
+    float    getGamma() { return _Gamma;}
+
+private:
+
+    cLoader*            _Loader;
+    cData*              _Data;
+
+    QVector <cGLData*>  _vGLData;
+
+    float               _Gamma;
+};
+
+
+
+
+#endif // ENGINE_H
