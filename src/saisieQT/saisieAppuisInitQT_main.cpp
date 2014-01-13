ed40e9002eae0464962d596e6bb600f7eb36c492
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2014-01-13 14:29:26 +0100

    Saisie : encapsulage draw polygon

---
+++ src/saisieQT/saisieAppuisInitQT_main.cpp
@@ -0,0 +1,42 @@
+#include "saisieQT_main.h"
+
+using namespace std;
+
+int saisieAppuisInitQT_main(QApplication &app, int argc, char *argv[])
+{
+    app.setApplicationName("SaisieAppuisInitQT");
+
+    if ((argc>0)&&(string(argv[0]).find("SaisieQT")!= string::npos))
+    {
+        argv++;
+        argc--;
+    }
+
+    Pt2di aSzW(800,600);
+    Pt2di aNbFen(1,1);
+    string aFullName, aDir, aName, aNamePt, aNameOri, aNameOut, aNameAuto, aPrefix2Add;
+    aNameAuto = "NONE";
+    aPrefix2Add = "";
+    bool aForceGray = false;
+
+    SaisieAppuisInit(argc, argv, aSzW, aNbFen, aFullName, aDir, aName, aNamePt, aNameOri, aNameOut, aNameAuto, aPrefix2Add, aForceGray);
+
+    MainWindow w(aSzW, aNbFen);
+
+    w.show();
+
+    list<string> aNamelist = RegexListFileMatch(aDir, aName, 1, false);
+    QStringList filenames;
+
+    for
+    (
+        list<string>::iterator itS=aNamelist.begin();
+        itS!=aNamelist.end();
+        itS++
+    )
+        filenames.push_back( QString((aDir + *itS).c_str()));
+
+    w.addFiles(filenames);
+
+    return app.exec();
+}
