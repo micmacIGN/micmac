94bdb2d2aa303d02e109ee4aa9e8fcbceb0da124
deseilligny <deseilligny@users.noreply.github.com>
2014-01-15 17:21:20 +0100

    Teste Cam precision

---
+++ src/EtalonnagePolygone/CPP_PointeInitPolyg.cpp
@@ -0,0 +1,248 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+   
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in 
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte 
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+#include "StdAfx.h"
+
+/************************************************/
+/*                                              */
+/*             cWPointeData                     */
+/*                                              */
+/************************************************/
+
+
+
+
+
+
+     //  =========   cParamPointeInitEtalonnage ===================
+
+
+class cParamPointeInitEtalonnage : public cParamPointeInit
+{
+      public :
+         cParamPointeInitEtalonnage(const std::string &,cParamEtal);
+      private :
+         cSetPointes1Im    SetPointe(const std::string &) ;
+	 std::string       NamePointeInit();
+
+	 std::string       NamePointeInterm()
+	 {
+             return mEtalon.NamePointeResult(mNameIm,true,false);
+	 }
+	 std::string       NamePointeFinal()
+	 {
+             return mEtalon.NamePointeResult(mNameIm,false,false);
+	 }
+
+	 bool SauvInterm() 
+	 {
+	     return mEtalon.Param().ParamRechInit().mUseCI == eUCI_Only;
+	 }
+	 bool SauvFinal() 
+	 {
+	     return mEtalon.Param().ParamRechDRad().mUseCI == eUCI_Only;
+	 }
+
+
+
+
+	 std::string       NameImageCamera();
+	 std::string       NameImagePolygone();
+	 std::string       NamePointePolygone();
+	 std::string       NamePolygone() const {return mParamEtal.NameCible3DPolygone();}
+         const cPolygoneEtal &   Polygone() const ;
+         eTyEtat EtatDepAff() const {return eNonSelec;}
+         bool PseudoPolyg() const { return false;}
+	 void SauvRot(ElRotation3D) const {}
+	 void    ConvPointeImagePolygone(Pt2dr&);
+
+	 const cPolygoneEtal::tContCible & CiblesInit() const
+	 {
+		 return Polygone().ListeCible();
+	 }
+
+	 NS_ParamChantierPhotogram::cPolygoneCalib * PC() const 
+	 {
+	     return Polygone().PC();
+	 }
+
+
+	 std::string        mNameIm;
+	 cParamEtal         mParamEtal;
+         cEtalonnage        mEtalon;
+	 Tiff_Im            mFileImPolygone;
+	 Pt2di              mSzImPolyg;
+};
+
+static std::vector<double>  NoParAdd;
+
+cParamPointeInitEtalonnage::cParamPointeInitEtalonnage
+(
+       const std::string & aNameIm,
+       cParamEtal aParam
+) :
+    cParamPointeInit  (
+			 new CamStenopeIdeale(true,aParam.FocaleInit(),aParam.SzIm()/2.0,NoParAdd)
+                      ),
+    mNameIm           (aNameIm),
+    mParamEtal        (aParam),
+    mEtalon           (false,aParam),
+    mFileImPolygone   (Tiff_Im::BasicConvStd(mParamEtal.NameImPolygone())),
+    mSzImPolyg        (mFileImPolygone.sz())
+{
+}
+
+
+cSetPointes1Im cParamPointeInitEtalonnage:: SetPointe(const std::string & aName)
+{
+   return cSetPointes1Im(mEtalon.Polygone(),aName);
+}
+
+std::string cParamPointeInitEtalonnage::NamePointeInit()
+{
+	return mEtalon.NamePointeInit(mNameIm);
+}
+
+std::string cParamPointeInitEtalonnage::NameImageCamera()
+{
+     return  mParamEtal.NameTiff(mNameIm);
+}
+
+
+std::string  cParamPointeInitEtalonnage::NameImagePolygone()
+{
+    return mParamEtal.NameImPolygone();
+}
+
+std::string  cParamPointeInitEtalonnage::NamePointePolygone()
+{
+     return mParamEtal.NamePointePolygone();
+}
+
+const cPolygoneEtal &   cParamPointeInitEtalonnage::Polygone() const 
+{
+   return mEtalon.Polygone();
+}
+
+void    cParamPointeInitEtalonnage::ConvPointeImagePolygone(Pt2dr& aP)
+{
+    if (mParamEtal.EgelsConvImage())
+       aP.y = mSzImPolyg.y-aP.y;
+}
+
+/*
+
+
+std::list<cPt2Im> cParamPointeInitEtalonnage::InitListePt2Is()
+{
+    std::list<cPt2Im> aRes;
+    cSetPointes1Im PPol(mEtalon.Polygone(),mEtalon.Param().NamePointePolygone());
+
+    for 
+    (
+       cSetPointes1Im::tCont::iterator itP = PPol.Pointes().begin();
+       itP != PPol.Pointes().end();
+       itP++
+    )
+    {
+	    aRes.push_back(cPt2Im(&itP->Cible(),itP->PosIm()));
+    }
+
+    aRes.push_back(cPt2Im(0,Pt2dr(0,0)));
+
+    return aRes;
+}
+*/
+
+
+int PointeInitPolyg_main(int argc,char ** argv)
+{
+   MMD_InitArgcArgv(argc,argv);
+
+   ELISE_ASSERT(argc>=3,"No Enough Arg");
+   std::string aNameIm = argv[2];
+
+    
+   argv[2] = argv[1];
+   cParamEtal aParam(argc-1,argv+1);
+
+   // cParamEtal aParam(2,argv);
+
+   cParamPointeInit * aPPI  = new cParamPointeInitEtalonnage(aNameIm,aParam);
+
+
+   PointesInitial(*aPPI,Pt2di(600,600),Pt2di(500,500));
+
+   // cEtalonnage::TestGrid(argc,argv,"GridTriangul");
+
+   return 0;
+}
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
---
+++ src/util/regex.cpp
@@ -0,0 +1,546 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+   
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in 
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte 
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+#include "StdAfx.h"
+
+
+
+
+std::string RegExAdapt(const std::string & aNameExprIn)
+{
+    const char * aC = aNameExprIn.c_str();
+    int aNb = strlen(aC);
+
+    return
+                 ( ((aNb==0) || (aC[0]!='^'))  ? "^" : "")
+             +   aNameExprIn
+             +   ( ((aNb==0) || (aC[aNb-1]!='$'))  ? "$" : "") ;
+}
+
+cElRegex::cElRegex(const std::string & aNameExprIn,int aNbMatchMax,int aCFlag) :
+     mNameExpr (RegExAdapt(aNameExprIn))
+{
+
+   mResMatch = -1;
+   mOkReplace = false;
+
+   mResCreate = regcomp(&mAutom,mNameExpr.c_str(),aCFlag);
+   if (! IsOk())
+      return;
+   regmatch_t aMatch;
+   mVMatch.reserve(aNbMatchMax);
+   for (int aK=0; aK<aNbMatchMax ; aK++)
+       mVMatch.push_back(aMatch);
+}
+
+cElRegex::~cElRegex()
+{
+   regfree(&mAutom);
+}
+
+bool cElRegex::IsOk() const
+{
+   return mResCreate==0;
+}
+
+void cElRegex::AssertOk() const
+{
+   if (!IsOk())
+      std::cout << "EXPR=" << mNameExpr << "\n";
+   assert(IsOk());
+}
+
+bool cElRegex::IsMatched() const
+{
+   return mResMatch==0;
+}
+
+void cElRegex::AssertIsMatched() const
+{
+   assert(IsMatched());
+}
+
+bool cElRegex::IsReplaced() const
+{
+    return mOkReplace;
+}
+void cElRegex::AssertIsReplaced() const
+{
+   assert(IsReplaced());
+}
+
+bool cElRegex::Match(const std::string & aName,int aCFlag)  
+{
+   AssertOk();
+   mResMatch = regexec(&mAutom,aName.c_str(),mVMatch.size(),&mVMatch[0],aCFlag);
+   if (IsMatched())
+      mLastNameMatched = aName;
+   return IsMatched();
+}
+
+const std::vector<regmatch_t>  & cElRegex::VMatch() const
+{
+   AssertIsMatched();
+   return mVMatch;
+}
+
+//int Str2I(const std::string & aStr)
+//{
+//}
+
+template <class Type> bool FromString(Type& x,const std::string & s);
+template <class Type> std::string ToString(const Type &);
+
+
+std::string cElRegex::KIemeExprPar(int aNum)
+{
+    if (! ((aNum >=0) && (aNum<int(mVMatch.size()))))
+    {
+       std::cout << "NUM="<< aNum << " NB=" << mVMatch.size() << "\n";
+       ELISE_ASSERT
+       (
+          (aNum >=0) && (aNum<int(mVMatch.size())),
+          "Bad Num in cElRegex::KIemeExprPar"
+       );
+    }
+    int aI0 = mVMatch[aNum].rm_so;
+    int aI1 = mVMatch[aNum].rm_eo;
+
+              
+    ELISE_ASSERT
+    (
+        (aI0>=0)&&(aI1>=0),
+	"Internal error KIemeExprPar"
+    );
+    return   mLastNameMatched.substr(aI0,aI1-aI0);
+}
+
+static const char * Month[12] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+
+double  cElRegex::VNumKIemeExprPar(int aNum,bool aVerif,double * aDefError)
+{
+  
+   double aRes;
+
+   bool Inv= false;
+   std::string aStr = KIemeExprPar(aNum);
+
+
+   if ((aStr.size() > 2) && ((aStr[0]=='1')||(aStr[0]=='f')) && (aStr[1]=='/') )
+   {
+       aStr = aStr.substr(2,aStr.size());
+       Inv=true;
+   }
+
+
+   bool aOK=  FromString(aRes,aStr);
+   if ((! aOK) && (aVerif))
+   {
+      for (int aK=0 ; aK<12; aK++)
+          if (aStr==Month[aK])
+             return aK+1;
+
+       if (aStr=="inf")
+       {
+          if (Inv) return 0;
+          return InfRegex;
+       }
+      {
+         if (aDefError) 
+            return *aDefError;
+         ELISE_ASSERT
+         (
+             false,
+             "Not Num in cElRegex::VNumKIemeExprPar"
+         );
+      }
+   }
+   return  Inv ? (1/aRes)  :aRes;
+}
+
+std::string  ToStringNBD(int aNb,int aNbDig)
+{
+   std::string aRes = ToString(aNb);
+   int aSz = aRes.size();
+   ELISE_ASSERT(aSz<=aNbDig,"Pas assez de digit dans ToStringNBD");
+   for (;aSz<aNbDig ; aSz++)
+   {
+       aRes = "0" + aRes;
+   }
+   return aRes;
+}
+
+const std::string & cElRegex::Error() const
+{
+   return mError;
+}
+
+bool  cElRegex::Replace(const std::string & aMotif)  
+{
+   mError = "";
+   AssertIsMatched();
+   mOkReplace = false;
+   mLastReplace = "";
+   for (const char * aC=aMotif.c_str(); *aC ; aC++)
+   {
+       if (*aC=='$')
+       {
+          aC++;
+          if (*aC=='$')
+          {
+             mLastReplace += *aC;
+          }
+          else
+          {
+             int aNum=-1;
+
+             int aOffset=0;
+	     bool isArithm = false;
+	     int  aNbDigit = -1;
+
+             char aCAritm = '0';
+
+	     //  $+234+1 -> ajoute 234 a $1
+	     //  $-456-A -> enleve 456 a $A
+	     //  $%5+234+6 -> ajoute 234 a $6 et ecrit le resultat sur 5 chiffres
+	     if ((*aC=='+') || (*aC=='-') || (*aC=='/') || (*aC=='%'))
+	     {
+                  aCAritm = *aC;
+	          isArithm = true;
+		  if (*aC=='%')
+		  {
+		      aC++;
+		      if (isdigit(*aC))
+                      {
+		         aNbDigit = *aC-'0';
+                         aC++;
+			 if ((*aC!='+') && (*aC!='-') && (*aC!='/'))
+			 {
+			      mError = "No + or - after %x";
+                             return false;
+                         }
+                      }
+		      else
+                      {
+                         mError = "No Digit after %";
+                         return false;
+                      }
+		  }
+
+                  const char *  aC0  = aC;
+		  aC++;
+		  std::string aS;
+		  while (*aC != *aC0)
+		  {
+		      if (isdigit(*aC))
+		          aS = aS + *aC;
+		      else
+                      {
+                         mError = "No Digit before closing +or-";
+                         return false;
+                      }
+                      aC++;
+		  }
+		  aC++;
+		  if (! FromString(aOffset,aS))
+		  {
+                         mError = "Internal error, should be a digit";
+                         return false;
+		  }
+	     }
+
+
+             if (isdigit(*aC))
+             {
+                aNum = *aC-'0';
+             }
+             else if (isalpha(*aC))
+             {
+                aNum = 10 +toupper(*aC)-'A';
+             }
+             if ((aNum<0) || (aNum>=int(mVMatch.size())))
+             {
+	         mError = "Nor digit nor alpha after $";
+                 return false;
+             }
+             int aI0 = mVMatch[aNum].rm_so;
+             int aI1 = mVMatch[aNum].rm_eo;
+
+              
+             if ((aI0<0) || (aI1<0))
+                 return false;
+             std::string aToInsert =  mLastNameMatched.substr(aI0,aI1-aI0);
+	     if (isArithm)
+	     {
+	         int aVal;
+		 FromString(aVal,aToInsert);
+                 if (aCAritm=='+')
+		    aVal += aOffset;
+                 if (aCAritm=='-')
+		    aVal -= aOffset;
+                 if (aCAritm=='/')
+		    aVal /= aOffset;
+		 if (aNbDigit==-1)
+		 {
+		     aNbDigit = aToInsert.size();
+		 }
+		 aToInsert = ToString(aVal);
+		 if (int(aToInsert.size()) >aNbDigit)
+		 {
+		    mError ="No enough digit for computed number";
+		    return false;
+                 }
+		 while (int(aToInsert.size()) <aNbDigit)
+		     aToInsert = '0' + aToInsert;
+	     }
+             mLastReplace += aToInsert;
+          }
+       }
+       else
+       {
+          mLastReplace += *aC;
+       }
+   }
+   mOkReplace = true;
+   return true;
+}
+
+const std::string & cElRegex::LastReplaced() const
+{
+   AssertIsReplaced();
+   return mLastReplace;
+}
+
+const std::string & cElRegex::NameExpr() const
+{
+   return mNameExpr;
+}
+
+
+std::list<cElRegex *> CompilePats(const  std::list<std::string> & aLS)
+{
+    std::list<cElRegex *> aRes;
+
+    for
+    (
+        std::list<std::string>::const_iterator itS=aLS.begin();
+        itS!=aLS.end();
+        itS++
+    )
+    {
+           aRes.push_back(new cElRegex(*itS,10));
+    }
+    return aRes;
+}
+
+bool AuMoinsUnMatch(const std::list<cElRegex *> & aLR,const  std::string & aName)
+{
+   for
+   (
+           std::list<cElRegex *>::const_iterator itR=aLR.begin();
+           (itR!=aLR.end()) ;
+           itR++
+   )
+   {
+         if ((*itR)->Match(aName))
+	    return true;
+   }
+   return false;
+}
+
+
+std::string  GetStringFromLineExprReg
+             (
+                  cElRegex_Ptr & aReg,
+                  const std::string & aNameFile,
+                  const std::string & aNameExpr,
+                  const std::string & aMotif,
+                  int * aPtrNbMatch
+             )
+{
+  std::string aRes;
+  string aBuf; //char aBuf[1000]; TEST_OVERFLOW
+  if (aReg == 0)
+     aReg = new cElRegex(aNameExpr,15);
+
+
+  bool endof=false;
+  ELISE_fp aFile(aNameFile.c_str(),ELISE_fp::READ);
+  int aNbMatch = 0;
+
+  while (!endof)
+  {
+      if (aFile.fgets(aBuf,endof)) //if (aFile.fgets(aBuf,1000,endof)) TEST_OVERFLOW
+      {
+          // printf("UgRl[%s,%s]\n",aBuf,aNameExpr.c_str());
+          if (aReg->Match(aBuf))
+	  {
+               aNbMatch++;
+               aRes = MatchAndReplace(*aReg,aBuf,aMotif);
+	  }
+      }
+  }
+
+  aFile.close();
+
+  if (aPtrNbMatch)
+  {
+     *aPtrNbMatch= aNbMatch;
+  }
+  else
+  {
+     if (aNbMatch!=1)
+     {
+         std::cout << "NB MATCH = " << aNbMatch << "\n";
+         std::cout << "  EXPR  = " << aNameExpr  << "\n";
+         ELISE_ASSERT(aNbMatch==1,"NbMatch != 1 in GetStringFromLineExprReg");
+     }
+  }
+
+  return aRes;
+}
+
+
+
+std::vector<double> GetValsNumFromLineExprReg
+                    (
+                       cElRegex_Ptr & aReg,
+                       const std::string & aNameFile,
+                       const std::string & aNameExpr,
+                       const std::string & aVInd,
+                       int * aPtrNbMatch
+                    )
+{
+  std::vector<double> aRes;
+  string aBuf; //char aBuf[1000];
+  if (aReg == 0)
+     aReg = new cElRegex(aNameExpr,15);
+
+
+  bool endof=false;
+  ELISE_fp aFile(aNameFile.c_str(),ELISE_fp::READ);
+  int aNbMatch = 0;
+
+  while (!endof)
+  {
+      if (aFile.fgets(aBuf,endof)) //if (aFile.fgets(aBuf,1000,endof))
+      {
+          if (aReg->Match(aBuf))
+	  {
+             aNbMatch++;
+	     for (const char * aC=aVInd.c_str(); *aC ; aC++)
+	     {
+                if (aPtrNbMatch)
+                {
+                    double aDef = -81234.89e15;
+                    double OneRes = aReg->VNumKIemeExprPar(*aC-'0',true,&aDef);
+
+                    if (OneRes==aDef)
+                    {
+                         *aPtrNbMatch = 0;
+                          aRes.clear();
+                          aFile.close();
+                          return aRes;
+                    }
+                    else
+	                aRes.push_back(OneRes);
+                }
+                else
+                {
+	            aRes.push_back(aReg->VNumKIemeExprPar(*aC-'0'));
+                }
+             }
+	  }
+      }
+  }
+
+  aFile.close();
+
+  if (aPtrNbMatch)
+  {
+     *aPtrNbMatch = aNbMatch;
+  }
+  else
+  {
+      if (aNbMatch!=1)
+      {
+          std::cout << "NB MATCH = " << aNbMatch << "\n";
+          std::cout << "  EXPR  = " << aNameExpr  << "\n";
+          ELISE_ASSERT(aNbMatch==1,"NbMatch != 1 in GetValsNumFromLineExprReg");
+      }
+  }
+
+  return aRes;
+}
+
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
