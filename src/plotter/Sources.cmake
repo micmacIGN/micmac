a423d030184529b56045031797ed22c6154cfda1
jbelvaux <jbelvaux@localhost>
2014-01-20 14:08:21 +0100

    - QT_VERSION devient ELISE_QT_VERSION pour ?viter les d?claration multiples         QT_VERSION est accessible lorsque les headers Qt sont inclus         ELISE_QT_VERSION est accessible partout dans elise

---
+++ src/plotter/Sources.cmake
@@ -0,0 +1,11 @@
+set(Plotter_Src_Files
+	${PLOTTER_DIR}/draw_plot1d.cpp
+	${PLOTTER_DIR}/gen_plot1.cpp
+)
+
+SOURCE_GROUP(Plotter FILES ${Plotter_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${Plotter_Src_Files}
+)
---
+++ src/saisieQT/GLWidgetSet.h
@@ -0,0 +1,30 @@
+#ifndef GLWIDGETGRID_H
+#define GLWIDGETGRID_H
+
+#include "GLWidget.h"
+#include <QVector>
+
+class GLWidget;
+
+class GLWidgetSet
+{
+public:
+    GLWidgetSet(uint aNb, QColor color1, QColor color2, bool modePt);
+    ~GLWidgetSet();
+
+    void setCurrentWidgetIdx(uint aK);
+    uint CurrentWidgetIdx(){return _currentWidget;}
+
+    GLWidget* getWidget(uint aK){return _Widgets[aK];}
+
+    GLWidget* CurrentWidget(){return _Widgets[_currentWidget];}
+
+    uint NbWidgets() const {return (uint) _Widgets.size();}
+
+private:
+
+    QVector <GLWidget*>  _Widgets;
+    uint                 _currentWidget;
+};
+
+#endif // GLWIDGETGRID_H
