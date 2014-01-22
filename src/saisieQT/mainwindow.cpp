44145f3c2e48cb43d7277a86014af228c3f67bd4
G?rald Choqueux <G?rald Choqueux@localhost>
2014-01-22 17:35:09 +0100

    GpGpu : Debug MacOS key event

--- src/saisieQT/mainwindow.cpp
+++ src/saisieQT/mainwindow.cpp
@@ -19,7 +19,16 @@ MainWindow::MainWindow(Pt2di aSzW, Pt2di aNbFen, int mode, QString pointName, QW
     style = style.arg(colorBG0.red()).arg(colorBG0.green()).arg(colorBG0.blue());
     style = style.arg(colorBG1.red()).arg(colorBG1.green()).arg(colorBG1.blue());
 
+#ifdef ELISE_Darwin
     _ui->OpenglLayout->setStyleSheet(style);
+
+    _ui->actionAdd->setShortcut(QKeySequence(Qt::Key_Space));
+    _ui->actionRemove->setShortcut(QKeySequence(Qt::Key_S));
+#endif
+
+    _ui->OpenglLayout->setStyleSheet(style);
+
+    _ui->actionAdd->setShortcut(QKeySequence(Qt::Key_Space));
 
     _ProgressDialog = new QProgressDialog("Loading files","Stop",0,100,this);
 
