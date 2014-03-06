df25f49f87f4b6e25dccc291b7a19beb4f608747
deseilligny <deseilligny@users.noreply.github.com>
2014-03-06 22:29:34 +0100

    V0 : combinaison de lumi?re rasante.

---
+++ BenchElise/bench/b_0_58.cpp
@@ -0,0 +1,107 @@
+/*eLiSe06/05/99
+  
+     Copyright (C) 1999 Marc PIERROT DESEILLIGNY
+
+   eLiSe : Elements of a Linux Image Software Environment
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+  Author: Marc PIERROT DESEILLIGNY    IGN/MATIS  
+Internet: Marc.Pierrot-Deseilligny@ign.fr
+   Phone: (33) 01 43 98 81 28
+eLiSe06/05/99*/
+
+#ifndef __TRACE_SYSTEM__
+   #define __TRACE_SYSTEM__
+#endif
+
+#include "StdAfx.h"
+#include "general/cElCommand.h"
+#include "bench.h"
+
+void bench_error( bool i_testHasFailed, const string &i_msg )
+{
+   if ( i_testHasFailed )
+   {
+      cerr << i_msg << endl;
+      exit( EXIT_FAILURE );
+   }
+}
+
+ctPath strToPath( const string i_str )
+{
+   ctPath p( i_str );
+   cout << i_str << " -> [" << p.str() << "] " << endl;
+   //p.trace();
+   return p;
+}
+
+void bench_path()
+{
+   // test la classe ctPath
+
+   cout << "current directory = ";
+   getWorkingDirectory().trace();
+
+   cout << "--- test path normalized form" << endl;
+   ctPath p0 = strToPath( "titi/to to/" ),
+	  p1 = strToPath( "titi\\to to" ),
+	  p2 = strToPath( "titi/./toto/../to to/" );
+   bench_error( p0!=p1, "p0!=p1" );
+   bench_error( p1!=p2, "p1!=p2" );
+   cout << "ok\n" << endl;
+
+   cout << "--- test cElFilename creation/deletion" << endl;
+   cElFilename filename( "balo_file" );
+   bench_error( filename.exists(), filename.str_unix()+" already exists" );
+   bench_error( !filename.create(), string("cannot create file [")+filename.str_unix()+"]" );
+   bench_error( !filename.remove(), string("cannot remove file [")+filename.str_unix()+"]" );
+   cout << "ok\n" << endl;
+   
+   cout << "--- test ctPath creation/deletion" << endl;
+   ctPath sampleDir( "balo_path" );
+   bench_error( !sampleDir.create(), string("cannot create directory [")+sampleDir.str()+"]" );
+   bench_error( !sampleDir.remove_empty(), string("cannot remove directory [")+sampleDir.str()+"]" );
+   cout << "ok\n" << endl;
+   
+	cout << "--- creating/deleting a basic tree" << endl;
+	ctPath treeName("toto");
+	bench_error( treeName.exists(), string("tree [")+treeName.str()+"] already exists" );
+	bench_error( !ctPath("toto").create() ||
+	             !ctPath("toto/a").create() ||
+	             !ctPath("toto/b").create() ||
+	             !ctPath("toto/c").create() ||
+	             !cElFilename("toto/a/aa").create() ||
+	             !cElFilename("toto/a/aabbb").create() ||
+	             !cElFilename("toto/a/aabbbbxxx").create() ||
+	             !cElFilename("toto/c/cc").create() ||
+	             !cElFilename("toto/c/ccddd").create() ||
+	             !cElFilename("toto/c/balo").create(),
+		"unable to create a basic tree" );
+	bench_error ( !ctPath("toto").isAncestorOf( ctPath("toto/a/") ), "directory \"toto\" is not an ancestor of \"toto/a\", which is odd" );
+	bench_error ( !ctPath("toto").isAncestorOf( cElFilename("toto/c/balo") ), "directory \"toto\" is not an ancestor of \"toto/c/balo\", which is odd" );
+	bench_error ( ctPath("toto").isAncestorOf( cElFilename("../toto") ), "directory \"toto\" is an ancestor of \"../titi\", which is odd" );
+	bench_error( !ctPath("toto").remove(), "failed to delete a basic tree" );
+	cout << "ok" << endl;
+}
+
+void bench_command()
+{
+   // test la classe cElCommand et ses classes connexes
+   bench_path();
+   //string command = "\"d:/dev/culture3d/bin/mm3d\" Apero \"d:/dev/culture3d/include/XML_MicMac/Apero-Glob.xml\"  DirectoryChantier=./  \"+PatternAllIm=.*.JPG\"  +AeroOut=-Arbitrary +Ext=dat +ModeleCam=eCalibAutomPhgrStdBasic +FileLibereParam=Param-Fraser.xml DoCompensation=1 +SeuilFE=-1.000000 +TetaLVM=0.010000 +CentreLVM=0.100000 +RayFEInit=0.850000 +CalibIn=-#@LL?~~XXXXXXXXXX +AeroIn=-#@LL?~~XXXXXXXXXX +VitesseInit=4 +PropDiagU=1.000000 +ValDec=eLiberte_Phgr_Std_Dec +ValDecPP=eLiberte_Dec1 +ValAffPP=eLiberteParamDeg_1 +ValAff=eLiberte_Phgr_Std_Aff";
+   //cout << "[" << command << "]" << endl;
+   //::System( command );
+}
---
+++ src/uti_phgrm/SaisiePts/cParamSaisiePts.h
@@ -0,0 +1,496 @@
+#ifndef Define_NotSaisiPts
+#define Define_NotSaisiPts
+// #include "general/all.h"
+// #include "private/all.h"
+using namespace NS_ParamChantierPhotogram;
+using namespace NS_SuperposeImage;
+namespace NS_SaisiePts{
+typedef enum
+{
+  eNSM_GeoCube,
+  eNSM_Plaquette,
+  eNSM_Pts,
+  eNSM_MaxLoc,
+  eNSM_MinLoc,
+  eNSM_NonValue
+} eTypePts;
+void xml_init(eTypePts & aVal,cElXMLTree * aTree);
+std::string  eToString(const eTypePts & aVal);
+
+eTypePts  Str2eTypePts(const std::string & aName);
+
+cElXMLTree * ToXMLTree(const std::string & aNameTag,const eTypePts & anObj);
+
+typedef enum
+{
+  eEPI_NonSaisi,
+  eEPI_Refute,
+  eEPI_Douteux,
+  eEPI_Valide,
+  eEPI_NonValue,
+  eEPI_Disparu,
+  eEPI_Highlight, //only for QT interface
+  eEPI_Deleted    //only for QT interface
+} eEtatPointeImage;
+void xml_init(eEtatPointeImage & aVal,cElXMLTree * aTree);
+std::string  eToString(const eEtatPointeImage & aVal);
+
+eEtatPointeImage  Str2eEtatPointeImage(const std::string & aName);
+
+cElXMLTree * ToXMLTree(const std::string & aNameTag,const eEtatPointeImage & anObj);
+
+class cContenuPt
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cContenuPt & anObj,cElXMLTree * aTree);
+
+
+        cTplValGesInit< std::string > & None();
+        const cTplValGesInit< std::string > & None()const ;
+    private:
+        cTplValGesInit< std::string > mNone;
+};
+cElXMLTree * ToXMLTree(const cContenuPt &);
+
+class cPointGlob
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cPointGlob & anObj,cElXMLTree * aTree);
+
+
+        eTypePts & Type();
+        const eTypePts & Type()const ;
+
+        std::string & Name();
+        const std::string & Name()const ;
+
+        cTplValGesInit< Pt3dr > & P3D();
+        const cTplValGesInit< Pt3dr > & P3D()const ;
+
+        cTplValGesInit< bool > & Mes3DExportable();
+        const cTplValGesInit< bool > & Mes3DExportable()const ;
+
+        cTplValGesInit< Pt3dr > & Incert();
+        const cTplValGesInit< Pt3dr > & Incert()const ;
+
+        cTplValGesInit< double > & LargeurFlou();
+        const cTplValGesInit< double > & LargeurFlou()const ;
+
+        cTplValGesInit< std::string > & None();
+        const cTplValGesInit< std::string > & None()const ;
+
+        cTplValGesInit< cContenuPt > & ContenuPt();
+        const cTplValGesInit< cContenuPt > & ContenuPt()const ;
+
+        cTplValGesInit< int > & NumAuto();
+        const cTplValGesInit< int > & NumAuto()const ;
+
+        cTplValGesInit< Pt3dr > & PS1();
+        const cTplValGesInit< Pt3dr > & PS1()const ;
+
+        cTplValGesInit< Pt3dr > & PS2();
+        const cTplValGesInit< Pt3dr > & PS2()const ;
+
+        cTplValGesInit< double > & SzRech();
+        const cTplValGesInit< double > & SzRech()const ;
+
+        cTplValGesInit< bool > & Disparu();
+        const cTplValGesInit< bool > & Disparu()const ;
+
+        cTplValGesInit< bool > & FromDico();
+        const cTplValGesInit< bool > & FromDico()const ;
+    private:
+        eTypePts mType;
+        std::string mName;
+        cTplValGesInit< Pt3dr > mP3D;
+        cTplValGesInit< bool > mMes3DExportable;
+        cTplValGesInit< Pt3dr > mIncert;
+        cTplValGesInit< double > mLargeurFlou;
+        cTplValGesInit< cContenuPt > mContenuPt;
+        cTplValGesInit< int > mNumAuto;
+        cTplValGesInit< Pt3dr > mPS1;
+        cTplValGesInit< Pt3dr > mPS2;
+        cTplValGesInit< double > mSzRech;
+        cTplValGesInit< bool > mDisparu;
+        cTplValGesInit< bool > mFromDico;
+};
+cElXMLTree * ToXMLTree(const cPointGlob &);
+
+/******************************************************/
+/******************************************************/
+/******************************************************/
+class cSetPointGlob
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cSetPointGlob & anObj,cElXMLTree * aTree);
+
+
+        std::list< cPointGlob > & PointGlob();
+        const std::list< cPointGlob > & PointGlob()const ;
+    private:
+        std::list< cPointGlob > mPointGlob;
+};
+cElXMLTree * ToXMLTree(const cSetPointGlob &);
+
+/******************************************************/
+/******************************************************/
+/******************************************************/
+class cOneSaisie
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cOneSaisie & anObj,cElXMLTree * aTree);
+
+
+        eEtatPointeImage & Etat();
+        const eEtatPointeImage & Etat()const ;
+
+        std::string & NamePt();
+        const std::string & NamePt()const ;
+
+        Pt2dr & PtIm();
+        const Pt2dr & PtIm()const ;
+    private:
+        eEtatPointeImage mEtat;
+        std::string mNamePt;
+        Pt2dr mPtIm;
+};
+cElXMLTree * ToXMLTree(const cOneSaisie &);
+
+class cSaisiePointeIm
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cSaisiePointeIm & anObj,cElXMLTree * aTree);
+
+
+        std::string & NameIm();
+        const std::string & NameIm()const ;
+
+        std::list< cOneSaisie > & OneSaisie();
+        const std::list< cOneSaisie > & OneSaisie()const ;
+    private:
+        std::string mNameIm;
+        std::list< cOneSaisie > mOneSaisie;
+};
+cElXMLTree * ToXMLTree(const cSaisiePointeIm &);
+
+/******************************************************/
+/******************************************************/
+/******************************************************/
+class cSetOfSaisiePointeIm
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cSetOfSaisiePointeIm & anObj,cElXMLTree * aTree);
+
+
+        std::list< cSaisiePointeIm > & SaisiePointeIm();
+        const std::list< cSaisiePointeIm > & SaisiePointeIm()const ;
+    private:
+        std::list< cSaisiePointeIm > mSaisiePointeIm;
+};
+cElXMLTree * ToXMLTree(const cSetOfSaisiePointeIm &);
+
+/******************************************************/
+/******************************************************/
+/******************************************************/
+class cSectionWindows
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cSectionWindows & anObj,cElXMLTree * aTree);
+
+
+        cTplValGesInit< Pt2di > & SzTotIm();
+        const cTplValGesInit< Pt2di > & SzTotIm()const ;
+
+        cTplValGesInit< Pt2di > & NbFenIm();
+        const cTplValGesInit< Pt2di > & NbFenIm()const ;
+
+        cTplValGesInit< Pt2di > & SzWZ();
+        const cTplValGesInit< Pt2di > & SzWZ()const ;
+
+        cTplValGesInit< bool > & ShowDet();
+        const cTplValGesInit< bool > & ShowDet()const ;
+
+        cTplValGesInit< bool > & RefInvis();
+        const cTplValGesInit< bool > & RefInvis()const ;
+    private:
+        cTplValGesInit< Pt2di > mSzTotIm;
+        cTplValGesInit< Pt2di > mNbFenIm;
+        cTplValGesInit< Pt2di > mSzWZ;
+        cTplValGesInit< bool > mShowDet;
+        cTplValGesInit< bool > mRefInvis;
+};
+cElXMLTree * ToXMLTree(const cSectionWindows &);
+
+/******************************************************/
+/******************************************************/
+/******************************************************/
+class cImportFromDico
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cImportFromDico & anObj,cElXMLTree * aTree);
+
+
+        eTypePts & TypePt();
+        const eTypePts & TypePt()const ;
+
+        std::string & File();
+        const std::string & File()const ;
+
+        cTplValGesInit< double > & LargeurFlou();
+        const cTplValGesInit< double > & LargeurFlou()const ;
+    private:
+        eTypePts mTypePt;
+        std::string mFile;
+        cTplValGesInit< double > mLargeurFlou;
+};
+cElXMLTree * ToXMLTree(const cImportFromDico &);
+
+class cSectionInOut
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cSectionInOut & anObj,cElXMLTree * aTree);
+
+
+        cTplValGesInit< std::string > & Prefix2Add2IdPt();
+        const cTplValGesInit< std::string > & Prefix2Add2IdPt()const ;
+
+        std::list< cImportFromDico > & ImportFromDico();
+        const std::list< cImportFromDico > & ImportFromDico()const ;
+
+        cTplValGesInit< bool > & FlouGlobEcras();
+        const cTplValGesInit< bool > & FlouGlobEcras()const ;
+
+        cTplValGesInit< bool > & TypeGlobEcras();
+        const cTplValGesInit< bool > & TypeGlobEcras()const ;
+
+        cTplValGesInit< std::string > & NamePointesImage();
+        const cTplValGesInit< std::string > & NamePointesImage()const ;
+
+        cTplValGesInit< std::string > & NamePointsGlobal();
+        const cTplValGesInit< std::string > & NamePointsGlobal()const ;
+
+        cTplValGesInit< std::string > & ExportPointeImage();
+        const cTplValGesInit< std::string > & ExportPointeImage()const ;
+
+        std::list< std::string > & FixedName();
+        const std::list< std::string > & FixedName()const ;
+
+        cTplValGesInit< std::string > & NameAuto();
+        const cTplValGesInit< std::string > & NameAuto()const ;
+
+        cTplValGesInit< bool > & EnterName();
+        const cTplValGesInit< bool > & EnterName()const ;
+    private:
+        cTplValGesInit< std::string > mPrefix2Add2IdPt;
+        std::list< cImportFromDico > mImportFromDico;
+        cTplValGesInit< bool > mFlouGlobEcras;
+        cTplValGesInit< bool > mTypeGlobEcras;
+        cTplValGesInit< std::string > mNamePointesImage;
+        cTplValGesInit< std::string > mNamePointsGlobal;
+        cTplValGesInit< std::string > mExportPointeImage;
+        std::list< std::string > mFixedName;
+        cTplValGesInit< std::string > mNameAuto;
+        cTplValGesInit< bool > mEnterName;
+};
+cElXMLTree * ToXMLTree(const cSectionInOut &);
+
+/******************************************************/
+/******************************************************/
+/******************************************************/
+class cSectionImages
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cSectionImages & anObj,cElXMLTree * aTree);
+
+
+        std::string & SetOfImages();
+        const std::string & SetOfImages()const ;
+
+        cTplValGesInit< bool > & ForceGray();
+        const cTplValGesInit< bool > & ForceGray()const ;
+
+        cTplValGesInit< std::string > & KeyAssocOri();
+        const cTplValGesInit< std::string > & KeyAssocOri()const ;
+    private:
+        std::string mSetOfImages;
+        cTplValGesInit< bool > mForceGray;
+        cTplValGesInit< std::string > mKeyAssocOri;
+};
+cElXMLTree * ToXMLTree(const cSectionImages &);
+
+/******************************************************/
+/******************************************************/
+/******************************************************/
+class cProfEstimator
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cProfEstimator & anObj,cElXMLTree * aTree);
+
+
+        cTplValGesInit< double > & ZMoyen();
+        const cTplValGesInit< double > & ZMoyen()const ;
+
+        cTplValGesInit< std::string > & ZMoyenInIma();
+        const cTplValGesInit< std::string > & ZMoyenInIma()const ;
+    private:
+        cTplValGesInit< double > mZMoyen;
+        cTplValGesInit< std::string > mZMoyenInIma;
+};
+cElXMLTree * ToXMLTree(const cProfEstimator &);
+
+class cSectionTerrain
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cSectionTerrain & anObj,cElXMLTree * aTree);
+
+
+        cTplValGesInit< double > & IntervPercProf();
+        const cTplValGesInit< double > & IntervPercProf()const ;
+
+        cTplValGesInit< double > & ZMoyen();
+        const cTplValGesInit< double > & ZMoyen()const ;
+
+        cTplValGesInit< std::string > & ZMoyenInIma();
+        const cTplValGesInit< std::string > & ZMoyenInIma()const ;
+
+        cTplValGesInit< cProfEstimator > & ProfEstimator();
+        const cTplValGesInit< cProfEstimator > & ProfEstimator()const ;
+    private:
+        cTplValGesInit< double > mIntervPercProf;
+        cTplValGesInit< cProfEstimator > mProfEstimator;
+};
+cElXMLTree * ToXMLTree(const cSectionTerrain &);
+
+/******************************************************/
+/******************************************************/
+/******************************************************/
+class cParamSaisiePts
+{
+    public:
+        cGlobXmlGen mGXml;
+
+        friend void xml_init(cParamSaisiePts & anObj,cElXMLTree * aTree);
+
+
+        cTplValGesInit< cChantierDescripteur > & DicoLoc();
+        const cTplValGesInit< cChantierDescripteur > & DicoLoc()const ;
+
+        cTplValGesInit< Pt2di > & SzTotIm();
+        const cTplValGesInit< Pt2di > & SzTotIm()const ;
+
+        cTplValGesInit< Pt2di > & NbFenIm();
+        const cTplValGesInit< Pt2di > & NbFenIm()const ;
+
+        cTplValGesInit< Pt2di > & SzWZ();
+        const cTplValGesInit< Pt2di > & SzWZ()const ;
+
+        cTplValGesInit< bool > & ShowDet();
+        const cTplValGesInit< bool > & ShowDet()const ;
+
+        cTplValGesInit< bool > & RefInvis();
+        const cTplValGesInit< bool > & RefInvis()const ;
+
+        cSectionWindows & SectionWindows();
+        const cSectionWindows & SectionWindows()const ;
+
+        cTplValGesInit< std::string > & Prefix2Add2IdPt();
+        const cTplValGesInit< std::string > & Prefix2Add2IdPt()const ;
+
+        std::list< cImportFromDico > & ImportFromDico();
+        const std::list< cImportFromDico > & ImportFromDico()const ;
+
+        cTplValGesInit< bool > & FlouGlobEcras();
+        const cTplValGesInit< bool > & FlouGlobEcras()const ;
+
+        cTplValGesInit< bool > & TypeGlobEcras();
+        const cTplValGesInit< bool > & TypeGlobEcras()const ;
+
+        cTplValGesInit< std::string > & NamePointesImage();
+        const cTplValGesInit< std::string > & NamePointesImage()const ;
+
+        cTplValGesInit< std::string > & NamePointsGlobal();
+        const cTplValGesInit< std::string > & NamePointsGlobal()const ;
+
+        cTplValGesInit< std::string > & ExportPointeImage();
+        const cTplValGesInit< std::string > & ExportPointeImage()const ;
+
+        std::list< std::string > & FixedName();
+        const std::list< std::string > & FixedName()const ;
+
+        cTplValGesInit< std::string > & NameAuto();
+        const cTplValGesInit< std::string > & NameAuto()const ;
+
+        cTplValGesInit< bool > & EnterName();
+        const cTplValGesInit< bool > & EnterName()const ;
+
+        cSectionInOut & SectionInOut();
+        const cSectionInOut & SectionInOut()const ;
+
+        std::string & SetOfImages();
+        const std::string & SetOfImages()const ;
+
+        cTplValGesInit< bool > & ForceGray();
+        const cTplValGesInit< bool > & ForceGray()const ;
+
+        cTplValGesInit< std::string > & KeyAssocOri();
+        const cTplValGesInit< std::string > & KeyAssocOri()const ;
+
+        cSectionImages & SectionImages();
+        const cSectionImages & SectionImages()const ;
+
+        cTplValGesInit< double > & IntervPercProf();
+        const cTplValGesInit< double > & IntervPercProf()const ;
+
+        cTplValGesInit< double > & ZMoyen();
+        const cTplValGesInit< double > & ZMoyen()const ;
+
+        cTplValGesInit< std::string > & ZMoyenInIma();
+        const cTplValGesInit< std::string > & ZMoyenInIma()const ;
+
+        cTplValGesInit< cProfEstimator > & ProfEstimator();
+        const cTplValGesInit< cProfEstimator > & ProfEstimator()const ;
+
+        cSectionTerrain & SectionTerrain();
+        const cSectionTerrain & SectionTerrain()const ;
+
+        std::string & DirectoryChantier();
+        const std::string & DirectoryChantier()const ;
+    private:
+        cTplValGesInit< cChantierDescripteur > mDicoLoc;
+        cSectionWindows mSectionWindows;
+        cSectionInOut mSectionInOut;
+        cSectionImages mSectionImages;
+        cSectionTerrain mSectionTerrain;
+        std::string mDirectoryChantier;
+};
+cElXMLTree * ToXMLTree(const cParamSaisiePts &);
+
+/******************************************************/
+/******************************************************/
+/******************************************************/
+};
+#endif // Define_NotSaisiPts
