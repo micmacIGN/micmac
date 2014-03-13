fed2ac3903f7332b8716a44cfeba2852f22a7780
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2014-03-13 11:47:20 +0100

    GpGpu : optimisation

---
+++ src/mm3dSat/mm3dSat_main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include "mm3dSat_mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    
+    return a.exec();
+}
