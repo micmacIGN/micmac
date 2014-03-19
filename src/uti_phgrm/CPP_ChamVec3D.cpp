7093cdd5dc702ba5c67af016e0747390ed3eb13e
deseilligny <deseilligny@users.noreply.github.com>
2014-03-19 18:49:49 +0100

    Differents modif pour lionel; SEL en mode L1, FieldDep3d

---
+++ src/uti_phgrm/CPP_ChamVec3D.cpp
@@ -0,0 +1,252 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+   
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in 
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte 
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+#include "StdAfx.h"
+
+#define DEF_OFSET -12349876
+
+
+static Fonc_Num FoncVec(std::vector<Im2D_REAL8> aRes)
+{
+
+   return Virgule
+          (
+              aRes[0].in_proj(),
+              aRes[1].in_proj(),
+              aRes[2].in_proj()
+          );
+}
+static Output OutVec(std::vector<Im2D_REAL8> aRes)
+{
+
+   return Virgule
+          (
+              aRes[0].out(),
+              aRes[1].out(),
+              aRes[2].out()
+          );
+}
+
+static Fonc_Num Grad(Im2D_REAL8 aRes)
+{
+    return  Polar_Def_Opun::polar(deriche(aRes.in_proj(),1),0);
+}
+
+static Fonc_Num Grad(std::vector<Im2D_REAL8> aRes)
+{
+     return Grad(aRes[0]) + Grad(aRes[1]) + Grad(aRes[2]);
+}
+
+
+
+int ChamVec3D_main(int argc,char ** argv)
+{
+   std::string aN3dA;
+   std::string aN3dB;
+   std::string aVecXB;
+   std::string aVecYB;
+
+   std::string aVecXA;
+   std::string aVecYA;
+
+    ElInitArgMain
+    (
+	argc,argv,
+	LArgMain()  << EAMC(aN3dA,"Name of first XYZ Images")
+                    << EAMC(aN3dB,"Name of second  XYZ Images")
+                    << EAMC(aVecXB,"Name of X-mapping ref to second image")
+                    << EAMC(aVecYB,"Name of Y-mapping ref to second image"),
+
+	LArgMain()  << EAM(aVecXA,"XFirst",true,"Name of X-mapping ref to first image")
+                    << EAM(aVecYA,"YFirst",true,"Name of X-mapping ref to first image")
+    );	
+
+
+    std::vector<Im2D_REAL8> aImA;
+    Im2D_REAL8::ReadAndPushTif(aImA,Tiff_Im(aN3dA.c_str()));
+    ELISE_ASSERT(aImA.size()==3,"Bad size for nuage");
+
+    std::vector<Im2D_REAL8> aImB;
+    Im2D_REAL8::ReadAndPushTif(aImB,Tiff_Im(aN3dB.c_str()));
+    ELISE_ASSERT(aImB.size()==3,"Bad size for nuage");
+
+
+
+    Im2D_REAL8 aDepX = Im2D_REAL8::FromFileStd(aVecXB);
+    Im2D_REAL8 aDepY = Im2D_REAL8::FromFileStd(aVecYB);
+
+    Pt2di aSz = aDepX.sz();
+
+    std::vector<Im2D_REAL8> aRes;
+    std::vector<Im2D_REAL8> aImA2;
+    for (int aK=0 ; aK< 3 ; aK++)
+    {
+        aRes.push_back(Im2D_REAL8(aSz.x,aSz.y));
+        aImA2.push_back(Im2D_REAL8(aSz.x,aSz.y));
+    }
+
+
+   ELISE_COPY
+   (
+        aRes[0].all_pts(),
+        FoncVec(aImB)[Virgule(FX+aDepX.in(),FY+aDepY.in())],
+        // Virgule(aDepX.in(),aDepY.in(),0),
+        OutVec(aImA2)
+   );
+
+
+   Im2D_REAL8 aGrad(aSz.x,aSz.y);
+   ELISE_COPY
+   (
+        aRes[0].all_pts(),
+        Grad(aImA) + Grad(aImA2),
+        // Virgule(aDepX.in(),aDepY.in(),0),
+        aGrad.out()
+   );
+
+
+   Tiff_Im::Create8BFromFonc
+   (
+       "aGrad.tif",
+        aSz,
+        aGrad.in()
+   );
+
+
+   
+   double aSeuilG = 5;
+   int aSzW = 10;
+   Fonc_Num aDif =  FoncVec(aImA) - FoncVec(aImA2);
+   // aDif = MedianBySort(aDif,2);
+   Fonc_Num aPds = 1/(1+Square(aGrad.in_proj()/aSeuilG));
+   for (int aK=0 ; aK < 3; aK++)
+   {
+       aDif = rect_som(aDif*aPds,aSzW) /  rect_som(aPds,aSzW);
+   }
+
+
+   ELISE_COPY
+   (
+        aRes[0].all_pts(),
+        aDif,
+        // Virgule(aDepX.in(),aDepY.in(),0),
+        OutVec(aRes) | Video_Win::WiewAv(aSz)
+   );
+
+/*
+   ELISE_COPY
+   (
+        aRes[0].all_pts(),
+        MedianBySort(MedianBySort(FoncVec(aRes),5),5),
+        // Virgule(aDepX.in(),aDepY.in(),0),
+        OutVec(aRes) | Video_Win::WiewAv(aSz)
+   );
+*/
+
+
+   Tiff_Im::Create8BFromFonc
+   (
+       "NormDep3d.tif",
+        aSz,
+        20*sqrt(Square(aRes[0].in())+Square(aRes[1].in())+Square(aRes[2].in()))
+   );
+
+   Tiff_Im::Create8BFromFonc
+   (
+       "NormDep2d.tif",
+        aSz,
+       10*sqrt(Square(aRes[0].in())+Square(aRes[1].in()))
+   );
+
+   Tiff_Im::Create8BFromFonc
+   (
+       "DepZ.tif",
+        aSz,
+        128 + 20 * aRes[2].in()
+   );
+
+   Tiff_Im::CreateFromFonc
+   (
+       "Teta.tif",
+        aSz,
+        Polar_Def_Opun::polar(Virgule(aRes[0].in(),aRes[1].in()),0).v1(),
+        GenIm::real4
+   );
+
+
+    BanniereMM3D();
+	
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
---
+++ src/uti_phgrm/SaisiePts/cX11Interface.cpp
@@ -0,0 +1,362 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#include "StdAfx.h"
+
+
+using namespace NS_SaisiePts;
+
+#if ELISE_windows == 0
+
+cX11_Interface::cX11_Interface(cAppli_SaisiePts &appli) :
+    mCurWinIm     (0),
+    mWZ           (0),
+    mWEnter       (0)
+{
+    mParam = &appli.Param();
+    mAppli = &appli;
+
+    mRefInvis = appli.Param().RefInvis().Val();
+
+    //  Init();
+}
+
+cX11_Interface::~cX11_Interface()
+{
+    delete mDisp;
+
+    delete mCurWinIm;
+    delete mWZ;
+    delete mZFON;
+    delete mMenuNamePoint;
+    delete mWEnter;
+}
+
+void cX11_Interface::Init()
+{
+    InitNbWindows();
+
+    Pt2di aSzF =  mParam->SectionWindows().SzTotIm().Val().dcbyc(mNb2W);;
+
+    int aCpt=0;
+    Video_Win * aLastW = 0;
+    Video_Win * aLastWX0 = 0;
+    Video_Win * aWTitle = 0;
+    Video_Win * aWY0XMax = 0;
+    Pt2di aSzTitle(aSzF.x,15);
+    for (int aKY =0 ; aKY<mNb2W.y ; aKY++)
+    {
+        for (int aKX =0 ; aKX<mNb2W.x ; aKX++)
+        {
+            if (aCpt<mNbW)
+            {
+                Video_Win * aRes = 0;
+                if (aCpt==0)
+                {
+                    aWTitle = Video_Win::PtrWStd(aSzTitle,true);
+                    aRes = new Video_Win(*aWTitle,Video_Win::eBasG,aSzF);
+                    // aRes = Video_Win::PtrWStd(aSzF,true);
+                    aLastWX0 = aRes;
+                    mDisp = new Video_Display(aRes->disp());
+                }
+                else
+                {
+                    if (aKX==0)
+                    {
+                        aWTitle = new Video_Win(*aLastWX0,Video_Win::eBasG,aSzTitle);
+                        aRes = new Video_Win(*aWTitle,Video_Win::eBasG,aSzF);
+                        aLastWX0 = aRes;
+                    }
+                    else
+                    {
+                        aWTitle = new Video_Win(*aLastW,Video_Win::eDroiteH,aSzTitle);
+                        aRes = new Video_Win(*aWTitle,Video_Win::eBasG,aSzF);
+                    }
+
+                }
+                ELISE_COPY(aWTitle->all_pts(),P8COL::yellow,aWTitle->odisc());
+                ELISE_COPY(aRes->all_pts(),P8COL::blue,aRes->odisc());
+                aLastW = aWTitle;
+                mWins.push_back(new cWinIm(*mAppli,*aRes,*aWTitle,*mAppli->image(aCpt)));
+                if ((aKY==0) && (aKX==(mNb2W.x -1)))
+                {
+                    aWY0XMax = aWTitle;
+                }
+            }
+            aCpt++;
+        }
+    }
+
+    Pt2di zoomWindowSize = mParam->SectionWindows().SzWZ().ValWithDef(round_ni(Pt2dr(mParam->SzTotIm().Val())*0.6));
+    mWZ =  new Video_Win(*aWY0XMax,Video_Win::eDroiteH, zoomWindowSize);
+    mZFON = new cFenOuiNon(*mWZ,Pt2di(200,20));
+
+    mVNameCase.push_back( cCaseNamePoint("Cancel",eCaseCancel) );
+
+    if (mParam->EnterName().Val())
+    {
+        mVNameCase.push_back( cCaseNamePoint("Enter New",eCaseSaisie) );
+    }
+
+    InitVNameCase();
+
+    mMenuNamePoint = new cFenMenu(*mWZ,Pt2di(120,20),Pt2di(1,mVNameCase.size()));
+
+    if (mParam->EnterName().Val())
+    {
+        mWEnter =  new Video_Win(mMenuNamePoint->W(),Video_Win::eDroiteH,Pt2di(150,20));
+        mWEnter->move_translate(Pt2di(0,20));
+        ELISE_COPY(mWEnter->all_pts(),P8COL::yellow,mWEnter->odisc());
+        //mWEnter->move_to(Pt2di(100,20));
+        mWEnter->lower();
+    }
+
+    ELISE_COPY(mWZ->all_pts(),P8COL::green,mWZ->odisc());
+}
+
+cCaseNamePoint *  cX11_Interface::GetIndexNamePoint()
+{
+    Video_Win aW = mMenuNamePoint->W();
+    aW.raise();
+
+    for (int aK=0 ; aK<int(mVNameCase.size()) ; aK++)
+    {
+        int aGr = (aK%2) ? 255 : 200 ;
+        Pt2di aPCase(0,aK);
+        mMenuNamePoint->ColorieCase(aPCase,aW.prgb()(aGr,aGr,aGr),1);
+        cCaseNamePoint & aCNP = mVNameCase[aK];
+
+        mMenuNamePoint->StringCase(aPCase,aCNP.mFree ? aCNP.mName : "***" ,true);
+    }
+
+    Clik aClk = aW.clik_in();
+    //aW.lower();
+
+    Pt2di aKse = mMenuNamePoint->Pt2Case(Pt2di(aClk._pt));
+    cCaseNamePoint * aRes =  &(mVNameCase[aKse.y]);
+
+    if (! aRes->mFree) return 0;
+
+    return aRes;
+}
+
+void cX11_Interface::_DeletePoint(cSP_PointGlob * aSG)
+{
+    if (! mZFON->Get("Kill " + aSG->PG()->Name() + "?")) return;
+
+    DeletePoint(aSG);
+
+    RedrawAllWindows();
+}
+
+std::pair<int,std::string> cX11_Interface::IdNewPts(cCaseNamePoint * aCNP)
+{
+   int aCptMax = mAppli->GetCptMax() + 1;
+
+   std::string aName = aCNP->mName;
+   if (aCNP->mTCP == eCaseAutoNum)
+   {
+       aName = nameFromAutoNum(aCNP, aCptMax);
+   }
+
+   if (aCNP->mTCP == eCaseSaisie)
+   {
+       mWEnter->raise();
+       ELISE_COPY(mWEnter->all_pts(),P8COL::yellow,mWEnter->odisc());
+
+       // std::cin >> aName ;
+       aName = mWEnter->GetString(Pt2dr(5,15),mWEnter->pdisc()(P8COL::black),mWEnter->pdisc()(P8COL::yellow));
+       mWEnter->lower();
+   }
+
+   mMenuNamePoint->W().lower();
+
+   // std::cout << "cAppli_SaisiePts::IdNewPts " << aCptMax << " " << aName << "\n";
+
+   return std::pair<int,std::string>(aCptMax,aName);
+}
+
+void cX11_Interface::DrawZoom(const Pt2dr & aPGlob)
+{
+     double aZoom = 10.0;
+
+     Pt2dr aPIm = aPGlob- Pt2dr(mAppli->DecRech());
+     Pt2dr aPMil = Pt2dr(mWZ->sz())/(2.0*aZoom);
+
+     Video_Win aWC = mWZ->chc(aPIm-aPMil,Pt2dr(aZoom,aZoom));
+     ELISE_COPY
+     (
+                aWC.all_pts(),
+                mAppli->ImRechVisu().in(0),
+                aWC.ogray()
+     );
+
+     aWC.draw_circle_abs(aPIm,4.0,Line_St(aWC.pdisc()(P8COL::blue),3.0));
+}
+
+cWinIm * cX11_Interface::WinImOfW(Video_Win aW)
+{
+    for (int aK=0 ; aK<mNbW; aK++)
+        if (mWins[aK]->W() == aW)
+           return mWins[aK];
+
+    return 0;
+}
+
+void cX11_Interface::TestClick(Clik aCl)
+{
+    cWinIm * aWIm = WinImOfW(aCl._w);
+    if (!aWIm)
+        return;
+    else
+        mCurWinIm = aWIm;
+
+    if (aCl._b==1)
+    {
+        aWIm->SetPt(aCl);
+        Save();
+    }
+
+    if ((aCl._b==4) || (aCl._b==5))
+    {
+        double aFactZ = 1.2;
+        aWIm->SetZoom(aCl._pt,(aCl._b==5) ? aFactZ: (1/aFactZ));
+        aWIm->ShowVect();
+    }
+
+    if (aCl._b==2)
+    {
+        if (aCl.controled() )
+        {
+            // std::cout << "Zzzz2222 " << aCl.controled() << "\n";
+           if (aCl.shifted())
+           {
+              mCurWinIm->SetFullImage();
+           }
+           else
+           {
+              mCurWinIm->AffNextPtAct(aCl);
+           }
+        }
+        aWIm->GrabScrTr(aCl);
+    }
+
+    if (aCl._b==3)
+    {
+        aWIm->MenuPopUp(aCl);
+    }
+}
+
+void cX11_Interface::BoucleInput()
+{
+    while(1)
+    {
+        Clik   aCl = mDisp->clik_press();
+
+        TestClick(aCl);
+    }
+}
+
+void cX11_Interface::SetInvisRef(bool aVal)
+{
+    mRefInvis = aVal;
+
+    for (int aKW=0 ; aKW < int(mWins.size()); aKW++)
+    {
+        mWins[aKW]->BCaseVR()->SetVal(aVal);
+        mWins[aKW]->Redraw();
+        mWins[aKW]->ShowVect();
+    }
+}
+
+void cX11_Interface::AddUndo(cOneSaisie * aSom)
+{
+    mAppli->AddUndo(*aSom, mCurWinIm->Image());
+}
+
+bool cX11_Interface::isDisplayed(cImage *anIm)
+{
+    return anIm->WAff() != NULL;
+}
+
+void cX11_Interface::RedrawAllWindows()
+{
+    for (int aK=0 ; aK< int(mWins.size()) ; aK++)
+        mWins[aK]->Redraw();
+}
+
+void cX11_Interface::Redraw()
+{
+    mCurWinIm->Redraw();
+}
+
+#endif
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant �  la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  �  l'utilisation,  �  la modification et/ou au
+développement et �  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe �
+manipuler et qui le réserve donc �  des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
+logiciel �  leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
