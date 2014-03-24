ca58a0e6ee11aa174cb88195cd478290033398fb
mdeveau <mdeveau@ASU1210P003-Ubuntu>
2014-03-24 16:44:39 +0100

    MMVisualMode

--- include/general/visual_mainwindow.h
+++ include/general/visual_mainwindow.h
@@ -19,6 +19,7 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <QSpinBox>
+#include <QToolBox>
 #include <QFileDialog>
 #include <QDesktopWidget>
 #include <QApplication>
@@ -40,12 +41,20 @@ public:
     visual_MainWindow(vector<cMMSpecArg> & aVAM, vector<cMMSpecArg> & aVAO, QWidget *parent = 0);
     ~visual_MainWindow();
 
-    void add_combo_line(QString);
-    void create_combo(int, list<string>);
-    void create_comment(string, int);
-    void create_select(int, cMMSpecArg);
-    void create_champ_int(int);
+    void create_combo(QGridLayout *layout, QWidget *parent, int, list<string>);
+    void create_comment(QGridLayout *layout, QWidget *parent, string, int ak);
+    void create_select(QGridLayout* layout, QWidget* parent, int, cMMSpecArg);
+    void create_champ_int(QGridLayout* layout, QWidget* parent, int);
     void set_argv_recup(string);
+
+    void buildUI(vector<cMMSpecArg>& aVAM, QGridLayout* layout, QWidget* parent);
+
+public slots:
+
+    void onRunCommandPressed();
+    void onSelectFilePressed(int);
+    void onSelectImgsPressed(int);
+    void onSelectDirPressed(int);
 
 protected:
 
@@ -54,7 +63,7 @@ protected:
     int id_unique;
     string argv_recup;
 
-    QWidget*     gridLayoutWidget;//il faut forcement passer par un QWidget pour le mettre en "CentralWidget" de la MainWindow
+    QWidget*     mainWidget;
     QGridLayout* gridLayout;
 
     QLabel*      label;
@@ -66,19 +75,14 @@ protected:
 
     vector <QComboBox*> vEnumValues;    //enum values
     vector <QLineEdit*> vLineEdit;      //LineEdit: display what has been selected (images, files, directories)
-    vector <QLabel*>    vComments;      //comments
+    //vector <QLabel*>    vComments;      //comments
 
     vector <eInputType> vInputTypes;
     vector <QWidget*>   vInputs;
 
     QString        mlastDir;
 
-public slots:
-
-    void onRunCommandPressed();
-    void onSelectFilePressed(int);
-    void onSelectImgsPressed(int);
-    void onSelectDirPressed(int);
+    list <string>  bList;
 };
 
 list<string> listPossibleValues(const cMMSpecArg & anArg);
