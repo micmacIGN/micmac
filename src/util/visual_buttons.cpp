156fbad5bb2bcf43cbcabe198f89e3f7d22c90d7
deseilligny <deseilligny@users.noreply.github.com>
2014-03-26 21:57:29 +0100

    Qq commentaire

---
+++ src/util/visual_buttons.cpp
@@ -0,0 +1,34 @@
+#if(ELISE_QT5)
+
+#include "general/visual_buttons.h"
+
+#include <iostream>
+
+
+int selectionButton::buttonNumber=0;
+
+selectionButton::selectionButton(QWidget *parent)
+    : QPushButton(parent),
+      unique_id(buttonNumber++)
+{
+    connect(this,SIGNAL(clicked()),this,SLOT(onClick()));
+}
+
+selectionButton::selectionButton(QString text, QWidget *parent):
+    selectionButton(parent)
+{
+    setText(text);
+}
+
+selectionButton::~selectionButton()
+{
+
+}
+
+void selectionButton::onClick()
+{
+    //std::cout<<"bouton unique_id: "<<unique_id<<std::endl;
+    emit my_click(unique_id);
+}
+
+ #endif // ELISE_QT5
