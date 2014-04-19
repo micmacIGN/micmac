4b8303b5c22478c904ab30736d283473ca2eea52
deseilligny <deseilligny@users.noreply.github.com>
2014-04-19 11:58:11 +0200

    Debut // calc Homogr ds RHH

---
+++ src/uti_phgrm/RHH/CalcHcImage.cpp
@@ -0,0 +1,310 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#include "StdAfx.h"
+#include "ReducHom.h"
+
+NS_RHH_BEGIN
+
+
+/*************************************************/
+/*                                               */
+/*                  cImagH                       */
+/*                                               */
+/*************************************************/
+
+
+
+
+/*  Read tie points if necessary, estimate the homography and the quality
+*/
+
+bool cImagH::ComputeLnkHom(cLink2Img & aLnk)
+{
+
+   if (! aLnk.OkHom()) 
+      return false;
+
+   int aNbPts = aLnk.NbPts();
+   mSomQual += ElMin(aLnk.QualHom(),mAppli.SeuilQual()) * aNbPts;
+   mSomNbPts += aNbPts;
+
+   return true;
+}
+
+/*
+   // const ElPackHomologue aPack=ElPackHomologue::FromFile(mAppli.Dir()+aLnk.NameH());
+   const ElPackHomologue & aPack=    aLnk.Pack() ; //   ElPackHomologue::FromFile(mAppli.Dir()+aLnk.NameH());
+   int aNbPts = aPack.size();
+   aLnk.NbPts() = aPack.size();
+
+   if (aNbPts < mAppli.MinNbPtH())
+      return false;
+
+   bool Ok;
+   double aQual;
+   cElHomographie  aHom12 = cElHomographie::RobustInit(&aQual,aPack,Ok,NB_RANSAC_H,90.0,1000);
+   // mLnk.push_back(cLink2Img(anI2,aNameH));
+
+   if (!Ok)
+     return false;
+
+   aLnk.Hom12() = aHom12;
+   aLnk.Qual() = aQual;
+
+   mSomQual += ElMin(aQual,mAppli.SeuilQual()) * aNbPts;
+   mSomNbPts += aNbPts;
+
+   return true;
+*/
+/*
+*/
+
+void cImagH::ComputeLnkHom()
+{
+    tMapName2Link  aNewL;
+    for ( tMapName2Link::iterator itL = mLnks.begin(); itL != mLnks.end(); itL++)
+    {
+        if (ComputeLnkHom(*(itL->second)))
+           aNewL[itL->first] = itL->second ;
+        else
+           delete itL->second;
+    }
+    mLnks = aNewL;
+    aNewL.clear();
+
+    if (mSomNbPts)
+    {
+       mSomQual /= mSomNbPts;
+       double aSeuilQual = mSomQual*mAppli.RatioQualMoy();
+       for (tMapName2Link::iterator itL = mLnks.begin(); itL != mLnks.end(); itL++)
+       {
+           bool Ok = itL->second->QualHom() < aSeuilQual;
+           if (Ok)
+           {
+              aNewL[itL->first] = itL->second ;
+           }
+           else
+           {
+               delete itL->second;
+           }
+           if (Ok)
+           {
+              if (mAppli.Show(eShowAll))
+              {
+                 std::cout
+                     << " - - IMS " << mName << " " << itL->second->Dest()->Name()
+                     << " QUAL " << itL->second->QualHom() << " NB " << itL->second->NbPts()
+                     << (Ok ? " " : "  ******")
+                     <<  "\n";
+              }
+           }
+       }
+    }
+    mLnks = aNewL;
+
+    if (mAppli.Show(eShowDetail))
+        std::cout << " - " << mName << " QMOY " << (mSomNbPts ? mSomQual : 1e10) << "\n";
+}
+
+
+void cImagH::VoisinsNonMarques(const std::vector<cImagH*> & aIn,std::vector<cImagH*> & aVois,int aFlagN,int aFlagT )
+{
+   aVois.clear();
+
+   for (int aKS=0 ; aKS<int(aIn.size()) ; aKS++)
+   {
+       cImagH * aIK1 = aIn[aKS];
+       for (tMapName2Link::iterator itL1 = aIK1->mLnks.begin(); itL1 != aIK1->mLnks.end(); itL1++)
+       {
+            cImagH * aImTest  = itL1->second->Dest();
+            if ((! aImTest->Marqued(aFlagN)) && (! aImTest->Marqued(aFlagT)))
+            {
+                aImTest->SetMarqued(aFlagT);
+                aVois.push_back(aImTest);
+            }
+        }
+    }
+    for (int aKT=0 ; aKT<int(aVois.size()) ; aKT++)
+        aVois[aKT]->SetUnMarqued(aFlagT);
+}
+
+void cImagH::VoisinsMarques(std::vector<cLink2Img*> & aVois,int aFlagN)
+{
+    aVois.clear();
+    for ( tMapName2Link::iterator itL = mLnks.begin(); itL != mLnks.end(); itL ++)
+    {
+        cImagH * aI2  = itL->second->Dest();
+        if (aI2->Marqued(aFlagN))
+            aVois.push_back(itL->second);
+     }
+}
+
+
+
+
+void  cAppliReduc::QuadrReestimFromVois(std::vector<cImagH*> & aVLocIm,int aFlag)
+{
+
+    for (int aK=0 ; aK<int(mIms.size()) ; aK++)
+    {
+         cImagH * anI =  mIms[aK];
+         anI->HF()->SetModeCtrl(anI->Marqued(aFlag) ? cNameSpaceEqF::eHomLibre : cNameSpaceEqF::eHomFigee);
+         // anI->HF()->SetModeCtrl( cNameSpaceEqF::eHomFigee);
+    }
+    aVLocIm[0]->HF()->SetModeCtrl(cNameSpaceEqF::eHomFigee);
+    for (int aK=0 ; aK<int(aVLocIm.size()) ; aK++)
+    {
+         cImagH * anI = aVLocIm[aK];
+         anI->HF()->ReinitHom(anI->Hi2t());
+    }
+         // anI->HF()->SetModeCtrl( cNameSpaceEqF::eHomFigee);
+
+
+    for (int aFois = 0 ; aFois<5 ; aFois++)
+    {
+         for (int aK=0 ; aK<int(mIms.size()) ; aK++)
+         {
+              cImagH * anI =  mIms[aK];
+              mSetEq.AddContrainte(anI->HF()->StdContraintes(),true);
+         }
+         mSetEq.SetPhaseEquation();
+
+         double aSomR=0;
+         double aSomP=0;
+         for (int aK=0 ; aK<int(aVLocIm.size()) ; aK++)
+         {
+             cImagH * anI1 =  aVLocIm[aK];
+             const tMapName2Link & aLL = anI1->Lnks();
+             for (tMapName2Link::const_iterator itL = aLL.begin(); itL != aLL.end(); itL++)
+             {
+                 cImagH * anI2 = itL->second->Dest();
+                 if (anI2->Marqued(aFlag))
+                 {
+                     cElHomographie aH12 = itL->second->Hom12();
+                     const std::vector<Pt3dr> &  anEchP1 = itL->second->EchantP1();
+                     cEqHomogFormelle * anEqF = itL->second->EqHF() ;
+                     for (int aK=0 ; aK<int(anEchP1.size()) ; aK++)
+                     {
+                         const Pt3dr & aQ3 = anEchP1[aK];
+                         double aPds = aQ3.z;
+                         Pt2dr aP1 (aQ3.x,aQ3.y);
+                         Pt2dr aP2  = aH12.Direct(aP1);
+                         Pt2dr aResidu = anEqF->StdAddLiaisonP1P2(aP1,aP2,aPds,false);
+                         aSomR+=square_euclid(aResidu) * aPds;
+                         aSomP+= aPds;
+                     }
+                 }
+             }
+         }
+         std::cout << "RES "  <<  sqrt(aSomR/aSomP)  <<  "\n";
+         mSetEq.SolveResetUpdate();
+    }
+    std::cout << "\n";
+    for (int aK=0 ; aK<int(aVLocIm.size()) ; aK++)
+    {
+        cImagH * anI =  aVLocIm[aK];
+        anI->Hi2t() =  anI->HF()->HomCur();
+    }
+}
+
+void cAppliReduc::TestMerge_CalcHcImage()
+{
+    cParamMerge  aParam (*this);
+
+    cAlgoMergingRec<cImagH,cAttrLnkIm,cParamMerge> anAlgo(mIms,aParam,0);
+
+    const std::set<tNodIm *> & aRoot = anAlgo.Roots();
+
+
+    for
+    (
+           std::set<tNodIm *>::const_iterator itN=aRoot.begin();
+           itN!=aRoot.end();
+           itN++
+    )
+    {
+              anAlgo.Show(*itN);
+    }
+}
+
+
+NS_RHH_END
+
+
+/*
+*/
+/*
+*/
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
---
+++ src/uti_phgrm/RHH/cImagH.cpp
@@ -0,0 +1,480 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#include "StdAfx.h"
+#include "ReducHom.h"
+
+NS_RHH_BEGIN
+
+
+cXmlRHHResLnk  ComputeHomographie
+               (
+                  const std::string & aName
+               )
+{
+  cXmlRHHResLnk  aRes;
+
+  ElPackHomologue aPack = ElPackHomologue::FromFile(aName);
+
+  double aQual;
+  bool   aOk;
+  cElHomographie   aHom = cElHomographie::RobustInit(&aQual,aPack,aOk,NB_RANSAC_H,90.0,1000);
+
+  if (aOk)
+     aRes.HomToIm() = aHom.ToXml();
+
+
+  return aRes;
+}
+
+int Comput_HomMain(int argc,char ** argv)
+{
+
+   std::string aNameHom,aNameCal;
+   std::string toto;
+
+    ElInitArgMain
+    (
+         argc,argv,
+         LArgMain()  << EAMC(aNameHom,"Hom points")
+                     << EAMC(aNameCal,"Calib "),
+         LArgMain()  << EAM(toto,"toto",true)
+   );
+
+   return 1;
+}
+
+
+
+
+
+/*************************************************/
+/*                                               */
+/*                cLink2Img                      */
+/*                                               */
+/*************************************************/
+
+cLink2Img::cLink2Img(cImagH * aSrce,cImagH * aDest,const std::string & aNameH) :
+   mNbPts      (0),
+   mNbPtsAttr  (0),
+   mSrce       (aSrce),
+   mDest       (aDest),
+   mAppli      (mSrce->Appli()),
+   mNameH      (aNameH),
+   mQualHom    (0),
+   mHom12      (cElHomographie::Id()),
+   mPckLoaded  (false),
+   mHomLoaded  (false),
+   mOkHom      (false)
+{
+}
+
+
+void  cLink2Img::LoadHomographie(bool ExigOk)
+{
+     LoadPack();
+     if (!mHomLoaded) 
+     {
+        mHomLoaded = true;
+        if (NbPts() >=  mAppli.MinNbPtH())
+        {
+            mHom12 = cElHomographie::RobustInit(&mQualHom,mPack,mOkHom,NB_RANSAC_H,90.0,1000);
+        }
+     }
+       
+     if (ExigOk)
+     {
+         ELISE_ASSERT(mOkHom,"Cannot get Hom12 : mOkHom=false");
+     }
+}
+
+void  cLink2Img::LoadHomographie(bool ExigOk) const
+{
+   const_cast<cLink2Img*>(this)->LoadHomographie(ExigOk);
+}
+
+void cLink2Img::LoadPack()
+{
+   if (mPckLoaded)
+      return;
+   mPckLoaded = true;
+   mPack = ElPackHomologue::FromFile(mSrce->Appli().Dir()+mNameH);
+   mNbPts =  mPack.size();
+
+   std::vector<Pt2dr>  aVP2;
+   for
+   (
+       ElPackHomologue::const_iterator itP=mPack.begin();
+       itP != mPack.end();
+       itP++
+   )
+   {
+       Pt2dr aP1 = itP->P1();
+       aVP2.push_back(aP1);
+       // mEchantP1.push_back(Pt3dr(aP1.x,aP1.y,1.0));
+   }
+
+   Pt2di aSz (3,3);
+   if (aVP2.size() > 100)
+      aSz = Pt2di(4,4);
+   if (aVP2.size() > 500)
+      aSz = Pt2di(5,5);
+   if (aVP2.size() > 2000)
+      aSz = Pt2di(6,6);
+
+   mEchantP1 = GetDistribRepresentative(mCdg1,aVP2,aSz);
+
+}
+
+void  cLink2Img::LoadPack() const
+{
+   const_cast<cLink2Img*>(this)->LoadPack();
+}
+
+
+const ElPackHomologue & cLink2Img::Pack() const
+{
+   LoadPack();
+   return mPack;
+}
+
+
+// Compute the quality of the "closing" relation
+//     mDest->Hi2t() * mHom12 = mSrce->Hi2t()
+//     This quality is estimated on the weighted set mEchantP1
+
+
+double cLink2Img::CoherenceH()
+{
+    LoadPack();
+
+    cElHomographie aI2T_A = CalcSrceFromDest();
+    cElHomographie aI2T_B = mSrce->Hi2t();
+
+    double aSomP=0;
+    double aSomDist=0;
+
+    for (int aKP=0 ; aKP < int(mEchantP1.size()) ; aKP++)
+    {
+         Pt3dr aP3(mEchantP1[aKP]);
+         double aPds (aP3.z);
+         Pt2dr aP1 (aP3.x,aP3.y);
+
+         Pt2dr aPA = aI2T_A.Direct(aP1);
+         Pt2dr aPB = aI2T_B.Direct(aP1);
+         double aDist = square_euclid(aPA-aPB);
+
+         aSomP += aPds;
+         aSomDist  += aDist * aPds;
+    }
+
+    return sqrt(aSomDist/aSomP);
+}
+
+
+
+cImagH * cLink2Img::Srce() const
+{
+   return mSrce;
+}
+cImagH * cLink2Img::Dest() const
+{
+   return mDest;
+}
+
+const std::string &  cLink2Img::NameH() const
+{
+    return mNameH;
+}
+
+
+const int   &  cLink2Img::NbPts() const
+{
+    LoadPack();
+    return mNbPts;
+}
+
+/*
+int   &  cLink2Img::NbPtsAttr()
+{
+    return mNbPtsAttr;
+}
+*/
+
+const bool & cLink2Img::OkHom() const
+{
+   LoadHomographie(false);
+   return mOkHom;
+}
+
+
+const double & cLink2Img::QualHom() const
+{
+   LoadHomographie(true);
+   return mQualHom;
+}
+
+const cElHomographie &  cLink2Img::Hom12() const
+{
+    LoadHomographie(true);
+    return mHom12;
+}
+
+cElHomographie cLink2Img::CalcSrceFromDest ()
+{
+    // return mHom12 * mSrce->Ht2i(); A CHANGER
+    return mDest->Hi2t() * Hom12();
+}
+
+const std::vector<Pt3dr> & cLink2Img::EchantP1() const
+{
+    LoadPack();
+    return mEchantP1;
+}
+
+
+cEqHomogFormelle * &  cLink2Img::EqHF()
+{
+    return mEqHF;
+}
+
+
+/*************************************************/
+/*                                               */
+/*                 cImagH                        */
+/*                                               */
+/*************************************************/
+
+
+cImagH::cImagH(const std::string & aName,cAppliReduc & anAppli,int aNum) :
+   mName     (aName),
+   mAppli    (anAppli),
+   mNum      (aNum),
+   mNumTmp   (-1),
+   mSomQual  (0),
+   mSomNbPts (0),
+   mHi2t     (cElHomographie::Id()),
+   mHTmp     (cElHomographie::Id())
+{
+}
+
+   //============ FONCTION DE GRAPHE IMAGE =========================
+
+cLink2Img * cImagH::GetLinkOfImage(cImagH* anI2)
+{
+   tMapName2Link::iterator anIt = mLnks.find(anI2);
+   if (anIt==mLnks.end())
+      return 0;
+   return anIt->second;
+}
+
+
+void cImagH::AddLink(cImagH * anI2,const std::string & aNameH)
+{
+      mLnks[anI2] = new cLink2Img(this,anI2,aNameH);
+}
+
+void cImagH::SetMarqued(int aK)
+{
+   mMarques.set_kth_true(aK);
+}
+void cImagH::SetUnMarqued(int aK)
+{
+   mMarques.set_kth_false(aK);
+}
+bool cImagH::Marqued(int aK) const
+{
+   return mMarques.kth(aK);
+}
+
+cElHomographie &   cImagH::Hi2t()
+{
+   return mHi2t;
+}
+
+cElHomographie &   cImagH::HTmp()
+{
+   return mHTmp;
+}
+
+
+
+   //============ FUSION DE POINT =========================
+
+const std::string & cImagH::Name() const
+{
+  return mName;
+}
+
+void cImagH::AddOnePtToExistingH(cPtHom * aH1,const Pt2dr & aP1,cImagH * aI2,const Pt2dr & aP2)
+{
+    aH1->AddMesureInImage(aI2,aP2);
+    // aI2->mMapH[aP2] = aH1;
+}
+
+
+void  cImagH::FusionneIn(cPtHom *aH1,const Pt2dr & aP1,cImagH *aI2,cPtHom *aH2,const Pt2dr & aP2)
+{
+
+   aH1->OkAbsorb(aH2);
+   // aI2->mMapH[aP2] = aH1;
+   // if
+}
+
+void  cImagH::SetPHom(const Pt2dr & aP,cPtHom * aH)
+{
+   mMapH[aP] = aH;
+}
+
+void cImagH::AddOnePair(const Pt2dr & aP1,cImagH * aI2,const Pt2dr & aP2)
+{
+    std::map<Pt2dr,cPtHom *>::iterator it1 = mMapH.find(aP1);
+    std::map<Pt2dr,cPtHom *>::iterator it2 = aI2->mMapH.find(aP2);
+
+    if ((it1==  mMapH.end()) && (it2==  aI2->mMapH.end()))
+    {
+        cPtHom * aH = cPtHom::NewGerm(this,aP1,aI2,aP2);
+        mMapH[aP1] = aH;
+        aI2->mMapH[aP2] = aH;
+    }
+    else if ((it1!= mMapH.end()) && (it2==  aI2->mMapH.end()))
+    {
+       it1->second->AddMesureInImage(aI2,aP2);
+       // AddOnePtToExistingH(it1->second,aP1,aI2,aP2);
+    }
+    else if ((it1 == mMapH.end()) && (it2!=aI2->mMapH.end()))
+    {
+       it2->second->AddMesureInImage(this,aP1);
+       // aI2->AddOnePtToExistingH(it2->second,aP2,this,aP1);
+    }
+    else if (it1->second==it2->second)
+    {
+         it1->second->IncrCptArc();
+    }
+    else
+    {
+         if (it1->second->NbIm() >= it2->second->NbIm())
+            FusionneIn(it1->second,aP1,aI2,it2->second,aP2);
+         else
+            aI2->FusionneIn(it2->second,aP2,this,it1->second,aP1);
+    }
+}
+
+
+
+void  cImagH::ComputePtsLink(cLink2Img & aLnk)
+{
+    const ElPackHomologue & aPack = aLnk.Pack();
+
+    // std::cout << "    LNK " << aLnk.NameH() << "  " << aPack.size() << "\n";
+    for
+    (
+       ElPackHomologue::const_iterator itP=aPack.begin();
+       itP != aPack.end();
+       itP++
+    )
+    {
+        //  std::cout << itP->P1() << " " << itP->P2() << "\n";
+         AddOnePair(itP->P1(),aLnk.Dest(),itP->P2());
+    }
+}
+
+
+void cImagH::ComputePts()
+{
+    std::cout << "Compute " << mName << "\n";
+
+
+    //  D'abod on complete avec les arcs
+    for ( tMapName2Link::iterator itL=mLnks.begin(); itL!=mLnks.end(); itL++)
+    {
+        ComputePtsLink(*(itL->second));
+    }
+}
+
+
+cHomogFormelle *  & cImagH::HF()
+{
+   return mHF;
+}
+
+cAppliReduc &     cImagH::Appli()
+{
+   return mAppli;
+}
+
+const tMapName2Link & cImagH::Lnks() const
+{
+   return mLnks;
+}
+
+NS_RHH_END
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
