30e5eab4b434ac803b39a53bd194f1f004bb3bdd
deseilligny <deseilligny@localhost>
2014-05-22 19:17:35 +0200

    Exo Simulation de tie point

--- src/uti_phgrm/CPP_MMByPair.cpp
+++ src/uti_phgrm/CPP_MMByPair.cpp
@@ -595,9 +595,9 @@ cAppliClipChantier::cAppliClipChantier(int argc,char ** argv) :
                     << EAMC(mOri,"Orientation", eSAM_IsExistDirOri)
                     << EAMC(mNameMasterIm,"Image corresponding to the box", eSAM_IsPatFile)
                     << EAMC(mBox,"Box to clip"),
-        LArgMain()  << EAM(aPrefClip,"PrefCliped","def= Cliped")
-                    << EAM(aOriOut,"OriOut","Out Orientation, def = input")
-                    << EAM(aMinSz,"MinSz","Min sz to select cliped def = 500")
+        LArgMain()  << EAM(aPrefClip,"PrefCliped",true,"def= Cliped")
+                    << EAM(aOriOut,"OriOut",true,"Out Orientation, def = input")
+                    << EAM(aMinSz,"MinSz",true,"Min sz to select cliped def = 500")
    );
 
    StdCorrecNameOrient(mOri,DirOfFile(mFullName));
@@ -1350,7 +1350,7 @@ int ChantierClip_main(int argc,char ** argv)
 
 /*Footer-MicMac-eLiSe-25/06/2007
 
-Ce logiciel est un programme informatique servant �  la mise en
+Ce logiciel est un programme informatique servant \C3  la mise en
 correspondances d'images pour la reconstruction du relief.
 
 Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
@@ -1366,17 +1366,17 @@ seule une responsabilité restreinte pèse sur l'auteur du programme,  le
 titulaire des droits patrimoniaux et les concédants successifs.
 
 A cet égard  l'attention de l'utilisateur est attirée sur les risques
-associés au chargement,  �  l'utilisation,  �  la modification et/ou au
-développement et �  la reproduction du logiciel par l'utilisateur étant
-donné sa spécificité de logiciel libre, qui peut le rendre complexe �
-manipuler et qui le réserve donc �  des développeurs et des professionnels
+associés au chargement,  \C3  l'utilisation,  \C3  la modification et/ou au
+développement et \C3  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe \C3
+manipuler et qui le réserve donc \C3  des développeurs et des professionnels
 avertis possédant  des  connaissances  informatiques approfondies.  Les
-utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
-logiciel �  leurs besoins dans des conditions permettant d'assurer la
+utilisateurs sont donc invités \C3  charger  et  tester  l'adéquation  du
+logiciel \C3  leurs besoins dans des conditions permettant d'assurer la
 sécurité de leurs systèmes et ou de leurs données et, plus généralement,
-�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+\C3  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
 
-Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
+Le fait que vous puissiez accéder \C3  cet en-tête signifie que vous avez
 pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
 termes.
 Footer-MicMac-eLiSe-25/06/2007*/
