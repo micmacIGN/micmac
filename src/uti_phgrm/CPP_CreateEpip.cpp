c38df1643106b35b5fd766f0f857cd3c7fed69b8
mdeveau <mattdeveau@users.noreply.github.com>
2014-06-04 17:44:55 +0200

    MMVisualMode

---
+++ src/uti_phgrm/CPP_CreateEpip.cpp
@@ -0,0 +1,201 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#include "StdAfx.h"
+
+
+
+cCpleEpip * StdCpleEpip
+          (
+             std::string  aDir,
+             std::string  aNameOri,
+             std::string  aNameIm1,
+             std::string  aNameIm2
+          )
+{
+    if (aNameIm1 > aNameIm2) ElSwap(aNameIm1,aNameIm2);
+    cInterfChantierNameManipulateur * anICNM = cInterfChantierNameManipulateur::BasicAlloc(aDir);
+
+    std::string aNameCam1 =  anICNM->Assoc1To1("NKS-Assoc-Im2Orient@-"+aNameOri,aNameIm1,true);
+    std::string aNameCam2 =  anICNM->Assoc1To1("NKS-Assoc-Im2Orient@-"+aNameOri,aNameIm2,true);
+
+    CamStenope * aCam1 = CamStenope::StdCamFromFile(true,aNameCam1,anICNM);
+    CamStenope * aCam2 = CamStenope::StdCamFromFile(true,aNameCam2,anICNM);
+    return new cCpleEpip (aDir,1,*aCam1,aNameIm1,*aCam2,aNameIm2);
+
+}
+
+
+int CreateEpip_main(int argc,char ** argv)
+{
+    Tiff_Im::SetDefTileFile(50000);
+    std::string aDir= ELISE_Current_DIR;
+    std::string aName1;
+    std::string aName2;
+    std::string anOri;
+    double  aScale=1.0;
+
+    bool Gray = true;
+    bool Cons16B = true;
+    bool InParal = true;
+    bool DoIm = true;
+    std::string aNameHom;
+    int  aDegre = -1;
+
+
+    ElInitArgMain
+    (
+    argc,argv,
+    LArgMain()  << EAMC(aName1,"Name first image", eSAM_IsExistFile)
+                << EAMC(aName2,"Name second image", eSAM_IsExistFile)
+                << EAMC(anOri,"Name orientation", eSAM_IsExistDirOri),
+    LArgMain()  << EAM(aScale,"Scale",true)
+                << EAM(aDir,"Dir",true,"directory (Def=current)", eSAM_IsDir)
+                    << EAM(Gray,"Gray",true,"One channel gray level image (Def=true)", eSAM_IsBool)
+                    << EAM(Cons16B,"16B",true,"Maintain 16 Bits images if avalaible (Def=true)", eSAM_IsBool)
+                    << EAM(InParal,"InParal",true,"Compute in parallel (Def=true)", eSAM_IsBool)
+                    << EAM(DoIm,"DoIm",true,"Compute image (def=true !!)", eSAM_IsBool)
+                    << EAM(aNameHom,"NameH",false,"Extension to compute Hom point in epi coord (def=none)")
+                    << EAM(aDegre,"Degre",true,"Degre of polynom to correct epi (def=1-, ,2,3)")
+    );
+
+    if (!MMVisualMode)
+    {
+    if (aName1 > aName2) ElSwap(aName1,aName2);
+
+    int aNbChan = Gray ? 1 : - 1;
+
+    cTplValGesInit<std::string>  aTplFCND;
+    cInterfChantierNameManipulateur * anICNM = cInterfChantierNameManipulateur::StdAlloc
+                                               (
+                                                   argc,argv,
+                                                   aDir,
+                                                   aTplFCND
+                                               );
+     anICNM->CorrecNameOrient(anOri);
+
+
+    std::string   aKey =  + "NKS-Assoc-Im2Orient@-" + anOri;
+
+     std::string aNameOr1 = anICNM->Assoc1To1(aKey,aName1,true);
+     std::string aNameOr2 = anICNM->Assoc1To1(aKey,aName2,true);
+
+     // std::cout << "RREEEEEEEEEEEEEEead cam \n";
+     CamStenope * aCam1 = CamStenope::StdCamFromFile(true,aNameOr1,anICNM);
+     // std::cout << "EPISZPPPpp " << aCam1->SzPixel() << "\n";
+
+     CamStenope * aCam2 = CamStenope::StdCamFromFile(true,aNameOr2,anICNM);
+
+     Tiff_Im aTif1 = Tiff_Im::StdConvGen(aDir+aName1,aNbChan,Cons16B);
+     Tiff_Im aTif2 = Tiff_Im::StdConvGen(aDir+aName2,aNbChan,Cons16B);
+
+
+
+      // aCam1->SetSz(aTif1.sz(),true);
+      // aCam2->SetSz(aTif2.sz(),true);
+
+// for (int aK=0; aK<13 ; aK++) std::cout << "SSSssssssssssssssssssiize !!!!\n"; getchar();
+
+  //  Test commit
+
+
+     cCpleEpip aCplE
+               (
+                    aDir,
+                    aScale,
+                    *aCam1,aName1,
+                    *aCam2,aName2
+               );
+
+     const char * aCarHom = 0;
+     if (EAMIsInit(&aNameHom))
+        aCarHom = aNameHom.c_str();
+
+     std::cout << "TimeEpi-0 \n";
+     ElTimer aChrono;
+     aCplE.ImEpip(aTif1,aNameOr1,true,InParal,DoIm,aCarHom,aDegre);
+     std::cout << "TimeEpi-1 " << aChrono.uval() << "\n";
+     aCplE.ImEpip(aTif2,aNameOr2,false,InParal,DoIm,aCarHom,aDegre);
+     std::cout << "TimeEpi-2 " << aChrono.uval() << "\n";
+
+     aCplE.SetNameLock("End");
+     aCplE.LockMess("End cCpleEpip::ImEpip");
+
+
+     return EXIT_SUCCESS;
+
+    }
+    else return EXIT_SUCCESS;
+}
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant �  la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  �  l'utilisation,  �  la modification et/ou au
+développement et �  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe �
+manipuler et qui le réserve donc �  des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
+logiciel �  leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
