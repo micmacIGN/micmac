313e20b9685fc29f5db116fa59b7e3c784ab1947
gmaillet <gmaillet@users.noreply.github.com>
2014-07-01 11:28:15 +0200

    version corrigee par Benj

---
+++ src/TpMMPD/Sources.cmake
@@ -0,0 +1,17 @@
+set(Src_TD_PPMD
+	${TDPPMD_DIR}/cTD_Camera.cpp
+	${TDPPMD_DIR}/cTD_SetAppuis.cpp
+	${TDPPMD_DIR}/TD_Exemple.cpp
+	${TDPPMD_DIR}/cTD_Im.cpp
+	${TDPPMD_DIR}/ExoMM_CorrelMulImage.cpp
+	${TDPPMD_DIR}/Match1.cpp
+	${TDPPMD_DIR}/ExoMM_SimulTieP.cpp
+	${TDPPMD_DIR}/RefineModel.cpp
+)
+
+#SOURCE_GROUP(Util FILES ${Util_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${Src_TD_PPMD}
+)
