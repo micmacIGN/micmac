6047864b76b65010e16826bcd7b778a335f789f8
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2014-07-21 18:04:41 +0200

    GpGpu : checkdependencies

--- src/uti_phgrm/Sources.cmake
+++ src/uti_phgrm/Sources.cmake
@@ -24,27 +24,32 @@ INCLUDE (${UTI_PHGRM_FUSION_NUAGES}/Sources.cmake)
 INCLUDE (${UTI_PHGRM_CASA_DIR}/Sources.cmake)
 
 #define __CUDA_API_VERSION 0x5050
+
 if(${CUDA_ENABLED})
-    set(OptionCuda 1)
-    if("${CUDA_VERSION}" MATCHES "6.0")
-        set(__CUDA_API_VERSION 0x6000)
-    elseif("${CUDA_VERSION}" MATCHES "5.5")
-        set(__CUDA_API_VERSION 0x5050)
-    elseif("${CUDA_VERSION}" MATCHES "5.0")
-        set(__CUDA_API_VERSION 0x5000)
-    elseif("${CUDA_VERSION}" MATCHES "4.0")
-        set(__CUDA_API_VERSION 0x4000)
-	elseif("${CUDA_VERSION}" MATCHES "3.2")
-        set(__CUDA_API_VERSION 0x3020)
-    elseif("${CUDA_VERSION}" MATCHES "3.0")
-        set(__CUDA_API_VERSION 0x3000)
-    endif()
+        set(OptionCuda 1)
+
+#        if("${CUDA_VERSION}" MATCHES "6.0")
+#            set(__CUDA_API_VERSION 0x6000)
+#        elseif("${CUDA_VERSION}" MATCHES "5.5")
+#            set(__CUDA_API_VERSION 0x5050)
+#        elseif("${CUDA_VERSION}" MATCHES "5.0")
+#            set(__CUDA_API_VERSION 0x5000)
+#        elseif("${CUDA_VERSION}" MATCHES "4.0")
+#            set(__CUDA_API_VERSION 0x4000)
+#            elseif("${CUDA_VERSION}" MATCHES "3.2")
+#            set(__CUDA_API_VERSION 0x3020)
+#        elseif("${CUDA_VERSION}" MATCHES "3.0")
+#            set(__CUDA_API_VERSION 0x3000)
+#        endif()
 
     INCLUDE (${UTI_PHGRM_GPGPU_DIR}/Sources.cmake)
 else()
     set(OptionCuda 0)
-    set(__CUDA_API_VERSION 0x0000)
+
+#    set(__CUDA_API_VERSION 0x0000)
+
 endif()
+
 
 if(${WITH_OPEN_MP})
     set(OPM_ENABLED 1)
@@ -56,6 +61,7 @@ configure_file(
     ${UTI_PHGRM_GPGPU_DIR}/GpGpu_BuildOptions.h.in
     ${PROJECT_SOURCE_DIR}/include/GpGpu/GpGpu_BuildOptions.h
 )
+
 
 set( Applis_phgrm_Src_Files
     ${UTI_PHGRM_DIR}/CPP_ChamVec3D.cpp
