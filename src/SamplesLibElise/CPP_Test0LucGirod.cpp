33d5fe77cc406d01994bd735de205fbd38c36aa6
luc-girod <luc-girod@localhost>
2014-08-04 13:05:42 +0200

    Apero2MeshLab (by Luca Penasa)

--- src/SamplesLibElise/CPP_Test0LucGirod.cpp
+++ src/SamplesLibElise/CPP_Test0LucGirod.cpp
@@ -257,7 +257,7 @@ int  Luc_main_corner_crop(int argc,char ** argv){
 	return 0;
 }
 
-int Luc_main(int argc,char ** argv)
+int Luc_main_XAlign(int argc,char ** argv)
 {
 	//MMD_InitArgcArgv(argc,argv,3);
 
@@ -340,6 +340,381 @@ int Luc_main(int argc,char ** argv)
 #endif
 
 
+class SpatioTempImage
+{
+public:
+	SpatioTempImage(){}
+	~SpatioTempImage(){}
+	Im2D_REAL4 RChan;
+	Im2D_REAL4 GChan;
+	Im2D_REAL4 BChan;
+	CamStenope *aCamera;
+	Pt2di SZ;
+};
+
+
+int FindMaltEtape(int ResolModel, std::string aNameIm, std::string aPatModel)
+{
+	//Getting full image size
+	Tiff_Im aTFforSize = Tiff_Im::StdConvGen(aNameIm, 1, false);
+	int aSzX = aTFforSize.sz().x;
+
+	std::string aDir, aPat;
+	SplitDirAndFile(aDir, aPat, aPatModel);
+
+	cInterfChantierNameManipulateur * aICNM = cInterfChantierNameManipulateur::BasicAlloc(aDir);
+	const std::vector<std::string> * aSetModel = aICNM->Get(aPat);
+
+	std::vector<std::string> aVectModel = *aSetModel;
+	int nbModel = aVectModel.size();
+
+	int aEtape = 0;
+	for (int i = 0; i<nbModel; i++)
+	{
+		cElNuage3DMaille * info3D = cElNuage3DMaille::FromFileIm(aDir + aVectModel[i]);
+		//cout<<info3D->SzGeom()<<endl;
+		int ResolThisFile = float(aSzX) / float(info3D->SzGeom().x) + 0.5;
+		//cout<<"ResolThisFile : "<<ResolThisFile<<endl;
+		if (ResolModel == ResolThisFile){ aEtape = i + 1; }
+	}
+	cout << "MicMac step to be used = num" << aEtape << endl;
+
+	return aEtape;
+}
+
+
+vector<SpatioTempImage> LoadGrpImages(string aDir, std::string aPatIm, std::string aOri)
+{
+	cInterfChantierNameManipulateur * aICNM = cInterfChantierNameManipulateur::BasicAlloc(aDir);
+	const std::vector<std::string> * aSetIm = aICNM->Get(aPatIm);
+	std::vector<std::string> aVectIm = *aSetIm;
+	int nbIm = aVectIm.size();
+
+	vector<SpatioTempImage> aGrIm;
+
+	for (int aK1 = 0; aK1<int(nbIm); aK1++)
+	{
+		SpatioTempImage aIm;
+
+		//Loading the camera
+		string aNameCam = "Ori-" + aOri + "/Orientation-" + aVectIm[aK1] + ".xml";
+		CamStenope * aCam = CamOrientGenFromFile(aNameCam, aICNM);
+		aIm.aCamera = aCam;
+		
+		//Loading the image
+		Tiff_Im aTF1 = Tiff_Im::StdConvGen(aDir + aVectIm[aK1], 3, false);
+		Pt2di aSz = aTF1.sz();
+		Im2D_REAL4  aIm1R(aSz.x, aSz.y);
+		Im2D_REAL4  aIm1G(aSz.x, aSz.y);
+		Im2D_REAL4  aIm1B(aSz.x, aSz.y);
+		ELISE_COPY
+			(
+			aTF1.all_pts(),
+			aTF1.in(),
+			Virgule(aIm1R.out(), aIm1G.out(), aIm1B.out())
+			);
+		
+		aIm.RChan = aIm1R;
+		aIm.GChan = aIm1G;
+		aIm.BChan = aIm1B;
+		aIm.SZ = aSz;
+		aGrIm.push_back(aIm);
+	}
+
+	return aGrIm;
+}
+
+Im2D_U_INT1 Window_Maker(SpatioTempImage aIm, Pt2di aCtrPt, int aSzW)
+{
+	//cout << "Creating Window for " << aCtrPt << endl;
+	Im2D_U_INT1 aWindow(1 + 2 * aSzW, 1 + 2 * aSzW, 0.0);
+	float aWSum = 0;
+	for (int i = -aSzW; i <= aSzW; i++)
+	{
+		vector<float> aLine;
+		for (int j = -aSzW; j <= aSzW; j++)
+		{
+			aWindow.data()[i + aSzW][j + aSzW] = aIm.RChan.data()[aCtrPt.y + j][aCtrPt.x + i];
+			aWSum = aWSum + aIm.RChan.data()[aCtrPt.y + j][aCtrPt.x + i];
+		}
+	}
+	//cout << "Normalization" << endl;
+	//Normalization
+	/*
+	for (int i = 0; i < aWindow.sz().x ; i++)
+	{
+		for (int j = 0; j < aWindow.sz().y; j++)
+		{
+			aWindow.data()[i][j] = aWindow.data()[i][j] / aWSum;
+		}
+	}
+	*/
+	//cout << aWindow << endl;
+	return aWindow;
+}
+
+double Correlator(vector<vector<float> > aWindow1, vector<vector<float> > aWindow2)
+{
+	double aScore=0;
+	int aSz = aWindow1.size();
+	for (int i = 0; i < aSz; i++)
+	{
+		for (int j = 0; j < aSz; j++)
+		{
+			aScore = aScore + abs(aWindow1[i][j] - aWindow2[i][j]);
+		}
+
+	}
+	
+	return aScore;
+}
+/*
+double  Correlator(Im2D_U_INT1 * aWindow1, Im2D_U_INT1 * aWindow2)
+{
+	int aSzW = 2;
+	double aNbW = ElSquare(1 + 2 * aSzW);
+	Symb_FNum aS1(aWindow1->in_proj());
+	Symb_FNum aS2(aWindow2->in_proj());
+
+	Symb_FNum  aSom =
+		rect_som(Virgule(aS1, aS2, aS1*aS2, Square(aS1), Square(aS2)), aSzW);
+
+	Symb_FNum aSum1(aSom.kth_proj(0) / aNbW);
+	Symb_FNum aSum2(aSom.kth_proj(1) / aNbW);
+	Symb_FNum aSum12(aSom.kth_proj(2) / aNbW - aSum1 * aSum2);
+	Symb_FNum aSum11(aSom.kth_proj(3) / aNbW - Square(aSum1));
+	Symb_FNum aSum22(aSom.kth_proj(4) / aNbW - Square(aSum2));
+
+	Fonc_Num aRes = aSum12 / sqrt(Max(1e-5, aSum11*aSum22));
+	double aResComp = aRes.compute(Arg_Fonc_Num_Comp(flxf));
+
+	return aRes;
+}
+
+*/
+
+int Luc_main(int argc, char ** argv)
+{
+	/*
+	std::string aFullPattern, aOri, aNameOut="PointsCordinates.txt";
+	//Reading the arguments
+	ElInitArgMain
+		(
+		argc, argv,
+		LArgMain()  << EAMC(aFullPattern, "Images Pattern 1", eSAM_IsPatFile)
+					<< EAMC(aOri, "Orientation", eSAM_IsPatFile),
+		LArgMain()  << EAM(aNameOut, "Out", true, "Output file (txt)")
+		);
+
+	string aPattern, aNameDir;
+	SplitDirAndFile(aNameDir, aPattern, aFullPattern);
+
+	//Reading input files
+	list<string> ListIm = RegexListFileMatch(aNameDir, aPattern, 1, false);
+	int nbIm = ListIm.size();
+
+	string oriFileName = aNameDir + aNameOut;
+	FILE *f = fopen(oriFileName.c_str(), "w");
+
+	for (int i = 1; i <= nbIm; i++)
+	{
+		//Processing the image
+		string aNameIm = ListIm.front();
+		ListIm.pop_front();
+		string aNameOut = aNameDir + aNameIm + ".tif";
+
+		//Loading the camera
+		string aNameCam = "Ori-" + aOri + "/Orientation-" + aNameIm + ".xml";
+		cInterfChantierNameManipulateur * anICNM = cInterfChantierNameManipulateur::BasicAlloc(aNameDir);
+		CamStenope * aCam = CamOrientGenFromFile(aNameCam, anICNM);
+		cout << aNameIm << " [ " << aCam->VraiOpticalCenter().x << " , " << aCam->VraiOpticalCenter().y << " , " << aCam->VraiOpticalCenter().z << " ]" << endl;
+		fprintf(f, "%s %0.6f %0.6f %0.6f\n", aNameIm, aCam->VraiOpticalCenter().x, aCam->VraiOpticalCenter().y, aCam->VraiOpticalCenter().z);
+
+	}
+	fclose(f);
+	
+	
+	
+	*/
+
+	std::string aFullPattern1, aFullPattern2, aFile3D1, aFile3D2, aOri, aDirOut = "Visualisation/";
+	int aSzW = 1;
+	double aSzMovArea = 5;
+	//Reading the arguments
+	ElInitArgMain
+		(
+		argc, argv,
+		LArgMain()	<< EAMC(aFullPattern1, "Images Pattern 1", eSAM_IsPatFile)
+					<< EAMC(aFullPattern2, "Images Pattern 2", eSAM_IsPatFile)
+					<< EAMC(aFile3D1, "File 3D 1", eSAM_IsPatFile)
+					<< EAMC(aFile3D2, "File 3D 2", eSAM_IsPatFile)
+					<< EAMC(aOri, "Orientation", eSAM_IsPatFile),
+		LArgMain()	<< EAM(aDirOut, "Out", true, "Output folder (end with /) and/or prefix (end with another char)")
+					<< EAM(aSzMovArea, "SzMovArea", true, "Max magnitude of movement in meters (def=5)")
+		);
+
+	//Creating vector of images
+	std::string aDir, aPatIm1, aPatIm2;
+	SplitDirAndFile(aDir, aPatIm1, aFullPattern1);
+	SplitDirAndFile(aDir, aPatIm2, aFullPattern2);
+	cInterfChantierNameManipulateur * aICNM = cInterfChantierNameManipulateur::BasicAlloc(aDir);
+	const std::vector<std::string> * aSetIm1 = aICNM->Get(aPatIm1);
+	const std::vector<std::string> * aSetIm2 = aICNM->Get(aPatIm2);
+	std::vector<std::string> aVectIm1 = *aSetIm1;
+	std::vector<std::string> aVectIm2 = *aSetIm2;
+
+	cout << "Set 1 size : " << aVectIm1.size() << endl;
+	cout << "Set 1 : " << aVectIm1 << endl;
+	cout << "Set 2 size : " << aVectIm2.size() << endl;
+	cout << "Set 2 : " << aVectIm2 << endl;
+
+	//loading 3D models
+	cElNuage3DMaille * info3D1 = cElNuage3DMaille::FromFileIm(aFile3D1);
+	cElNuage3DMaille * info3D2 = cElNuage3DMaille::FromFileIm(aFile3D2);
+	cout << "Sz geom 1: " << info3D1->SzGeom() << endl;
+	cout << "Sz geom 2: " << info3D2->SzGeom() << endl;
+	cout << "Resol geom 1: " << info3D1->ResolSolGlob() << endl;
+	cout << "Resol geom 2: " << info3D2->ResolSolGlob() << endl;
+
+	//Loading images
+	vector<SpatioTempImage> aGrIm1 = LoadGrpImages(aDir, aPatIm1, aOri);
+	vector<SpatioTempImage> aGrIm2 = LoadGrpImages(aDir, aPatIm2, aOri);
+	cout << "Loaded " << aGrIm1.size() << " images for group 1 and " << aGrIm2.size() << " for group 2" << endl;
+
+
+	//Bulding the output file system
+	ELISE_fp::MkDirRec(aDir + aDirOut);
+	std::string aNameOut = "banane.tif";
+	//Reading the image and creating the objects to be manipulated
+	aNameOut = aDir + aDirOut + aNameOut;
+
+	Pt2di aSz( info3D1->SzGeom().x , info3D1->SzGeom().y );
+
+	Tiff_Im aTF = Tiff_Im(aNameOut.c_str(), aSz, GenIm::real4, Tiff_Im::No_Compr, Tiff_Im::BlackIsZero);
+
+	Im2D_REAL4  aIm(aSz.x, aSz.y);
+
+	ELISE_COPY
+		(
+		aTF.all_pts(),
+		aTF.in(),
+		aIm.out()
+		);
+
+	REAL4 ** aData = aIm.data();
+
+	
+	for (int aY = aSzW; aY < aSz.y - aSzW; aY++)
+	{
+		for (int aX = aSzW; aX < aSz.x - aSzW; aX++)
+		{
+			/*******************************
+			HOW TO
+			//transform XY pixel coordinates to terrain coordinates
+			Pt2dr pos2DPtIm1; pos2DPtIm1.x = 10050 + aX*0.14999999999999999*2; pos2DPtIm1.y = 10350 - aY*0.14999999999999999*2;
+			//Go back to pix coordinates
+			pos2DPtIm1 = info3D->Plani2Index(pos2DPtIm1);
+			********************************/
+			Pt2dr pos2DMNT( aX , aY );
+			//Test if there is data
+			if (info3D1->CaptHasData(pos2DMNT)){
+				//Get 3D info at XY
+				Pt3d<double> pos3DMNT = info3D1->PreciseCapteur2Terrain(pos2DMNT);
+				//cout << "pos3DMNT1 = " << pos3DMNT << endl;
+				//Get Im1(i,j)
+				Pt2di pos2DIm1( int(aGrIm1[0].aCamera->Ter2Capteur(pos3DMNT).x) , int(aGrIm1[0].aCamera->Ter2Capteur(pos3DMNT).y) );
+				cout << "pos2DIm1 = " << pos2DIm1 << endl;
+				//Extracting window from Im1
+				Im2D_U_INT1 aWindow1 = Window_Maker(aGrIm1[0], pos2DIm1, 1);
+
+				Fonc_Num aScoreMin = 100;
+				/*
+				for (int bX = pos3DMNT.x - aSzMovArea; bX <= pos3DMNT.x + aSzMovArea; bX++)
+				{
+					for (int bY = pos3DMNT.y - aSzMovArea; bY <= pos3DMNT.y + aSzMovArea; bY++)
+					{
+						Pt2di aPos(int(aGrIm2[0].aCamera->Ter2Capteur(pos3DMNT).x), int(aGrIm2[0].aCamera->Ter2Capteur(pos3DMNT).y));
+						//Extracting window from Im2
+						Im2D_U_INT1 aWindow2 = Window_Maker(aGrIm2[0], aPos, 1);
+						double aScore = Correlator(&aWindow1, &aWindow2);
+						if (aScore < aScoreMin)
+						{
+							//CACACACACACACACCACACACACA TESTER SI PT EXIST DANS INFO3D2=====================================================================================
+							aScoreMin = aScore;
+							Pt2dr aPos2D(bX, bY);
+							Pt3dr aPos3D = info3D2->PreciseCapteur2Terrain(info3D2->Plani2Index(aPos2D));
+							cout << "pos set 1 = " << pos3DMNT << endl;
+							cout << "pos set 2 = " << aPos3D << endl;
+							aData[aY][aX] = square_euclid(pos3DMNT, aPos3D);
+							cout << aData[aY][aX] << endl;
+						}
+					}
+				}
+
+				
+				//Get Im2(i,j)
+				Pt2di pos2DIm2( int(aGrIm2[0].aCamera->Ter2Capteur(pos3DMNT).x) , int(aGrIm2[0].aCamera->Ter2Capteur(pos3DMNT).y) );
+				cout << "pos2DIm2 = " << pos2DIm2 << endl;
+				//Define researsh area in pixels from input info in meters
+				int aSzMovAreaPix = aSzMovArea / aGrIm2[0].aCamera->ResolutionSol();
+				cout <<"Pouet"<<endl;
+				double aScoreMin = 100;
+				for (int bX = pos2DIm2.x - aSzMovAreaPix; bX <= pos2DIm2.x + aSzMovAreaPix; bX++)
+				{
+					for (int bY = pos2DIm2.y - aSzMovAreaPix; bY <= pos2DIm2.y + aSzMovAreaPix; bY++)
+					{
+						Pt2di aPos( bX, bY );
+						//Extracting window from Im2
+						vector<vector<float> > aWindow2 = Window_Maker(aGrIm2[0], aPos, 1);
+						cout << aWindow2 << endl;
+						double aScore = Correlator(aWindow1, aWindow2);
+						cout << aScore << endl;
+						if (aScore < aScoreMin)
+						{
+							aScoreMin = aScore;
+							Pt2dr aPosR(aPos.x, aPos.y);
+							cout << "aPos " << aPos << endl;
+							cout << "aPosR " << aPosR << endl;
+
+							Pt3dr aPosPt3D = aGrIm2[0].aCamera->PreciseCapteur2Terrain(aPosR);
+							cout << aPosPt3D << endl;
+							Pt2dr aPos2DPtIm2 = info3D2->Plani2Index(aPosR);
+							cout << aPos2DPtIm2 << endl;
+							aData[aY][aX] = square_euclid(pos3DMNT, info3D2->PreciseCapteur2Terrain(aPos2DPtIm2));
+							cout << aData[aY][aX] << endl;
+						}
+					}
+				}
+				*/
+
+			}
+			else
+			{
+				aData[aY][aX] = 1;
+				//cout << "not in Masq" << endl;
+			}
+		}
+	}
+
+	Tiff_Im  aTOut
+		(
+		aNameOut.c_str(),
+		aSz,
+		GenIm::real4,
+		Tiff_Im::No_Compr,
+		Tiff_Im::BlackIsZero
+		);
+
+	ELISE_COPY
+		(
+		aTOut.all_pts(),
+		aIm.in(),
+		aTOut.out()
+		);
+	
+	return 0;
+
+}
 
 
 /*Footer-MicMac-eLiSe-25/06/2007
---
+++ src/uti_files/CPP_Apero2Meshlab.cpp
@@ -0,0 +1,474 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+/**********************************************************/
+/*	   Author: Luca Penasa, after Girod's Apero2PMVS 	  */
+/**********************************************************/
+
+#include "StdAfx.h"
+#include <algorithm>
+
+
+void Apero2Meshlab_Banniere()
+{
+  std::cout << "\n  *************************************************\n";
+  std::cout << "  **                                             **\n";
+  std::cout << "  **                   Apero                     **\n";
+  std::cout << "  **                     2                       **\n";
+  std::cout << "  **                 Meshlab                     **\n";
+  std::cout << "  **                                             **\n";
+  std::cout << "  *************************************************\n";
+}
+
+///
+/// \brief The MeshlabRasterProjectWriter class provides a simple way to write
+/// a valid mlp file for meshlab containing raster informations.
+/// The best solution would be to write it using the xml support within micmac
+/// but it seems to be pretty complicated and an hard-coded solution have been used!
+/// For using it remember to call :
+/// MeshlabRasterProjectWriter writer;
+/// writer.setOutputFileName(meshlabProjectFileName);
+/// writer.openFile();
+/// writer.writeHeaderStuff();
+///
+///then for each raster:
+/// writer.writeMLRasterStart(aRawName + ".jpg");
+/// writer.writeVCGCamera(Trans,  NRot, distortions, viewport, pixelsize, principal_point, focal);
+/// writer.writePlaneSemantic(aRawName + ".jpg");
+/// writer.writeMLRasterEnd();
+///
+/// and at the end:
+/// writer.writeFooterStuff();
+/// writer.writeAllToFile();
+///
+///
+class MeshlabRasterProjectWriter
+{
+public:
+
+  //some useful types
+  typedef Pt3d<double> centerT;
+  typedef Pt2d<double> vector2d;
+  typedef Pt2d<int> vector2dInt;
+
+  typedef ElMatrix<double> rotationT;
+
+  ///
+  /// \brief MeshlabProjectWriter def constructor
+  ///
+  MeshlabRasterProjectWriter(){}
+
+  void openFile()
+  {
+    file_.open(filename_.c_str());
+  }
+
+  void setOutputFileName(std::string filename)
+  {
+    filename_ = filename;
+  }
+
+  void writeHeaderStuff()
+  {
+    writeMeshlabDocumentHeader();
+    writerMeshlabProjectStart();
+    writeRasterGroupStart();
+  }
+
+  void writeMLRasterStart(const std::string filename_and_path)
+  {
+    std::string text = std::string("<MLRaster label=\"" ) + filename_and_path + std::string("\">");
+    writeString(text.c_str());
+  }
+
+  void writeVCGCamera(centerT center, rotationT rotation, vector2d distortions, vector2dInt viewportPX, vector2d pixelsizeMM, vector2d principal_pointPX, double focalMM)
+  {
+    stringstream s;
+
+    s << "<VCGCamera TranslationVector=\"" << center.x << " " << center.y << " " << center.z << " 1\"" ;
+    s << " LensDistortion=\"" << distortions.x << " " << distortions.y << "\"";
+    s << " ViewportPx=\"" << viewportPX.x << " " << viewportPX.y << "\"";
+    s << " PixelSizeMm=\"" << pixelsizeMM.x << " " << pixelsizeMM.y << "\"" ;
+    s << " CenterPx=\"" << principal_pointPX.x << " " << principal_pointPX.y << "\"" ;
+    s << " FocalMm=\"" << focalMM << "\"" ;
+    s << " RotationMatrix=\"";
+    for (int i = 0; i < rotation.tx(); ++i)
+      {
+        for (int j=0; j < rotation.ty(); ++j)
+          {
+            s << rotation(i, j) << " ";
+          }
+        s << 0.0 << " ";
+      }
+
+    s << 0.0 << " " <<   0.0 << " " << 0.0 << " " << 1.0;
+
+    s << "\"/>";
+
+    writeString(s.str().c_str());
+  }
+
+  void writePlaneSemantic(std::string filename, std::string semantic= "rgb")
+  {
+    std::string s = std::string("<Plane semantic=\"") + semantic + "\" " + "fileName=\"" + filename + "\"/>";
+
+    writeString(s.c_str());
+  }
+
+  void writeMLRasterEnd()
+  {
+    writeString("</MLRaster>");
+  }
+
+  void writeFooterStuff()
+  {
+    writeRasterGroupEnd();
+    writeMeshlabProjectEnd();
+  }
+
+  void writeAllToFile()
+  {
+    file_ << stream_.str().c_str();
+    file_.close();
+  }
+
+
+private:
+
+  void writeMeshlabDocumentHeader()
+  {
+    writeString("<!DOCTYPE MeshLabDocument>") ;
+  }
+
+  void writerMeshlabProjectStart()
+  {
+    writeString("<MeshLabProject>") ;
+  }
+
+  void writeRasterGroupStart()
+  {
+    writeString("<RasterGroup>");
+  }
+
+
+
+  void writeRasterGroupEnd()
+  {
+    writeString("</RasterGroup>");
+  }
+
+  void writeMeshlabProjectEnd()
+  {
+    writeString("</MeshLabProject>");
+  }
+
+
+
+  void writeString(const char * str)
+  {
+    stream_ << str << "\n";
+  }
+
+
+
+private:
+
+  ofstream file_;
+  std::string filename_;
+
+  stringstream stream_;
+
+
+
+};
+
+void Apero2Meshlab(string aFullPattern, string aOri, int aUnDist)
+{
+  string aPattern,aNameDir;
+  SplitDirAndFile(aNameDir,aPattern,aFullPattern);
+
+
+  //Bulding the output file system
+  ELISE_fp::MkDirRec(aNameDir + "meshlabRast-"+ aOri + "/");
+
+  //Reading the list of input files
+  list<string> ListIm=RegexListFileMatch(aNameDir,aPattern,1,false);
+  int nbIm=ListIm.size();
+  cout<<"Images to process: "<<nbIm<<endl;
+
+  string cmdDRUNK,cmdConv, cmdCopyMeta;
+  list<string> ListDrunk,ListConvert, ListCopyMeta, ListSimpleCopy;
+
+  string meshlabProjectFileName=aNameDir + "/meshlabRast-"+ aOri + "/meshlabProj.mlp";
+
+
+  MeshlabRasterProjectWriter writer;
+  writer.setOutputFileName(meshlabProjectFileName);
+  writer.openFile();
+  writer.writeHeaderStuff();
+
+  //prepare the list of command if undistortion is required. If not just write out the right things in the mlp file
+  for(int i=0;i<nbIm;i++)
+    {
+      //Reading the images list
+      string aFullName=ListIm.front();
+
+      int lastindex = aFullName.find_last_of(".");
+      string aRawName = aFullName.substr(0, lastindex);
+
+      cout<<aFullName<<" ("<<i+1<<" of "<<nbIm<<")"<<endl;
+      ListIm.pop_front();
+
+      if (aUnDist)
+        {
+          //Creating the numerical format for the output files names
+          char nb[9];
+          sprintf(nb, "%08d", i);
+
+          //Creating the lists of DRUNK and Convert commands
+          cmdDRUNK=MMDir() + "bin/Drunk " + aNameDir + aFullName + " " + aOri + " Out=" + "meshlabRast-" + aOri + "/ Talk=0";
+          ListDrunk.push_back(cmdDRUNK);
+
+
+#if (ELISE_unix || ELISE_Cygwin || ELISE_MacOs)
+          cmdConv="convert ephemeral:" + aNameDir + "meshlabRast-" + aOri + "/" + aFullName + ".tif " + aNameDir + "meshlabRast-"+ aOri +"/"+ aRawName + ".jpg";
+#endif
+#if (ELISE_windows)
+          cmdConv=MMDir() + "binaire-aux/convert ephemeral:" + aNameDir + "meshlabRast-" + aOri + "/" + aFullName + ".tif " + aNameDir + "meshlabRast-"+ aOri +"/"+ aRawName + ".jpg";
+#endif
+          ListConvert.push_back(cmdConv);
+
+          //also copy the metadata from the original
+#if (ELISE_unix || ELISE_Cygwin || ELISE_MacOs)
+          cmdCopyMeta = "exiftool -overwrite_original -TagsFromFile " + aNameDir + aFullName + " " + aNameDir + "meshlabRast-"+ aOri +"/"+ aRawName + ".jpg";
+#endif
+#if (ELISE_windows)
+          cmdCopyMeta =MMDir() + "binaire-aux/exiftool -overwrite_original -TagsFromFile " + aNameDir + aFullName + " " + aNameDir + "meshlabRast-"+ aOri +"/"+ aRawName + ".jpg";
+#endif
+
+          ListCopyMeta.push_back(cmdCopyMeta);
+
+        }
+
+
+      //Formating the camera name
+      string aNameCam="Ori-"+aOri+"/Orientation-"+aFullName+".xml";
+
+      //Loading the camera
+      cInterfChantierNameManipulateur * anICNM = cInterfChantierNameManipulateur::BasicAlloc(aNameDir);
+      CamStenope * aCS = CamOrientGenFromFile(aNameCam,anICNM);
+
+      ElCamera * aCam = Cam_Gen_From_File(aNameCam, "OrientationConique" , anICNM);
+
+
+      ElMatrix<double> Rot(3,3,0.0);
+      ElMatrix<double> RotX(3,3,0.0);
+
+      Rot=aCS->Orient().Mat();
+      //rotate the above matrix with this one:
+      RotX(0,0) = 1;
+      RotX(1,1) = -1;
+      RotX(2,2) = -1;
+      ElMatrix<double> NRot = RotX * Rot;
+      NRot = NRot.transpose();
+
+      //get the translation - this is ok as it is!  just get the negatives
+      Pt3d<double> Trans;
+      Trans= -aCS->OrigineProf();
+
+      //Loading EXIF data, to get FocMm and Calculate PixelSizeMm
+      cMetaDataPhoto aMetaData = cMetaDataPhoto::CreateExiv2(aFullName);
+      double rap35MmThisFrame = aMetaData.Foc35()/aMetaData.FocMm();
+      double PxSize35Mm = euclid(Pt2di(24,36))/euclid(aMetaData.TifSzIm());
+      double PxSizeMm = PxSize35Mm / rap35MmThisFrame;
+
+
+      //the focal
+      double focal = aCS->Focale()*PxSizeMm;
+
+      //and the other internal parameters
+      Pt2d<double> distortions, pixelsize, principal_point;
+      Pt2di viewport;
+      viewport = aCS->Sz();
+
+      pixelsize.x = PxSizeMm; pixelsize.y = PxSizeMm;
+
+      if (aUnDist)
+        {
+          distortions.x = 0.0 ; distortions.y = 0.0; //images have been undistorted!
+          principal_point = viewport / 2.0;
+        }
+      else
+        {
+
+#if (ELISE_unix || ELISE_Cygwin || ELISE_MacOs)
+          cmdConv="convert " +  aNameDir + aFullName  + " " + aNameDir + "meshlabRast-"+ aOri +"/"+ aRawName + ".jpg";
+#endif
+#if (ELISE_windows)
+          cmdConv=MMDir() + "binaire-aux/convert "  +  aNameDir + aFullName  + " " + aNameDir + "meshlabRast-"+ aOri +"/"+ aRawName + ".jpg";
+#endif
+          ListConvert.push_back(cmdConv);
+
+          NS_ParamChantierPhotogram::cCalibrationInternConique cal_xml = aCam->ExportCalibInterne2XmlStruct(aCam->Sz());
+
+          vector<cCalibDistortion> dists = cal_xml.CalibDistortion();
+
+          cCalibDistortion dist = dists.at(0); //get the first one
+          cTplValGesInit<cCalibrationInterneRadiale> qua = dist.ModRad();
+
+          cCalibrationInterneRadiale rad = qua.Val();
+          vector<double> coeffs = rad.CoeffDist();
+
+          distortions.x = coeffs.at(0);
+          distortions.y = coeffs.at(1);
+
+          //in principle this will be the principal point of the camera
+          principal_point = cal_xml.PP();
+
+          //BUT it seems that is causing problems when importing in meshlab.
+
+          principal_point = viewport / 2.0;
+
+
+        }
+
+
+
+
+      writer.writeMLRasterStart(aRawName + ".jpg");
+      writer.writeVCGCamera(Trans,  NRot, distortions, viewport, pixelsize, principal_point, focal);
+      if (aUnDist)
+        writer.writePlaneSemantic(aRawName + ".jpg");
+      else
+        writer.writePlaneSemantic(aRawName + ".jpg");
+
+      writer.writeMLRasterEnd();
+
+
+
+    }//end of "for each image"
+
+  writer.writeFooterStuff();
+
+  writer.writeAllToFile();
+
+
+  if (aUnDist)
+    {
+      //Undistorting the images with Drunk
+      cout<<"Undistorting the imagaRawNamees with Drunk"<<endl;
+      cEl_GPAO::DoComInParal(ListDrunk,aNameDir + "MkDrunk");
+
+      //Converting into .jpg
+      cout<<"Converting into .jpg"<<endl;
+      cEl_GPAO::DoComInParal(ListConvert,aNameDir + "MkConvert");
+
+
+      //Copy the metadata - it is better to keep metadatas sync
+      cout<<"Copying metadata"<<endl;
+      cEl_GPAO::DoComInParal(ListCopyMeta,aNameDir + "MkCopyMetadata");
+    }
+  else
+    {
+      cout<<"Converting into .jpg"<<endl;
+      cEl_GPAO::DoComInParal(ListConvert,aNameDir + "MkConvert");
+    }
+
+
+  Apero2Meshlab_Banniere();
+}
+
+
+int Apero2Meshlab_main(int argc,char ** argv)
+{
+  MMD_InitArgcArgv(argc,argv);
+
+  //Reading the arguments
+  string aFullPattern,aOri;
+  bool aUnDist = false;
+
+  ElInitArgMain
+      (
+        argc,argv,
+        LArgMain()  << EAMC(aFullPattern,"Images' name pattern")
+        << EAMC(aOri,"Orientation name"),
+        LArgMain() << EAM(aUnDist,"UnDist", true, "Undistort images using Drunk")
+        );
+
+  Apero2Meshlab(aFullPattern, aOri, aUnDist);
+
+  return EXIT_SUCCESS;
+}
+
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
