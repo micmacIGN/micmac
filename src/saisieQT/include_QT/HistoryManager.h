f9931a48d9bac5ed177469dc743279728cd2f07c
luc-girod <luc-girod@localhost>
2014-08-05 11:19:03 +0200

    Apero2MeshLab - contribution credit to Luca Penasa

---
+++ src/saisieQT/include_QT/HistoryManager.h
@@ -0,0 +1,63 @@
+#ifndef _HISTORYMANAGER_H
+#define _HISTORYMANAGER_H
+
+#include "Elise_QT.h"
+#include <iostream>
+
+
+
+struct selectInfos
+{
+    selectInfos(){}
+    selectInfos(QVector <QPointF> pol,int mode)
+    {
+        poly = pol;
+        selection_mode = mode;
+    }
+    //! polyline infos
+    QVector <QPointF> poly;
+
+    //! selection mode
+    int         selection_mode;
+
+    GLdouble    mvmatrix[16];
+    GLdouble    projmatrix[16];
+    GLint       glViewport[4];
+};
+
+class HistoryManager
+{
+public:
+
+    HistoryManager();
+
+    void    push_back(selectInfos &infos);
+
+    //! Get the selection infos stack
+    QVector <selectInfos> getSelectInfos(){ return _infos; }
+
+    void   setFilename(QString name){ _filename = name; }
+
+    int    getActionIdx(){ return _actionIdx; }
+
+    int    size() { return _infos.size(); }
+
+    void   undo() { if (_actionIdx > 0)  _actionIdx--; }
+
+    void   redo() { if (_actionIdx < _infos.size()) _actionIdx++; }
+
+    void   reset(){ _actionIdx = 0; _infos.clear(); }
+
+    void   save();
+
+private:
+    //! selection infos stack
+    QVector <selectInfos> _infos;
+
+    //! current action index
+    int        _actionIdx;
+
+    QString    _filename;
+};
+
+#endif
---
+++ src/uti_phgrm/Sources.cmake
@@ -0,0 +1,146 @@
+set(UTI_PHGRM_APERO_DIR ${UTI_PHGRM_DIR}/Apero)
+set(UTI_PHGRM_MICMAC_DIR ${UTI_PHGRM_DIR}/MICMAC)
+set(UTI_PHGRM_MAXFLOW_DIR ${UTI_PHGRM_DIR}/MaxFlow)
+set(UTI_PHGRM_REDUCHOM_DIR ${UTI_PHGRM_DIR}/ReducHom)
+set(UTI_PHGRM_RHH_DIR ${UTI_PHGRM_DIR}/RHH)
+set(UTI_PHGRM_CASA_DIR ${UTI_PHGRM_DIR}/CASA)
+
+set(UTI_PHGRM_PORTO_DIR ${UTI_PHGRM_DIR}/Porto)
+set(UTI_PHGRM_SAISIEPTS_DIR ${UTI_PHGRM_DIR}/SaisiePts)
+set(UTI_PHGRM_GPGPU_DIR ${UTI_PHGRM_DIR}/GpGpu)
+
+set(UTI_PHGRM_FUSION_NUAGES ${UTI_PHGRM_DIR}/FusionNuage)
+
+set(SrcGrp_Uti_PHGRM uti_phgrm)
+
+INCLUDE (${UTI_PHGRM_APERO_DIR}/Sources.cmake)
+INCLUDE (${UTI_PHGRM_MICMAC_DIR}/Sources.cmake)
+INCLUDE (${UTI_PHGRM_MAXFLOW_DIR}/Sources.cmake)
+INCLUDE (${UTI_PHGRM_REDUCHOM_DIR}/Sources.cmake)
+INCLUDE (${UTI_PHGRM_RHH_DIR}/Sources.cmake)
+INCLUDE (${UTI_PHGRM_PORTO_DIR}/Sources.cmake)
+INCLUDE (${UTI_PHGRM_SAISIEPTS_DIR}/Sources.cmake)
+INCLUDE (${UTI_PHGRM_FUSION_NUAGES}/Sources.cmake)
+INCLUDE (${UTI_PHGRM_CASA_DIR}/Sources.cmake)
+
+#define __CUDA_API_VERSION 0x5050
+
+if(${CUDA_ENABLED})
+        set(OptionCuda 1)
+
+#        if("${CUDA_VERSION}" MATCHES "6.0")
+#            set(__CUDA_API_VERSION 0x6000)
+#        elseif("${CUDA_VERSION}" MATCHES "5.5")
+#            set(__CUDA_API_VERSION 0x5050)
+#        elseif("${CUDA_VERSION}" MATCHES "5.0")
+#            set(__CUDA_API_VERSION 0x5000)
+#        elseif("${CUDA_VERSION}" MATCHES "4.0")
+#            set(__CUDA_API_VERSION 0x4000)
+#            elseif("${CUDA_VERSION}" MATCHES "3.2")
+#            set(__CUDA_API_VERSION 0x3020)
+#        elseif("${CUDA_VERSION}" MATCHES "3.0")
+#            set(__CUDA_API_VERSION 0x3000)
+#        endif()
+
+    INCLUDE (${UTI_PHGRM_GPGPU_DIR}/Sources.cmake)
+else()
+    set(OptionCuda 0)
+
+#    set(__CUDA_API_VERSION 0x0000)
+
+endif()
+
+
+if(${WITH_OPEN_MP})
+    set(OPM_ENABLED 1)
+else()
+    set(OPM_ENABLED 0)
+endif()
+
+configure_file(
+    ${UTI_PHGRM_GPGPU_DIR}/GpGpu_BuildOptions.h.in
+    ${PROJECT_SOURCE_DIR}/include/GpGpu/GpGpu_BuildOptions.h
+)
+
+
+set( Applis_phgrm_Src_Files
+    ${UTI_PHGRM_DIR}/CPP_ChamVec3D.cpp
+    ${UTI_PHGRM_DIR}/CPP_NuageBascule.cpp
+    ${UTI_PHGRM_DIR}/CPP_ReechInvEpip.cpp
+    ${UTI_PHGRM_DIR}/CPP_CreateEpip.cpp
+    ${UTI_PHGRM_DIR}/CPP_VideoVisage.cpp
+    ${UTI_PHGRM_DIR}/CPP_SEL.cpp
+    ${UTI_PHGRM_DIR}/CPP_AperiCloud.cpp
+    ${UTI_PHGRM_DIR}/CPP_AperoChImMM.cpp
+    ${UTI_PHGRM_DIR}/CPP_MMPyram.cpp
+    ${UTI_PHGRM_DIR}/CPP_MMModelInitial.cpp
+    ${UTI_PHGRM_DIR}/CPP_MMAllAuto.cpp
+    ${UTI_PHGRM_DIR}/CPP_MM2DPostSism.cpp
+    ${UTI_PHGRM_DIR}/CPP_Apero.cpp
+    ${UTI_PHGRM_DIR}/CPP_SaisiePts.cpp
+    ${UTI_PHGRM_DIR}/CPP_Bascule.cpp
+    ${UTI_PHGRM_DIR}/CPP_Campari.cpp
+    ${UTI_PHGRM_DIR}/CPP_MMTestOrient.cpp
+    ${UTI_PHGRM_DIR}/CPP_MMHomCorOri.cpp
+    ${UTI_PHGRM_DIR}/CPP_CmpCalib.cpp
+    ${UTI_PHGRM_DIR}/CPP_Gri2Bin.cpp
+    ${UTI_PHGRM_DIR}/CPP_GCPBascule.cpp
+    ${UTI_PHGRM_DIR}/CPP_CenterBascule.cpp
+    ${UTI_PHGRM_DIR}/CPP_MakeGrid.cpp
+    ${UTI_PHGRM_DIR}/CPP_Malt.cpp
+    ${UTI_PHGRM_DIR}/CPP_MMByPair.cpp
+    ${UTI_PHGRM_DIR}/CPP_Mascarpone.cpp
+    ${UTI_PHGRM_DIR}/CPP_MergePly.cpp
+    ${UTI_PHGRM_DIR}/CPP_MICMAC.cpp
+    ${UTI_PHGRM_DIR}/CPP_Nuage2Ply.cpp
+    ${UTI_PHGRM_DIR}/CPP_Pasta.cpp
+    ${UTI_PHGRM_DIR}/CPP_Pastis.cpp
+    ${UTI_PHGRM_DIR}/CPP_Porto.cpp
+    ${UTI_PHGRM_DIR}/CPP_ReducHom.cpp
+    ${UTI_PHGRM_DIR}/CPP_RepLocBascule.cpp
+    ${UTI_PHGRM_DIR}/CPP_ScaleNuage.cpp
+    ${UTI_PHGRM_DIR}/CPP_SBGlobBascule.cpp
+    ${UTI_PHGRM_DIR}/CPP_Tapas.cpp
+    ${UTI_PHGRM_DIR}/CPP_NewTapas.cpp
+    ${UTI_PHGRM_DIR}/CPP_Tapioca.cpp
+    ${UTI_PHGRM_DIR}/CPP_Tarama.cpp
+    ${UTI_PHGRM_DIR}/CPP_Tawny.cpp
+    ${UTI_PHGRM_DIR}/CPP_TestCam.cpp
+    ${UTI_PHGRM_DIR}/CPP_TestChantier.cpp
+    ${UTI_PHGRM_DIR}/CPP_SaisieMasq.cpp
+    ${UTI_PHGRM_DIR}/CPP_SaisieQT.cpp
+    ${UTI_PHGRM_DIR}/CPP_SaisieAppuisPredic.cpp
+    ${UTI_PHGRM_DIR}/CPP_SaisieAppuisInit.cpp
+    ${UTI_PHGRM_DIR}/CPP_SaisieBasc.cpp
+    ${UTI_PHGRM_DIR}/CPP_SysCoordPolyn.cpp
+    ${UTI_PHGRM_DIR}/CPP_ChgSysCo.cpp
+    ${UTI_PHGRM_DIR}/CPP_XYZ2Im.cpp
+    ${UTI_PHGRM_DIR}/CPP_GrapheHom.cpp
+    ${UTI_PHGRM_DIR}/CPP_MMOnePair.cpp
+    ${UTI_PHGRM_DIR}/CPP_VisuCoupeEpip.cpp
+    ${UTI_PHGRM_DIR}/CPP_HomFilterMasq.cpp
+    ${UTI_PHGRM_DIR}/CPP_InitCamFromAppuis.cpp
+)
+
+SOURCE_GROUP(${SrcGrp_Uti_PHGRM} FILES ${uti_phgrm_Src_Files})
+SOURCE_GROUP(${SrcGrp_Uti_PHGRM}\\Applis FILES ${Applis_phgrm_Src_Files})
+SOURCE_GROUP(${SrcGrp_Uti_PHGRM}\\Apero FILES ${uti_phgrm_Apero_Src_Files})
+SOURCE_GROUP(${SrcGrp_Uti_PHGRM}\\MicMac FILES ${uti_phgrm_MICMAC_Src_Files})
+SOURCE_GROUP(${SrcGrp_Uti_PHGRM}\\MaxFlow FILES ${uti_phgrm_MaxFlow_Src_Files})
+SOURCE_GROUP(${SrcGrp_Uti_PHGRM}\\Porto FILES ${uti_phgrm_Porto_Src_Files})
+SOURCE_GROUP(${SrcGrp_Uti_PHGRM}\\ReducHom FILES ${uti_phgrm_Porto_Src_Files})
+
+if(${CUDA_ENABLED})
+        SOURCE_GROUP(${SrcGrp_Uti_PHGRM}\\GpGpu FILES ${uti_phgrm_GpGpu_Src_Files})
+        SOURCE_GROUP(${SrcGrp_Uti_PHGRM}\\GpGpu FILES ${GpGpuTools_Src_Files})
+endif()
+
+list( APPEND uti_phgrm_Src_Files ${Applis_phgrm_Src_Files})
+list( APPEND uti_phgrm_Src_Files ${uti_phgrm_Apero_Src_Files})
+list( APPEND uti_phgrm_Src_Files ${uti_phgrm_MICMAC_Src_Files} )
+list( APPEND uti_phgrm_Src_Files ${uti_phgrm_MaxFlow_Src_Files} )
+list( APPEND uti_phgrm_Src_Files ${uti_phgrm_Porto_Src_Files})
+list( APPEND uti_phgrm_Src_Files ${uti_phgrm_ReducHom_Src_Files})
+list( APPEND uti_phgrm_Src_Files ${uti_phgrm_RHH_Src_Files})
+list( APPEND uti_phgrm_Src_Files ${uti_phgrm_Casa_Src_Files})
+list( APPEND Elise_Src_Files ${uti_phgrm_Src_Files})
