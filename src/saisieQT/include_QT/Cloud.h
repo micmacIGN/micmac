8f7031358ffbe6cab38eecf1ee566bd7c44054fd
deseilligny <deseilligny@users.noreply.github.com>
2014-08-20 14:32:49 +0200

    Interface modif pour Basc Non Lineaire

---
+++ src/saisieQT/include_QT/Cloud.h
@@ -0,0 +1,48 @@
+﻿#ifndef __CLOUD__
+#define __CLOUD__
+
+#include <vector>
+#include <string>
+
+#include <fstream>
+#include <iostream>
+
+#include "3DObject.h"
+#include "Elise_QT.h"
+
+class GlVertex : public cObjectGL
+{
+public:
+    GlVertex(Pt3dr pos = Pt3dr(0.f,0.f,0.f), QColor color_default = Qt::white) :
+        cObjectGL(pos, color_default)
+    {}
+    void draw(){}
+};
+
+class GlCloud : public cObjectGL
+{
+public:
+    GlCloud(){}
+    GlCloud(vector<GlVertex> const &);
+
+    static GlCloud* loadPly(string,  int *incre = NULL);
+
+    void    addVertex( const GlVertex & vertex);
+    GlVertex& getVertex( uint );
+    int     size();
+
+    void    clear();
+
+    void    setBufferGl(bool onlyColor=false);
+
+    void    draw();
+
+private:
+    vector<GlVertex> _vertices;
+
+    QGLBuffer   _vertexbuffer;
+    QGLBuffer   _vertexColor;
+};
+
+
+#endif
