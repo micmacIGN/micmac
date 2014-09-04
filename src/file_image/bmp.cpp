4ff906df9db5327368287e11ca0886b2a2813151
jbelvaux <jbelvaux@localhost>
2014-09-04 11:45:37 +0200

    Fusionner

--- src/file_image/bmp.cpp
+++ src/file_image/bmp.cpp
@@ -161,8 +161,10 @@ cout << "sz_header "<< _sz_header << "\n";
      {
           _szx = fp.lsb_read_U_INT2();
           _szy = fp.lsb_read_U_INT2();
-          INT nb_pl = fp.lsb_read_U_INT2(); // nb_plane
-          ASSERT_INTERNAL(nb_pl==1,"incoherenc in bmp file");
+          //INT nb_pl = fp.lsb_read_U_INT2(); // nb_plane
+          //ASSERT_INTERNAL(nb_pl==1,"incoherenc in bmp file");
+
+          ASSERT_INTERNAL(fp.lsb_read_U_INT2()==1,"incoherenc in bmp file");
           _bits_pp = fp.lsb_read_U_INT2();
           _compr = Bmp_Im::no_compr;
 
@@ -671,7 +673,7 @@ Output Bmp_Im::out()
 
 /*Footer-MicMac-eLiSe-25/06/2007
 
-Ce logiciel est un programme informatique servant à la mise en
+Ce logiciel est un programme informatique servant �  la mise en
 correspondances d'images pour la reconstruction du relief.
 
 Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
@@ -687,17 +689,17 @@ seule une responsabilité restreinte pèse sur l'auteur du programme,  le
 titulaire des droits patrimoniaux et les concédants successifs.
 
 A cet égard  l'attention de l'utilisateur est attirée sur les risques
-associés au chargement,  à l'utilisation,  à la modification et/ou au
-développement et à la reproduction du logiciel par l'utilisateur étant
-donné sa spécificité de logiciel libre, qui peut le rendre complexe à
-manipuler et qui le réserve donc à des développeurs et des professionnels
+associés au chargement,  �  l'utilisation,  �  la modification et/ou au
+développement et �  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe � 
+manipuler et qui le réserve donc �  des développeurs et des professionnels
 avertis possédant  des  connaissances  informatiques approfondies.  Les
-utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
-logiciel à leurs besoins dans des conditions permettant d'assurer la
+utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
+logiciel �  leurs besoins dans des conditions permettant d'assurer la
 sécurité de leurs systèmes et ou de leurs données et, plus généralement,
-à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
 
-Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
 pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
 termes.
 Footer-MicMac-eLiSe-25/06/2007*/
