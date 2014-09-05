54df254b5f072a6e1eacc10f971621434fbbcc5a
deseilligny <deseilligny@users.noreply.github.com>
2014-09-05 19:38:27 +0200

    Ajout du bras de levier

---
+++ src/uti_image/MpDcraw/Sources.cmake
@@ -0,0 +1,9 @@
+set(uti_image_MpDcraw_Src_Files
+    ${UTI_IMAGE_MPDCRAW_DIR}/cArgMpDCRaw.cpp
+    ${UTI_IMAGE_MPDCRAW_DIR}/cNChanel.cpp
+    ${UTI_IMAGE_MPDCRAW_DIR}/cOneChanel.cpp
+)
+
+#SOURCE_GROUP(${SGUti_Image}\\MpDcraw  FILES ${uti_image_MpDcraw_Src_Files})
+
+list( APPEND uti_image_Src_Files ${uti_image_MpDcraw_Src_Files} )
