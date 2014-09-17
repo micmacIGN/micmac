938c90449542ecdac8cba0d1452bc63e8fea475e
Ana-Maria Rosu <Ana-Maria Rosu@localhost>
2014-09-17 11:24:24 +0200

    removed old mm3dsat

---
+++ applis/OLD-Apero-OLD/cAA_LevenbergMarkard.cpp
@@ -0,0 +1,195 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+   
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in 
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte 
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+#include "general/all.h"
+
+#include "Apero.h"
+
+#include "ext_stl/numeric.h" 
+
+namespace NS_ParamApero
+{
+
+    /************************************************/
+    /*                                              */
+    /*                                              */
+    /*                                              */
+    /************************************************/
+
+void cAppliApero::AddRappelOnAngle(const cRappelOnAngles & aRAO,double aMult,cStatObs & aSO)
+{
+    const cParamForceRappel & aPFR = aRAO.ParamF();
+     // std::cout << "---------LVM::INC---- " << aPFR.Incertitude() / aMult << "\n";
+    for (int aKP=0;aKP<int(mVecPose.size()) ; aKP++)
+    {
+        cPoseCam & aPC = *(mVecPose[aKP]);
+        if (aRAO.ParamF().PatternNameApply()->Match(aPC.Name()))
+        {
+            const std::vector<double>&  aVI = aPFR.Incertitude();
+            const std::vector<int> & aVA = aRAO.TetaApply();
+            ELISE_ASSERT(aVI.size()<=aVA.size(),"To many Incertitude in RappelOnAngles");
+            double aSomPond = 0.0;
+            for (int aKA=0 ; aKA<int(aVA.size()) ; aKA++)
+            {
+               int aKI = ElMin(aKA,int(aVI.size()-1));
+               aSomPond += aPC.RF().AddRappelOnAngles
+                           (
+                               aPFR.OnCur().ValWithDef(true),
+                               aVA[aKA],
+                               aPFR.Incertitude()[aKI] / aMult,
+                               aSO.AddEq()
+                           );
+            }
+            aSO.AddSEP(aSomPond);
+        }
+    }
+}
+
+void cAppliApero::AddRappelOnCentre(const cRappelOnCentres & aRAC,double aMult,cStatObs & aSO)
+{
+    const cParamForceRappel & aPFR = aRAC.ParamF();
+     // std::cout << "---------LVM::INC---- " << aPFR.Incertitude() / aMult << "\n";
+    for (int aKP=0;aKP<int(mVecPose.size()) ; aKP++)
+    {
+        cPoseCam & aPC = *(mVecPose[aKP]);
+        if (    (aRAC.ParamF().PatternNameApply()->Match(aPC.Name()))
+             && ((! aRAC.OnlyWhenNoCentreInit().Val()) || (!aPC.HasObsCentre()))
+           )
+        {
+            const std::vector<double>&  aVI = aPFR.Incertitude();
+            ELISE_ASSERT(aVI.size()<=3,"Bas size Incertitude in cAppliApero::AddRappelOnCentre");
+            Pt3dr anI;
+            if (aVI.size()==1) anI = Pt3dr(aVI[0],aVI[0],aVI[0]);
+            if (aVI.size()==2) anI = Pt3dr(aVI[0],aVI[0],aVI[1]);
+            if (aVI.size()==3) anI = Pt3dr(aVI[0],aVI[1],aVI[2]);
+            aPC.RF().AddRappelOnCentre(aPFR.OnCur().ValWithDef(true),anI/aMult,aSO.AddEq());
+        }
+    }
+}
+
+void cAppliApero::AddOneLevenbergMarkard
+     (
+        const cSectionLevenbergMarkard * aSLM,
+        double                           aMult,
+        cStatObs & aSO
+     )
+{
+   if (! aSLM) return;
+   if (aMult<=0 ) return;
+   for 
+   (
+        std::list<cRappelOnAngles>::const_iterator itR=aSLM->RappelOnAngles().begin();
+        itR!=aSLM->RappelOnAngles().end();
+        itR++
+   )
+   {
+         AddRappelOnAngle(*itR,aMult,aSO);
+   }
+
+   for 
+   (
+        std::list<cRappelOnCentres>::const_iterator itR=aSLM->RappelOnCentres().begin();
+        itR!=aSLM->RappelOnCentres().end();
+        itR++
+   )
+   {
+         AddRappelOnCentre(*itR,aMult,aSO);
+   }
+
+}
+void cAppliApero::AddLevenbergMarkard(cStatObs & aSO)
+{
+    AddOneLevenbergMarkard(mCurSLMGlob,mMulSLMGlob,aSO);
+    AddOneLevenbergMarkard(mCurSLMEtape,mMulSLMEtape,aSO);
+    AddOneLevenbergMarkard(mCurSLMIter,mMulSLMIter,aSO);
+}
+
+void  cAppliApero::InitLVM
+      (
+           const cSectionLevenbergMarkard*&  aPtr,
+           const cTplValGesInit<cSectionLevenbergMarkard> & anOpt,
+           double & aMult,
+           const cTplValGesInit<double> & aOpMul
+
+       )
+{
+   if (anOpt.IsInit())
+     aPtr = anOpt.PtrVal();
+
+   if (aOpMul.IsInit())
+     aMult = aOpMul.Val();
+}
+
+
+
+
+};
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
---
+++ src/saisieQT/main/saisieQT_main.cpp
@@ -0,0 +1,225 @@
+﻿#include "saisieQT_main.h"
+
+int helpMessage(const QApplication &app, QString text)
+{
+    QString title = QObject::tr("Help for ") + app.applicationName();
+#ifdef WIN32
+    QMessageBox msgBox(QMessageBox::NoIcon, title, text, QMessageBox::Ok);
+    return msgBox.exec();
+#else
+    printf("\n%s\n", title.toStdString().c_str());
+    printf("\n%s", text.toStdString().c_str());
+    return 0;
+#endif
+}
+
+#if ( ( defined WIN32 ) && ( ELISE_QT_VERSION==5 ) )
+class Win32CommandLineConverter
+{
+private:
+    std::unique_ptr<char*[]> argv_;
+    std::vector<std::unique_ptr<char[]>> storage_;
+public:
+    Win32CommandLineConverter()
+    {
+        LPWSTR cmd_line = GetCommandLineW();
+        int argc;
+        LPWSTR* w_argv = CommandLineToArgvW(cmd_line, &argc);
+        argv_ = std::unique_ptr<char*[]>(new char*[argc]);
+        storage_.reserve(argc);
+        for(int i=0; i<argc; ++i) {
+            storage_.push_back(ConvertWArg(w_argv[i]));
+            argv_[i] = storage_.back().get();
+        }
+        LocalFree(w_argv);
+    }
+    int argc() const
+    {
+        return static_cast<int>(storage_.size());
+    }
+    char** argv() const
+    {
+        return argv_.get();
+    }
+    static std::unique_ptr<char[]> ConvertWArg(LPWSTR w_arg)
+    {
+        int size = WideCharToMultiByte(CP_UTF8, 0, w_arg, -1, nullptr, 0, nullptr, nullptr);
+        std::unique_ptr<char[]> ret(new char[size]);
+        WideCharToMultiByte(CP_UTF8, 0, w_arg, -1, ret.get(), size, nullptr, nullptr);
+        return ret;
+    }
+};
+#endif
+
+
+#if ( ( defined WIN32 ) && ( ELISE_QT_VERSION == 5 ) )
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow)
+{
+    Win32CommandLineConverter cmd_line;
+    int argc = cmd_line.argc();
+    char **argv = cmd_line.argv();
+#else
+int main(int argc, char *argv[])
+{
+#endif
+    
+#if defined(__APPLE__) || defined(__MACH__)
+	//mise a jour des variables d'environnement pour resources du bundle sous MacOSX
+	{       
+        std::string aDir,aPat;
+        SplitDirAndFile(aDir,aPat,argv[0]);
+    
+        std::string libraryPath(aDir);
+        libraryPath+="../../Libraries/plugins";
+        
+        QStringList libraryPaths;
+        libraryPaths<<aDir.c_str();
+        libraryPaths<<libraryPath.c_str();       
+        QCoreApplication::setLibraryPaths(libraryPaths);
+	}
+#endif
+    
+
+    QApplication app(argc, argv);
+
+    //app.setStyle("fusion");
+
+    // QT Modifie le comportement de sscanf !!!!! problematique quand on parse les fichiers XML
+    setlocale(LC_NUMERIC,"C");
+
+    app.setOrganizationName("Culture3D");
+    app.setApplicationName("QT graphical tools");
+
+    setStyleSheet(app);
+
+    QStringList cmdline_args = QCoreApplication::arguments();
+
+    QString cmds = QObject::tr("Allowed commands:") + "\n\n" +
+            QString("SaisieMasqQT\n") +
+            QString("SaisieAppuisInitQT\n") +
+            QString("SaisieAppuisPredicQT\n")+
+            QString("SaisieBascQT\n")+
+            QString("SaisieBoxQT\n\n");
+
+    if (cmdline_args.size() > 1)
+    {
+        for (int i=0; i < cmdline_args.size(); ++i)
+        {
+            QString str = cmdline_args[i];
+#ifdef _DEBUG
+            cout << "\ncommand: " << str.toStdString().c_str()<<endl;
+#endif
+
+            if (!str.contains("SaisieQT"))
+            {
+                if (str.contains("SaisieMasqQT"))
+                    saisieMasqQT_main(app, argc, argv);
+                else if (str.contains("SaisieAppuisInitQT"))
+                    saisieAppuisInitQT_main(app, argc, argv);
+                else if (str.contains("SaisieAppuisPredicQT"))
+                    saisieAppuisPredicQT_main(app, argc, argv);
+                else if (str.contains("SaisieBoxQT"))
+                    saisieBoxQT_main(app, argc, argv);
+                else if (str.contains("SaisieBascQT"))
+                    saisieBascQT_main(app, argc, argv);
+                else
+                {
+                    QString text = str + QObject::tr(" is not a valid command!!!") + "\n\n" + cmds;
+                    helpMessage(app, text);
+
+                    return EXIT_FAILURE;
+                }
+
+                return EXIT_SUCCESS;
+            }
+        }
+    }
+    else
+        helpMessage(app, cmds);
+
+    return EXIT_SUCCESS;
+}
+
+bool checkNamePt(QString text)
+{
+    if (text.contains(".txt") && QFileInfo(text).isAbsolute())
+    {
+        QMessageBox::critical(NULL, QObject::tr("Error"), QObject::tr("Don't use an absolute path for point file name!"));
+        return false;
+    }
+    return true;
+}
+
+QStringList getFilenames(string aDir, string aName)
+{
+    list<string> aNamelist = RegexListFileMatch(aDir, aName, 1, false);
+    QStringList filenames;
+
+    for
+    (
+        list<string>::iterator itS=aNamelist.begin();
+        itS!=aNamelist.end();
+        itS++
+    )
+        filenames.push_back( QString((aDir + *itS).c_str()));
+
+    return filenames;
+}
+
+void loadTranslation(QApplication &app)
+{
+    QSettings settings(QApplication::organizationName(), QApplication::applicationName());
+    settings.beginGroup("Language");
+    int lang = settings.value("lang", 0).toInt();
+    settings.endGroup();
+
+    if(lang>0)
+    {
+        QString sLang = "saisie_";
+        QString path = app.applicationDirPath() + QDir::separator() + "../include/qt/translations";
+
+        //cf Settings.h
+        if (lang == 1)
+            sLang += "fr";
+        else if (lang == 2)
+            sLang += "es";
+        else if (lang == 3)
+            sLang += "cn";
+        else if (lang == 4)
+            sLang += "ar";
+        else if (lang == 5)
+            sLang += "ru";
+
+        sLang += ".qm";
+
+        QTranslator *qtTranslator = new QTranslator(QApplication::instance());
+
+        if ( qtTranslator->load(sLang, path) )
+        {
+            QApplication::instance()->installTranslator(qtTranslator);
+        }
+        else
+        {
+            QMessageBox::critical(NULL, "Error", "Can't load translation file: " + sLang + "\n" +
+                                         "In: " + path);
+        }
+    }
+}
+
+void updateSettings(QSettings &settings, Pt2di aSzWin, Pt2di aNbFen, bool aForceGray)
+{
+    settings.beginGroup("MainWindow");
+    if (aSzWin.x > 0)
+        settings.setValue("size", QSize(aSzWin.x, aSzWin.y));
+    else if (!settings.contains("MainWindow/size"))
+    {
+        settings.setValue("size", QSize(800, 800));
+        aSzWin.x = aSzWin.y = 800;
+    }
+    settings.setValue("NbFen", QPoint(aNbFen.x, aNbFen.y));
+    settings.endGroup();
+
+    settings.beginGroup("Drawing settings");
+    settings.setValue("forceGray",     aForceGray  );
+    settings.endGroup();
+}
