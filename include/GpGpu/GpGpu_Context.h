f7e3e3952a655ff7815d1dadb586360d4e34be44
Matthieu Deveau <mattdeveau@users.noreply.github.com>
2014-10-14 18:04:01 +0200

    RefineModel

---
+++ include/GpGpu/GpGpu_Context.h
@@ -0,0 +1,439 @@
+#ifndef GPGPU_CONTEXT_H
+#define GPGPU_CONTEXT_H
+
+//#include "StdAfx.h"
+#include <list>
+#include <map>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "GpGpu_CommonHeader.h"
+#include "GpGpu_eLiSe.h"
+#include "GpGpu_BuildOptions.h"
+#include "GpGpu_Data.h"
+
+template<class T> class CData;
+
+#ifdef OPENCL_ENABLED
+#ifdef __APPLE__
+#include "OpenCL/opencl.h"
+#else
+#include "CL/cl.h"
+#endif
+#endif
+
+extern void kMultTab();
+
+enum GPGPUCONTEXT
+{
+    CUDA_CONTEXT,
+    OPENCL_CONTEXT
+};
+
+template<int typecontext>
+class vGpuContext
+{
+public:
+    int typeContext()
+    {
+        return typecontext;
+    }
+    static unsigned short       _nbArg;
+    static std::vector<void*>   _kernelArgs;
+};
+
+class cudaContext : public vGpuContext<CUDA_CONTEXT>
+{
+
+};
+
+#ifdef OPENCL_ENABLED
+class openClContext : public vGpuContext<OPENCL_CONTEXT>
+{
+public:
+
+    static cl_context           _contextOpenCL;
+    static cl_command_queue     _commandQueue;
+    static cl_kernel            _kernel;
+};
+#endif
+
+
+template<class context>
+class CGpGpuContext
+{
+public:
+
+    CGpGpuContext(){}
+
+    static void createContext();
+
+    static void deleteContext(){}
+
+    static  void OutputInfoGpuMemory(){}
+
+    static  void check_Cuda(){}
+
+    static  void createKernel(string file,string kernelName){}
+
+    static  void launchKernel(){}
+
+    template< class T, template< class O, class G > class U >
+    static  void addKernelArg( U<T,context> &arg);
+
+    template<class T>
+    static  void addKernelArg(T &arg){}
+
+#ifdef OPENCL_ENABLED
+
+    static cl_context contextOpenCL(){return NULL;}
+
+    static cl_command_queue commandQueue(){return NULL;}
+
+    static cl_kernel kernel(){return NULL;}
+
+    static void errorOpencl(cl_int error,string erName)
+	{
+		if(error ==CL_SUCCESS)
+			printf("Success create %s\n",erName.c_str());
+		else
+			printf("Error create %s = %d\n",erName.c_str(),error);
+	}
+#endif
+
+    static  void* arg(int id)
+    {
+
+        return _sContext._kernelArgs[id];
+    }
+
+    static int typeContext()
+    {
+        return _sContext.typeContext();
+    }
+
+private:
+
+    template<class T>
+    static  void addKernelArgSDK( CData<T> &arg){}
+
+    static  context             _sContext;
+};
+
+
+template<class context>
+void CGpGpuContext<context>::createContext(){}
+
+template <> inline
+void CGpGpuContext<cudaContext>::OutputInfoGpuMemory()
+{
+    size_t free;
+    size_t total;
+    checkCudaErrors( cudaMemGetInfo(&free, &total));
+    cout << "Memoire video       : " << (float)free / pow(2.0f,20) << " / " << (float)total / pow(2.0f,20) << "Mo" << endl;
+}
+
+template <> inline
+void CGpGpuContext<cudaContext>::check_Cuda()
+{
+    cout << "Cuda runtime version ";
+
+    int apiVersion = 0;
+
+    cudaRuntimeGetVersion(&apiVersion);
+
+//    DUMP_INT(apiVersion)
+
+    switch(apiVersion)
+    {
+        case 3000:
+            cout << "3.0";
+            break;
+        case 3020:
+            cout << "3.2";
+            break;
+        case 4000:
+            cout << "4.0";
+            break;
+        case 5000:
+            cout << "5.0";
+            break;
+        case 5050:
+            cout << "5.5";
+            break;
+        case 6000:
+            cout << "6.0";
+            break;
+        case 6050:
+            cout << "6.5";
+            break;
+    }
+    cout << endl;
+
+    int device_count = 0;
+
+    checkCudaErrors(cudaGetDeviceCount(&device_count));
+
+    if(device_count == 0)
+        printf("NO NVIDIA GRAPHIC CARD FOR USE CUDA");
+    else
+    {
+
+        // Creation du contexte GPGPU
+        cudaDeviceProp deviceProp;
+        // Obtention de l'identifiant de la carte la plus puissante
+        int devID = gpuGetMaxGflopsDeviceId();
+
+        // Initialisation du contexte
+        checkCudaErrors(cudaSetDevice(devID));
+        // Obtention des proprietes de la carte
+        checkCudaErrors(cudaGetDeviceProperties(&deviceProp, devID));
+        // Affichage des proprietes de la carte
+        printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID, deviceProp.name, deviceProp.major, deviceProp.minor);
+
+    }
+}
+
+
+template <> inline
+void CGpGpuContext<cudaContext>::createContext() {
+
+    //srand ((uint)time(NULL));
+    // Creation du contexte GPGPU
+    cudaDeviceProp deviceProp;
+    // Obtention de l'identifiant de la carte la plus puissante
+    int devID = gpuGetMaxGflopsDeviceId();
+
+    //ELISE_ASSERT(devID == 0 , "NO GRAPHIC CARD FOR USE CUDA");
+    if(devID != 0 )
+        printf("NO GRAPHIC CARD FOR USE CUDA\n");
+
+    // Initialisation du contexte
+    checkCudaErrors(cudaSetDevice(devID));
+    // Obtention des proprietes de la carte
+    checkCudaErrors(cudaGetDeviceProperties(&deviceProp, devID));
+    // Affichage des proprietes de la carte
+    //printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID, deviceProp.name, deviceProp.major, deviceProp.minor);
+}
+
+
+template <> inline
+void CGpGpuContext<cudaContext>::deleteContext()
+{
+    checkCudaErrors( cudaDeviceReset() );
+}
+
+#if OPENCL_ENABLED
+template <> inline
+void CGpGpuContext<openClContext>::createContext() {
+
+    cl_uint platformIdCount = 0;
+    clGetPlatformIDs (0, NULL, &platformIdCount);
+
+    std::vector<cl_platform_id> platformIds (platformIdCount);
+    clGetPlatformIDs (platformIdCount, platformIds.data (), NULL);
+
+    cl_uint deviceIdCount = 0;
+    clGetDeviceIDs (platformIds [0], CL_DEVICE_TYPE_ALL, 0, NULL,
+            &deviceIdCount);
+
+    std::vector<cl_device_id> deviceIds (deviceIdCount);
+    clGetDeviceIDs (platformIds [0], CL_DEVICE_TYPE_ALL, deviceIdCount,
+            deviceIds.data (), NULL);
+
+    const cl_context_properties contextProperties [] =
+    {
+        CL_CONTEXT_PLATFORM,
+        reinterpret_cast<cl_context_properties> (platformIds [0]),
+        0, 0
+    };
+
+    cl_int error;
+
+    openClContext::_contextOpenCL = clCreateContext (
+                contextProperties, deviceIdCount,
+                deviceIds.data (), NULL,
+                NULL, &error);
+
+    if(error == CL_SUCCESS)
+    {
+        printf("CONTEXT OPENCL OK\n");
+        printf("platform Id Count %d\n",platformIdCount);
+        printf("device Id Count %d\n",deviceIdCount);
+    }
+
+    openClContext::_commandQueue = clCreateCommandQueue(openClContext::_contextOpenCL,deviceIds[0], 0, &error);
+
+    errorOpencl(error,"CommandQueue");
+
+}
+
+template <> inline
+void CGpGpuContext<openClContext>::deleteContext()
+{
+    if(clReleaseContext(openClContext::_contextOpenCL) == CL_SUCCESS)
+        printf("CONTEXT OPENCL RELEASE\n");
+}
+
+template <> inline
+cl_context CGpGpuContext<openClContext>::contextOpenCL()
+{
+    return openClContext::_contextOpenCL;
+}
+
+template <> inline
+cl_command_queue CGpGpuContext<openClContext>::commandQueue()
+{
+    return openClContext::_commandQueue;
+}
+
+template <> inline
+cl_kernel CGpGpuContext<openClContext>::kernel()
+{
+    return openClContext::_kernel;
+}
+
+template <> inline
+void CGpGpuContext<openClContext>::createKernel(string fileName,string kernelName)
+{
+    cl_int error = -1;
+
+    char buffer[1024];
+    char* path_end;
+
+#ifndef _WIN32
+    if (readlink ("/proc/self/exe", buffer, sizeof(buffer)) <= 0)
+        return ;
+#endif
+
+    path_end = strrchr (buffer, '/');
+    if (path_end == NULL)
+        return ;
+
+    ++path_end;
+
+    *path_end = '\0';
+
+    std::string binPath(buffer);
+
+    fileName = binPath + fileName;
+
+    std::ifstream   file(fileName.c_str());
+    std::string     prog(std::istreambuf_iterator<char>(file),(std::istreambuf_iterator<char>()));
+    std::string     fileNameDefine = binPath + "../src/uti_phgrm/GpGpu/GpGpu_OpenCL_Kernel_Define.cl";
+
+    std::ifstream   fileDefine(fileNameDefine .c_str());
+    std::string     progDefine(std::istreambuf_iterator<char>(fileDefine),(std::istreambuf_iterator<char>()));
+
+    prog = progDefine + "//"  + prog;
+
+    //    if(file.is_open())
+    //        printf("%s\n",prog.c_str());
+
+    const char* sourceProg = prog.c_str();
+    size_t sourceSize[] = {strlen(prog.c_str())};
+    cl_program program = clCreateProgramWithSource(contextOpenCL(),1,&sourceProg,sourceSize,&error);
+
+    errorOpencl(error,"Program");
+
+    errorOpencl(clBuildProgram(program,0,NULL,NULL,NULL,NULL),"Build");
+
+    openClContext::_kernel = clCreateKernel(program,kernelName.c_str(),&error);
+
+    errorOpencl(error,"Kernel");
+
+}
+
+template <> inline
+void CGpGpuContext<openClContext>::launchKernel()
+{
+
+    cl_int error = -1;
+
+    size_t global_item_size = 5;
+    size_t local_item_size  = 1;
+
+    error  = clEnqueueNDRangeKernel(openClContext::_commandQueue,openClContext::_kernel,1,NULL,&global_item_size,&local_item_size,0,NULL,NULL);
+    errorOpencl(error,"Enqueue");
+
+}
+
+template <>
+template <class T> inline
+void CGpGpuContext<openClContext>::addKernelArg(T &arg)
+{
+
+    cl_int error = -1;
+
+    error = clSetKernelArg(CGpGpuContext<openClContext>::kernel(),(cl_uint)_sContext._nbArg,sizeof(T),&arg);
+
+    errorOpencl(error,"Kernel Arg");
+
+    _sContext._nbArg++;
+
+}
+
+template<>
+template <class T> inline
+void CGpGpuContext<openClContext>::addKernelArgSDK( CData<T> &arg)
+{
+    cl_int error = -1;
+
+    cl_mem memBuffer = arg.clMem();
+
+    error = clSetKernelArg(CGpGpuContext<openClContext>::kernel(),(cl_uint)_sContext._nbArg,sizeof(memBuffer),&memBuffer);
+
+    errorOpencl(error,"Kernel Arg OPENCL buffer");
+
+}
+
+#endif
+
+template<>
+template <class T> inline
+void CGpGpuContext<cudaContext>::addKernelArgSDK( CData<T> &arg)
+{
+    printf("Add cuda kernel argument buffer : %d\n",_sContext._nbArg);
+
+    _sContext._kernelArgs.push_back((void*)&arg);
+}
+
+template <> inline
+void CGpGpuContext<cudaContext>::launchKernel()
+{
+    kMultTab();
+}
+template <>
+template <class T> inline
+void CGpGpuContext<cudaContext>::addKernelArg(T &arg)
+{
+
+    printf("Add cuda kernel argument : %d\n",_sContext._nbArg);
+
+    T* p = &arg;
+
+    _sContext._kernelArgs.push_back((void*)p);
+
+    _sContext._nbArg++;
+
+}
+
+template <class context>
+template <class T , template< class O, class G > class U>
+void CGpGpuContext<context>::addKernelArg(U <T,context > &arg)
+{
+    addKernelArgSDK(arg);
+
+    _sContext._nbArg++;
+}
+
+#endif // GPGPU_CONTEXT_H
+
+
+
+
+
+
+
+
---
+++ src/uti_phgrm/GpGpu/GpGpu_Cuda_Optimisation.cu
@@ -0,0 +1,382 @@
+#ifndef _OPTIMISATION_KERNEL_Z_H_
+#define _OPTIMISATION_KERNEL_Z_H_
+
+#include "GpGpu/GpGpu_StreamData.cuh"
+#include "GpGpu/SData2Optimize.h"
+
+// On pourrait imaginer un buffer des tailles calculer en parallel
+// SIZEBUFFER[threadIdx.x] = count(lI[threadIdx.x]);
+
+__device__ void GetConeZ(short2 & aDz, int aZ, int MaxDeltaZ, short2 aZ_Next, short2 aZ_Prev)
+{
+    aDz.x =   aZ_Prev.x-aZ;
+    if (aZ != aZ_Next.x)
+        aDz.x = max(aDz.x,-MaxDeltaZ);
+
+    aDz.y = aZ_Prev.y-1-aZ;
+    if (aZ != aZ_Next.y-1)
+        aDz.y = min(aDz.y,MaxDeltaZ);
+
+    if (aDz.x > aDz.y)
+        if (aDz.y <0)
+            aDz.x = aDz.y;
+        else
+            aDz.y = aDz.x;
+}
+
+__device__ void BasicComputeIntervaleDelta
+              (
+                  short2 & aDz,
+                  int aZ,
+                  int MaxDeltaZ,
+                  short2 aZ_Prev
+              )
+{
+   aDz.x = max(-MaxDeltaZ,aZ_Prev.x-aZ);
+   aDz.y = min(MaxDeltaZ,aZ_Prev.y-1-aZ);
+}
+
+inline __device__ uint minR(uint *sMin, uint &globalMin){ // TODO attention ajout de inline
+    ushort  thread2;
+    uint    temp;
+    //
+
+    int nTotalThreads = WARPSIZE;	// Total number of threads, rounded up to the next power of two
+
+    while(nTotalThreads > 1)
+    {
+        int halfPoint = (nTotalThreads >> 1);	// divide by two
+        // only the first half of the threads will be active.
+
+        if (threadIdx.x < halfPoint)
+        {
+            thread2 = threadIdx.x + halfPoint;
+            // Skipping the fictious threads blockDim.x ... blockDim_2-1
+            if (thread2 < blockDim.x)
+            {
+                // Get the shared value stored by another thread
+                temp = sMin[thread2];
+                if (temp < sMin[threadIdx.x])
+                    sMin[threadIdx.x] = temp;
+            }
+        }
+        // Reducing the binary tree size by two:
+        nTotalThreads = halfPoint;
+    }
+
+    const uint minus = sMin[0];
+
+    if(minus < globalMin) globalMin = minus;
+
+    return minus;
+}
+
+template<bool sens> __device__
+void connectCellsLine(
+                SimpleStream<short3>    &streamIndex,
+                SimpleStream<uint>      &streamFCost,
+                SimpleStream<ushort>    &streamICost,
+                SimpleStream<uint>      &streamDefCor,
+                short3     *S_Bf_Index,
+                ushort     *ST_Bf_ICost,
+                uint       *S_FCost[2],
+                p_ReadLine &p
+)
+{
+
+    short3* ST_Bf_Index = S_Bf_Index + p.tid + (sens ? 0 : -WARPSIZE + 1);
+
+    __shared__ uint minCost[WARPSIZE];
+    short2  ConeZ;
+    uint globMinFCost;
+
+    bool lined = p.line.id < p.line.lenght;
+
+    const int regulZ  = (int)((float)10000.f*p.ZRegul);
+
+    // Remarque
+    // p.seg.id = 1 au premier passage, car simple copie des initcost
+
+    #ifdef CUDA_DEFCOR
+    //////////////////////////////////////////////////
+    /// TODO!!!! : quel doit etre prevDefCor p.costTransDefMask + p.costDefMask ou p.costDefMask
+    /////////////////////////////////////////////////
+    uint         prevDefCor   =/* p.costTransDefMask + */p.prevDefCor; // TODO Voir la valeur à mettre!!!
+    const ushort idGline = p.line.id + p.seg.id;
+
+    streamDefCor.SetOrAddValue<sens>(sens ? idGline : p.line.lenght  - idGline,prevDefCor);
+    uint         prevMinCostCells    = 0; // TODO cette valeur doit etre determiner
+    #endif
+
+    uint         prevMinCost         = 0;
+
+
+    while(lined)
+    {
+        while(p.seg.id < p.seg.lenght)
+        {
+            const short3 dTer       = S_Bf_Index[sgn(p.seg.id)];
+            const short2 indexZ     = make_short2(dTer.x,dTer.y);
+#ifdef CUDA_DEFCOR
+            const ushort cDefCor    = dTer.z;
+            const bool   maskTer    = cDefCor == 0;
+#endif
+            const ushort dZ         = count(indexZ); // creer buffer de count
+            ushort       z          = 0;
+            globMinFCost            = max_cost;
+
+            while( z < dZ)
+            {                
+                // Lecture du stream si le buffer est vide | TODO VERIFIER si > ou >=
+                if(p.ID_Bf_Icost >= p.sizeBuffer)
+                {
+                    streamICost.read<sens>(ST_Bf_ICost);    //  Lecture des couts correlations
+                    streamFCost.incre<sens>();              //  Pointage sur la sortie
+                    p.ID_Bf_Icost = 0;                      //  Pointage la première valeur du buffer des couts correlations
+                }
+
+                uint    fCostMin        = max_cost;
+#ifdef CUDA_DEFCOR
+                uint    costInit        = maskTer ? 500000 : ST_Bf_ICost[sgn(p.ID_Bf_Icost)];
+#else
+                uint    costInit        = ST_Bf_ICost[sgn(p.ID_Bf_Icost)];
+#endif
+                const ushort tZ         = z + p.stid<sens>();
+                const short  Z          = ((sens) ? tZ + indexZ.x : indexZ.y - tZ - 1);
+                const short  pitPrZ     = ((sens) ? Z - p.prev_Dz.x : p.prev_Dz.y - Z - 1);
+
+#ifdef CUDA_DEFCOR
+                BasicComputeIntervaleDelta(ConeZ,Z,p.pente,p.prev_Dz);
+#else
+                GetConeZ(ConeZ,Z,p.pente,indexZ,p.prev_Dz);
+#endif
+                uint* prevFCost = S_FCost[p.Id_Buf] + sgn(pitPrZ);
+
+                ConeZ.y = min(p.sizeBuffer - pitPrZ,ConeZ.y );
+
+                for (short i = ConeZ.x; i <= ConeZ.y; ++i) //--> TO DO cette etape n'est pas necessaire si nous sommes en dehors du masque Ter
+                    fCostMin = min(fCostMin, costInit + prevFCost[i] + abs((int)i)*regulZ);
+#ifdef CUDA_DEFCOR
+                // NOTE DEFCOR
+                // LES PROBLEMES
+                    // les cellules dans la zone masquée
+                    // les cellules dont la valeur le coef de corrélation n'a pas été calculé -> 1.01234 --> 10123
+                    //  ces cellules contaminent les voisines en mode DEFCOR....
+                if(!maskTer) // OkOut
+                    fCostMin = min(fCostMin, costInit + prevDefCor  + p.costTransDefMask );
+#endif
+
+                if(tZ < dZ && p.ID_Bf_Icost +  p.stid<sens>() < p.sizeBuffer && tZ < p.sizeBuffer)
+                {                    
+
+                    fCostMin                    -= prevMinCost;
+                    minCost[p.tid]               = fCostMin;
+                    S_FCost[!p.Id_Buf][sgn(tZ)]  = fCostMin;
+
+                    streamFCost.SetOrAddValue<sens>(sgn(p.ID_Bf_Icost),fCostMin,fCostMin - costInit);                    
+                }
+                else
+                    minCost[p.tid] = max_cost;
+
+                minR(minCost,globMinFCost); // TODO verifier cette fonction elle peut lancer trop de fois..... Attentioncd ,inline en attendant
+
+                const ushort pIdCost = p.ID_Bf_Icost;
+                p.ID_Bf_Icost       += min(dZ - z               , WARPSIZE);
+                z                   += min(p.sizeBuffer-pIdCost , WARPSIZE);
+
+            }
+
+#ifdef CUDA_DEFCOR
+
+            uint defCor = prevDefCor + cDefCor;
+
+            if(p.prevDefCor != 0)
+                defCor = min(defCor,cDefCor + prevMinCostCells + p.costTransDefMask);
+
+
+            prevDefCor = defCor - prevMinCost;
+                //prevDefCor  = min(prevDefCor,prevMinCost + p.costTransDefMask ) + cDefCor - prevMinCost;
+
+            prevMinCostCells = globMinFCost;
+
+            prevMinCost = min(globMinFCost,prevDefCor);
+
+            p.prevDefCor = cDefCor;
+            if(p.tid == 0)
+            {
+                const ushort idGline = p.line.id + p.seg.id;
+                streamDefCor.SetOrAddValue<sens>(sens ? idGline : p.line.lenght  - idGline,prevDefCor,prevDefCor-cDefCor);
+            }
+
+
+#else
+            prevMinCost = globMinFCost;
+#endif
+
+            p.prev_Dz = indexZ;
+            p.seg.id++;
+            p.swBuf();
+
+//            if(!sens) // retranche la cout minimum à toutes les cellules de même coordonnées terrain
+//            {
+//                const short piSFC = -p.ID_Bf_Icost + dZ ;
+//                for (ushort i = 0; i < dZ - p.stid<sens>(); i+=WARPSIZE)
+//                    streamFCost.SubValue(piSFC - i,globMinFCost);
+//            }
+        }
+
+        p.line.id += p.seg.lenght;
+
+        lined = p.line.id < p.line.lenght;
+
+        if(lined)
+        {
+            streamIndex.read<sens>(ST_Bf_Index);
+            p.seg.lenght  = min(p.line.LOver(),WARPSIZE);
+            p.seg.id      = 0; // position dans le segment du stream index des Z
+        }
+    }
+}
+
+// TODO Passer les parametres en variable constante !!!!!!!!!!!
+
+template<class T> __global__
+void Kernel_OptimisationOneDirection(ushort* g_ICost, short3* g_Index, uint* g_FCost, uint* g_DefCor, uint3* g_RecStrParam, uint penteMax, float zReg,float zRegQuad, ushort costDefMask,ushort costTransDefMask,ushort sizeBuffer)
+{
+
+    extern __shared__ float sharedMemory[];
+
+    ushort*   S_BuffICost0 = (ushort*)  sharedMemory;
+    uint*     S_BuffFCost0 = (uint*)    &S_BuffICost0[sizeBuffer + 2*WARPSIZE];
+    uint*     S_BuffFCost1 = (uint*)    &S_BuffFCost0[sizeBuffer + 2*WARPSIZE];
+    short3*   S_BuffIndex  = (short3*)  &S_BuffFCost1[sizeBuffer + 2*WARPSIZE];
+    uint*     pit_Id       = (uint*)    &S_BuffIndex[WARPSIZE];
+    uint*     pit_Stream   = pit_Id + 1;
+
+    p_ReadLine p(threadIdx.x,penteMax,zReg,zRegQuad,costDefMask,costTransDefMask,sizeBuffer);
+
+    uint*    S_BuffFCost[2] = {S_BuffFCost0 + WARPSIZE,S_BuffFCost1 + WARPSIZE};
+    ushort*  S_BuffICost    = S_BuffICost0 + WARPSIZE + p.tid;
+
+    if(!threadIdx.x)
+    {
+        *pit_Stream          = g_RecStrParam[blockIdx.x].x;
+        *pit_Id              = g_RecStrParam[blockIdx.x].y;
+    }
+
+    __syncthreads();
+
+    p.line.lenght   = g_RecStrParam[blockIdx.x].z;
+    p.seg.lenght    = min(p.line.LOver(),WARPSIZE);
+
+    SimpleStream<ushort>    streamICost(    g_ICost     + *pit_Stream   ,sizeBuffer);
+    SimpleStream<uint>      streamFCost(    g_FCost     + *pit_Stream   ,sizeBuffer);
+    SimpleStream<short3>    streamIndex(    g_Index     + *pit_Id       ,WARPSIZE);
+    SimpleStream<uint>      streamDefCor(   g_DefCor    + *pit_Id       ,WARPSIZE);
+
+   if(p.tid == 0)
+        streamDefCor.SetValue(0,0); // car la premiere ligne n'est calculer
+                                    // Attention voir pour le retour arriere
+
+    streamICost.read<eAVANT>(S_BuffICost);
+    streamIndex.read<eAVANT>(S_BuffIndex + p.tid);
+
+    p.prev_Dz       = make_short2(S_BuffIndex[0].x,S_BuffIndex[0].y);
+    p.prevDefCor    = S_BuffIndex[0].z;
+    p.ID_Bf_Icost   = count(p.prev_Dz);
+
+    for (ushort i = 0; i < p.ID_Bf_Icost - p.tid; i+=WARPSIZE)
+    {
+        S_BuffFCost[p.Id_Buf][i + p.tid] = S_BuffICost[i];
+        streamFCost.SetValue(i,S_BuffICost[i]);
+    }
+
+    connectCellsLine<eAVANT>(streamIndex,streamFCost,streamICost,streamDefCor,S_BuffIndex,S_BuffICost,S_BuffFCost,p);
+
+    streamIndex.ReverseIncre<eARRIERE>();
+    streamFCost.incre<eAVANT>();
+    streamFCost.reverse<eARRIERE>();
+
+    S_BuffFCost[0]  += sizeBuffer;
+    S_BuffFCost[1]  += sizeBuffer;
+    S_BuffICost     += sizeBuffer - WARPSIZE;
+
+    streamICost.readFrom<eARRIERE>(S_BuffFCost[p.Id_Buf] + p.tid, sizeBuffer - p.ID_Bf_Icost);
+    streamICost.ReverseIncre<eARRIERE>();
+
+    p.reverse(S_BuffIndex,sizeBuffer);
+
+    if(p.ID_Bf_Icost > sizeBuffer)
+    {
+        p.ID_Bf_Icost -= sizeBuffer;
+        streamICost.read<eARRIERE>(S_BuffICost);
+        streamFCost.incre<eARRIERE>();
+    }
+
+    uint* locFCost = S_BuffFCost[p.Id_Buf] - p.stid<eARRIERE>();
+
+    for (ushort i = 0; i < sizeBuffer; i+=WARPSIZE)
+        locFCost[-i] = S_BuffICost[-i];
+
+    connectCellsLine<eARRIERE>( streamIndex,streamFCost,streamICost,streamDefCor,S_BuffIndex + WARPSIZE - 1,S_BuffICost,S_BuffFCost,p);
+}
+
+extern "C" void Gpu_OptimisationOneDirection(Data2Optimiz<CuDeviceData3D> &d2O)
+{
+    uint    deltaMax         = d2O.penteMax();
+    float   zReg             = (float)d2O.zReg();
+    float   zRegQuad         = d2O.zRegQuad();
+    ushort  costDefMask      = d2O.CostDefMasked();
+    ushort  costTransDefMask = d2O.CostTransMaskNoMask();
+
+    dim3 Threads(WARPSIZE,1,1);
+    dim3 Blocks(d2O.NBlines(),1,1);
+
+    ushort sizeBuff = min(d2O.DzMax(),4096);  //NAPPEMAX;
+    ushort cacheLin = sizeBuff + 2 * WARPSIZE;
+
+
+    // Calcul de l'allocation dynamique de la memoire partagée
+    uint   sizeSharedMemory =
+            cacheLin * sizeof(ushort)   + // S_BuffICost0
+            cacheLin * sizeof(uint)     + // S_BuffFCost0
+            cacheLin * sizeof(uint)     + // S_BuffFCost1
+            WARPSIZE * sizeof(short3)   + // S_BuffIndex
+          //  WARPSIZE * sizeof(uint)     + // S_BuffDefCor
+            sizeof(uint)                + // pit_Id
+            sizeof(uint);                 // pit_Stream
+
+
+    Kernel_OptimisationOneDirection< uint ><<<Blocks,Threads,sizeSharedMemory>>>
+                                                       (
+                                                           d2O.pInitCost(),
+                                                           d2O.pIndex(),
+                                                           d2O.pForceCostVol(),
+                                                           d2O.pDefCor(),
+                                                           d2O.pParam(),
+                                                           deltaMax,
+                                                           zReg,
+                                                           zRegQuad,
+                                                           costDefMask,
+                                                           costTransDefMask,
+                                                           sizeBuff
+                                                           );
+
+    cudaError_t err = cudaGetLastError();
+
+    if (cudaSuccess != err)
+    {        
+        printf("Error CUDA Gpu_OptimisationOneDirection\n");
+        printf("%s",cudaGetErrorString(err));
+        DUMP_UINT(d2O.NBlines());
+        DUMP_UINT(sizeSharedMemory);
+        DUMP_UINT(d2O.DzMax());
+    }
+
+    getLastCudaError("TestkernelOptiOneDirection failed");
+
+}
+
+
+#endif //_OPTIMISATION_KERNEL_Z_H_
+
