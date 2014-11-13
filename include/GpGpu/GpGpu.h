03ad20cb101f5207d725a779c17ebca69cabb71a
deseilligny <deseilligny@users.noreply.github.com>
2014-11-13 14:51:11 +0100

    Dans C3DC

---
+++ include/GpGpu/GpGpu.h
@@ -0,0 +1,13 @@
+#ifndef __GPGPU_H__
+#define __GPGPU_H__
+
+#include "GpGpu/GpGpu_BuildOptions.h"
+
+#if CUDA_ENABLED
+#include "GpGpu/GpGpu_InterCorrel.h"
+#include "GpGpu/GpGpu_InterOptimisation.h"
+#include "GpGpu/GpGpu_eLiSe.h"
+#include "GpGpu/GpGpu_Context.h"
+#endif
+
+#endif // __GPGPU_H__
