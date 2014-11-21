d6259f98b63ebbf0a4f59cdd84c822a6199d52a3
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2014-11-21 10:56:00 +0100

    GpGpu : Consensus

---
+++ src/uti_phgrm/GpGpu/Sources.cmake
@@ -0,0 +1,26 @@
+set(GpGpuTools_Src_Files
+         ${UTI_PHGRM_GPGPU_DIR}/GpGpu_Object.cpp
+         ${UTI_PHGRM_GPGPU_DIR}/GpGpu_Data.cpp
+         ${UTI_PHGRM_GPGPU_DIR}/GpGpu_Tools.cpp
+         ${UTI_PHGRM_GPGPU_DIR}/GpGpu_MultiThreadingCpu.cpp
+)
+
+set(uti_phgrm_GpGpu_Src_Files
+         ${UTI_PHGRM_GPGPU_DIR}/GpGpu_InterfaceCorrel.cpp
+        ${UTI_PHGRM_GPGPU_DIR}/GpGpu_Interface_Census.cpp
+	 ${UTI_PHGRM_GPGPU_DIR}/SData2Correl.cpp
+         ${UTI_PHGRM_GPGPU_DIR}/GpGpu_Cuda_Correlation.cu
+         ${UTI_PHGRM_GPGPU_DIR}/GpGpu_Census.cu
+	 ${UTI_PHGRM_GPGPU_DIR}/cudaFilters.cu
+	 #${UTI_PHGRM_GPGPU_DIR}/GpGpuTextureTools.cu
+)
+
+set(uti_phgrm_Opt_GpGpu_Src_Files        
+        ${UTI_PHGRM_GPGPU_DIR}/GpGpu_Cuda_Optimisation.cu
+        ${UTI_PHGRM_GPGPU_DIR}/GpGpu_InterfaceOptimisation.cpp
+)
+
+set(uti_Test_Opt_GpGpu_Src_Files
+        ${UTI_PHGRM_GPGPU_DIR}/GpGpu_UnitTestingKernel.cu
+        ${UTI_PHGRM_GPGPU_DIR}/GpGpu_UnitTesting.cpp
+)
