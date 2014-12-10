1d470108217da563d6af4ec8ea2b1191eb6296ef
Ana-Maria Rosu <Ana-Maria Rosu@localhost>
2014-12-10 14:58:29 +0100

    sake: modifs params

---
+++ src/uti_image/Digeo/clean_compiled_convolutions.sh
@@ -0,0 +1,11 @@
+rm GenConvolSpec_real4.classes.h
+touch GenConvolSpec_real4.classes.h
+
+rm GenConvolSpec_real4.instantiations.h
+touch GenConvolSpec_real4.instantiations.h
+
+rm GenConvolSpec_u_int2.classes.h
+touch GenConvolSpec_u_int2.classes.h
+
+rm GenConvolSpec_u_int2.instantiations.h
+touch GenConvolSpec_u_int2.instantiations.h
