e58b30a2542931bd9a59366c1a027d73b23b709b
deseilligny <deseilligny@users.noreply.github.com>
2014-12-10 15:07:28 +0100

    Block Rigid valide (incomplet : manque global + AMD + GL)

---
+++ src/doc_ex/Sources.cmake
@@ -0,0 +1,13 @@
+set(Src_DocEx
+	${DocEx}/intro0.cpp
+	${DocEx}/introd2.cpp
+	${DocEx}/introfiltr.cpp
+	${DocEx}/introanalyse.cpp
+)
+
+#SOURCE_GROUP(Util FILES ${Util_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${Src_DocEx}
+)
