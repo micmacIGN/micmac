9de5ea7ba0d84540763bfc27b868f5f515e0a51a
deseilligny <deseilligny@users.noreply.github.com>
2014-12-12 10:38:15 +0100

    Dans TD Epip,

---
+++ BenchElise/bench/bug_vecto.cpp
@@ -0,0 +1,98 @@
+/*eLiSe06/05/99
+  
+     Copyright (C) 1999 Marc PIERROT DESEILLIGNY
+
+   eLiSe : Elements of a Linux Image Software Environment
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+  Author: Marc PIERROT DESEILLIGNY    IGN/MATIS  
+Internet: Marc.Pierrot-Deseilligny@ign.fr
+   Phone: (33) 01 43 98 81 28
+eLiSe06/05/99*/
+
+
+
+#include "general/all.h"
+#include "private/all.h"
+#include "ext_stl/fifo.h"
+#include "bench.h"
+
+
+
+class BugAV 
+{
+    public :
+       BugAV (Im2D_U_INT2 )  {}
+
+       ~BugAV () 
+       {
+	       cout << "Kill a BugAV \n";
+       }
+
+    private  :
+
+};
+
+Fonc_Num TOTO(Fonc_Num, BugAV ){return 0;}
+
+int main(int,char **)
+{
+    Pt2di sz(10,10);
+    Im2D_U_INT2 Ifl(sz.x,sz.y,255);
+
+
+cout << "bench_vecto AAAA\n";
+
+    // cout << Ifl.ptr() << "\n";
+
+    INT aBid;
+   ELISE_COPY
+   (
+       rectangle(Pt2di(0,0),sz),
+      // Ifl.all_pts(),
+      TOTO  
+      (
+         Fonc_Num(11), 
+         BugAV (Ifl)
+      ),
+      sigma(aBid)
+   );
+cout << "bench_vecto BBBBB\n";
+exit(-1);
+
+
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
