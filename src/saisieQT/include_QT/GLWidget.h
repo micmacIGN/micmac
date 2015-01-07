07518809f1d93f2c84768ab8a4dc1e3c93032615
deseilligny <deseilligny@users.noreply.github.com>
2015-01-07 18:52:49 +0100

    Separation PIMs et PIMs2Ply achevee, PIMs2MNT a peine ebauchee

---
+++ src/saisieQT/include_QT/GLWidget.h
@@ -0,0 +1,216 @@
+#ifndef _GLWIDGET_H
+#define _GLWIDGET_H
+
+#include "Engine.h"
+#include "ContextMenu.h"
+
+class GLWidget : public QGLWidget
+{
+    Q_OBJECT
+
+public:
+
+    //! Default constructor
+    GLWidget(int idx, const QGLWidget *shared);
+
+    //! Destructor
+    ~GLWidget(){}
+
+    //! States if data (cloud, camera or image) is loaded
+    bool hasDataLoaded(){ return (m_GLData != NULL); }
+
+    //! Sets camera to a predefined view (top, bottom, etc.)
+    void setView(VIEW_ORIENTATION orientation);
+
+    //! Get current zoom
+    float getZoom();
+
+    void zoomFit();
+
+    void zoomFactor(int percent);
+
+    //! Switch between move mode and selection mode (only in 3D)
+    void setInteractionMode(int mode, bool showmessage, bool showcams);
+
+    bool getInteractionMode(){return m_interactionMode;}
+
+    void setOption(QFlags<cGLData::Option> option, bool show = true);
+
+    //! Apply selection to data
+    void Select(int mode, bool saveInfos = true);
+
+    void applyInfos();
+
+    //! Avoid all past actions
+    void reset();
+
+    //! Reset view
+    void resetView(bool zoomfit = true, bool showMessage = true, bool showCams = true, bool resetMatrix = true, bool resetPoly = true);
+
+    ViewportParameters* getParams()         { return &_vp_Params;      }
+    HistoryManager*     getHistoryManager() { return &_historyManager; }
+    cMessages2DGL*      getMessageManager() { return &_messageManager; }
+    MatrixManager*      getMatrixManager()  { return &_matrixManager;  }
+
+    void        setGLData(cGLData* aData, bool showMessage = true, bool showCams = true, bool doZoom = true, bool resetPoly = true);
+    cGLData*    getGLData(){ return m_GLData; }
+
+    void setBackgroundColors(QColor const &col0, QColor const &col1)
+    {
+        _BGColor0 = col0;
+        _BGColor1 = col1;
+    }
+
+    float imWidth() { return m_GLData->glImage()._m_image->width();  }
+    float imHeight(){ return m_GLData->glImage()._m_image->height(); }
+
+    bool  isPtInsideIm(QPointF const &pt) { return m_GLData->glImage()._m_image->isPtInside(pt); }
+
+    GLint vpWidth() { return _matrixManager.vpWidth();  }
+    GLint vpHeight(){ return _matrixManager.vpHeight(); }
+
+    cPolygon* polygon(int id);
+    cPolygon* polygon();
+
+    void setCursorShape(QPointF pos, QPointF mPos);
+
+    void addGlPoint(QPointF pt, cOneSaisie *aSom, QPointF pt1, QPointF pt2, bool highlight);
+
+    void setTranslation(Pt3dr trans);
+
+    ContextMenu *contextMenu();
+
+    void setParams(cParameters *aParams);
+
+    void movePointWithArrows(QKeyEvent* event);
+
+    void overlay();
+
+    bool isFirstAction() { return m_bFirstAction; }
+
+    int  getWindowMeanValue(QPoint pos, int r = 7); //pos = image position, r = half size of window
+
+    void checkTiles(); //compute tiles if needed
+
+    void setZone(QRectF aRect);
+
+public slots:
+
+    void centerViewportOnImagePosition(QPointF pt, float zoom = -1);
+
+    void lineThicknessChanged(float);
+    void gammaChanged(float);
+    void pointDiameterChanged(float);
+    void selectionRadiusChanged(int);
+    void shiftStepChanged(float);
+    void forceGray(bool);
+    void showMasks(bool);
+
+    //! Sets current zoom
+    void setZoom(float val);
+
+    void selectPoint(QString namePt);
+
+    void setCenterType(int);
+
+signals:
+
+    //! Signal emitted when files are dropped on the window
+    void filesDropped(const QStringList& filenames, bool setGLData = true);
+
+    void newImagePosition(QPointF pt);
+
+    void overWidget(void* widget);
+
+    void gammaChangedSgnl(float gamma);
+
+    void addPoint(QPointF point);
+
+    void movePoint(int idPt);
+
+    void selectPoint(int idPt);
+
+    void removePoint(int state, int idPt);
+
+    void newRectanglePosition(QVector <QPointF> points);
+
+    void maskEdited();
+
+protected:
+    //! inherited from QGLWidget
+    void resizeGL(int w, int h);
+    void paintGL();
+
+    //! inherited from QWidget
+    void mouseDoubleClickEvent  (QMouseEvent *event);
+    void mousePressEvent        (QMouseEvent *event);
+    void mouseReleaseEvent      (QMouseEvent *event);
+    void mouseMoveEvent         (QMouseEvent *event);
+
+    void keyPressEvent  (QKeyEvent *event);
+    void keyReleaseEvent(QKeyEvent *event);
+
+    void wheelEvent(QWheelEvent *event);
+
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+
+    void contextMenuEvent(QContextMenuEvent *event);
+
+    void enterEvent(QEvent *event);
+
+    //! Current interaction mode (with mouse)
+    int  m_interactionMode;
+
+    bool m_bFirstAction;
+
+    //! Data to display
+    cGLData    *m_GLData;
+
+    //! states if display is 2D or 3D
+    bool        m_bDisplayMode2D;
+
+    QPointF     m_lastMoveImage;
+    QPoint      m_lastClickZoom;
+
+    QPointF     m_lastPosImage;
+    QPoint      m_lastPosWindow;
+
+    bool        imageLoaded();
+
+private:
+
+    //! Window parameters (zoom, etc.)
+    ViewportParameters _vp_Params;
+
+    void        computeFPS(MessageToDisplay &dynMess);
+
+    int         _frameCount;
+
+    int         _previousTime;
+
+    int         _currentTime;
+
+    QTime       _time;
+
+    MatrixManager   _matrixManager;
+
+    cMessages2DGL   _messageManager;
+
+    HistoryManager  _historyManager;
+
+    ContextMenu     _contextMenu;
+
+    int             _widgetId;
+
+    QColor      _BGColor0;
+
+    QColor      _BGColor1;
+
+    cParameters* _params;
+
+   // QPainter*   _painter;
+};
+
+#endif  /* _GLWIDGET_H */
+
---
+++ src/uti_phgrm/CPP_C3DC.cpp
@@ -0,0 +1,451 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#include "StdAfx.h"
+#include <algorithm>
+
+// ffmpeg -i MVI_0001.MOV  -ss 30 -t 20 Im%5d_Ok.png
+
+// Im*_Ok => OK
+// Im*_Nl => Image Nulle (eliminee)
+
+
+// =============  ::  ===================================
+
+
+
+/*
+Debut de pipeline statue :
+
+mm3d MMByP Statue "IMGP703[0-5].*JPG" Ori-Ori-CalPerIm/ ZoomF=2 Masq3D=AperiCloud_Ori-CalPerIm_selectionInfo.xml Purge=true Do=APMCR
+
+*/
+
+class cAppli_C3DC : public cAppliWithSetImage
+{
+     public :
+         cAppli_C3DC(int argc,char ** argv,bool DoMerge);
+         void DoAll();
+
+     private :
+
+        void ExeCom(const std::string & aCom);
+
+         void PipelineQuickMack();
+         void PipelineStatue();
+         void DoMergeAndPly();
+
+         std::string mStrType;
+         eTypeMMByP  mType;
+         std::string mOriFull;
+
+         std::string mArgMasq3D;
+         std::string mStrImOri;
+         std::string mBaseComMMByP;
+         std::string mBaseComEnv;
+         std::string mComMerge;
+         std::string mComCatPly;
+
+
+    // Param opt
+         bool        mTuning;
+         bool        mPurge;
+         bool        mPlyCoul;
+         std::string mMergeOut;
+         bool        mDoPoisson;
+         std::string mMasq3D;
+         int         mSzNorm;
+         double      mDS;
+         int         mZoomF;
+         std::string mStrZ0ZF;
+         bool        mDoMerge;
+         cMMByImNM * mMMIN;
+};
+
+cAppli_C3DC::cAppli_C3DC(int argc,char ** argv,bool DoMerge) :
+   cAppliWithSetImage  (argc-2,argv+2,TheFlagDev16BGray|TheFlagAcceptProblem),
+   mTuning             (MPD_MM()),
+   mPurge              (true),
+   mPlyCoul            (true),
+   mMergeOut           ("C3DC.ply"),
+   mSzNorm             (2),
+   mDS                 (1.0),
+   mZoomF              (1),
+   mDoMerge            (DoMerge),
+   mMMIN               (0)
+{
+
+
+    if (argc<2)
+    {
+        ELISE_ASSERT(false,"No arg to C3CD");
+    }
+    mStrType = argv[1];
+    StdReadEnum(mModeHelp,mType,mStrType,eNbTypeMMByP);
+
+
+   ElInitArgMain
+   (
+        argc,argv,
+        LArgMain()  << EAMC(mStrType,"Type in enumerated values", eSAM_None,ListOfVal(eNbTypeMMByP,"e"))
+                    << EAMC(mEASF.mFullName,"Full Name (Dir+Pattern)", eSAM_IsPatFile)
+                    << EAMC(mOriFull,"Orientation", eSAM_IsExistDirOri),
+        LArgMain()
+                    << EAM(mMasq3D,"Masq3D",true,"3D masq for point selection")
+                    << EAM(mMergeOut,"Out",true,"final result (Def=C3DC.ply)")
+                    << EAM(mSzNorm,"SzNorm",true,"Sz of param for normal evaluation (<=0 if none, Def=2 mean 5x5) ")
+                    << EAM(mPlyCoul,"PlyCoul",true,"Colour in ply ? Def = true")
+                    << EAM(mTuning,"Tuning",true,"Will disappear on day ...")
+                    << EAM(mPurge,"Purge",true,"Purge result, def=true")
+                    << EAM(mDS,"DownScale",true,"DownScale of Final result, Def depenf of mode")
+                    << EAM(mZoomF,"ZoomF",true,"Zoom final, Def depenf of mode")
+   );
+
+   if (!EAMIsInit(&mDS))
+   {
+      // if (mType==eQuickMac) mDS = 2.0;
+   }
+   if (!EAMIsInit(&mZoomF))
+   {
+      if (mType==eBigMac)   mZoomF = 2;
+      if (mType==eMicMac)   mZoomF = 4;
+      if (mType==eQuickMac) mZoomF = 8;
+      if (mType==eStatue)   mZoomF = 2;
+   }
+
+
+
+
+   if (! EAMIsInit(&mMergeOut)) mMergeOut = "C3DC_"+ mStrType + ".ply";
+
+   mStrImOri =  BLANK + QUOTE(mEASF.mFullName) +  BLANK + Ori() + BLANK;
+   mArgMasq3D = "";
+   if (EAMIsInit(&mMasq3D))
+      mArgMasq3D = std::string(" Masq3D=" + mMasq3D + BLANK) ;
+
+
+  //=====================================
+
+   mBaseComMMByP =    MM3dBinFile("MMByP ")
+                   +  BLANK + mStrType
+                   +  mStrImOri
+                   +  mArgMasq3D;
+
+
+  //=====================================
+   mBaseComEnv =      MM3dBinFile("TestLib MMEnvlop ")
+                   +  mStrImOri
+                   +  std::string(" 16 ")  + ToString(mZoomF) + " " 
+                   +  mArgMasq3D
+                   +  std::string(" AutoPurge=") + ToString(mPurge)
+                   +  " Out=" + mStrType
+                 ;
+
+/*
+   if (mTuning)
+   {
+      mBaseComEnv = mBaseComEnv + " DoPlyDS=true";
+   }
+*/
+
+  //=====================================
+
+  mComMerge =      MM3dBinFile("TestLib  MergeCloud ")
+                +  mStrImOri + " ModeMerge=" + mStrType
+                +  " DownScale=" +ToString(mDS)
+               ;
+
+  if (mSzNorm>=0)
+  {
+     mComMerge = mComMerge + " SzNorm=" + ToString(1+2*mSzNorm);
+  }
+
+   mComMerge +=  " PlyCoul=" + ToString(mPlyCoul);
+
+   mMMIN = cMMByImNM::ForGlobMerge(Dir(),mDS,mStrType);
+
+  //=====================================
+
+  std::string aDirFusMM = mMMIN->FullDir();
+
+   mComCatPly =  MM3dBinFile("MergePly ") + QUOTE( aDirFusMM + ".*Merge.*ply") + " Out="  + mMergeOut;
+
+   mStrZ0ZF = " Zoom0=" + ToString(mZoomF) + " ZoomF=" + ToString(mZoomF);
+   mMMIN->SetOriOfEtat(mOri);
+}
+
+
+void cAppli_C3DC::ExeCom(const std::string & aCom)
+{
+
+   std::cout << aCom << "\n\n";
+   if (!mTuning) System(aCom);
+}
+
+void cAppli_C3DC::DoMergeAndPly()
+{
+    mMMIN->AddistofName(mEASF.SetIm());
+    if (mDoMerge)
+    {
+       ExeCom(mComMerge);
+       ExeCom(mComCatPly);
+    }
+    if (MPD_MM())
+    {
+        // std::cout << "KKKKey " <<  mMMIN->KeyFileLON() << "\n";
+    }
+}
+
+void  cAppli_C3DC::PipelineQuickMack()
+{
+    ExeCom(mBaseComMMByP + " Do=AMP " + mStrZ0ZF);
+    ExeCom(mBaseComEnv + " DownScale=" + ToString(mDS));
+    DoMergeAndPly();
+}
+
+
+void  cAppli_C3DC::PipelineStatue()
+{
+    ExeCom(mBaseComMMByP + " Purge=" + ToString(mPurge) + " Do=APMCR ZoomF=" + ToString(mZoomF)  );
+    ExeCom(mBaseComEnv + " Glob=false");
+    ExeCom(mBaseComMMByP + " Purge=" +  ToString(mPurge) + " Do=F " );
+    DoMergeAndPly();
+/*
+*/
+}
+
+
+
+void cAppli_C3DC::DoAll()
+{
+    switch (mType)
+    {
+         case eBigMac :
+         case eMicMac :
+         case eQuickMac :
+              PipelineQuickMack();
+         break;
+ 
+         case eStatue :
+              PipelineStatue();
+         break;
+
+         default :
+              std::cout <<  mStrType  << " : not supported for now\n";
+              ELISE_ASSERT(false,"Unsupported value in C3DC");
+         break;
+    }
+
+}
+
+
+int C3DC_main(int argc,char ** argv)
+{
+    cAppli_C3DC anAppli(argc,argv,true);
+    anAppli.DoAll();
+    return EXIT_SUCCESS;
+}
+
+
+int MPI_main(int argc,char ** argv)
+{
+    cAppli_C3DC anAppli(argc,argv,false);
+    anAppli.DoAll();
+    return EXIT_SUCCESS;
+}
+
+
+//====================================================
+
+class cChantierFromMPI
+{
+     public :
+       cChantierFromMPI(const std::string &,double aScale);
+         
+       cMMByImNM *    mMMI;
+       std::string    mOri;
+       std::string    mStrImOri;
+       std::string    mStrType;
+       std::string    mFullDirPIm;
+       std::string    mFullDirChantier;
+};
+
+
+cChantierFromMPI::cChantierFromMPI(const std::string & aStr,double aScale) :
+    mMMI             (cMMByImNM::FromExistingDirOrMatch(aStr,aScale)),
+    mOri             (mMMI->Etat().NameOri().ValWithDef("")),
+    mStrImOri        (std::string(" ") + mMMI->KeyFileLON() + " " + mOri),
+    mStrType         (mMMI->NameType()),
+    mFullDirPIm      (mMMI->FullDir()),
+    mFullDirChantier (mMMI->DirGlob())
+{
+    if (mOri=="")
+    {
+        std::cout << "For Name=" << aStr  << " Scale=" << aScale << "\n";
+        ELISE_ASSERT(false,"Reused PIMs was not correctly terminated");
+    }
+}
+
+
+//====================================================
+
+class cAppli_MPI2Ply
+{
+     public :
+         cAppli_MPI2Ply(int argc,char ** argv);
+         void DoAll();
+
+     private :
+         std::string mName;
+         double      mDS;
+         cChantierFromMPI * mCFPI;
+         std::string mMergeOut;
+         std::string mComNuageMerge;
+         std::string mComCatPly;
+};
+
+
+cAppli_MPI2Ply::cAppli_MPI2Ply(int argc,char ** argv):
+    mDS (1.0)
+{
+   ElInitArgMain
+   (
+        argc,argv,
+        LArgMain()  << EAMC(mName,"Dir or PMI-Type (QuickMac ....)"),
+        LArgMain()
+                    << EAM(mDS,"DS",true,"Dowscale, Def=1.0")
+                    << EAM(mMergeOut,"Out",true,"Ply File Results")
+    );
+     
+    mCFPI = new cChantierFromMPI(mName,mDS);
+ 
+    mComNuageMerge =       MM3dBinFile("TestLib  MergeCloud ")
+                  +   mCFPI-> mStrImOri 
+                  + " ModeMerge=" + mCFPI->mStrType
+                  +  " DownScale=" +ToString(mDS)
+               ;
+
+
+   if (! EAMIsInit(&mMergeOut)) mMergeOut =  mCFPI->mFullDirChantier+"C3DC_"+ mCFPI->mStrType + ".ply";
+   mComCatPly =  MM3dBinFile("MergePly ") + QUOTE( mCFPI->mFullDirPIm + ".*Merge.*ply") + " Out="  + mMergeOut;
+
+}
+
+void cAppli_MPI2Ply::DoAll()
+{
+   System(mComNuageMerge);
+   System(mComCatPly);
+}
+
+int MPI2Ply_main(int argc,char ** argv)
+{
+    cAppli_MPI2Ply anAppli(argc,argv);
+    anAppli.DoAll();
+    return EXIT_SUCCESS;
+}
+
+//====================================================
+
+class cAppli_MPI2Mnt
+{
+     public :
+         cAppli_MPI2Mnt(int argc,char ** argv);
+         void DoAll();
+
+     private :
+         std::string mName;
+         double      mDS;
+         cChantierFromMPI * mCFPI;
+         std::string mComMNTMNT;
+         std::string mRep;
+};
+
+cAppli_MPI2Mnt::cAppli_MPI2Mnt(int argc,char ** argv) :
+    mDS  (1.0)
+{
+   ElInitArgMain
+   (
+        argc,argv,
+        LArgMain()  << EAMC(mName,"Dir or PMI-Type (QuickMac ....)"),
+        LArgMain()
+                    << EAM(mDS,"DS",true,"Dowscale, Def=1.0")
+                    << EAM(mRep,"Repere",true,"Repair (Euclid or Cyl)")
+   );
+     
+}
+
+
+int MPI2Mnt_main(int argc,char ** argv)
+{
+    return EXIT_SUCCESS;
+}
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant �  la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  �  l'utilisation,  �  la modification et/ou au
+développement et �  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe �
+manipuler et qui le réserve donc �  des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
+logiciel �  leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
