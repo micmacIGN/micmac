e6e7472c7b7c011adf8dfbc705ebe3a418e9e01a
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2015-01-22 17:56:53 +0100

    GpGpu : CorMultiScale

---
+++ applis/OLD-Apero-OLD/Apero.cpp
@@ -0,0 +1,162 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+   
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in 
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte 
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#include "general/all.h"
+#include "private/all.h"
+#include "Apero.h"
+
+using namespace NS_ParamApero;
+
+extern bool L2SYM;
+
+void Apero_Banniere()
+{
+    std::cout <<  "\n";
+    std::cout <<  " *********************************\n";
+    std::cout <<  " *     A-erotriangulation        *\n";
+    std::cout <<  " *     P-hotogrammetrique        *\n";
+    std::cout <<  " *     E-xperimentale            *\n";
+    std::cout <<  " *     R-elativement             *\n";
+    std::cout <<  " *     O-perationelle            *\n";
+    std::cout <<  " *********************************\n\n";
+}
+
+extern const char * theNameVar_ParamApero[];
+
+#if ELISE_windows
+int __cdecl main(int argc,char ** argv)
+#else
+int main(int argc,char ** argv)
+#endif 
+{
+{
+   MMD_InitArgcArgv(argc,argv);
+
+//for (int aK=0 ; aK<20 ; aK++)
+//std::cout << "RRRRRRRRRRRRRRRRRRRRRRAAAAAAAAAAAAAAAAAAAAAAAAYYON UTILE \n";
+   AddEntryStringifie
+   (
+#if ELISE_windows
+        "include\\XML_GEN\\ParamApero.xml",
+#else
+		"include/XML_GEN/ParamApero.xml",
+#endif
+         theNameVar_ParamApero,
+         true
+   );
+  // cAppliApero * anAppli = cAppliMICMAC::Alloc(argc,argv,eAllocAM_STD);
+
+  //if (0) delete anAppli;
+    
+   ELISE_ASSERT(argc>=2,"Not enough arg");
+
+   //  cElXMLTree aTree(argv[1]);  : A priori inutile depuis la stringification
+
+
+   std::string aNameSauv = "SauvApero.xml";
+
+   cResultSubstAndStdGetFile<cParamApero> aP2 
+                                          ( argc-2,argv+2,
+											argv[1],
+											StdGetFileXMLSpec("ParamApero.xml"),
+											"ParamApero",
+											"ParamApero",
+                                            "DirectoryChantier",
+                                            "FileChantierNameDescripteur",
+                                            aNameSauv.c_str() );
+
+   ::DebugPbCondFaisceau = aP2.mObj->DebugPbCondFaisceau().Val();
+
+   L2SYM = aP2.mObj->AllMatSym().Val();
+   cAppliApero   anAppli (aP2);
+
+   if (anAppli.ModeMaping())
+   {
+       anAppli.DoMaping(argc,argv);
+   }
+   else
+   {
+      anAppli.DoCompensation();
+   }
+
+
+   Apero_Banniere();
+
+   cElWarning::ShowWarns(anAppli.DC() + "WarnApero.txt");
+
+}
+   ShowFClose();
+   return 0;
+
+}
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
---
+++ src/uti_phgrm/GpGpu/GpGpu_Tools.cpp
@@ -0,0 +1,175 @@
+#include "GpGpu/GpGpu_Tools.h"
+
+void GpGpuTools::SetParamterTexture(textureReference &textRef)
+{
+    textRef.addressMode[0]	= cudaAddressModeBorder;
+    textRef.addressMode[1]	= cudaAddressModeBorder;
+    textRef.filterMode		= cudaFilterModeLinear; //cudaFilterModePoint cudaFilterModeLinear
+    textRef.normalized		= false;
+}
+
+std::string GpGpuTools::GetImagesFolder()
+{
+
+#ifdef _WIN32
+
+	TCHAR name [ UNLEN + 1 ];
+	DWORD size = UNLEN + 1;
+	GetUserName( (TCHAR*)name, &size );
+
+	std::string suname = name;
+	std::string ImagesFolder = "C:\\Users\\" + suname + "\\Pictures\\";
+#else
+	struct passwd *pw = getpwuid(getuid());
+
+	const char *homedir = pw->pw_dir;
+
+	std::string ImagesFolder = std::string(homedir) + "/Images/";
+
+#endif
+
+	return ImagesFolder;
+}
+
+void GpGpuTools::OutputReturn( char * out )
+{
+
+#ifndef DISPLAYOUTPUT
+	return;
+#else
+	
+	std::cout << std::string(out) << "\n";
+
+#endif
+
+}
+
+void GpGpuTools::OutputGpu()
+{
+#if (ELISE_windows)
+	Sleep(10);
+	std::cout << "\b\\" << std::flush;
+	Sleep(10);
+	std::cout << "\b|" << std::flush;
+	Sleep(10);
+	std::cout << "\b/" << std::flush;
+	Sleep(10);
+	std::cout << "\b-" << std::flush;
+#endif
+}
+
+void GpGpuTools::NvtxR_Push(const char* message, int32_t color)
+{
+#ifdef NVTOOLS
+    nvtxEventAttributes_t initAttrib = {0};
+
+    initAttrib.version = NVTX_VERSION;
+    initAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
+    initAttrib.color = color;
+    initAttrib.colorType = NVTX_COLOR_ARGB;
+    initAttrib.message.ascii = message;
+    initAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII;
+
+    nvtxRangePushEx(&initAttrib);
+#endif
+}
+
+void GpGpuTools::Nvtx_RangePop()
+{
+#ifdef NVTOOLS
+	nvtxRangePop();
+#endif
+}
+
+float GpGpuTools::fValue( float value,float factor )
+{
+	return value * factor;
+}
+
+float GpGpuTools::fValue( float2 value,float factor )
+{
+	return (float)value.x * factor;
+}
+
+std::string GpGpuTools::toStr( uint2 tt )
+{
+	stringstream sValS (stringstream::in | stringstream::out);
+
+	sValS << "(" << tt.x << "," << tt.y << ")";
+
+    return sValS.str();
+}
+
+const char *GpGpuTools::conca(const char *texte, int t)
+{
+    stringstream sValS (stringstream::in | stringstream::out);
+
+    sValS << texte << t;
+
+    return sValS.str().c_str();
+}
+
+//void GpGpuTools::OutputInfoGpuMemory()
+//{
+//	size_t free;
+//	size_t total;
+//    checkCudaErrors( cudaMemGetInfo(&free, &total));
+//    cout << "Memoire video       : " << (float)free / pow(2.0f,20) << " / " << (float)total / pow(2.0f,20) << "Mo" << endl;
+//}
+/*
+void GpGpuTools::check_Cuda()
+{
+    cout << "CUDA build enabled\n";
+
+//    int apiVersion = 0;
+
+//    cudaRuntimeGetVersion(&apiVersion);
+
+            //DUMP_INT(apiVersion)
+
+//	switch (__CUDA_API_VERSION)
+//	{
+//	case 0x3000:
+//		cout << "3.0";
+//		break;
+//	case 0x3020:
+//		cout << "3.2";
+//		break;
+//	case 0x4000:
+//		cout << "4.0";
+//		break;
+//	case 0x5000:
+//		cout << "5.0";
+//		break;
+//	case 0x5050:
+//		cout << "5.5";
+//		break;
+//	case 0x6000:
+//		cout << "6.0";
+//		break;
+//	}
+//	cout << endl;
+
+	int device_count = 0;
+	 
+	checkCudaErrors(cudaGetDeviceCount(&device_count));
+
+	if(device_count == 0)
+        printf("NO NVIDIA GRAPHIC CARD FOR USE CUDA");
+	else
+	{
+
+		// Creation du contexte GPGPU
+		cudaDeviceProp deviceProp;
+		// Obtention de l'identifiant de la carte la plus puissante
+		int devID = gpuGetMaxGflopsDeviceId();
+
+		// Initialisation du contexte
+		checkCudaErrors(cudaSetDevice(devID));
+		// Obtention des proprietes de la carte
+		checkCudaErrors(cudaGetDeviceProperties(&deviceProp, devID));
+		// Affichage des proprietes de la carte
+		printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID, deviceProp.name, deviceProp.major, deviceProp.minor);
+
+	}
+}*/
