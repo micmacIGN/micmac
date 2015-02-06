e4ee0721d5daeeda55a919b381276f41a14b0194
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2015-02-06 12:14:55 +0100

    SaisieQT : cmake modif

---
+++ src/saisieQT/Data.cpp
@@ -0,0 +1,249 @@
+#include "Data.h"
+
+cData::cData()
+{
+    reset();
+}
+
+cData::~cData()
+{
+    clearAll();
+}
+
+void cData::addCloud(GlCloud * aCloud)
+{
+    _Clouds.push_back(aCloud);
+}
+
+void cData::addObject(cObject * aObj)
+{
+    _vPolygons.push_back((cPolygon*) aObj);
+}
+
+void cData::replaceCloud(GlCloud *cloud, int id)
+{
+    if(id < _Clouds.size())
+    {
+        _Clouds[id] = cloud;
+        computeBBox(id);
+        computeCloudsCenter(id);
+    }
+}
+
+void cData::addReplaceCloud(GlCloud *cloud, int id)
+{
+    if(id < _Clouds.size())
+        _Clouds[id] = cloud;
+    else
+        addCloud(cloud);
+
+    computeBBox(id);
+    computeCloudsCenter(id);
+}
+
+void cData::addCamera(CamStenope * aCam)
+{
+    _Cameras.push_back(aCam);
+}
+
+void cData::pushBackMaskedImage(QMaskedImage *maskedImage)
+{
+
+    _MaskedImages.push_back(maskedImage);
+
+}
+
+void cData::clearClouds()
+{
+    qDeleteAll(_Clouds);
+    _Clouds.clear();
+
+    reset();
+}
+
+void cData::clearCameras()
+{
+    qDeleteAll(_Cameras);
+
+    _Cameras.clear();
+
+    reset();
+}
+
+void cData::clearImages()
+{
+    _MaskedImages.clear();
+    reset();
+}
+
+void cData::clearObjects()
+{
+    qDeleteAll(_vPolygons);
+
+    _vPolygons.clear();
+
+    reset();
+}
+
+void cData::clearAll()
+{
+    clearClouds();
+    clearCameras();
+    clearImages();
+    clearObjects();
+}
+
+void cData::clear(int aK)
+{
+    if (_Clouds.size())
+    {
+        if(_Clouds[aK])
+        {
+            delete _Clouds[aK];
+            _Clouds[aK] = NULL;
+        }
+    }
+    if (_Cameras.size())
+    {
+        if(_Cameras[aK])
+        {
+            delete _Cameras[aK];
+            _Cameras[aK] = NULL;
+        }
+    }
+	if (_MaskedImages.size())
+	{
+		if(_MaskedImages[aK])
+			delete _MaskedImages[aK];
+	}
+}
+
+int cData::idPolygon(cPolygon *polygon)
+{
+    return _vPolygons.indexOf(polygon);
+}
+
+void cData::reset()
+{
+    _min.x = _min.y = _min.z =  FLT_MAX;
+    _max.x = _max.y = _max.z = -FLT_MAX;
+}
+
+void cData::cleanCameras()
+{
+    _Cameras.clear();
+}
+
+void cData::deleteCloud(int idCloud)
+{
+   GlCloud* cloud = getCloud(idCloud);
+   if(cloud)
+       delete cloud;
+}
+
+int cData::getCloudsSize()
+{
+    int sizeClouds = 0;
+    for (int aK=0; aK < _Clouds.size();++aK)
+        sizeClouds += _Clouds[aK]->size();
+
+    return sizeClouds;
+}
+
+void cData::getMinMax(Pt3dr pt)
+{
+    if (pt.x > _max.x) _max.x = pt.x;
+    if (pt.x < _min.x) _min.x = pt.x;
+    if (pt.y > _max.y) _max.y = pt.y;
+    if (pt.y < _min.y) _min.y = pt.y;
+    if (pt.z > _max.z) _max.z = pt.z;
+    if (pt.z < _min.z) _min.z = pt.z;
+}
+
+//compute bounding box
+void cData::computeBBox(int idCloud)
+{
+    for (int bK=0; bK < _Clouds.size();++bK)
+    {
+        if(idCloud == -1 || bK == idCloud)
+        {
+            GlCloud * aCloud = _Clouds[bK];
+
+            for (int aK=0; aK < aCloud->size(); ++aK)
+            {
+                getMinMax(aCloud->getVertex(aK).getPosition());
+            }
+        }
+    }
+
+    if(idCloud == -1)
+    for (int  cK=0; cK < _Cameras.size();++cK)
+    {
+        CamStenope * aCam= _Cameras[cK];
+
+        QVector <Pt3dr> vert;
+        Pt3dr c1, c2, c3, c4;
+
+        aCam->Coins(c1,c2,c3,c4,1.f);
+        vert.push_back(aCam->VraiOpticalCenter());
+        vert.push_back(c1);
+        vert.push_back(c2);
+        vert.push_back(c3);
+        vert.push_back(c4);
+
+        for (int aK=0; aK < vert.size(); ++aK)
+        {
+            getMinMax(vert[aK]);
+        }
+    }
+}
+
+// compute BBox center
+Pt3dr cData::getBBoxCenter()
+{
+    return Pt3dr((_min.x + _max.x) * .5f, (_min.y + _max.y) * .5f, (_min.z + _max.z) * .5f);
+}
+
+// compute BB max size
+float cData::getBBoxMaxSize()
+{
+    return max(_max.x-_min.x, max(_max.y-_min.y, _max.z-_min.z));
+}
+
+//compute data centroid
+void cData::computeCloudsCenter(int idCloud)
+{
+    Pt3dr sum(0.,0.,0.);
+    int cpt = 0;
+
+    for (int bK=0; bK < _Clouds.size();++bK)
+    {
+        if(idCloud == -1 || bK == idCloud)
+        {
+            GlCloud * aCloud = _Clouds[bK];
+
+            sum = sum + aCloud->getSum();
+            cpt += aCloud->size();
+        }
+    }
+
+    if(idCloud == -1)
+    for (int  cK=0; cK < _Cameras.size();++cK)
+    {
+        CamStenope * aCam= _Cameras[cK];
+
+        Pt3dr c1, c2, c3, c4;
+
+        aCam->Coins(c1,c2,c3,c4,1.f);
+        sum = sum + aCam->VraiOpticalCenter();
+        sum = sum + c1;
+        sum = sum + c2;
+        sum = sum + c3;
+        sum = sum + c4;
+
+        cpt += 5;
+    }
+
+    _centroid = sum / cpt;
+}
+
---
+++ src/uti_files/CPP_Ori2Xml.cpp
@@ -0,0 +1,162 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#include "StdAfx.h"
+
+
+/*********************************************/
+/*                                           */
+/*                ::                         */
+/*                                           */
+/*********************************************/
+
+
+int Ori2XML_main(int argc,char ** argv)
+{
+    MMD_InitArgcArgv(argc,argv);
+
+    std::string aFullOri,anOut;
+    std::string toto;
+
+
+    ElInitArgMain
+    (
+           argc,argv,
+           LArgMain() << EAMC(aFullOri,"Full pattern", eSAM_IsPatFile)
+                      << EAMC(anOut,"Dir for result"),
+           LArgMain() << EAM(toto,"toto",true, "todo", eSAM_InternalUse)
+    );
+
+    std::string aDir,aFileOriIn;
+    SplitDirAndFile(aDir,aFileOriIn,aFullOri);
+
+    cInterfChantierNameManipulateur * anICNM = cInterfChantierNameManipulateur::BasicAlloc(aDir);
+
+   std::string aKeyOut = "NKS-Assoc-Im2Orient@-" + anOut;
+
+
+    const std::vector<std::string> * aVName = anICNM->Get(aFileOriIn);
+
+    for (int aK=0 ; aK<int(aVName->size()) ; aK++)
+    {
+        std::string aNameIn = (*aVName)[aK];
+        std::string aNameFile = StdPrefix(aNameIn) + ".tif";
+
+        CamStenope * aCam = CamStenope::StdCamFromFile(true,aNameIn,anICNM);
+        // CamStenope * aCam = CamOrientGenFromFile(aNameIn,anICNM);
+
+
+        std::string aNameOut = anICNM->Assoc1To1(aKeyOut,aNameFile,true);
+        std::cout << "FFF= " << aCam->Focale() << " " << aNameOut  << "\n";
+        //cCalibrationInternConique  aCIO = aCam->ExportCalibInterne2XmlStruct(aCam->Sz());
+        cOrientationConique anOC = aCam->StdExportCalibGlob();
+
+        MakeFileXML(anOC,aDir+aNameOut);
+    }
+
+/*
+    CamStenope * aCam = CamStenope::StdCamFromFile(true,aFileOriIn,anICNM);
+
+    std::cout << "FFF= " << aCam->Focale() << "\n";
+
+
+    if (aFileOriOut=="")
+    {
+        aFileOriOut =StdPrefix(aFileOriIn) + ".xml";
+    }
+
+    Ori3D_Std anOri(aFileOriIn.c_str());
+    if (aCal=="")
+    {
+       ElRotation3D aR = anOri.GetOrientation();
+       XML_SauvFile(aR,aFileOriOut,"Ori2XML",true);
+    }
+    else
+    {
+        CamStenope * aCS=0;
+        aCS = Std_Cal_From_File(aCal);
+
+
+       aCS->SetOrientation(anOri.GetOrientation().inv());
+       cOrientationConique anOC = aCS->ExportCalibGlob(anOri.SzIm(),1,1,3,true,(char *)0);
+
+       MakeFileXML(anOC,aFileOriOut);
+    }
+*/
+
+    return 0;
+}
+
+
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant �  la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  �  l'utilisation,  �  la modification et/ou au
+développement et �  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe �
+manipuler et qui le réserve donc �  des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
+logiciel �  leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
