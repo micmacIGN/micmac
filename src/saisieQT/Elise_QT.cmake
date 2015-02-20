0908a1a0d1eccfea51784039c97f19e419d093b9
deseilligny <deseilligny@users.noreply.github.com>
2015-02-20 14:13:33 +0100

    Dans PIMs2Mnt

---
+++ src/saisieQT/Elise_QT.cmake
@@ -0,0 +1,117 @@
+# Interface Qt au sein d'Elise
+
+if ( NOT ${qt_version} EQUAL 0 )
+
+#    INCLUDE_DIRECTORIES(${SAISIE_DIR}/include_QT)
+
+
+INCLUDE_DIRECTORIES(${SAISIE_DIR}/include_QT)
+#INCLUDE_DIRECTORIES(${SAISIE_DIR})
+
+    set(vmm_SRCS    ${SAISIE_DIR}/GLWidget.cpp
+                    ${SAISIE_DIR}/saisieQT_window.cpp
+                    ${SAISIE_DIR}/Cloud.cpp
+                    ${SAISIE_DIR}/Data.cpp
+                    ${SAISIE_DIR}/Engine.cpp
+                    ${SAISIE_DIR}/3DObject.cpp
+                    ${SAISIE_DIR}/cgldata.cpp
+                    ${SAISIE_DIR}/GLWidgetSet.cpp
+                    ${SAISIE_DIR}/MatrixManager.cpp
+                    ${SAISIE_DIR}/HistoryManager.cpp
+                    ${SAISIE_DIR}/ContextMenu.cpp
+                    ${SAISIE_DIR}/Settings.cpp
+                    ${SAISIE_DIR}/QT_interface_Elise.cpp
+                    ${SAISIE_DIR}/Tree.cpp
+                    ${SAISIE_DIR}/mmglu.cpp)
+
+if ( ${qt_version} EQUAL 4)
+    set(vmm_SRCS ${vmm_SRCS} ${SAISIE_DIR}/gl_core_2_1.c)
+endif()
+
+    set( HEADERS_nomoc
+       ${SAISIE_DIR}/include_QT/Elise_QT.h
+       ${SAISIE_DIR}/include_QT/HistoryManager.h
+       ${SAISIE_DIR}/include_QT/MatrixManager.h
+       ${SAISIE_DIR}/include_QT/Cloud.h
+       ${SAISIE_DIR}/include_QT/saisieQT_main.h
+       ${SAISIE_DIR}/include_QT/Data.h
+       ${SAISIE_DIR}/include_QT/SaisieGlsl.glsl
+       ${SAISIE_DIR}/include_QT/Engine.h
+       ${SAISIE_DIR}/include_QT/GLWidgetSet.h
+       ${SAISIE_DIR}/include_QT/3DObject.h
+       ${SAISIE_DIR}/include_QT/cgldata.h
+       ${SAISIE_DIR}/include_QT/mmglu.h )
+
+    set( HEADERS_tomoc
+       ${SAISIE_DIR}/include_QT/GLWidget.h
+       ${SAISIE_DIR}/include_QT/saisieQT_window.h
+       ${SAISIE_DIR}/include_QT/ContextMenu.h
+       ${SAISIE_DIR}/include_QT/Settings.h
+       ${SAISIE_DIR}/include_QT/QT_interface_Elise.h
+       ${SAISIE_DIR}/include_QT/Tree.h
+    )
+
+    set (ui_toWrap
+     ${SAISIE_DIR}/ui/saisieQT_window.ui
+     ${SAISIE_DIR}/ui/Settings.ui
+     ${SAISIE_DIR}/ui/Help.ui
+    )
+
+   set (FILES_TO_TRANSLATE ${FILES_TO_TRANSLATE} ${vmm_SRCS} ${ui_toWrap} ${HEADERS_nomoc} ${HEADERS_Tomoc})
+
+   if ( ${qt_version} EQUAL 5 )
+
+      set(CMAKE_INCLUDE_CURRENT_DIR ON)
+
+      QT5_WRAP_CPP(HEADERS_moced ${HEADERS_tomoc} )
+
+      QT5_ADD_RESOURCES( RC_SRCS ${SAISIE_DIR}/icones/icones.qrc )
+
+      qt5_wrap_ui(saisie_ui ${ui_toWrap})
+
+      add_definitions(-DTWEAK)
+
+      if ( WIN32 )
+         add_definitions(-DELISE_windows)
+      ENDIF()
+
+      find_package(Qt5Core REQUIRED)
+
+      if( Qt5Core_FOUND )
+
+        include_directories(${Qt5Widgets_INCLUDE_DIRS})
+        include_directories(${Qt5Core_INCLUDE_DIRS})
+        include_directories(${Qt5Concurrent_INCLUDE_DIRS})
+        include_directories(${Qt5OpenGL_INCLUDE_DIRS})
+        include_directories(${Qt5Xml_INCLUDE_DIRS})
+        include_directories(${Qt5Gui_INCLUDE_DIRS})
+
+         # Use the compile definitions defined in the Qt 5 Widgets module
+         add_definitions(${Qt5Widgets_DEFINITIONS})
+
+        # Add compiler flags for building executables (-fPIE)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${Qt5Widgets_EXECUTABLE_COMPILE_FLAGS}")
+
+      endif()
+
+   elseif ( ${qt_version} EQUAL 4 )
+
+      QT4_ADD_RESOURCES( RC_SRCS ${SAISIE_DIR}/icones/icones.qrc )
+
+      QT4_WRAP_UI(saisie_ui ${ui_toWrap})
+      QT4_WRAP_CPP(HEADERS_moced ${HEADERS_tomoc} )
+
+      INCLUDE(${QT_USE_FILE})
+      include_directories( ${PROJECT_BINARY_DIR} )
+      INCLUDE_DIRECTORIES(${CMAKE_CURRENT_BINARY_DIR})
+   endif()
+
+   set(QT_ALLFILES ${vmm_SRCS} ${RC_SRCS} ${HEADERS_moced} ${HEADERS_tomoc} ${HEADERS_nomoc} ${saisie_ui})
+   
+   SOURCE_GROUP(QT\\ui FILES ${saisie_ui})
+   SOURCE_GROUP(QT\\include FILES ${HEADERS_nomoc})
+   SOURCE_GROUP(QT\\include FILES ${HEADERS_tomoc})
+   SOURCE_GROUP(QT\\src FILES ${RC_SRCS})
+   SOURCE_GROUP(QT\\src FILES ${vmm_SRCS})
+
+endif()
---
+++ src/saisieQT/include_QT/Settings.h
@@ -0,0 +1,254 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include "StdAfx.h"
+
+#include "Elise_QT.h"
+
+namespace Ui {
+class SettingsDialog;
+class HelpDialog;
+}
+
+//Min and max zoom ratio (relative)
+const float GL_MAX_ZOOM = 50.f;
+const float GL_MIN_ZOOM = 0.01f;
+
+float zoomClip(float val);
+
+//! Interface mode
+enum APP_MODE { BOX2D,          /**< BOX 2D mode **/
+                MASK2D,         /**< Image mask mode  **/
+                MASK3D,         /**< Point cloud mask **/
+                POINT2D_INIT,	/**< Points in Image (SaisieAppuisInit) - uses cAppli_SaisiePts **/
+                POINT2D_PREDIC, /**< Points in Image (SaisieAppuisPredic) - uses cAppli_SaisiePts **/
+                BASC            /**< 2 lines and 1 point (SaisieBasc) - uses cAppli_SaisiePts **/
+};
+typedef enum
+{
+   eEnglish = 0,
+   eFrench  = 1,
+   eSpanish = 2,
+   /*eChinese = 3,
+   eArabic  = 4,
+   eRussian = 5,*/
+   eEsperanto
+} eLANG;
+
+typedef enum
+{
+   eCentroid     = 0,
+   eBBoxCenter   = 1,
+   eOriginCenter = 2,
+   eDefault
+} eSceneCenterType;
+
+typedef enum
+{
+   eNavig_Ball,
+   eNavig_Orbital,
+} eNavigationType;
+
+
+
+string eToString(const eLANG& langue);
+
+class cParameters
+{
+public:
+    cParameters();
+    ~cParameters(){}
+
+    //! Setters
+    void setFullScreen(bool aBool)      { _fullScreen = aBool;   }
+    void setPosition(QPoint pos)        { _position = pos;       }
+    void setNbFen(QPoint const &aNbFen) { _nbFen = aNbFen;       }
+    void setSzFen(QSize aSzFen)         { _szFen = aSzFen;       }
+
+    void setZoomWindowValue(int aZoom)  { _zoomWindow = aZoom;   }
+    void setDefPtName(QString name)     { _ptName  = name;       }
+    void setPostFix(QString name)       { _postFix = name;       }
+
+    void setLineThickness(float val)    { _lineThickness = val;  }
+    void setPointDiameter(float val)    { _pointDiameter = val;  }
+    void setGamma(float val)            { _gamma = val;          }
+    void setForceGray(bool val)         { _forceGray = val;      }
+    void setShowMasks(bool val)         { _showMasks = val;      }
+    void setCenterType(int val)         { _sceneCenterType = val; }
+
+    void setSelectionRadius(int val)    { _radius = val;         }
+    void setShiftStep(float val)        {_shiftStep = val;       }
+
+    void setPtCreationMode(eTypePts mode){ _eType = mode;        }
+    void setPtCreationWindowSize(double sz){ _sz = sz;           }
+
+    void setLanguage(int lang)          { _lang = lang;          }
+
+    //! Getters
+    bool    getFullScreen()             { return _fullScreen;    }
+    QPoint  getPosition()               { return _position;      }
+    QPoint  getNbFen()                  { return _nbFen;         }
+    QSize   getSzFen()                  { return _szFen;         }
+
+    float   getZoomWindowValue()        { return _zoomWindow;    }
+    QString getDefPtName()              { return _ptName;        }
+    QString getPostFix()                { return _postFix;       }
+
+    float getLineThickness()            { return _lineThickness; }
+    float getPointDiameter()            { return _pointDiameter; }
+    float getGamma()                    { return _gamma;         }
+    bool  getForceGray()                { return _forceGray;     }
+    bool  getShowMasks()                { return _showMasks;     }
+    int   getSceneCenterType()          { return _sceneCenterType; }
+
+    int   getSelectionRadius()          { return _radius;        }
+    float getShiftStep()                { return _shiftStep;     }
+
+    eTypePts getPtCreationMode()        { return _eType;         }
+    double getPtCreationWindowSize()    { return _sz;            }
+
+    int    getLanguage()                { return _lang;          }
+
+    //! Copy operator
+    cParameters& operator =(const cParameters& params);
+
+    //! Comparison operator
+    bool operator != (cParameters &p);
+
+    void    read();
+    void    write();
+
+	eNavigationType eNavigation() const;
+	void setENavigation(const eNavigationType& eNavigation);
+
+private:
+	//! Main window parameters
+	bool        _fullScreen;
+	QPoint      _position;
+    QPoint      _nbFen;
+    QSize       _szFen;
+
+    //! Drawing settings
+    float       _lineThickness;
+    float       _pointDiameter;
+    float       _gamma;
+    bool        _forceGray;
+    bool        _showMasks;
+    int         _sceneCenterType;
+
+    //! Other parameters
+    float       _zoomWindow;
+    QString     _ptName;
+    QString     _postFix;
+    int         _radius;
+    float       _shiftStep;
+
+    //! Point creation mode
+    eTypePts    _eType;
+	eNavigationType _eNavigation;
+    double      _sz;
+
+    //! Language
+    int         _lang;
+};
+
+//! Dialog to setup display settings
+class cSettingsDlg : public QDialog
+{
+    Q_OBJECT
+
+public:
+
+    //! Default constructor
+    cSettingsDlg(QWidget* parent, cParameters *params, int appMode);
+    ~cSettingsDlg();
+
+    void setParameters(cParameters &params);
+
+    void enableMarginSpinBox(bool show = true);
+
+signals:
+    void lineThicknessChanged(float);
+    void pointDiameterChanged(float);
+    void gammaChanged(float);
+    void forceGray(bool);
+    void showMasks(bool);
+    void zoomWindowChanged(float);
+    void selectionRadiusChanged(int);
+    void prefixTextEdit(QString);
+    void shiftStepChanged(float);
+    void setCenterType(int);
+	void setNavigationType(int);
+    void langChanged(int);
+
+protected slots:
+
+    void on_okButton_clicked();
+    void on_cancelButton_clicked();
+
+    //!drawing settings
+    void on_LineThickness_doubleSpinBox_valueChanged(double);
+    void on_PointDiameter_doubleSpinBox_valueChanged(double);
+    void on_GammaDoubleSpinBox_valueChanged(double);
+    void on_forceGray_checkBox_toggled(bool);
+    void on_showMasks_checkBox_toggled(bool);
+    void on_radioButton_centroid_toggled(bool);
+    void on_radioButton_bbox_center_toggled(bool);
+    void on_radioButton_origin_center_toggled(bool);
+	void on_radioButtonBall_toggled(bool val);
+	void on_radionButtonOrbital_toggled(bool);
+
+
+    //!other display settings
+    void on_zoomWin_spinBox_valueChanged(int);
+    void on_RadiusSpinBox_valueChanged(int);
+    void on_shiftStep_doubleSpinBox_valueChanged(double);
+    void on_PrefixTextEdit_textChanged(QString);
+
+    //!point creation mode
+    void on_radioButtonStd_toggled(bool);
+    void on_radioButtonMin_toggled(bool);
+    void on_radioButtonMax_toggled(bool);
+    void on_doubleSpinBoxSz_valueChanged(double);
+
+     //!translation
+    void on_comboBox_activated(int);
+
+protected:
+
+    //! Refreshes dialog to reflect new settings values
+    void refresh();
+
+    //! settings
+    cParameters *_parameters;
+
+    Ui::SettingsDialog* _ui;
+
+    bool    lineItemHidden;
+
+    int    _appMode;
+};
+
+class cHelpDlg : public QDialog
+{
+    Q_OBJECT
+
+public:
+
+    //! Default constructor
+    cHelpDlg(QString title, QWidget* parent);
+    ~cHelpDlg();
+
+    void populateTableView(const QStringList &shortcuts, const QStringList &actions);
+
+protected slots:
+
+    void on_okButton_clicked();
+
+protected:
+
+    Ui::HelpDialog*         _ui;
+};
+
+
+#endif
