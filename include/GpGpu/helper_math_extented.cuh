76a406b28700f1742c79ef9d6720fe754bc42426
G?rald Choqueux <G?rald Choqueux@localhost>
2015-02-24 18:21:08 +0100

    GpGpu : meta programation

--- include/GpGpu/helper_math_extented.cuh
+++ include/GpGpu/helper_math_extented.cuh
@@ -191,73 +191,104 @@ SUPPRESS_NOT_USED_WARN static int iDivUp32(uint a)
     //return (a % b != 0) ? (a / b + 1) : (a / b);
 }
 
-template<int b,class T>
-T __div(T a)
+template<int val>
+int __nBitRotation()
 {
-	return a / b;
+	printf("ERROR __nBitRotation no define for %d\n",val);
+	return 0;
 }
 
-//template<class T>
-//T __div<32,T>(T a)
-//{
-//	return a >> 5;
-//}
+#define __div_mult(val,rot) \
+	template<> \
+	inline int __nBitRotation<val>()\
+{\
+	return rot;\
+	}\
+	template<typename T2>\
+	struct Bar<val, T2>\
+{\
+	T2 __opDiv( T2 const& t2)\
+{\
+	return  t2>>rot ;\
+	}\
+	T2 __opMult( T2 const& t2)\
+{\
+	return  t2<<rot ;\
+	}\
+	T2 _iDivUp( T2 const& a)\
+{\
+	const T2 div = __opDiv(a);\
+	return ((a - (__opMult(div))) != 0) ? (div + 1) : (div);\
+	}\
+	T2 _modulo( T2 const& a)\
+	{\
+		const T2 div = __opDiv(a);\
+		return a - __opMult(div);\
+	}\
+	};
 
-template<typename T1, typename T2>
+template<int T1, typename T2>
 struct Bar
 {
- void operator()(T1 const& t1, T2 const& t2)
+ T2 __opDiv( T2 const& t2)
  {
-	std::cerr << "In Bar<T1, T2>(" << t1 << ", " << t2 << ")\n";
+	return t2/T1 ;
  }
+ T2 __opMult( T2 const& t2)
+ {
+	return  t2*T1 ;
+ }
+ T2 _iDivUp( T2 const& a)
+ {
+	 const T2 div = __opDiv(a);
+	 return ((a - (__opMult(div))) != 0) ? (div + 1) : (div);
+ }
+ T2 _modulo( T2 const& a)
+ {
+	 const T2 div = __opDiv(a);
+	 return a - __opMult(div);
+ }
+
 };
 
-template<typename T2>
-struct Bar<int, T2>
-{
- void operator()(int t1, T2 const& t2)
- {
-	std::cerr << "In Bar<int, T2>(" << t1 << ", " << t2 << ")\n";
- }
-};
+__div_mult(2,1)
+__div_mult(4,2)
+__div_mult(8,3)
+__div_mult(16,4)
+__div_mult(32,5)
+__div_mult(64,6)
+__div_mult(128,7)
+__div_mult(256,8)
+__div_mult(512,9)
+__div_mult(1024,10)
 
-template<typename T1, typename T2>
-void bar(T1 const& t1, T2 const& t2)
+template<int T1, typename T2>
+T2 __div(T2 const& t2)
 {
    Bar<T1, T2> b;
-   b(t1, t2);
+   return b.__opDiv(t2);
 }
 
-//template<>
-//uint __div<uint,32>(uint a)
-//{
-//	return a >> 5;
-//}
-
-template<class T,int b>
-static T __mult(T a)
+template<int T1, typename T2>
+T2 __mult(T2 const& t2)
 {
-	return a * b;
+   Bar<T1, T2> b;
+   return b.__opMult(t2);
 }
 
-
-template<class T,int b>
-static T _iDivUp(T a)
+template<int T1, typename T2>
+T2 __iDivUp(T2 const& t2)
 {
-	T div = __div<T,b>(a);
-	return ((a - (div << b)) != 0) ? (div + 1) : (div);
+   Bar<T1, T2> b;
+   return b._iDivUp(t2);
 }
 
-template<class T,int b>
-static T iDivUp(T a)
+template<int T1, typename T2>
+T2 __mod(T2 const& t2)
 {
-	T div = a / b;
-	return ((a - div * b) != 0) ? (div + 1) : (div);
-	//return (a % b != 0) ? (a / b + 1) : (a / b);
+   Bar<T1, T2> b;
+   return b._modulo(t2);
 }
-
-
-
 
 SUPPRESS_NOT_USED_WARN static uint2 iDivUp(uint2 a, uint b)
 {
