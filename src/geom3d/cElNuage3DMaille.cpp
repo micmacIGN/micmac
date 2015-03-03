976c6b6bff58d3fb2b2f72431e3446fea380dd54
deseilligny <deseilligny@localhost>
2015-03-03 11:39:51 +0100

    Modif ToFOMOriStdRound

--- src/geom3d/cElNuage3DMaille.cpp
+++ src/geom3d/cElNuage3DMaille.cpp
@@ -1970,8 +1970,9 @@ void ToFOMOriStdRound(double & aVal,const double & aResol)
 {
     double aRatio = aVal / aResol;
 
-	cDecimal aDec = StdRound(aRatio);
-    double aIR = aDec.RVal();
+//	cDecimal aDec = StdRound(aRatio);
+//    double aIR = aDec.RVal();
+    double aIR = round_ni(aRatio);
     double aDif = ElAbs(aRatio-aIR);
     // std::cout << "ORI ToFOMStdRound; Dif= " << aDif << "\n";
     ELISE_ASSERT(aDif < 1e-7,"ORI ToFOMStdRound");
