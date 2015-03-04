aa1bc55bebc58702e96e7ec0089b0c7d00587f80
deseilligny <deseilligny@users.noreply.github.com>
2015-03-04 22:40:40 +0100

    Init par MatESS+ Ransac quasi finie

--- include/general/photogram.h
+++ include/general/photogram.h
@@ -3209,6 +3209,10 @@ class cCamStenopeBilin : public CamStenope
 
 
 
+extern double ExactCostMEP(Pt3dr &  anI,const ElRotation3D & aRot,const Pt2dr & aP1,const Pt2dr & aP2,double aTetaMax) ;
+extern double ExactCostMEP(const ElPackHomologue & aPack,const ElRotation3D & aRot,double aTetaMax) ;
+
+
 
 
 #endif // !  _ELISE_GENERAL_PHOTOGRAM_H
