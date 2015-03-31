967a26075d93e2808ac987d894709efd80ea74be
jbelvaux <jbelvaux@localhost>
2015-03-31 14:13:35 +0200

    - ignore les fichiers supprim?s

---
+++ src/uti_phgrm/Apero/Sources.cmake
@@ -0,0 +1,37 @@
+set(uti_phgrm_Apero_Src_Files
+    ${UTI_PHGRM_APERO_DIR}/Basculement.cpp
+    ${UTI_PHGRM_APERO_DIR}/cAA_Compensation.cpp
+    ${UTI_PHGRM_APERO_DIR}/cAA_InitInconnues.cpp
+    ${UTI_PHGRM_APERO_DIR}/cAA_LevenbergMarkard.cpp
+    ${UTI_PHGRM_APERO_DIR}/cAppliApero.cpp
+    ${UTI_PHGRM_APERO_DIR}/CarteProfExport.cpp
+    ${UTI_PHGRM_APERO_DIR}/cCalib.cpp
+    ${UTI_PHGRM_APERO_DIR}/cChoixImagesMicMac.cpp
+    ${UTI_PHGRM_APERO_DIR}/cCorrel.cpp
+    ${UTI_PHGRM_APERO_DIR}/cExport.cpp
+#    ${UTI_PHGRM_APERO_DIR}/cImageAppuis.cpp
+    ${UTI_PHGRM_APERO_DIR}/cLayersIm.cpp
+    ${UTI_PHGRM_APERO_DIR}/cPackAppuis.cpp
+    ${UTI_PHGRM_APERO_DIR}/cPackMultipleLiaison.cpp
+    ${UTI_PHGRM_APERO_DIR}/cPackObsLiaison.cpp
+    ${UTI_PHGRM_APERO_DIR}/cParamApero.cpp
+    ${UTI_PHGRM_APERO_DIR}/cPonderateur.cpp
+    ${UTI_PHGRM_APERO_DIR}/cPose.cpp
+    ${UTI_PHGRM_APERO_DIR}/cImplemBlockCam.cpp
+    ${UTI_PHGRM_APERO_DIR}/cSurfParam.cpp
+    ${UTI_PHGRM_APERO_DIR}/ExportNuage.cpp
+#    ${UTI_PHGRM_APERO_DIR}/GenParamApero.cpp
+    ${UTI_PHGRM_APERO_DIR}/GraphePose.cpp
+#    ${UTI_PHGRM_APERO_DIR}/ImProfAndL2App.cpp
+    ${UTI_PHGRM_APERO_DIR}/PowelOptimize.cpp
+    ${UTI_PHGRM_APERO_DIR}/PtsFlottant.cpp
+    ${UTI_PHGRM_APERO_DIR}/Redressement.cpp
+    ${UTI_PHGRM_APERO_DIR}/Simule.cpp
+    ${UTI_PHGRM_APERO_DIR}/Tracage.cpp
+    ${UTI_PHGRM_APERO_DIR}/VisuMult.cpp
+)
+
+
+
+
+
