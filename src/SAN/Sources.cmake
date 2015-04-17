c399614096b463540966c69138f43cfeb60c7ff6
Matthieu Deveau <mattdeveau@users.noreply.github.com>
2015-04-17 18:08:44 +0200

    Un peu de tout (supp Mascarpone, Tequila, vCommandes)

---
+++ src/SAN/Sources.cmake
@@ -0,0 +1,12 @@
+set(San_Src_Files
+	${SAN_DIR}/cylindre.cpp
+	${SAN_DIR}/ortho_cyl.cpp
+	${SAN_DIR}/torique.cpp
+)
+
+SOURCE_GROUP(San FILES ${San_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${San_Src_Files}
+)
