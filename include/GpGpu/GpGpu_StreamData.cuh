0157f7d60169f25cf8fce394eeee96dc94d815ef
Gérald Choqueux <gchoqueux@users.noreply.github.com>
2015-05-18 17:36:19 +0200

    GpGpu : Doc + Clean

--- include/GpGpu/GpGpu_StreamData.cuh
+++ include/GpGpu/GpGpu_StreamData.cuh
@@ -3,215 +3,8 @@
 
 #include "GpGpu/GpGpu_Data.h"
 
-using namespace std;
-
-static __constant__ ushort  dMapIndex[WARPSIZE];
-
-/// @cond DEV
-/// \class CDeviceStream
-/// \brief Classe gerant un flux de données en memoire video
-template< class T >
-class CDeviceStream
-{
-public:
-
-	__device__ CDeviceStream(T* buf,T* stream, uint sizeStream):
-		_bufferData(buf),
-		_streamData(stream),
-		_curStreamId(0),
-		_curBufferId(WARPSIZE),
-		_sizeStream(sizeStream)
-	{}
-
-
-	template<bool sens>
-	__device__ short getLen2Read(short2 &index)
-	{
-		index = make_short2(0,0);
-		return 1;
-	}
-
-//    __device__ virtual short getLen2ReadAR(short2 &index)
-//    {
-//        index = make_short2(0,0);
-//        return 1;
-//    }
-
-	template<bool sens, class D> __device__ short2 read(D* destData, ushort tid, T def);
-
-private:
-
-	template<bool sens> __device__ inline short vec();//{ return 1 - 2 * !sens; }
-
-	template<bool sens> __device__ short GetNbToCopy(ushort nTotal,ushort nCopied)
-	{
-		return min(nTotal - nCopied , MaxReadBuffer<sens>());
-	}
-
-	template<bool sens> __device__ inline ushort MaxReadBuffer();
-
-
-
-
-	T*                          _bufferData;
-	T*                          _streamData;
-	uint                        _curStreamId;
-	ushort                      _curBufferId;
-	uint                        _sizeStream;
-};
-/// @endcond
-
-template<bool sens>
-__device__ inline ushort __mxReadBuffer(ushort &curBufferId)
-{
-	return 0;
-}
-
-template<>
-__device__ inline ushort __mxReadBuffer<true>(ushort &curBufferId)
-{
-	return ((ushort)WARPSIZE - curBufferId);
-}
-
-template<>
-__device__ inline ushort __mxReadBuffer<false>(ushort &curBufferId)
-{
-	return curBufferId;
-}
-
-template< class T > template<bool sens>
-__device__ inline ushort CDeviceStream<T>::MaxReadBuffer()
-{
-	return __mxReadBuffer<sens>(_curBufferId);
-}
-
-template<bool sens>
-__device__ inline short __vec()
-{
-	return 0;
-}
-
-template<>
-__device__ inline short __vec<true>()
-{
-	return 1;
-}
-
-
-template<>
-__device__ inline short __vec<false>()
-{
-	return -1;
-}
-
-template< class T >
-template <bool sens> __device__ inline short CDeviceStream<T>::vec()
-{
-	return __vec<sens>();
-}
-
-
-
-template< class T > template<bool sens, class D>  __device__
-short2 CDeviceStream<T>::read(D *destData, ushort tid, T def)
-{
-	short2  index;
-	ushort  NbCopied    = 0 , NbTotalToCopy = getLen2Read<sens>(index);// : getLen2ReadAR(index);
-	short   PitSens     = !sens * WARPSIZE;
-
-	while(NbCopied < NbTotalToCopy)
-	{
-		ushort NbToCopy = GetNbToCopy<sens>(NbTotalToCopy,NbCopied);
-
-		if(!NbToCopy)
-		{
-			uint idStream =_curStreamId + threadIdx.x - 2 * PitSens;
-			if(idStream < _sizeStream)
-				_bufferData[threadIdx.x] = _streamData[idStream];
-			_curBufferId   = PitSens;
-			_curStreamId   = _curStreamId  + (vec<sens>()<<5); // * 32
-
-			NbToCopy = GetNbToCopy<sens>(NbTotalToCopy,NbCopied);
-		}
-
-		ushort idDest = tid + (sens ? NbCopied : NbTotalToCopy - NbCopied - NbToCopy);
-
-		if(!(idDest>>8)) // < 256
-		{
-			if (tid < NbToCopy && idDest < NbTotalToCopy)
-				destData[idDest] = _bufferData[_curBufferId + tid - !sens * NbToCopy];
-			else if (idDest >= NbTotalToCopy)
-				destData[idDest] = def;
-
-		}
-		_curBufferId  = _curBufferId + vec<sens>() * NbToCopy;
-		NbCopied     += NbToCopy;
-   }
-
-   return index;
-}
-
-
-/// @cond DEV
-template< class T >
-///
-/// \brief The CDeviceDataStream class
-/// Cette classe est un lecteur de données GpGpu  de la mémoire.
-/// Elle lit comme un flux de données.
-/// Les données sont lues par paquet de 32. Elle permet d'optimiser les requètes dans la mémoire globale
-class CDeviceDataStream : public CDeviceStream<T>
-{
-public:
-
-	///
-	/// \brief CDeviceDataStream Constructeur
-	/// \param buf Buffer pour mettre en cache les données lues. Cette espace mémoire doit etre en mémoire partagée
-	/// \param stream pointeur sur le début du flux de donnée en mémoire globale
-	/// \param bufId
-	/// \param streamId
-	/// \param sizeStream
-	/// \param sizeStreamId
-	/// \return
-	///
-	__device__ CDeviceDataStream(T* buf,T* stream,short2* bufId,short2* streamId, uint sizeStream, uint sizeStreamId):
-		CDeviceStream<T>(buf,stream,sizeStream),
-		_streamIndex(bufId,streamId,sizeStreamId)
-	{}
-
-	template<bool sens>
-	///
-	/// \brief getLen2Read
-	/// \param index
-	/// \return
-	///
-	__device__ inline  short getLen2Read(short2 &index);
-
-private:
-
-	CDeviceStream<short2>       _streamIndex;
-
-};
-
-/// @endcond
-
 #define sgn s<sens>
 #define max_cost 1e9
-
-template<bool sens>
-
-__device__ inline short __getLen2Read(short2 &index,CDeviceStream<short2>   &streamIndex)
-{
-	streamIndex.read<sens>(&index,0,make_short2(0,0));
-
-	return diffYX(index);
-}
-
-template<class T>
-template<bool sens>
-__device__ inline  short CDeviceDataStream<T>::getLen2Read(short2 &index)
-{
-	return __getLen2Read<sens>(index,_streamIndex);
-}
 
 template<bool sens>
 __device__ inline int s(int v)
@@ -285,23 +78,52 @@ public:
 
 	void                __device__
 	///
-	/// \brief SetValue
+	/// \brief SetValue Affecte la valeur à l'index id
 	/// \param id
-	/// \param value
+	/// \param value La valeur
 	///
 	SetValue(int id, T value);
 
-    void                __device__  SubValue(int id, T value);
+	void                __device__
+	///
+	/// \brief SubValue Soustrait la valeur à la donnée situé à l'index id
+	/// \param id
+	/// \param value
+	///
+	SubValue(int id, T value);
 
-    void                __device__  AddValue(int id, T value);
+	void                __device__
+	///
+	/// \brief AddValue Additione la valeur à la donnée situé à l'index id
+	/// \param id
+	/// \param value
+	///
+	AddValue(int id, T value);
 
-    template<bool sens> __device__ void SetOrAddValue(int id, T value);
+	template<bool sens> __device__ void
+	///
+	/// \brief SetOrAddValue Affecte si sens == true et additionne si sens == false
+	/// \param id
+	/// \param value
+	///
+	SetOrAddValue(int id, T value);
 
-    template<bool sens> __device__ void SetOrAddValue(int id, T setValue,T addValue);
+	template<bool sens> __device__
+	///
+	/// \brief SetOrAddValue Affecte si sens == true et additionne si sens == false
+	/// \param id
+	/// \param setValue
+	/// \param addValue
+	///
+	void SetOrAddValue(int id, T setValue,T addValue);
 
     //long int                __device__  GetGiD(){return _idG;}
 
-    template<bool sens> __device__  void reverse();
+	template<bool sens> __device__
+	///
+	/// \brief reverse Prépare pour le sens de lecture
+	///
+	void reverse();
 
     void                __device__  output();
 
