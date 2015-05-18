e21a314eb80f39546e67580bf155573a2f58c7c4
Matthieu Deveau <mattdeveau@users.noreply.github.com>
2015-05-18 13:48:43 +0200

    Compil Visual

---
+++ src/uti_phgrm/CPP_HomFilterMasq.cpp
@@ -0,0 +1,288 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+#include "StdAfx.h"
+#include <algorithm>
+
+/*
+
+
+
+Bascule ".*.jpg" RadialExtended B2   ImRep=00000002.jpg  P1Rep=[463,1406] P2Rep=[610,284] Teta=90
+Bascule ".*.jpg" RadialExtended R2.xml   ImRep=00000002.jpg  P1Rep=[463,1406] P2Rep=[610,284] Teta=45
+
+
+Bascule ".*.jpg" RadialExtended B2 MesureIm=OutAligned-Im.xml Teta=90
+Tarama ".*.jpg" B2
+
+Bascule ".*.jpg" RadialExtended R2.xml MesureIm=OutAligned.xml Teta=180
+Tarama ".*.jpg" RadialExtended Repere=R2.xml
+
+Bascule ".*.jpg" RadialExtended R3.xml MesureIm=OutAligned.xml Teta=180
+
+*/
+
+#define DEF_OFSET -12349876
+
+struct cImFMasq
+{
+
+};
+
+
+Im2D_Bits<1>  GetMasqSubResol(const std::string & aName,double aResol)
+{
+    Tiff_Im aTF = Tiff_Im::BasicConvStd(aName);
+
+     Pt2di aSzF = aTF.sz();
+     Pt2di aSzR = round_ni(Pt2dr(aSzF)/aResol);
+
+     Im2D_Bits<1> aRes(aSzR.x,aSzR.y);
+
+
+     Fonc_Num aFonc = aTF.in_bool_proj();
+     aFonc = StdFoncChScale(aFonc,Pt2dr(0,0),Pt2dr(aResol,aResol));
+     aFonc = aFonc > 0.75;
+
+     ELISE_COPY (aRes.all_pts(), aFonc,aRes.out());
+
+
+
+
+     return aRes;
+}
+
+
+
+int HomFilterMasq_main(int argc,char ** argv)
+{
+    // MemoArg(argc,argv);
+    MMD_InitArgcArgv(argc,argv);
+    std::string  aDir,aPat,aFullDir;
+    bool ExpTxt=false;
+    std::string PostPlan="_Masq";
+    std::string KeyCalcMasq;
+    std::string KeyEquivNoMasq;
+    std::string MasqGlob;
+    double  aResol=10;
+    bool AcceptNoMask;
+    std::string aPostIn= "";
+    std::string aPostOut= "MasqFiltered";
+
+
+
+    ElInitArgMain
+    (
+    argc,argv,
+    LArgMain()  << EAMC(aFullDir,"Full name (Dir+Pat)", eSAM_IsPatFile),
+    LArgMain()
+                    << EAM(PostPlan,"PostPlan",true,"Post to plan, Def : toto ->toto_Masq.tif like with SaisieMasq")
+                    << EAM(MasqGlob,"GlobalMasq",true,"Global Masq to add to all image")
+                    << EAM(KeyCalcMasq,"KeyCalculMasq",true,"For tuning masq per image")
+                    << EAM(KeyEquivNoMasq,"KeyEquivNoMasq",true,"When given if KENM(i1)==KENM(i2), don't masq")
+                    << EAM(aResol,"Resol",true,"Sub Resolution for masq storing, Def=10")
+                    << EAM(AcceptNoMask,"ANM",true,"Accept no mask, def = true if MasqGlob and false else")
+                    << EAM(ExpTxt,"ExpTxt",true,"Ascii format for in and out, def=false")
+                    << EAM(aPostIn,"PostIn",true,"Post for Input dir Hom, Def=")
+                    << EAM(aPostOut,"PostOut",true,"Post for Output dir Hom, Def=MasqFiltered")
+
+    );
+
+    #if (ELISE_windows)
+        replace( aFullDir.begin(), aFullDir.end(), '\\', '/' );
+     #endif
+    SplitDirAndFile(aDir,aPat,aFullDir);
+    if (EAMIsInit(&PostPlan))
+    {
+        CorrecNameMasq(aDir,aPat,PostPlan);
+    }
+
+    if (!EAMIsInit(&AcceptNoMask))
+       AcceptNoMask = EAMIsInit(&MasqGlob);
+
+
+    cInterfChantierNameManipulateur * anICNM = cInterfChantierNameManipulateur::BasicAlloc(aDir);
+
+
+    Im2D_Bits<1>  aImMasqGlob(1,1);
+    if (EAMIsInit(&MasqGlob))
+       aImMasqGlob = GetMasqSubResol(aDir+MasqGlob,aResol);
+
+
+    const std::vector<std::string> *  aVN = anICNM->Get(aPat);
+    std::vector<Im2D_Bits<1> >  aVMasq;
+
+    for (int aKN = 0 ; aKN<int(aVN->size()) ; aKN++)
+    {
+        std::string aNameIm = (*aVN)[aKN];
+        Tiff_Im aTF = Tiff_Im::StdConvGen(aNameIm,1,false);
+        Pt2di aSzG = aTF.sz();
+        Pt2di aSzR (round_ni(Pt2dr(aSzG)/aResol));
+        Im2D_Bits<1> aImMasq(aSzR.x,aSzR.y,1);
+
+
+        std::string aNameMasq = StdPrefix(aNameIm)+PostPlan + ".tif";
+        if (EAMIsInit(&KeyCalcMasq))
+        {
+            aNameMasq = anICNM->Assoc1To1(KeyCalcMasq,aNameIm,true);
+        }
+
+        if (ELISE_fp::exist_file(aNameMasq))
+        {
+            Im2D_Bits<1> aImMasqLoc = GetMasqSubResol(aDir+aNameMasq,aResol);
+            ELISE_COPY(aImMasq.all_pts(),aImMasq.in() && aImMasqLoc.in(0),aImMasq.out());
+        }
+        else
+        {
+             if (!AcceptNoMask)
+             {
+                 std::cout << "For Im " << aNameIm << " file " << aNameMasq << " does not exist\n";
+                 ELISE_ASSERT(false,"Masq not found");
+             }
+        }
+
+        if (EAMIsInit(&MasqGlob))
+        {
+
+            ELISE_COPY(aImMasq.all_pts(),aImMasq.in() && aImMasqGlob.in(0),aImMasq.out());
+        }
+
+        aVMasq.push_back(aImMasq);
+        // Tiff_Im::CreateFromIm(aImMasq,"SousRes"+aNameMasq);
+    }
+
+    std::string anExt = ExpTxt ? "txt" : "dat";
+
+
+    std::string aKHIn =   std::string("NKS-Assoc-CplIm2Hom@")
+                       +  std::string(aPostIn)
+                       +  std::string("@")
+                       +  std::string(anExt);
+    std::string aKHOut =   std::string("NKS-Assoc-CplIm2Hom@")
+                        +  std::string(aPostOut)
+                        +  std::string("@")
+                       +  std::string(anExt);
+
+
+
+
+    for (int aKN1 = 0 ; aKN1<int(aVN->size()) ; aKN1++)
+    {
+        for (int aKN2 = 0 ; aKN2<int(aVN->size()) ; aKN2++)
+        {
+             std::string aNameIm1 = (*aVN)[aKN1];
+             std::string aNameIm2 = (*aVN)[aKN2];
+
+             std::string aNameIn = aDir + anICNM->Assoc1To2(aKHIn,aNameIm1,aNameIm2,true);
+
+             if (ELISE_fp::exist_file(aNameIn))
+             {
+                  bool UseMasq = true;
+                  if (EAMIsInit(&KeyEquivNoMasq))
+                  {
+                       UseMasq =  (anICNM->Assoc1To1(KeyEquivNoMasq,aNameIm1,true) != anICNM->Assoc1To1(KeyEquivNoMasq,aNameIm2,true) );
+                  }
+
+
+                  TIm2DBits<1>  aMasq1 ( aVMasq[aKN1]);
+                  TIm2DBits<1>  aMasq2 ( aVMasq[aKN2]);
+
+                  ElPackHomologue aPackIn =  ElPackHomologue::FromFile(aNameIn);
+                  ElPackHomologue aPackOut;
+                  for (ElPackHomologue::const_iterator itP=aPackIn.begin(); itP!=aPackIn.end() ; itP++)
+                  {
+                      Pt2dr aP1 = itP->P1();
+                      Pt2dr aP2 = itP->P2();
+                      Pt2di aQ1 = round_ni(aP1/aResol);
+                      Pt2di aQ2 = round_ni(aP2/aResol);
+
+                      if ((aMasq1.get(aQ1,0) && aMasq2.get(aQ2,0)) || (! UseMasq))
+                      {
+                          ElCplePtsHomologues aCple(aP1,aP2);
+                          aPackOut.Cple_Add(aCple);
+                      }
+                  }
+                  std::string aNameOut = aDir + anICNM->Assoc1To2(aKHOut,aNameIm1,aNameIm2,true);
+                  aPackOut.StdPutInFile(aNameOut);
+                  std::cout << "IN " << aNameIn << " " << aNameOut  << " UseM " << UseMasq << "\n";
+             }
+        }
+    }
+    // std::vector<cImFMasq *> mVIm;
+
+
+
+   return 0;
+}
+
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant �  la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  �  l'utilisation,  �  la modification et/ou au
+développement et �  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe �
+manipuler et qui le réserve donc �  des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
+logiciel �  leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
---
+++ src/uti_phgrm/NewOri/cNewO_OptimTriplet.cpp
@@ -0,0 +1,277 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+
+
+#include "NewOri.h"
+
+class cAppliOptimTriplet;
+class cPairOfTriplet;
+class cImOfTriplet ;
+
+class cImOfTriplet
+{
+   public :
+         cImOfTriplet(cAppliOptimTriplet &,cNewO_OneIm *,const ElRotation3D & aR);
+         cNewO_OneIm * Im() {return mIm;}
+         const ElRotation3D & Rot() {return mRot;}
+         cAppliOptimTriplet &Appli() {return mAppli;}
+         std::vector<Pt2df> &  VPOf3() {return mVPOf3;}  // Points triples
+   private :
+         cAppliOptimTriplet & mAppli;
+         cNewO_OneIm * mIm;
+         ElRotation3D  mRot;
+         std::vector<Pt2df>    mVPOf3;
+};
+
+
+class cPairOfTriplet
+{
+     public :
+          cPairOfTriplet(cImOfTriplet * aI1,cImOfTriplet *aI2);
+          double ResiduMoy();
+          const ElRotation3D & R1() const {return mIm1->Rot();}
+          const ElRotation3D & R2() const {return mIm2->Rot();}
+     private :
+          cAppliOptimTriplet & mAppli;
+          cImOfTriplet *        mIm1;
+          cImOfTriplet *        mIm2;
+          std::vector<Pt2df>    mVP1;
+          std::vector<Pt2df>    mVP2;
+};
+
+
+class cAppliOptimTriplet
+{
+      public :
+          cAppliOptimTriplet(int argc,char ** argv);
+          cNewO_NameManager * NM() {return mNM;}
+          double ResiduTriplet();
+      private :
+          std::string mNameOriCalib;
+          std::string mDir;
+          cNewO_NameManager * mNM;
+          cImOfTriplet *   mIm1;
+          cImOfTriplet *   mIm2;
+          cImOfTriplet *   mIm3;
+          cPairOfTriplet * mP12;
+          cPairOfTriplet * mP13;
+          cPairOfTriplet * mP23;
+};
+
+/**************************************************/
+/*                                                */
+/*            cImOfTriplet                        */
+/*                                                */
+/**************************************************/
+
+cImOfTriplet::cImOfTriplet(cAppliOptimTriplet & anAppli,cNewO_OneIm * anIm,const ElRotation3D & aR) :
+    mAppli (anAppli),
+    mIm    (anIm),
+    mRot   (aR)
+{
+}
+/*
+*/
+
+
+/**************************************************/
+/*                                                */
+/*            cPairOfTriplet                      */
+/*                                                */
+/**************************************************/
+
+cPairOfTriplet::cPairOfTriplet(cImOfTriplet * aI1,cImOfTriplet *aI2) :
+    mAppli (aI1->Appli()),
+    mIm1   (aI1),
+    mIm2   (aI2)
+{
+   mAppli.NM()->LoadHomFloats(mIm1->Im(),mIm2->Im(),&mVP1,&mVP2);
+
+    std::cout << "cPairOfTriplet " << mVP1.size() << " " << mVP2.size() << "\n";
+}
+
+
+double cPairOfTriplet::ResiduMoy()
+{
+    std::vector<double> aVRes;
+    for (int aK=0 ; aK<int(mVP1.size()) ; aK++)
+    {
+        std::vector<Pt3dr> aW1;
+        std::vector<Pt3dr> aW2;
+        AddSegOfRot(aW1,aW2,R1(),mVP1[aK]);
+        AddSegOfRot(aW1,aW2,R2(),mVP2[aK]);
+        bool OkI;
+        Pt3dr aI = InterSeg(aW1,aW2,OkI);
+        if (OkI)
+        {
+            double aRes1 = Residu(mIm1->Im(),R1(),aI,mVP1[aK]);
+            double aRes2 = Residu(mIm2->Im(),R2(),aI,mVP2[aK]);
+
+            aVRes.push_back((aRes1+aRes2)/2.0);
+        }
+    }
+    return MedianeSup(aVRes);
+}
+
+/**************************************************/
+/*                                                */
+/*            cAppliOptimTriplet                  */
+/*                                                */
+/**************************************************/
+
+
+double cAppliOptimTriplet::ResiduTriplet()
+{
+    std::vector<double> aVRes;
+    for (int aK=0 ; aK<int(mIm1->VPOf3().size()) ; aK++)
+    {
+        std::vector<Pt3dr> aW1;
+        std::vector<Pt3dr> aW2;
+        AddSegOfRot(aW1,aW2,mIm1->Rot(),mIm1->VPOf3()[aK]);
+        AddSegOfRot(aW1,aW2,mIm2->Rot(),mIm2->VPOf3()[aK]);
+        AddSegOfRot(aW1,aW2,mIm3->Rot(),mIm3->VPOf3()[aK]);
+        bool OkI;
+        Pt3dr aI = InterSeg(aW1,aW2,OkI);
+        if (OkI)
+        {
+            double aRes1 = Residu(mIm1->Im(),mIm1->Rot(),aI,mIm1->VPOf3()[aK]);
+            double aRes2 = Residu(mIm2->Im(),mIm2->Rot(),aI,mIm2->VPOf3()[aK]);
+            double aRes3 = Residu(mIm3->Im(),mIm3->Rot(),aI,mIm3->VPOf3()[aK]);
+/*
+            double aRes2 = Residu(mIm2->Im(),R2(),aI,mVP2[aK]);
+*/
+
+            aVRes.push_back((aRes1+aRes2+aRes3)/3.0);
+        }
+    }
+    return MedianeSup(aVRes);
+}
+
+cAppliOptimTriplet::cAppliOptimTriplet(int argc,char ** argv)  :
+    mDir ("./")
+{
+   std::string aN1,aN2,aN3;
+   ElInitArgMain
+   (
+        argc,argv,
+        LArgMain() << EAMC(aN1,"Image one", eSAM_IsExistFile)
+                   << EAMC(aN2,"Image two", eSAM_IsExistFile)
+                   << EAMC(aN3,"Image three", eSAM_IsExistFile),
+        LArgMain() << EAM(mNameOriCalib,"OriCalib",true,"Orientation for calibration ", eSAM_IsExistDirOri)
+                   << EAM(mDir,"Dir",true,"Directory, Def=./ ",eSAM_IsDir)
+   );
+   if (MMVisualMode) return;
+
+   cTplTriplet<std::string> a3S(aN1,aN2,aN3);
+
+   mNM = new cNewO_NameManager(mDir,mNameOriCalib,"dat");
+
+   cNewO_OneIm * aIm1 = new cNewO_OneIm(*mNM,a3S.mV0);
+   cNewO_OneIm * aIm2 = new cNewO_OneIm(*mNM,a3S.mV1);
+   cNewO_OneIm * aIm3 = new cNewO_OneIm(*mNM,a3S.mV2);
+
+   std::string  aName3R = mNM->NameOriInitTriplet(true,aIm1,aIm2,aIm3);
+   cXml_Ori3ImInit aXml3Ori = StdGetFromSI(aName3R,Xml_Ori3ImInit);
+
+   mIm1 = new  cImOfTriplet(*this,aIm1,ElRotation3D::Id);
+   mIm2 = new  cImOfTriplet(*this,aIm2,Xml2El(aXml3Ori.Ori2On1()));
+   mIm3 = new  cImOfTriplet(*this,aIm3,Xml2El(aXml3Ori.Ori3On1()));
+
+   mP12 = new cPairOfTriplet(mIm1,mIm2);
+   mP13 = new cPairOfTriplet(mIm1,mIm3);
+   mP23 = new cPairOfTriplet(mIm2,mIm3);
+
+   mNM->LoadTriplet(mIm1->Im(),mIm2->Im(),mIm3->Im(),&mIm1->VPOf3(),&mIm2->VPOf3(),&mIm3->VPOf3());
+
+   std::cout << "NB TRIPLE " << mIm2->VPOf3().size()  << " Resi3: " <<  ResiduTriplet() << "\n";
+
+
+   if (1)
+   {
+      std::cout << "RESIDU/PAIRES " << mP12->ResiduMoy() << " " << mP13->ResiduMoy() << " " << mP23->ResiduMoy() << " " << "\n";
+   }
+}
+
+
+/**************************************************/
+/*                                                */
+/*            ::                                  */
+/*                                                */
+/**************************************************/
+
+int CPP_OptimTriplet_main(int argc,char ** argv)
+{
+   cAppliOptimTriplet anAppli(argc,argv);
+   return EXIT_SUCCESS;
+}
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant �  la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  �  l'utilisation,  �  la modification et/ou au
+développement et �  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe �
+manipuler et qui le réserve donc �  des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
+logiciel �  leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
