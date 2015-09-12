44994ce0c13a55718b8f1e1ebb66c826b54a9c5c
deseilligny <deseilligny@users.noreply.github.com>
2015-09-12 12:00:49 +0200

    Dans Epip Gen (sur base ancienne Epip Pol)

--- src/uti_phgrm/CPP_CreateEpip.cpp
+++ src/uti_phgrm/CPP_CreateEpip.cpp
@@ -61,8 +61,144 @@ cCpleEpip * StdCpleEpip
 
 }
 
+class cApply_CreateEpip_main
+{
+   public :
+      cApply_CreateEpip_main(int argc,char ** argv);
 
-void  MyCreateEpip_main(int argc,char ** argv)
+     cInterfChantierNameManipulateur * mICNM;
+
+
+      int mDegre;
+      double mLengthMax ;
+      cBasicGeomCap3D *  mGenI1;
+      cBasicGeomCap3D *  mGenI2;
+
+      void DoEpipGen();
+
+      Pt2dr DirEpipIm2(cBasicGeomCap3D * aG1,cBasicGeomCap3D * aG2,int aNb,ElPackHomologue & aPack,bool AddToP1);
+
+      void Ressample(cBasicGeomCap3D * aG1,EpipolaireCoordinate & E1,double aStep);
+};
+
+Pt2dr  cApply_CreateEpip_main::DirEpipIm2(cBasicGeomCap3D * aG1,cBasicGeomCap3D * aG2,int aNbGlob,ElPackHomologue & aPack,bool AddToP1)
+{
+    Pt2dr aSz =  Pt2dr(aG1->SzBasicCapt3D());
+ 
+    int aNbZ = 2;
+
+    Pt2dr aSomTens2(0,0);
+
+    double aIProf = aG1->GetVeryRoughInterProf();
+
+    double aEps = 5e-4;
+
+    double aLenghtSquare = ElMin(mLengthMax,sqrt((aSz.x*aSz.y) / (aNbGlob*aNbGlob)));
+
+
+    int aNbX = ElMax(1+2*mDegre,round_up(aSz.x /aLenghtSquare));
+    int aNbY = ElMax(1+2*mDegre,round_up(aSz.y /aLenghtSquare));
+
+
+    std::cout << "NBBBB " << aNbX << " " << aNbY << "\n";
+     
+
+    for (int aKX=0 ; aKX<= aNbX ; aKX++)
+    {
+        double aPdsX = ElMax(aEps,ElMin(1-aEps,aKX /double(aNbX)));
+        for (int aKY=0 ; aKY<= aNbY ; aKY++)
+        {
+            double aPdsY = ElMax(aEps,ElMin(1-aEps,aKY/double(aNbY)));
+            Pt2dr aPIm1 = aSz.mcbyc(Pt2dr(aPdsX,aPdsY));
+            if (aG1->CaptHasData(aPIm1))
+            {
+                Pt3dr aPT1;
+                Pt3dr aC1;
+                aG1->GetCenterAndPTerOnBundle(aC1,aPT1,aPIm1);
+
+                std::vector<Pt2dr> aVPIm2;
+                for (int aKZ = -aNbZ ; aKZ <= aNbZ ; aKZ++)
+                {
+                     Pt3dr aPT2 = aC1 + (aPT1-aC1) * (1+(aIProf*aKZ) / aNbZ);
+                     if (aG1->PIsVisibleInImage(aPT2) && aG2->PIsVisibleInImage(aPT2))
+                     {
+                        aVPIm2.push_back(aG2->Ter2Capteur(aPT2));
+                        ElCplePtsHomologues aCple(aPIm1,aVPIm2.back(),1.0);
+                        if (! AddToP1) 
+                           aCple.SelfSwap();
+                        aPack.Cple_Add(aCple);
+                     }
+                }
+                if (aVPIm2.size() >=2)
+                {
+                    Pt2dr aDir2 = vunit(aVPIm2.back()-aVPIm2[0]);
+                    aSomTens2 = aSomTens2 + aDir2 * aDir2; // On double l'angle pour en faire un tenseur
+                }
+            }
+        }
+    }
+    Pt2dr aRT = Pt2dr::polar(aSomTens2,0.0);
+    return Pt2dr::FromPolar(1.0,aRT.y/2.0);
+}
+
+
+/*
+void cApply_CreateEpip_main::Ressample(cBasicGeomCap3D * aG1,EpipolaireCoordinate & anE,double aStep)
+{
+}
+*/
+
+
+
+
+void cApply_CreateEpip_main::DoEpipGen()
+{
+      mDegre = 5;
+      mLengthMax = 500.0;
+      ElPackHomologue aPack;
+      Pt2dr aDir2 =  DirEpipIm2(mGenI1,mGenI2,50,aPack,true);
+      Pt2dr aDir1 =  DirEpipIm2(mGenI2,mGenI1,50,aPack,false);
+
+      std::cout << "Compute Epip\n";
+      CpleEpipolaireCoord * aCple = CpleEpipolaireCoord::PolynomialFromHomologue(aPack,mDegre,aDir1,aDir2);
+
+      EpipolaireCoordinate & e1 = aCple->EPI1();
+      EpipolaireCoordinate & e2 = aCple->EPI2();
+
+      Pt2dr aInf1(1e20,1e20),aSup1(-1e20,-1e20);
+      Pt2dr aInf2(1e20,1e20),aSup2(-1e20,-1e20);
+
+      double aErrMax = 0.0;
+      double aErrMoy = 0.0;
+      int    mNbP = 0;
+
+      for (ElPackHomologue::const_iterator itC=aPack.begin() ; itC!= aPack.end() ; itC++)
+      {
+           Pt2dr aP1 = e1.Direct(itC->P1());
+           Pt2dr aP2 = e2.Direct(itC->P2());
+           aInf1 = Inf(aInf1,aP1);
+           aSup1 = Sup(aSup1,aP1);
+           aInf2 = Inf(aInf2,aP2);
+           aSup2 = Sup(aSup2,aP2);
+
+           double anErr = ElAbs(aP1.y-aP2.y);
+           mNbP++;
+           aErrMax = ElMax(anErr,aErrMax);
+           aErrMoy += anErr;
+      }
+
+
+      std::cout << aInf1 << " " <<  aSup1 << "\n";
+      std::cout << aInf2 << " " <<  aSup2 << "\n";
+      std::cout << "DIR " << aDir1 << " " << aDir2 << "\n";
+      std::cout << "Epip Rect Accuracy, Moy " << aErrMoy/mNbP << " Max " << aErrMax << "\n";
+}
+
+
+
+
+
+cApply_CreateEpip_main::cApply_CreateEpip_main(int argc,char ** argv)
 {
     Tiff_Im::SetDefTileFile(50000);
     std::string aDir= ELISE_Current_DIR;
@@ -99,28 +235,39 @@ void  MyCreateEpip_main(int argc,char ** argv)
     {
     if (aName1 > aName2) ElSwap(aName1,aName2);
 
+
+
     int aNbChan = Gray ? 1 : - 1;
 
     cTplValGesInit<std::string>  aTplFCND;
-    cInterfChantierNameManipulateur * anICNM = cInterfChantierNameManipulateur::StdAlloc
+    mICNM = cInterfChantierNameManipulateur::StdAlloc
                                                (
                                                    argc,argv,
                                                    aDir,
                                                    aTplFCND
                                                );
-     anICNM->CorrecNameOrient(anOri);
+     mICNM->CorrecNameOrient(anOri);
 
+
+     mGenI1 = mICNM->StdCamGenOfNames(anOri,aName1);
+     mGenI2 = mICNM->StdCamGenOfNames(anOri,aName2);
+
+     if ((mGenI1->DownCastCS()==0) || (mGenI2->DownCastCS()==0))
+     {
+         DoEpipGen();
+         return;
+     }
 
      std::string   aKey =  + "NKS-Assoc-Im2Orient@-" + anOri;
 
-     std::string aNameOr1 = anICNM->Assoc1To1(aKey,aName1,true);
-     std::string aNameOr2 = anICNM->Assoc1To1(aKey,aName2,true);
+     std::string aNameOr1 = mICNM->Assoc1To1(aKey,aName1,true);
+     std::string aNameOr2 = mICNM->Assoc1To1(aKey,aName2,true);
 
      // std::cout << "RREEEEEEEEEEEEEEead cam \n";
-     CamStenope * aCam1 = CamStenope::StdCamFromFile(true,aNameOr1,anICNM);
+     CamStenope * aCam1 = CamStenope::StdCamFromFile(true,aNameOr1,mICNM);
      // std::cout << "EPISZPPPpp " << aCam1->SzPixel() << "\n";
 
-     CamStenope * aCam2 = CamStenope::StdCamFromFile(true,aNameOr2,anICNM);
+     CamStenope * aCam2 = CamStenope::StdCamFromFile(true,aNameOr2,mICNM);
 
      Tiff_Im aTif1 = Tiff_Im::StdConvGen(aDir+aName1,aNbChan,Cons16B);
      Tiff_Im aTif2 = Tiff_Im::StdConvGen(aDir+aName2,aNbChan,Cons16B);
@@ -158,16 +305,16 @@ void  MyCreateEpip_main(int argc,char ** argv)
      aCplE.LockMess("End cCpleEpip::ImEpip");
 
 
-     return EXIT_SUCCESS;
+     return ;
 
     }
-    else return EXIT_SUCCESS;
+    else return ;
 }
 
 
 int CreateEpip_main(int argc,char ** argv)
 {
-     MyCreateEpip_main(argc,argv);
+     cApply_CreateEpip_main(argc,argv);
 
      return EXIT_SUCCESS;
 }
