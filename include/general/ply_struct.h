ae4621c02f38be7613e57b35ce9cbe178dd85fb5
deseilligny <deseilligny@users.noreply.github.com>
2015-10-16 17:44:18 +0200

    Bug dans C3DC revele par correction de retour dans TestLib

---
+++ include/general/ply_struct.h
@@ -0,0 +1,71 @@
+#ifndef __PLY_STRUCT_H__
+#define __PLY_STRUCT_H__
+
+#define PLY_START_TYPE 0
+#define PLY_CHAR       1
+#define PLY_SHORT      2
+#define PLY_INT        3
+#define PLY_UCHAR      4
+#define PLY_USHORT     5
+#define PLY_UINT       6
+#define PLY_FLOAT      7
+#define PLY_DOUBLE     8
+#define PLY_INT_8      9
+#define PLY_UINT_8     10
+#define PLY_INT_16     11
+#define PLY_UINT_16    12
+#define PLY_INT_32     13
+#define PLY_UINT_32    14
+#define PLY_FLOAT_32   15
+#define PLY_FLOAT_64   16
+
+#define PLY_END_TYPE   17
+
+typedef struct sVertex
+{
+    float x,y,z;             /* the usual 3-space position of a vertex */
+} sVertex;
+
+typedef struct sPlyOrientedVertex
+{
+    float x, y, z, nx, ny, nz;
+} sPlyOrientedVertex;
+
+typedef struct sFace
+{
+    unsigned char nverts;    /* number of vertex indices in list */
+    int *verts;              /* vertex index list */
+} sFace;
+
+typedef struct ElPlyFace
+{
+    unsigned char nr_vertices;
+    int *vertices;
+    int segment;
+} ElPlyFace;
+
+typedef struct sPlyColoredVertex
+{
+    float x, y, z;
+    unsigned char red, green, blue;
+} sPlyColoredVertex;
+
+typedef struct sPlyColoredVertexWithAlpha
+{
+    float x, y, z;
+    unsigned char red, green, blue, alpha;
+} sPlyColoredVertexWithAlpha;
+
+typedef struct sPlyOrientedColoredVertex
+{
+    float x, y, z, nx, ny, nz;
+    unsigned char red, green, blue;
+} sPlyOrientedColoredVertex;
+
+typedef struct sPlyOrientedColoredAlphaVertex
+{
+    float x, y, z, nx, ny, nz;
+    unsigned char red, green, blue, alpha;
+} sPlyOrientedColoredAlphaVertex;
+
+#endif
