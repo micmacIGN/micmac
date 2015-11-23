2e2502830324df621237d109acaf4af610fca672
deseilligny <deseilligny@users.noreply.github.com>
2015-11-23 19:05:09 +0100

    Dans Vino

---
+++ include/graphes/graphe_implem.h
@@ -0,0 +1,374 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+   
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in 
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte 
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#ifndef _ELISE_GRAPHES_GRAPHE_IMPLEM_H 
+#define  _ELISE_GRAPHES_GRAPHE_IMPLEM_H 
+
+#include "graphes/graphe.h"
+
+
+/**************************************************************/
+/*                                                            */
+/*                   ElTabDyn                                 */
+/*                                                            */
+/**************************************************************/
+
+
+template<class T> ElTabDyn<T>::~ElTabDyn()
+{
+    if (_tvals)
+    {
+         for (INT k0 =0 ; k0<NB ; k0++) 
+         {
+             if (_tvals[k0])
+             {
+                 for (INT k1 =0 ; k1<NB ; k1++) 
+                 {
+                      if (_tvals[k0][k1])
+                      {
+                         STD_DELETE_TAB(_tvals[k0][k1]);
+                      }
+                         
+                 }
+                 STD_DELETE_TAB(_tvals[k0]);
+             }
+         }
+         STD_DELETE_TAB(_tvals);
+    }
+}
+
+template<class T> void ElTabDyn<T>::augment_index()
+{
+    if (! _tvals) 
+    {
+       _tvals     =  STD_NEW_TAB((INT)NB,T**); 
+        MEM_RAZ(_tvals,NB);
+     }
+    if (! t1(_nb) ) 
+    {
+         t1(_nb)  =  STD_NEW_TAB((INT)NB,T*) ;
+        MEM_RAZ(t1(_nb),NB);
+    }
+    if (! t2(_nb) ) 
+    {
+        t2(_nb)  =  STD_NEW_TAB((INT)NB,T);
+    }
+    
+    _nb++;
+}
+
+template<class T> void ElTabDyn<T>::set_intexable_until(INT aNb)
+{
+     while (nb()<aNb) augment_index();
+}
+
+/**************************************************************/
+/*                                                            */
+/*                   ElSom                                    */
+/*                                                            */
+/**************************************************************/
+
+template <class AttrSom,class AttrArc>
+         ElSom<AttrSom,AttrArc>::ElSom
+         (
+               ElGraphe<AttrSom,AttrArc> * gr,
+               const AttrSom & attr,
+               INT             Num
+         ) :
+                 _alive   (true),
+                 _gr      (gr),
+                 _succ    (0),
+                 _attr    (attr),
+                 _num     (Num)
+{
+}
+
+
+template <class AttrSom,class AttrArc>
+         ElSom<AttrSom,AttrArc>::~ElSom()
+{
+     TGraphe::kill_arc(_succ);
+}
+
+template <class AttrSom,class AttrArc> 
+          ElArc<AttrSom,AttrArc> * ElSom<AttrSom,AttrArc>::_remove_succ
+                                   (ElSom<AttrSom,AttrArc> * s2)
+{
+
+     for ( TArc * prec=0,*a =_succ  ;  a  ;  prec=a,a=a->_next)
+         if (&(a->s2()) == s2)
+         {
+             if (prec)
+                 prec->_next = a->_next;
+             else
+                 _succ = a->_next;
+             a->s1()._gr->add_free(a);
+             return a;
+         }
+    // ELISE_ASSERT(false,"ElSom::_remove_succ");
+
+     return 0;
+}
+
+
+
+template <class AttrSom,class AttrArc> void ElSom<AttrSom,AttrArc>::remove()
+{
+   _gr->OnKillSom(*this);
+   _alive = false;
+   _gr->_free_number.pushlast(_num);
+   _gr->_nb_som --;
+   for (TArc * la = _succ; la; )
+   {
+         if (la->_s2->_remove_succ(la->_s1) ==0)
+            ELISE_ASSERT(false,"ElSom::remove");
+         TArc * next = la->_next;
+         _gr->add_free(la);
+         la = next;
+    }
+    _succ = 0;
+}
+
+template <class AttrSom,class AttrArc> 
+         INT ElSom<AttrSom,AttrArc>::nb_succ(ElSubGraphe<AttrSom,AttrArc> & sub)
+{
+     if (! sub.inS(*this)) return 0;
+
+     INT res=0;
+     ElArc<AttrSom,AttrArc> * a = _succ;
+     for (; a ; a = a->_next)
+         res += sub.inA(*a) && sub.inS(a->s2());
+     return res;
+}
+
+template <class AttrSom,class AttrArc>  
+         ElSom<AttrSom,AttrArc> &  ElSom<AttrSom,AttrArc>::uniq_succ(ElSubGraphe<AttrSom,AttrArc> & sub)
+{
+     TArc * res = 0;
+     for ( TArc * a =_succ  ;  a   ; a=a->_next)
+	 {
+		 if (sub.inA(*a))
+		 {
+			ELISE_ASSERT(res==0,"Multiple succ in uniq_succ");
+			res = a;
+		 }
+	 }
+	 ELISE_ASSERT(res!=0,"No succ in uniq_succ");
+	 return res->s2();
+}
+
+template <class AttrSom,class AttrArc>  
+         ElSom<AttrSom,AttrArc> &  ElSom<AttrSom,AttrArc>::uniq_pred(ElSubGraphe<AttrSom,AttrArc> & sub)
+{
+     TArc * res = 0;
+     for ( TArc * a =_succ  ;  a   ; a=a->_next)
+	 {
+		 if (sub.inA(a->arc_rec()))
+		 {
+			ELISE_ASSERT(res==0,"Multiple succ in uniq_pred");
+			res = a;
+		 }
+	 }
+	 ELISE_ASSERT(res!=0,"No succ in uniq_pred");
+	 return res->s2();
+}
+
+template <class AttrSom,class AttrArc>
+         bool  ElSom<AttrSom,AttrArc>::in_sect_angulaire
+               (
+                     Pt2dr pt,
+                     ElSubGraphe<AttrSom,AttrArc> & sub
+               )
+{
+		Pt2dr orig = sub.pt(*this);
+		pt -= orig;
+		return pt.in_sect_angulaire
+			   ( 
+				    sub.pt(uniq_succ(sub))-orig,
+				    sub.pt(uniq_pred(sub))-orig
+			    );
+}
+
+
+
+
+
+
+/**************************************************************/
+/*                                                            */
+/*                   ElGraphe                                 */
+/*                                                            */
+/**************************************************************/
+
+template <class  AttrSom,class AttrArc> 
+         ElGraphe<AttrSom,AttrArc>::~ElGraphe()
+{
+     kill_arc(_larc_free);
+}
+
+template <class  AttrSom,class AttrArc> 
+         void  ElGraphe<AttrSom,AttrArc>::kill_arc(ElArc<AttrSom,AttrArc> * la)
+{
+     while (la)
+     {
+           TArc * next = la->_next;
+           DELETE_ONE(la);
+           la = next;
+     }
+}
+
+
+template <class  AttrSom,class AttrArc> 
+         ElArc<AttrSom,AttrArc>*  ElGraphe<AttrSom,AttrArc>::arc_s1s2
+         (
+                ElSom<AttrSom,AttrArc> & s1,
+                ElSom<AttrSom,AttrArc> & s2
+         )
+{
+     TArc * a =s1._succ  ;  
+     for 
+     ( 
+                                                 ;  
+           a && (&(a->s2()) !=  (TSom *) &(s2))  ; 
+           a=a->_next
+     )
+     ;
+
+      return a;
+}
+
+
+template <class  AttrSom,class AttrArc> 
+         ElArc<AttrSom,AttrArc>&  ElGraphe<AttrSom,AttrArc>::_add_arc
+         (
+                ElSom<AttrSom,AttrArc> & s1,
+                ElSom<AttrSom,AttrArc> & s2,
+                const AttrArc &          attr
+         )
+{
+
+     TArc * res = 0;
+     if (_larc_free)
+     {
+         res = _larc_free;
+         _larc_free = _larc_free->_next;
+         new (res) TArc  (s1,s2,attr);
+     }
+     else
+       res = CLASS_NEW_ONE(TArc,(s1,s2,attr));
+     res->_next = s1._succ;
+    s1._succ = res;
+    return * res;
+}
+
+template <class  AttrSom,class AttrArc> 
+  ElSom<AttrSom,AttrArc> & ElGraphe<AttrSom,AttrArc>::new_som(const AttrSom & attr)
+{
+    _nb_som++;
+    if (_free_number.empty())
+    {
+         _free_number.pushlast(_tsom.nb());
+         _tsom.augment_index();
+    }
+    INT num = _free_number.poplast();
+    _tsom[num] = TSom(this,attr,num);
+    TSom & aRes =  _tsom[num];
+    OnNewSom(aRes);
+
+    return aRes;
+}
+
+
+/**************************************************************/
+/*                                                            */
+/*                   ElArc                                    */
+/*                                                            */
+/**************************************************************/
+
+template <class AttrSom,class AttrArc> 
+         void  ElArc<AttrSom,AttrArc>::remove()
+{
+     TSom & S1 = s1();
+     TSom & S2 = s2();
+     S1.gr().OnKillArc(*this);
+     ELISE_ASSERT(S1._alive&&S2._alive,"ElArc::remove()");
+     if ((S1._remove_succ(&S2)==0)||(S2._remove_succ(&S1)==0))
+     {
+        ELISE_ASSERT(false,"ElArc::remove()(Internal error ??? maybe)");
+     }
+}
+
+#endif //  _ELISE_GRAPHES_GRAPHE_IMPLEM_H 
+
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
---
+++ src/uti_image/ImVis/cAppliVino.cpp
@@ -0,0 +1,385 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+   
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in 
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte 
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#include "Vino.h"
+
+
+#if (ELISE_X11)
+
+
+
+std::string NameVinoPyramImage(const std::string & aDir,const std::string & aName,int aZoom)
+{
+     if (aZoom==1) return  aName;
+
+     return  aDir + "Tmp-MM-Dir/" + StdPrefix(NameWithoutDir(aName)) +  "_Zoom" + ToString(aZoom) + ".tif";
+}
+
+std::string cAppli_Vino::NamePyramImage(int aZoom)
+{
+     return NameVinoPyramImage(mDir,mNameTiffIm,aZoom);
+}
+
+std::string  cAppli_Vino::CalculName(const std::string & aName,INT aZoom)
+{
+   return NameVinoPyramImage(mDir,aName,aZoom);
+}
+
+/*
+*/
+
+extern Video_Win * TheWinAffRed ;
+
+
+cAppli_Vino::cAppli_Vino(int argc,char ** argv) :
+    mSzIncr            (400,400),
+    mNbPixMinFile      (2e6)
+{
+    mNameXmlIn = Basic_XML_MM_File("Def_Xml_EnvVino.xml");
+    if (argc>1)
+    {
+       mNameXmlOut =  DirOfFile(argv[1]) + "Tmp-MM-Dir/" + "EnvVino.xml";
+       if (ELISE_fp::exist_file(mNameXmlOut))
+          mNameXmlIn = mNameXmlOut;
+    }
+    EnvXml() = StdGetFromPCP(mNameXmlIn,Xml_EnvVino);
+
+
+
+    ElInitArgMain
+    (
+        argc,argv,
+        LArgMain()  << EAMC(mNameIm, "Image", eSAM_IsExistFile),
+        LArgMain()  << EAM(SzW(),"SzW",true,"size of window")
+                    << EAM(ZoomBilin(),"Bilin",true,"Bilinear mode")
+                    << EAM(SpeedZoomGrab(),"SZG",true,"Speed Zoom Grab")
+                    << EAM(SpeedZoomMolette(),"SZM",true,"Speed Zoom Molette")
+                    << EAM(LargAsc(),"WS",true,"Width Scroller")
+    );
+
+// Files
+    mDir = DirOfFile(mNameIm);
+    ELISE_fp::MkDirSvp(mDir+"Tmp-MM-Dir/");
+    MakeFileXML(EnvXml(),mNameXmlOut);
+
+
+
+    mNameIm = NameWithoutDir(mNameIm);
+    mTiffIm = new Tiff_Im(Tiff_Im::StdConvGen(mDir+mNameIm,-1,true,false));
+    mNameTiffIm = mTiffIm->name();
+    mTifSz = mTiffIm->sz();
+    mNbPix = double(mTifSz.x) * double(mTifSz.y);
+    mNbChan = mTiffIm->nb_chan();
+    mCoul = ( mNbChan == 3) ;
+
+    mSzEl = (mNbChan  * mTiffIm->bitpp()) /8.0;
+
+// window
+
+    mRatioFulXY = Pt2dr(SzW()).dcbyc(Pt2dr(mTifSz));
+    mRatioFul = ElMin(mRatioFulXY.x,mRatioFulXY.y);
+    // mSzW = round_up(Pt2dr(mTifSz) * mRatioFul);
+    mWAscH = Video_Win::PtrWStd(Pt2di(SzW().x,LargAsc()),true);
+    mW =    new  Video_Win(*mWAscH,Video_Win::eBasG,SzW());
+    mWAscV = new Video_Win(*mW,Video_Win::eDroiteH,Pt2di(LargAsc(),SzW().y));
+
+
+    mTitle = std::string("MicMac/Vino -> ") + mNameIm;
+    mW->set_title(mTitle.c_str());
+    mDisp = new Video_Display(mW->disp());
+
+
+
+    
+// Scrollers
+
+    mVVE = new  VideoWin_Visu_ElImScr(*mW,(mCoul?Elise_Palette(mW->prgb()):Elise_Palette(mW->pgray())),mSzIncr);
+
+    mVEch.push_back(1); 
+    int aSc = 1;
+    while ( ((mNbPix*mSzEl)/ElSquare(aSc))  > mNbPixMinFile)
+    {
+        aSc *= 2;
+        mVEch.push_back(aSc); 
+        std::string aName = NamePyramImage(mVEch.back());
+
+        if (! ELISE_fp::exist_file(aName))
+        {
+             std::string aMes = "Reduce : " +ToString(aSc);
+             mW->fixed_string(Pt2dr(100,SzW().y-50),aMes.c_str() ,mWAscH->prgb()(255,0,0),true);
+             TheWinAffRed = mW;
+             MakeTiffRed2
+             (
+                 NamePyramImage(mVEch.back()/2),
+                 aName,
+                 mTiffIm->type_el(),
+                 16,
+                 false,
+                 -1
+             );
+             TheWinAffRed = 0;
+             mW->clear();
+        }
+    }
+    mWAscH->clear();
+
+    InitMenu();
+}
+
+void cAppli_Vino::PostInitVirtual()
+{
+    mVVE->SetEtalDyn(0,255);
+    mScr = ElPyramScroller::StdPyramide(*mVVE,mNameTiffIm,&mVEch,false,false,this);
+    mScr->SetAlwaysQuick(false);
+    mScr->SetAlwaysQuickInZoom(!ZoomBilin());
+    // mScr = new ImFileScroller<U_INT1> (*mVVE,*mTiffIm,1.0);
+    // mScr->ReInitTifFile(*mTiffIm);
+    mScr->set_max();
+    ShowAsc();
+
+}
+
+void cAppli_Vino::InitMenu()
+{
+    mSzCase        = MMIcone("Exit").sz();
+    mPopUpBase = new GridPopUpMenuTransp(*mW,mSzCase,Pt2di(2,3),Pt2di(1,1));
+    mCaseExit  = new CaseGPUMT (*mPopUpBase,"titi",Pt2di(0,0), MMIcone("Exit").in(1) *255);
+
+}
+
+void cAppli_Vino::End()
+{
+    std::cout << "   ******************************************\n";
+    std::cout << "   *                                        *\n";
+    std::cout << "   *    V-isualizer of                      *\n";
+    std::cout << "   *    I-mages                             *\n";
+    std::cout << "   *    N-ot                                *\n";
+    std::cout << "   *    O-versized                          *\n";
+    std::cout << "   *                                        *\n";
+    std::cout << "   ******************************************\n";
+
+    exit(EXIT_SUCCESS);
+
+}
+
+
+void  cAppli_Vino::MenuPopUp()
+{
+    mPopUpCur = 0;
+    if ((!mCtrl0) && (!mShift0)) mPopUpCur = mPopUpBase;
+
+    if (mPopUpCur==0)  return;
+
+    mModeGrab=eModeVinoPopUp;
+
+    mPopUpCur->UpCenter(Pt2di(mP0Click));
+    mW->grab(*this);
+    CaseGPUMT * aCase = mPopUpCur->PopAndGet();
+
+
+    if (mPopUpCur==mPopUpBase)
+    {
+        if (aCase== mCaseExit)
+        {
+            End();
+        }
+    }
+}
+
+
+
+
+
+void cAppli_Vino::Boucle()
+{
+    while (1)
+    {
+        Clik aCl = mDisp->clik_press();
+
+        mP0Click =  aCl._pt;
+        mScale0  =  mScr->sc();
+        mTr0     =  mScr->tr();
+        mBut0    = aCl._b;
+        mCtrl0  = aCl.controled();
+        mShift0  = aCl.shifted();
+
+        // Click sur la fenetre principale 
+        if (aCl._w == *mW)
+        {
+ 
+            if (mBut0==2)
+               ExeClikGeom(aCl);
+            if ((mBut0==4) || (mBut0==5))
+            {
+                ZoomMolette();
+                ShowAsc();
+            }
+
+            if (mBut0==1)
+            {
+                ShowOneVal();
+            }
+            if (mBut0==3)
+            {
+                MenuPopUp();
+            }
+        }
+        if (aCl._w == *mWAscH)
+        {
+             mModeGrab= eModeGrapAscX;
+             mWAscH->grab(*this);
+             ShowAsc();
+        }
+        if (aCl._w == *mWAscV)
+        {
+             mModeGrab= eModeGrapAscY;
+             mWAscV->grab(*this);
+             ShowAsc();
+        }
+    }
+}
+
+
+/********************************************/
+/*                                          */
+/*  Lecture                                 */
+/*                                          */
+/********************************************/
+
+
+
+cXml_StatVino  cAppli_Vino::StatRect(Pt2di  &aP0,Pt2di & aP1)
+{
+    cXml_StatVino aRes;
+    CorrectRect(aP0,aP1,mTifSz);
+
+    FillStat(aRes,rectangle(aP0,aP1),mTiffIm->in());
+
+    return aRes;
+
+}
+
+
+
+void cAppli_Vino::EffaceVal()
+{
+    if (mInitP0StrVal)
+    {
+        int aR = 7;
+        mScr->VisuIm(mP0StrVal-Pt2di(aR,aR),mP1StrVal+Pt2di(aR,aR),false);
+    }
+}
+
+void  cAppli_Vino::ShowOneVal()
+{
+    mInitP0StrVal = false;
+    mModeGrab = eModeGrapShowRadiom;
+    mW->grab(*this);
+    EffaceVal();
+}
+
+void  cAppli_Vino::ShowOneVal(Pt2dr aPW)
+{
+    EffaceVal();
+    Pt2di  aP = round_ni(mScr->to_user(aPW));
+    Pt2di  aPp1 = aP+Pt2di(1,1);
+
+    cXml_StatVino aStat = StatRect(aP,aPp1);
+
+    //std::cout << "PPPPP " << aP << " " << mSom[0] << "\n";
+
+    std::string aMesXY = " x=" + ToString(aP.x) + " y=" + ToString(aP.y);
+    std::string aMesV =  " V=";
+    for (int aK=0 ; aK<mNbChan; aK++)
+        aMesV = aMesV  + SimplString(ToString(aStat.Soms()[aK])) + " ";
+
+    // aMes = aMes + "      ";
+
+    // mP0StrVal = Pt2di(mP0Click)+Pt2di(-20,30);
+    mP0StrVal = Pt2di(aPW)+Pt2di(-20,30);
+
+    mW->fixed_string(Pt2dr(mP0StrVal),aMesV.c_str(),mW->pdisc()(P8COL::black),true);
+    Pt2di aSzV =  mW->SizeFixedString(aMesV);
+    Pt2di aSzXY =  mW->SizeFixedString(aMesXY);
+
+    Pt2di aP0XY = mP0StrVal + Pt2di(0,aSzXY.y+3);
+    mP0StrVal.y -= aSzV.y;
+    mW->fixed_string(Pt2dr(aP0XY),aMesXY.c_str(),mW->pdisc()(P8COL::black),true);
+
+
+    mP1StrVal = aP0XY + Pt2di(ElMax(aSzV.x,aSzXY.x),0);
+    mInitP0StrVal = true;
+}
+
+#endif
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
