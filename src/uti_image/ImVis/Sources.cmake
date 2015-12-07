b686da62c3c575bd6953e329aa2104070d180866
deseilligny <deseilligny@localhost>
2015-12-07 19:15:38 +0100

    Vino reorg

--- src/uti_image/ImVis/Sources.cmake
+++ src/uti_image/ImVis/Sources.cmake
@@ -3,6 +3,7 @@ set(uti_image_vino
     ${UTI_IMAGE_VINO_DIR}/cAppliVino.cpp
     ${UTI_IMAGE_VINO_DIR}/Vino_Geom.cpp
     ${UTI_IMAGE_VINO_DIR}/Extern_Vino.cpp
+    ${UTI_IMAGE_VINO_DIR}/Extern_XmlX11.cpp
     ${UTI_IMAGE_VINO_DIR}/Vino_PopUp.cpp
     ${UTI_IMAGE_VINO_DIR}/Vino_Radiom.cpp
     ${UTI_IMAGE_VINO_DIR}/Vino_Messages.cpp
