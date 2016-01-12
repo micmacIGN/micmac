98c3b233de5614df5aec49abfadcb4b9509198d0
jbelvaux <jbelvaux@localhost>
2016-01-12 21:04:52 +0100

    - compil apple llvm 4.2, qt 5.5

---
+++ src/uti_phgrm/TiepRed/Sources.cmake
@@ -0,0 +1,11 @@
+set(uti_image_tiepred
+    ${UTI_PHGRM_TieRed_DIR}/cAppliTieRed.cpp
+    ${UTI_PHGRM_TieRed_DIR}/cCameraTiepRed.cpp
+)
+
+
+list( APPEND uti_phgrm_Src_Files
+        ${uti_image_tiepred}
+)
+
+
