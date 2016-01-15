1bee59758c3e6d8f379e9db7b446772a4f5634aa
jbelvaux <jbelvaux@localhost>
2016-01-15 15:37:25 +0100

    - pas besoin de Qt5LinguistTools

---
+++ src/output/Sources.cmake
@@ -0,0 +1,13 @@
+set(Output_Src_Files
+	${OUTPUT_DIR}/oper.cpp
+	${OUTPUT_DIR}/out_general.cpp
+	${OUTPUT_DIR}/reduction.cpp
+	${OUTPUT_DIR}/transform_intern.cpp
+)
+
+SOURCE_GROUP(Output FILES ${Output_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${Output_Src_Files}
+)
