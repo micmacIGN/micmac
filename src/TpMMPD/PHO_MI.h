b8a66a3289c8e25110408e8f3f730226ed45274c
gnguyen <gnguyen@localhost>
2016-02-10 18:16:39 +0100

    PHO_MI:ajoute hypothese sphere

--- src/TpMMPD/PHO_MI.h
+++ src/TpMMPD/PHO_MI.h
@@ -65,7 +65,7 @@ class CplImg : public cCorrelImage
           void SupposeVecSruf1er(Pt2dr dirX, Pt2dr dirY);
           void ValPtsLia(vector<double> NorSur);
 
-          vector<bool> CalVectorSurface(string mImg3eme);
+          vector<bool> CalVectorSurface(string mImg3eme, string ModeSurf);
           //bool IsInside(Pt2dr checkPoint, double w, double h);
           Video_Win * mW ; Video_Win * mW1 ; Video_Win * mW2 ;
 };
