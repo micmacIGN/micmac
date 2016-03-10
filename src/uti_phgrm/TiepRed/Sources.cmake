3b0426827057308ed857cc6ab09280399351daec
jbelvaux <jbelvaux@localhost>
2016-03-10 12:36:19 +0100

    - test expression r?guli?re non compilable (? suivre)

---
+++ src/uti_phgrm/TiepRed/Sources.cmake
@@ -0,0 +1,14 @@
+set(uti_image_tiepred
+    ${UTI_PHGRM_TieRed_DIR}/cAppliTieRed.cpp
+    ${UTI_PHGRM_TieRed_DIR}/cAppliTieRed_Algo.cpp
+    ${UTI_PHGRM_TieRed_DIR}/cCameraTiepRed.cpp
+    ${UTI_PHGRM_TieRed_DIR}/cLnk2ImTiepRed.cpp
+    ${UTI_PHGRM_TieRed_DIR}/cPMulTiepRed.cpp
+)
+
+
+list( APPEND uti_phgrm_Src_Files
+        ${uti_image_tiepred}
+)
+
+
