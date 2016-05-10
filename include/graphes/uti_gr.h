0271a2285ed259130be96b00cf78970dc4ecc1a9
jmmuller <jmmuller@localhost>
2016-05-10 16:38:22 +0200

    Appli extractAppui3D

---
+++ include/graphes/uti_gr.h
@@ -0,0 +1,236 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+   
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in 
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte 
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+
+#ifndef _ELISE_GRAPHES_UTI_GR_H
+#define _ELISE_GRAPHES_UTI_GR_H
+
+#include "graphes/graphe.h"
+
+template <class AttrSom,class AttrArc> 
+         void set_flag_all_soms
+              (
+                    ElGraphe<AttrSom,AttrArc> &          gr,
+                    ElSubGraphe<AttrSom,AttrArc> &       sub,
+                    INT                                  flag,
+                    bool                                 val
+              )
+{
+      for
+      (
+            ElSomIterator<AttrSom,AttrArc> sit = gr.begin(sub) ;
+            sit.go_on()                        ;
+            sit++
+      )
+      {
+           ElSom<AttrSom,AttrArc> & s =   * sit;
+           s.flag_set_kth(flag,val);
+      }
+}
+template <class AttrSom,class AttrArc> 
+         void set_flag_all_soms
+              (
+                    ElGraphe<AttrSom,AttrArc> &          gr,
+                    INT                                  flag,
+                    bool                                 val
+              )
+{
+     ElSubGraphe<AttrSom,AttrArc>  sub;
+     set_flag_all_soms(gr,sub,flag,val);
+}
+
+
+template <class AttrSom,class AttrArc> 
+         void sym_set_flag_all_arcs
+              (
+                    ElGraphe<AttrSom,AttrArc> &          gr,
+                    ElSubGraphe<AttrSom,AttrArc> &       sub,
+                    INT                                  flag,
+                    bool                                 val
+              )
+{
+      for
+      (
+            ElSomIterator<AttrSom,AttrArc> sit = gr.begin(sub) ;
+            sit.go_on()                        ;
+            sit++
+      )
+      {
+           ElSom<AttrSom,AttrArc> & s =   * sit;
+           for
+           (
+                  ElArcIterator<AttrSom,AttrArc> ait = s.begin(sub) ;
+                  ait.go_on()                                        ;
+                  ait++
+           )
+           {
+                 (*ait).sym_flag_set_kth(flag,val);
+           }
+     }
+
+}
+
+template <class AttrSom,class AttrArc> 
+         void set_flag_all_arcs
+              (
+                    ElGraphe<AttrSom,AttrArc> &          gr,
+                    INT                                  flag,
+                    bool                                 val
+              )
+{
+     ElSubGraphe<AttrSom,AttrArc>  sub;
+     sym_set_flag_all_arcs(gr,sub,flag,val);
+}
+
+
+template <class TSubGr> class cSubGrFlagSom : public  ElSubGraphe<typename TSubGr::ASom,typename TSubGr::AArc>
+{
+     public :
+          cSubGrFlagSom(TSubGr & aSub,int aFlag) :
+              mSub(aSub),
+              mFlag(aFlag)
+          {
+          }
+          bool   inS(typename TSubGr::TSom & aS)  { return aS.flag_kth(mFlag) && (mSub.inS(aS)); }
+          bool   inA(typename TSubGr::TArc & anA) { return mSub.inA(anA);}
+          // Pt2dr pt(TSom & aSom) {return mSub.pt(aSom);}
+     private :
+          TSubGr & mSub;
+          int mFlag;
+};
+
+template <class TSubGr> class cSubGrFlagArc : public  ElSubGraphe<typename TSubGr::ASom,typename TSubGr::AArc>
+{
+     public :
+          cSubGrFlagArc(TSubGr & aSub,int aFlag) :
+              mSub(aSub),
+              mFlag(aFlag)
+          {
+          }
+          bool   inS(typename TSubGr::TSom & aS)  { return mSub.inS(aS); }
+          bool   inA(typename TSubGr::TArc & anA) { return anA.flag_kth(mFlag) && mSub.inA(anA);}
+          Pt2dr pt(typename TSubGr::TSom & aSom) {return mSub.pt(aSom);}
+     private :
+          TSubGr & mSub;
+          int mFlag;
+};
+
+
+
+
+template <class TSubGr> class cSubGrFlagSomArc : public ElSubGraphe<typename TSubGr::ASom,typename TSubGr::AArc>
+{
+     public :
+          cSubGrFlagSomArc(const TSubGr & aSub,int aFlagSom,int aFlagArc) :
+              mSub    (aSub),
+              mFlagSom  (aFlagSom),
+              mFlagArc  (aFlagArc)
+          {
+          }
+          bool   inS(typename TSubGr::TSom & aS)  { return aS.flag_kth(mFlagSom) && (mSub.inS(aS)); }
+          bool   inA(typename TSubGr::TArc & anA)  { return anA.flag_kth(mFlagArc) && (mSub.inA(anA));}
+     private :
+          TSubGr & mSub;
+          int mFlagSom;
+          int mFlagArc;
+};
+
+
+
+
+/*
+template <class TSubGr> class cSubGrFlagSom : public TSubGr
+{
+     public :
+          TSubGr(int aFlag) :
+              mFlag(aFag)
+          {
+          }
+          bool   inS(typename TSom::TSom &)  
+          {
+               return aS.flag_kth(mFlag);
+          }
+     private :
+          int mFlag;
+};
+*/
+
+
+
+
+#endif // _ELISE_GRAPHES_UTI_GR_H
+
+
+
+
+
+
+
+
+/*Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant à la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007*/
