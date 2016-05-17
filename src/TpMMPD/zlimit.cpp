e8ab2823f21a20ba67fd66d54dd8e1324d892a2a
deseilligny <deseilligny@users.noreply.github.com>
2016-05-17 17:26:48 +0200

    Eta POly On ????

---
+++ src/TpMMPD/zlimit.cpp
@@ -0,0 +1,198 @@
+/*Header-MicMac-eLiSe-25/06/2007
+
+    MicMac : Multi Image Correspondances par Methodes Automatiques de Correlation
+    eLiSe  : ELements of an Image Software Environnement
+
+    www.micmac.ign.fr
+
+
+    Copyright : Institut Geographique National
+    Author : Marc Pierrot Deseilligny
+    Contributors : Gregoire Maillet, Didier Boldo.
+
+[1] M. Pierrot-Deseilligny, N. Paparoditis.
+    "A multiresolution and optimization-based image matching approach:
+    An application to surface reconstruction from SPOT5-HRS stereo imagery."
+    In IAPRS vol XXXVI-1/W41 in ISPRS Workshop On Topographic Mapping From Space
+    (With Special Emphasis on Small Satellites), Ankara, Turquie, 02-2006.
+
+[2] M. Pierrot-Deseilligny, "MicMac, un lociel de mise en correspondance
+    d'images, adapte au contexte geograhique" to appears in
+    Bulletin d'information de l'Institut Geographique National, 2007.
+
+Francais :
+
+   MicMac est un logiciel de mise en correspondance d'image adapte
+   au contexte de recherche en information geographique. Il s'appuie sur
+   la bibliotheque de manipulation d'image eLiSe. Il est distibue sous la
+   licences Cecill-B.  Voir en bas de fichier et  http://www.cecill.info.
+
+
+English :
+
+    MicMac is an open source software specialized in image matching
+    for research in geographic information. MicMac is built on the
+    eLiSe image library. MicMac is governed by the  "Cecill-B licence".
+    See below and http://www.cecill.info.
+
+Header-MicMac-eLiSe-25/06/2007*/
+#include "StdAfx.h"
+
+
+/**
+ * Zlimit: crop a MNT in depth
+ * */
+
+//
+//----------------------------------------------------------------------------
+
+int Zlimit_main(int argc,char ** argv)
+{
+    std::string aNameOriMNT;
+    std::string aMasqSup="?";
+    double aMaxZ,aMinZ;
+    
+    cout<<"Zlimit: create masq for depth image."<<endl;
+    ElInitArgMain
+    (
+    argc,argv,
+    //mandatory arguments
+    LArgMain()  << EAMC(aNameOriMNT, "MNT xml file name", eSAM_IsExistFile)
+                << EAMC(aMinZ, "Min Z (m)")
+                << EAMC(aMaxZ, "Max Z (m)"),
+    //optional arguments
+    LArgMain()  << EAM(aMasqSup,"MasqSup",true,"Supplementary masq")
+    );
+
+    if (MMVisualMode) return EXIT_SUCCESS;
+    
+    ELISE_ASSERT(aMinZ<aMaxZ,"Please try with MinZ<MaxZ...");
+    
+    string aMasqTifName=aNameOriMNT+"_MasqZminmax.tif";
+    string aMasqXmlName=aNameOriMNT+"_MasqZminmax.xml";
+    
+    cFileOriMnt aOriMnt = StdGetFromPCP(aNameOriMNT,FileOriMnt);
+    cout<<aOriMnt.OriginePlani()<<" "<<aOriMnt.ResolutionPlani()<<endl;
+
+    string aNameFileMnt=aOriMnt.NameFileMnt();
+    string aNameFileMasque="";
+    
+    if (aOriMnt.NameFileMasque().IsInit())
+        aNameFileMasque=aOriMnt.NameFileMasque().Val();
+    double aOrigineAlti=aOriMnt.OrigineAlti();
+    double aResolutionAlti=aOriMnt.ResolutionAlti();
+    cout<<"z: *"<<aResolutionAlti<<" +"<<aOrigineAlti<<endl;
+    cout<<"MinZ: "<<aMinZ<<"    MaxZ:"<<aMaxZ<<endl;
+    
+    Tiff_Im aFileMnt(aNameFileMnt.c_str());
+    TIm2D<U_INT1,INT4> tmpImage1T(aOriMnt.NombrePixels());//image for max
+    Im2D<U_INT1,INT4>  tmpImage1(tmpImage1T._the_im);
+    TIm2D<U_INT1,INT4> tmpImage2T(aOriMnt.NombrePixels());//image for min
+    Im2D<U_INT1,INT4>  tmpImage2(tmpImage2T._the_im);
+
+    TIm2D<U_INT1,INT4> masqImageT(aOriMnt.NombrePixels());//image for masq
+    Im2D<U_INT1,INT4>  masqImage(masqImageT._the_im);
+
+    ELISE_COPY(tmpImage1.all_pts(),((1/(aFileMnt.in()*aResolutionAlti+aOrigineAlti))<aMaxZ),tmpImage1.out());
+    ELISE_COPY(tmpImage2.all_pts(),((1/(aFileMnt.in()*aResolutionAlti+aOrigineAlti))>aMinZ),tmpImage2.out());
+ 
+    if (aMasqSup!="?")
+    {
+      Tiff_Im aFileMasqSup(aMasqSup.c_str());
+      ELISE_ASSERT(aFileMasqSup.sz()==aOriMnt.NombrePixels(),"Masq Sup and MNT must have the same size!");
+      TIm2D<U_INT1,INT4> aMasqSupImT(aFileMasqSup.sz());//image for masq sup
+      Im2D<U_INT1,INT4>  aMasqSupIm(aMasqSupImT._the_im);
+
+      ELISE_COPY(aMasqSupIm.all_pts(),(aFileMasqSup.in()>0),aMasqSupIm.out());
+      Tiff_Im::CreateFromIm(aMasqSupIm,"tmp.tif");
+
+      ELISE_COPY(masqImage.all_pts(),(tmpImage1.in()*tmpImage2.in()*aMasqSupIm.in()),masqImage.out());
+    }else{
+      ELISE_COPY(masqImage.all_pts(),(tmpImage1.in()*tmpImage2.in()),masqImage.out());
+    }
+
+    ELISE_COPY(masqImage.all_pts(),(masqImage.in()*255),masqImage.out());
+
+
+    Tiff_Im::CreateFromIm(masqImage,aMasqTifName);
+
+    //for debug
+    /*ELISE_COPY(tmpImage1.all_pts(),((1/(aFileMnt.in()*aResolutionAlti+aOrigineAlti))<aMaxZ)*255,tmpImage1.out());
+    ELISE_COPY(tmpImage2.all_pts(),((1/(aFileMnt.in()*aResolutionAlti+aOrigineAlti))>aMinZ)*255,tmpImage2.out());
+    Tiff_Im::CreateFromIm(tmpImage1,"tmp1.tif");
+    Tiff_Im::CreateFromIm(tmpImage2,"tmp2.tif");*/
+
+    
+    cFileOriMnt anOriMasq;
+    anOriMasq.NameFileMnt() = aMasqTifName;
+    anOriMasq.NombrePixels() = aOriMnt.NombrePixels();
+    anOriMasq.OriginePlani() = Pt2dr(0,0);
+    anOriMasq.ResolutionPlani() = Pt2dr(1.0,1.0);
+    anOriMasq.OrigineAlti() = 0.0;
+    anOriMasq.ResolutionAlti() = 1.0;
+    anOriMasq.Geometrie() = eGeomMNTFaisceauIm1PrCh_Px1D;
+    MakeFileXML(anOriMasq,aMasqXmlName);
+    
+    cout<<"New masq created: "<<aMasqTifName<<endl;
+    
+    //for debug
+    /*TIm2D<REAL4,REAL8> mDepthImageT(aFileMnt.sz());
+    Im2D<REAL4,REAL8>  mDepthImage(mDepthImageT._the_im);
+    ELISE_COPY(mDepthImage.all_pts(),aFileMnt.in(),mDepthImage.out());
+    double min=5000;
+    double max=-5000;
+    double val;
+    for (int anY=0 ; anY<mDepthImage.sz().y ; anY++)
+    {
+        for (int anX=0 ; anX<mDepthImage.sz().x ; anX++)
+        {
+            Pt2di aP(anX,anY);
+            val=1/(mDepthImageT.get(aP)*aResolutionAlti+aOrigineAlti);
+            //cout<<val<<" ";
+            if (val<min) min=val;
+            if (val>max) max=val;
+        }
+        //cout<<endl;
+    }
+    cout<<"min: "<<min<<"   max: "<<max<<endl;*/
+    
+    
+    /*ELISE_COPY(tmpImage2.all_pts(),(1/(aFileMnt.in()*aResolutionAlti+aOrigineAlti)),tmpImage2.out());
+    Tiff_Im::CreateFromIm(tmpImage2,"out.tif");*/
+
+
+    return EXIT_SUCCESS;
+}
+
+/* Footer-MicMac-eLiSe-25/06/2007
+
+Ce logiciel est un programme informatique servant �  la mise en
+correspondances d'images pour la reconstruction du relief.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  �  l'utilisation,  �  la modification et/ou au
+développement et �  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe �
+manipuler et qui le réserve donc �  des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
+logiciel �  leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+
+Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+Footer-MicMac-eLiSe-25/06/2007/*/
---
+++ src/saisieQT/main/saisieQT_main.cpp
@@ -0,0 +1,227 @@
+﻿#include "saisieQT_main.h"
+
+const cArgLogCom cArgLogCom::NoLog(-1);
+
+vector<cMMCom> getSaisieQtCommands()
+{
+	vector<cMMCom> result;
+
+	result.push_back(cMMCom("SaisieAppuisInitQT", saisieAppuisInitQT_main, " Interactive tool for initial capture of GCP"));
+	result.push_back(cMMCom("SaisieAppuisPredicQT", saisieAppuisPredicQT_main, " Interactive tool for assisted capture of GCP"));
+	result.push_back(cMMCom("SaisieBascQT", saisieBascQT_main, " Interactive tool to capture information on the scene"));
+	result.push_back(cMMCom("SaisieCylQT", SaisieCylQT_main, " Interactive tool to capture information on the scene for cylinders"));
+	result.push_back(cMMCom("SaisieMasqQT", saisieMasqQT_main, " Interactive tool to capture masq"));
+	result.push_back(cMMCom("SaisieBoxQT", saisieBoxQT_main, " Interactive tool to capture 2D box"));
+
+	return result;
+}
+
+int qtpopup(const string &aText)
+{
+	return QMessageBox(QMessageBox::NoIcon, QString("popup"), QString(aText.c_str()), QMessageBox::Ok).exec();
+}
+
+int helpMessage(const QApplication &app, QString text)
+{
+    QString title = QObject::tr("Help for ") + app.applicationName();
+#ifdef WIN32
+    QMessageBox msgBox(QMessageBox::NoIcon, title, text, QMessageBox::Ok);
+    return msgBox.exec();
+#else
+    printf("\n%s\n", title.toStdString().c_str());
+    printf("\n%s", text.toStdString().c_str());
+    return 0;
+#endif
+}
+
+#if ( ( defined WIN32 ) && ( ELISE_QT_VERSION>=4 ) )
+class Win32CommandLineConverter
+{
+private:
+    char** argv_;
+    std::vector<char*> storage_;
+public:
+    Win32CommandLineConverter()
+    {
+        LPWSTR cmd_line = GetCommandLineW();
+
+        int argc;
+
+        LPWSTR* w_argv = CommandLineToArgvW(cmd_line, &argc);
+
+        argv_ = new char*[argc];
+
+        storage_.reserve(argc);
+
+        for(int i=0; i<argc; ++i) {
+            storage_.push_back(ConvertWArg(w_argv[i]));
+            argv_[i] = storage_.back();
+        }
+        LocalFree(w_argv);
+    }
+    int argc() const
+    {
+        return static_cast<int>(storage_.size());
+    }
+    char** argv() const
+    {
+        return argv_;
+    }
+    static char* ConvertWArg(LPWSTR w_arg)
+    {
+        int size = WideCharToMultiByte(CP_UTF8, 0, w_arg, -1, nullptr, 0, nullptr, nullptr);
+        char* ret = new char[size];
+        WideCharToMultiByte(CP_UTF8, 0, w_arg, -1, ret, size, nullptr, nullptr);
+        return ret;
+    }
+};
+#endif
+
+QApplication & getQApplication()
+{
+	QApplication *result = (QApplication *)QCoreApplication::instance();
+	if (result == NULL) ELISE_ERROR_EXIT("no QApplication available");
+	return *result;
+}
+
+#if ( ( defined WIN32 ) && ( ELISE_QT_VERSION >= 4 ) )
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow)
+{
+    Win32CommandLineConverter cmd_line;
+    int argc = cmd_line.argc();
+    char **argv = cmd_line.argv();
+#else
+int main(int argc, char *argv[])
+{
+	//~ gDefaultDebugErrorHandler->setAction(MessageHandler::CIN_GET);
+#endif
+
+    MMD_InitArgcArgv( argc, argv );
+    initQtLibraryPath();
+
+    QApplication app(argc, argv);
+
+    // QT Modifie le comportement de sscanf !!!!! problematique quand on parse les fichiers XML
+    setlocale(LC_NUMERIC,"C");
+
+    app.setOrganizationName("Culture3D");
+    app.setApplicationName("QT graphical tools");
+
+    setStyleSheet(app);
+
+	vector<cMMCom> commands = getSaisieQtCommands();
+
+	tCommande commandFunction = NULL;
+	string commandStr, lowCommandStr;
+	if (argc > 1)
+	{
+		commandStr = argv[1];
+		lowCommandStr = StrToLower(commandStr);
+	}
+
+	for (size_t iCommand = 0; iCommand < commands.size(); iCommand++)
+		if (lowCommandStr == commands[iCommand].mLowName) commandFunction = commands[iCommand].mCommand;
+
+	if (commandFunction == NULL)
+	{
+		ostringstream ss;
+
+		if ( !commandStr.empty())
+			ss << "[" << commandStr << "]" << QObject::tr(" is not a valid command!!!").toStdString() << "\n" << endl;
+
+		ss << "valid commands are: \n" << endl;
+		for (size_t iCommand = 0; iCommand < commands.size(); iCommand++)
+			ss << commands[iCommand].mName << endl;
+		ss << endl;
+
+		helpMessage(app, QString(ss.str().c_str()));
+
+		return EXIT_FAILURE;
+	}
+
+	return (*commandFunction)(argc, argv);
+}
+
+bool checkNamePt(QString text)
+{
+    if (text.contains(".txt") && QFileInfo(text).isAbsolute())
+    {
+        QMessageBox::critical(NULL, QObject::tr("Error"), QObject::tr("Don't use an absolute path for point file name!"));
+        return false;
+    }
+    return true;
+}
+
+QStringList getFilenames(string aDir, string aName)
+{
+    list<string> aNamelist = RegexListFileMatch(aDir, aName, 1, false);
+    QStringList filenames;
+
+    for
+        (
+        list<string>::iterator itS=aNamelist.begin();
+    itS!=aNamelist.end();
+    itS++
+        )
+        filenames.push_back( QString((aDir + *itS).c_str()));
+
+    return filenames;
+}
+
+void loadTranslation(QApplication &app)
+{
+    QSettings settings(QApplication::organizationName(), QApplication::applicationName());
+    settings.beginGroup("Language");
+    int lang = settings.value("lang", 0).toInt();
+    settings.endGroup();
+
+    if(lang>0)
+    {
+        QString sLang = "saisie_";
+        QString path = app.applicationDirPath() + QDir::separator() + "../include/qt/translations";
+
+        //cf Settings.h
+        if (lang == 1)
+            sLang += "fr";
+        else if (lang == 2)
+            sLang += "es";
+        else if (lang == 3)
+            sLang += "cn";
+        else if (lang == 4)
+            sLang += "ar";
+        else if (lang == 5)
+            sLang += "ru";
+
+        sLang += ".qm";
+
+        QTranslator *qtTranslator = new QTranslator(QApplication::instance());
+
+        if ( qtTranslator->load(sLang, path) )
+        {
+            QApplication::instance()->installTranslator(qtTranslator);
+        }
+        else
+        {
+            QMessageBox::critical(NULL, "Error", "Can't load translation file: " + sLang + "\n" +
+                "In: " + path);
+        }
+    }
+}
+
+void updateSettings(QSettings &settings, Pt2di aSzWin, Pt2di aNbFen, bool aForceGray)
+{
+    settings.beginGroup("MainWindow");
+    if (aSzWin.x > 0)
+        settings.setValue("size", QSize(aSzWin.x, aSzWin.y));
+    else if (!settings.contains("MainWindow/size"))
+    {
+        settings.setValue("size", QSize(800, 800));
+        aSzWin.x = aSzWin.y = 800;
+    }
+    settings.setValue("NbFen", QPoint(aNbFen.x, aNbFen.y));
+    settings.endGroup();
+
+    settings.beginGroup("Drawing settings");
+    settings.setValue("forceGray",     aForceGray  );
+    settings.endGroup();
+}
