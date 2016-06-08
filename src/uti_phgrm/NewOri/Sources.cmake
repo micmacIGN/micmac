1f89e21609ec59a61ac1a0aee123dc28944d1645
rupnik <rupnik@localhost>
2016-06-08 16:42:57 +0200

    Campari : NbLiais ajoutee

---
+++ src/uti_phgrm/NewOri/Sources.cmake
@@ -0,0 +1,27 @@
+set(uti_phgrm_NewOri_Src_Files
+    ${UTI_PHGRM_NEW_ORI}/TestNewOri.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_Appli.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_CpleIm.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_OneIm.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_NameManager.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_ProjPts.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_CombineCple.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_LinearCpleI.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_AmbigCpleI.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_GenTriplets.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_OldGenTriplets.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_PointsTriples.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_OptimTriplet.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_SolGlobInit.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_SolGlobInit_Build.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_SolGlob_PondApriori.cpp
+    ${UTI_PHGRM_NEW_ORI}/cNewO_DynFusPtsMul.cpp
+    #${UTI_PHGRM_NEW_ORI}/cNewO_Ellips.cpp
+)
+
+
+list( APPEND uti_phgrm_Src_Files
+	${uti_phgrm_NewOri_Src_Files}
+)
+
+
