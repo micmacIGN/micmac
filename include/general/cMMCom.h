dcc516ae358f2950133ebc12bb15a12a79cc548f
deseilligny <deseilligny@users.noreply.github.com>
2016-07-07 23:00:32 +0200

    Bug correc :   frozn in C3DC (heuristic correction) + ConvertBundle (pattern mode)

---
+++ include/general/cMMCom.h
@@ -0,0 +1,42 @@
+#ifndef __C_MM_COM__
+#define __C_MM_COM__
+
+typedef int (*tCommande) (int,char**);
+
+class cArgLogCom
+{
+    public :
+
+        cArgLogCom(int aNumArg,const std::string &aDirSup = "") :
+            mNumArgDir (aNumArg),
+            mDirSup    (aDirSup)
+        {
+        }
+
+        int mNumArgDir ;
+        std::string  mDirSup;
+
+        static const cArgLogCom NoLog;
+};
+
+// CMMCom is a descriptor of a MicMac Command
+class cMMCom
+{
+public :
+	cMMCom(const std::string &aName, tCommande aCommand, const std::string &aComment, const cArgLogCom &aLog = cArgLogCom::NoLog):
+		mName(aName),
+		mLowName(StrToLower(aName)),
+		mCommand(aCommand),
+		mComment(aComment),
+		mLog(aLog)
+	{
+	}
+
+	std::string  mName;
+	std::string  mLowName;
+	tCommande    mCommand;
+	std::string  mComment;
+	cArgLogCom  mLog;
+};
+
+#endif
---
+++ src/SateLib/Sources.cmake
@@ -0,0 +1,21 @@
+set(SateLib_Src_Files
+	${SATELIB_DIR}/CPP_RPC.cpp
+	${SATELIB_DIR}/CPP_AsterDestrip.cpp
+	${SATELIB_DIR}/Dimap2Grid.cpp
+	${SATELIB_DIR}/RefineModel.cpp
+	${SATELIB_DIR}/RefineASTER.cpp
+	${SATELIB_DIR}/CPP_ApplyParralaxCor.cpp
+	${SATELIB_DIR}/DigitalGlobe2Grid.cpp
+	${SATELIB_DIR}/Aster2Grid.cpp
+	${SATELIB_DIR}/CPP_SATtoBundle.cpp
+	${SATELIB_DIR}/CPP_SATDef2D.cpp
+	#~ ${SATELIB_DIR}/ASTERGT2MM.cpp
+)
+
+# JE NE SAIS PAS SI CA SERT ???
+SOURCE_GROUP(SateLib FILES ${SateLib_Src_Files})
+
+set(Elise_Src_Files
+	${Elise_Src_Files}
+	${SateLib_Src_Files}
+)
