fdc223bb03dcd67bf9635e1e0ee9e775c8f0e66f
gnguyen <nguyengiang41@users.noreply.github.com>
2016-08-10 13:31:25 +0200

    InitOri:add turn

--- src/TpMMPD/TiePByMesh/InitOriLinear.h
+++ src/TpMMPD/TiePByMesh/InitOriLinear.h
@@ -9,12 +9,22 @@ class SerieCamLinear
 {
     public:
 
-    SerieCamLinear( string aPatImgREF, string aPatImgNEW, string aOri, string aOriOut, int index);
+    SerieCamLinear( string aPatImgREF, string aPatImgNEW,
+                    string aOri, string aOriOut,
+                    string aAxeOrient, int index);
     void calPosRlt();
     void saveSystem(vector<SerieCamLinear*> aSystem){this->mSystem = aSystem;}
     Pt3dr calVecMouvement();
     void initSerie(Pt3dr vecMouvCam0 , vector<string> aVecPoseTurn, vector<double> aVecAngleTurn);
     void initSerieByRefSerie(SerieCamLinear* REFSerie);
+    void partageSection(vector<string> aVecPoseTurn,
+                                        vector<double> aVecAngleTurn);
+    void initSerieWithTurn( Pt3dr vecMouvCam0 ,
+                            vector<string> aVecPoseTurn,
+                            vector<double> aVecAngleTurn    );
+    void calCodageMatrRot(cTypeCodageMatr ref, double angle,
+                            cTypeCodageMatr &out, string axe);
+    Pt3dr calVecMouvementTurn(Pt3dr vecRef, double angle, string axe);
 
 
 	vector<string> mSetImgREF;
@@ -25,10 +35,12 @@ class SerieCamLinear
 	string mPatImgNEW;
     string mOri;
     string mOriOut;
+    string mAxeOrient;
     cInterfChantierNameManipulateur * mICNM;
     int mIndexCam;
     vector<Pt3dr> posRltWithOtherCam;
     vector<SerieCamLinear*> mSystem;
     Pt3dr mVecMouvement;
+    vector< vector<string> >mSections;
 };
 #endif
