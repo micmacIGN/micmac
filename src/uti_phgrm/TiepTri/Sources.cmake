02d1302a8d3ce8ccd82a3f07cd667b118e4e7dd4
deseilligny <deseilligny@localhost>
2016-09-26 18:04:43 +0200

    Debut TiePTri

---
+++ src/uti_phgrm/TiepTri/Sources.cmake
@@ -0,0 +1,13 @@
+set(uti_image_TiePTri
+    ${UTI_PHGRM_TiePTri_DIR}/cAppliTieTri.cpp
+    ${UTI_PHGRM_TiePTri_DIR}/TiepTri.cpp
+    ${UTI_PHGRM_TiePTri_DIR}/cImMasterTieTri.cpp
+    ${UTI_PHGRM_TiePTri_DIR}/cImSecTieTri.cpp
+)
+
+
+list( APPEND uti_phgrm_Src_Files
+        ${uti_image_TiePTri}
+)
+
+
