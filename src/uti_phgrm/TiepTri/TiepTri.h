0c3d60c8cf23397ae918bab130b07e4ede7da3eb
nguyengiang <nguyengiang41@users.noreply.github.com>
2016-10-27 16:06:23 +0200

    TiepTri:write homol to disk

--- src/uti_phgrm/TiepTri/TiepTri.h
+++ src/uti_phgrm/TiepTri/TiepTri.h
@@ -179,6 +179,7 @@ class cImTieTri
            virtual bool IsMaster() const = 0;
            const Pt2di  &   Decal() const;
            const int & Num() const;
+           string NameIm() {return mNameIm;}
       protected :
            int  IsExtrema(const TIm2D<tElTiepTri,tElTiepTri> &,Pt2di aP);
            void MakeInterestPoint
