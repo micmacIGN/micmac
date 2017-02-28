20d682aeeac0bde1c542790db0651289446422a5
Maxime SEGUIN <maxime.seguin@inrap.fr>
2017-02-28 15:46:38 +0100

    Add "data" directory to binary archive
    Fix the issue #20 "Data directory missing in precompiled release package for linux"

--- script_create_bin_dist.sh
+++ script_create_bin_dist.sh
@@ -3,6 +3,7 @@ echo 'creation de l archive binaire : '$nomArchive
 mkdir $nomArchive
 cp -R ../bin $nomArchive
 cp -R ../binaire-aux $nomArchive
+cp -R ../data $nomArchive
 mkdir $nomArchive/include
 cp -R ../include/XML_MicMac $nomArchive/include
 cp -R ../include/XML_GEN $nomArchive/include
