8c45f180ee02c51f4e8c24ddbe8cea0f9fe2d7ba
Romain Janvier <romain.janvier@archeos.eu>
2017-02-28 15:46:05 +0100

    [Build] Add missing data dir to linux dist
    fix #20

--- script_create_bin_dist.sh
+++ script_create_bin_dist.sh
@@ -4,6 +4,8 @@ mkdir $nomArchive
 cp -R ../bin $nomArchive
 cp -R ../binaire-aux $nomArchive
 mkdir $nomArchive/include
+mkdir $nomArchive/data
 cp -R ../include/XML_MicMac $nomArchive/include
 cp -R ../include/XML_GEN $nomArchive/include
+cp -R ../data $nomArchive/data
 tar -czf $nomArchive.tgz $nomArchive
