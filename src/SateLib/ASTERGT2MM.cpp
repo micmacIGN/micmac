aac9b97f6eb8f494bee5f2731222fee75a205f5b
Luc Girod <luc.girod@gmail.com>
2017-04-25 17:58:24 +0200

    Compile unix (Issue with GCC and CPP11)

--- src/SateLib/ASTERGT2MM.cpp
+++ src/SateLib/ASTERGT2MM.cpp
@@ -103,13 +103,13 @@ string ReadDate(string aNameFile)
 	struct tm * curtime = localtime(&_tm);
 	std::ostringstream oss;
 	
-	#if __cplusplus < 201103L
+	//#if __cplusplus < 201103L
 		char buffer[256];
 		strftime(buffer, 256, "%Y%m%d", curtime);
 		oss << buffer;
-	#else
+	/*#else
 		oss << std::put_time(curtime, "%Y%m%d");
-	#endif
+	#endif*/
 	aDate=oss.str();
 
 return aDate;
