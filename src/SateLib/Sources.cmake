7ad96bbbc7748a041df70a8a4741edd0d68eaff6
Luc Girod <luc.girod@gmail.com>
2017-04-25 18:28:17 +0200

    Moved ALGLIB

--- src/SateLib/Sources.cmake
+++ src/SateLib/Sources.cmake
@@ -11,33 +11,33 @@ set(SateLib_Src_Files
 	${SATELIB_DIR}/CPP_SATDef2D.cpp
 	${SATELIB_DIR}/ASTERGT2MM.cpp
 	
-	${SATELIB_DIR}/ALGLIB/alglibinternal.cpp
-	${SATELIB_DIR}/ALGLIB/alglibinternal.h
-	${SATELIB_DIR}/ALGLIB/alglibmisc.cpp
-	${SATELIB_DIR}/ALGLIB/alglibmisc.h
-	${SATELIB_DIR}/ALGLIB/ap.cpp
-	${SATELIB_DIR}/ALGLIB/ap.h
-	${SATELIB_DIR}/ALGLIB/dataanalysis.cpp
-	${SATELIB_DIR}/ALGLIB/dataanalysis.h
-	${SATELIB_DIR}/ALGLIB/diffequations.cpp
-	${SATELIB_DIR}/ALGLIB/diffequations.h
-	${SATELIB_DIR}/ALGLIB/fasttransforms.cpp
-	${SATELIB_DIR}/ALGLIB/fasttransforms.h
-	${SATELIB_DIR}/ALGLIB/integration.cpp
-	${SATELIB_DIR}/ALGLIB/integration.h
-	${SATELIB_DIR}/ALGLIB/interpolation.cpp
-	${SATELIB_DIR}/ALGLIB/interpolation.h
-	${SATELIB_DIR}/ALGLIB/linalg.cpp
-	${SATELIB_DIR}/ALGLIB/linalg.h
-	${SATELIB_DIR}/ALGLIB/optimization.cpp
-	${SATELIB_DIR}/ALGLIB/optimization.h
-	${SATELIB_DIR}/ALGLIB/solvers.cpp
-	${SATELIB_DIR}/ALGLIB/solvers.h
-	${SATELIB_DIR}/ALGLIB/specialfunctions.cpp
-	${SATELIB_DIR}/ALGLIB/specialfunctions.h
-	${SATELIB_DIR}/ALGLIB/statistics.cpp
-	${SATELIB_DIR}/ALGLIB/statistics.h
-	${SATELIB_DIR}/ALGLIB/stdafx.h
+	#${SATELIB_DIR}/ALGLIB/alglibinternal.cpp
+	#${SATELIB_DIR}/ALGLIB/alglibinternal.h
+	#${SATELIB_DIR}/ALGLIB/alglibmisc.cpp
+	#${SATELIB_DIR}/ALGLIB/alglibmisc.h
+	#${SATELIB_DIR}/ALGLIB/ap.cpp
+	#${SATELIB_DIR}/ALGLIB/ap.h
+	#${SATELIB_DIR}/ALGLIB/dataanalysis.cpp
+	#${SATELIB_DIR}/ALGLIB/dataanalysis.h
+	#${SATELIB_DIR}/ALGLIB/diffequations.cpp
+	#${SATELIB_DIR}/ALGLIB/diffequations.h
+	#${SATELIB_DIR}/ALGLIB/fasttransforms.cpp
+	#${SATELIB_DIR}/ALGLIB/fasttransforms.h
+	#${SATELIB_DIR}/ALGLIB/integration.cpp
+	#${SATELIB_DIR}/ALGLIB/integration.h
+	#${SATELIB_DIR}/ALGLIB/interpolation.cpp
+	#${SATELIB_DIR}/ALGLIB/interpolation.h
+	#${SATELIB_DIR}/ALGLIB/linalg.cpp
+	#${SATELIB_DIR}/ALGLIB/linalg.h
+	#${SATELIB_DIR}/ALGLIB/optimization.cpp
+	#${SATELIB_DIR}/ALGLIB/optimization.h
+	#${SATELIB_DIR}/ALGLIB/solvers.cpp
+	#${SATELIB_DIR}/ALGLIB/solvers.h
+	#${SATELIB_DIR}/ALGLIB/specialfunctions.cpp
+	#${SATELIB_DIR}/ALGLIB/specialfunctions.h
+	#${SATELIB_DIR}/ALGLIB/statistics.cpp
+	#${SATELIB_DIR}/ALGLIB/statistics.h
+	#${SATELIB_DIR}/ALGLIB/stdafx.h
 
 
 )
