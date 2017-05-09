444a59a9342225f54ad4c63d91e819dabaa13b25
antoine Lavenant <antoine.lavenant@ign.fr>
2017-05-09 17:58:56 +0200

    GCPFromBDOrtho...

--- src/TpMMPD/GCPfromBDCarrefour.cpp
+++ src/TpMMPD/GCPfromBDCarrefour.cpp
@@ -11,32 +11,7 @@
     #pragma GCC diagnostic ignored "-Wunused-result"
 #endif
 
-/*{
- Pt2di ptImage_NO (int(ptImage.x),int(ptImage.y));
- if (getCorelCoef(ptImage_NO,orthoSz,aNameImage,fenOrtho,NoData)>coefOk)
- {
- mPix.insert(std::pair<Pt3dr,Pt2dr>(itGCP->first,ptImage));
- break;
- }
- Pt2di ptImage_NE (int(ptImage.x),int(ptImage.y+1));
- if (getCorelCoef(ptImage_NE,orthoSz,aNameImage,fenOrtho,NoData)>coefOk)
- {
- mPix.insert(std::pair<Pt3dr,Pt2dr>(itGCP->first,ptImage));
- break;
- }
- Pt2di ptImage_SO (int(ptImage.x+1),int(ptImage.y));
- if (getCorelCoef(ptImage_SO,orthoSz,aNameImage,fenOrtho,NoData)>coefOk)
- {
- mPix.insert(std::pair<Pt3dr,Pt2dr>(itGCP->first,ptImage));
- break;
- }
- Pt2di ptImage_SE (int(ptImage.x+1),int(ptImage.y+1));
- if (getCorelCoef(ptImage_SE,orthoSz,aNameImage,fenOrtho,NoData)>coefOk)
- {
- mPix.insert(std::pair<Pt3dr,Pt2dr>(itGCP->first,ptImage));
- break;
- }
- }*/
+// to do : gerer cas central...
 
 ///
 ///
@@ -80,7 +55,6 @@ std::string getGPPAccess(const std::string aHttpProxy)
     return oss.str();
 }
 
-
 ///
 ///
 ///
@@ -104,7 +78,6 @@ void getVignetteGPP(const std::string nomDalleOrtho, const std::string aKeyGPP, 
     std::ostringstream ossGPP;
     ossGPP << std::fixed << gppAccess << " -o "<<nomDalleOrtho<< " \"http://wxs-i.ign.fr/"<<aKeyGPP<<"/geoportail/r/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=ORTHOIMAGERY.ORTHOPHOTOS&STYLES=normal&FORMAT=image/geotiff&BBOX="<< xminDalle<<","<<yminDalle<<","<<xmaxDalle<<","<<ymaxDalle<<"&CRS=EPSG:2154&WIDTH="<<ncDalle<<"&HEIGHT="<<nlDalle<<"\"";
     
-    std::cout << ossGPP.str() << std::endl;
     system(ossGPP.str().c_str());
 }
 
@@ -130,10 +103,7 @@ double getCorelCoef(const Pt2di& ptImage, const Pt2di& orthoSz, const std::strin
                     const std::vector<double> fenRef, const double NoData)
 {
     std::vector<double> fenImage_i;
-    int cmin = ptImage.x - orthoSz.x/2;
-    int lmin = ptImage.y - orthoSz.y/2 ;
-    Pt2di min(cmin,lmin);
-    std_unique_ptr<TIm2D<U_INT2,INT4> > cropImg (createTIm2DFromFile<U_INT2,INT4>(nomImage,min,orthoSz));
+    std_unique_ptr<TIm2D<U_INT2,INT4> > cropImg (createTIm2DFromFile<U_INT2,INT4>(nomImage,ptImage,orthoSz));
     getVecFromTIm2D(cropImg,fenImage_i,NoData);
     
     //correlation :
@@ -143,12 +113,46 @@ double getCorelCoef(const Pt2di& ptImage, const Pt2di& orthoSz, const std::strin
 ///
 ///
 ///
-void calcPseudoOrtho(const std::string& aNameImage, const Pt3dr& ptCentre, const Pt2di& size,
-                     const float resolOrtho, std_unique_ptr<ElCamera>& aCam)
+double minVec(std::vector<double>& c)
 {
-    std_unique_ptr<TIm2D<U_INT2,INT4> > cropImg (new TIm2D<U_INT2,INT4>(size));
+    double ret = c[0];
+    for (size_t i(1); i<c.size(); i++)
+        if (c[i]<ret)
+            ret = c[i];
+    return ret;
+}
+double maxVec(std::vector<double>& c)
+{
+    double ret = c[0];
+    for (size_t i(1); i<c.size(); i++)
+        if (c[i]>ret)
+            ret = c[i];
+    return ret;
+}
+///
+///
+///
+std_unique_ptr<TIm2D<U_INT2,INT4> > calcPseudoOrtho(const std::string& aNameImage, const Pt3dr& ptCentre, const Pt2di& size,
+                                                    const float resolOrtho, std_unique_ptr<ElCamera>& aCam, int buffer)
+{
+    std_unique_ptr<TIm2D<U_INT2,INT4> > vignette (new TIm2D<U_INT2,INT4>(size));
     
-    Pt3dr PtNO (ptCentre.x-size.x*resolOrtho,ptCentre.y+size.y*resolOrtho,ptCentre.z);
+    Pt3dr PtNO (ptCentre.x-size.x*resolOrtho,ptCentre.y+size.y*resolOrtho,ptCentre.z); Pt2dr ptINO = aCam->Ter2Capteur(PtNO);
+    Pt3dr PtNE (ptCentre.x+size.x*resolOrtho,ptCentre.y+size.y*resolOrtho,ptCentre.z); Pt2dr ptINE = aCam->Ter2Capteur(PtNE);
+    Pt3dr PtSO (ptCentre.x-size.x*resolOrtho,ptCentre.y-size.y*resolOrtho,ptCentre.z); Pt2dr ptISO = aCam->Ter2Capteur(PtSO);
+    Pt3dr PtSE (ptCentre.x+size.x*resolOrtho,ptCentre.y-size.y*resolOrtho,ptCentre.z); Pt2dr ptISE = aCam->Ter2Capteur(PtSE);
+    
+    std::vector<double> vc; vc.push_back(ptINO.x); vc.push_back(ptINE.x); vc.push_back(ptISO.x); vc.push_back(ptISE.x);
+    std::vector<double> vl; vl.push_back(ptINO.y); vl.push_back(ptINE.y); vl.push_back(ptISO.y); vl.push_back(ptISE.y);
+
+    double cmin = minVec(vc);
+    double cmax = maxVec(vc);
+    double lmin = minVec(vl);
+    double lmax = maxVec(vl);
+    
+    Pt2di minIm (cmin-buffer,lmin-buffer);
+    Pt2di sizeIm (cmax-cmin+2,lmax-lmin+2);
+    std_unique_ptr<TIm2D<U_INT2,INT4> > cropImg_cl (createTIm2DFromFile<U_INT2,INT4>(aNameImage,minIm,sizeIm));
     
     for (size_t l(0); l<size.y ; l++)
     {
@@ -156,12 +160,14 @@ void calcPseudoOrtho(const std::string& aNameImage, const Pt3dr& ptCentre, const
         {
             Pt3dr pt (PtNO.x + c*resolOrtho, PtNO.y - l*resolOrtho, PtNO.z);
             Pt2dr ptImage = aCam->Ter2Capteur(pt);
-            
-            //double radio = aVImages[best_image]->getr(best_Pt,0);
-            //cropImg->oset(Pt2di(c,l),(int)radio);
+
+            Pt2dr ptImageCrop (ptImage.x-cmin+buffer,ptImage.y-lmin+buffer);
+            double radio = cropImg_cl->getr(ptImageCrop);
+            vignette->oset(Pt2di(c,l),(int)radio);
         }
     }
     
+    return vignette;
 }
 
 ///
@@ -206,7 +212,7 @@ Pt2di ImageSize(std::string const &aName)
 ///
 bool getPoints(const std::string aBDCFileName, std::map<size_t,Pt3dr>& mGCP)
 {
-    std::ifstream fic (aBDCFileName);
+    std::ifstream fic (aBDCFileName.c_str());
     if (!fic.good())
     {
         cerr << "Error in reading "<<aBDCFileName<< " !" << std::endl;
@@ -251,7 +257,7 @@ int GCP_From_BDCarrefour_main(int argc, char **argv)
                     << EAMC(aNameImageOri,"Name of the image orientation")
                     << EAMC(aOutDir,"Name of output directory")
                     << EAMC(aKeyGPP,"GPP Key"),
-        LArgMain()  << EAM(aHttpProxy,"Proxy",true,"http proxy for GPP access")
+         LArgMain() << EAM(aHttpProxy,"Proxy",true,"http proxy for GPP access")
     );
     
     //lecture des points de contrôle
@@ -273,6 +279,7 @@ int GCP_From_BDCarrefour_main(int argc, char **argv)
     double NoData = -1.;
     int marge = 10;
     double coefOk=0.9;
+    int buffer (1);
     
     //  lecture de la BDOrtho et recuperation d'un crop
     //  on trouve le point par correlation
@@ -293,12 +300,12 @@ int GCP_From_BDCarrefour_main(int argc, char **argv)
         //info de l'image :
         int ncDalle = 2*sizeCropBDOrtho*resolBDOrtho/resolImageSat;
         int nlDalle = 2*sizeCropBDOrtho*resolBDOrtho/resolImageSat;
-        Pt2di orthoSz (ncDalle,nlDalle);
-        if (verbose2) std::cout << "[GCP_From_BDCarrefour_main] point " << orthoSz.x << " " << orthoSz.y << std::endl;
+        Pt2di Sz (ncDalle,nlDalle);
+        if (verbose2) std::cout << "[GCP_From_BDCarrefour_main] point " << Sz.x << " " << Sz.y << std::endl;
         
         //BDOrtho => tab
         std::vector<double> fenOrtho;
-        std_unique_ptr<TIm2D<U_INT2,INT4> > cropBDOrtho (createTIm2DFromFile<U_INT2,INT4>(nomDalleOrtho,Pt2di(0,0),orthoSz));
+        std_unique_ptr<TIm2D<U_INT2,INT4> > cropBDOrtho (createTIm2DFromFile<U_INT2,INT4>(nomDalleOrtho,Pt2di(0,0),Sz));
         getVecFromTIm2D(cropBDOrtho,fenOrtho,NoData);
         
         //point dans l'image :
@@ -306,28 +313,46 @@ int GCP_From_BDCarrefour_main(int argc, char **argv)
         if (verbose2) std::cout << "[GCP_From_BDCarrefour_main] point in input image " << ptImage.x << " " << ptImage.y << std::endl;
         double CoefCorelMax(-1);
 
+        //calcul d'une pseudoOrtho :
+        Pt2di orthoSz (Sz.x+2*marge,Sz.y+2*marge);
+        std_unique_ptr<TIm2D<U_INT2,INT4> > pseudoOrtho = calcPseudoOrtho(aNameImage,itGCP->second,orthoSz,resolBDOrtho,aCam,buffer);
+        
+        //export de l'image d'ortho
+        std::ostringstream ossNomPseudoOrtho;
+        ossNomPseudoOrtho << "PseudoOrtho_"<<itGCP->first<<".tif";
+        std::string nomPseudoOrtho = ossNomPseudoOrtho.str();
+        Tiff_Im out(nomPseudoOrtho.c_str(), pseudoOrtho->sz(),GenIm::u_int2,Tiff_Im::No_Compr,Tiff_Im::BlackIsZero);
+        ELISE_COPY(pseudoOrtho->_the_im.all_pts(),pseudoOrtho->_the_im.in(),out.out());
+        
         //on regarde si le pt image est bon :
         // si la corel marche bien sur un pixel autour du point de loc, dans le doute on garde la loc...
         // todo : faire mieux...
-     
         
         //correlations :
-        for (int i(0); i<marge;i++)
+        for (size_t l(0); l<orthoSz.y-Sz.y;l++)
         {
-            Pt2di ptImage_i (int(ptImage.x)-marge+i,int(ptImage.y)-marge+i);
-            double cofCorel = getCorelCoef(ptImage_i,orthoSz,aNameImage,fenOrtho,NoData);
-            
-            if(cofCorel>CoefCorelMax)
+            for (size_t c(0); c<orthoSz.x-Sz.x;c++)
             {
-                CoefCorelMax = cofCorel;
-                if (verbose2) std::cout << "[GCP_From_BDCarrefour_main] " << itGCP->first << " : " << cofCorel << "  pt (" << ptImage_i.x <<"," <<ptImage_i.y<<")" << std::endl;
-                
-                itPix = mPix.find(itGCP->first);
-                Pt2dr ptr (ptImage_i.x,ptImage_i.y);
-                if (itPix==mPix.end())
-                    mPix.insert(std::pair<size_t,Pt2dr>(itGCP->first,ptr));
-                else
-                    itPix->second = ptr;
+                double cofCorel =  getCorelCoef(Pt2di(c,l),Sz,nomPseudoOrtho,fenOrtho,NoData);
+
+                if(cofCorel>CoefCorelMax)
+                {
+                    Pt2di ptCentralPO (c+Sz.x/2,l+Sz.y/2);
+                    Pt3dr ptCentralPOTer ( itGCP->second.x - resolBDOrtho*(Sz.x/2 - ptCentralPO.x),
+                                           itGCP->second.y - resolBDOrtho*(Sz.y/2 - ptCentralPO.y),
+                                           itGCP->second.z);
+                    Pt2dr ptImage_i = aCam->Ter2Capteur(ptCentralPOTer);
+                    
+                    CoefCorelMax = cofCorel;
+                    if (verbose2) std::cout << "[GCP_From_BDCarrefour_main] " << itGCP->first << " : " << cofCorel << "  pt (" << ptImage_i.x <<"," <<ptImage_i.y<<")" << std::endl;
+                    
+                    itPix = mPix.find(itGCP->first);
+                    Pt2dr ptr (ptImage_i.x,ptImage_i.y);
+                    if (itPix==mPix.end())
+                        mPix.insert(std::pair<size_t,Pt2dr>(itGCP->first,ptr));
+                    else
+                        itPix->second = ptr;
+                }
             }
         }
         
@@ -342,10 +367,10 @@ int GCP_From_BDCarrefour_main(int argc, char **argv)
             
             if (verbose2) std::cout << "[GCP_From_BDCarrefour_main] (" << ptImage.x << ";" << ptImage.y << ") => (" << itPix->second.x << ";" << itPix->second.y << ")" << std::endl;
             
-            int cmin = itPix->second.x - orthoSz.x/2;
-            int lmin = itPix->second.y - orthoSz.y/2 ;
+            int cmin = itPix->second.x - Sz.x/2;
+            int lmin = itPix->second.y - Sz.y/2 ;
             
-            std_unique_ptr<TIm2D<U_INT2,INT4> > cropImg (createTIm2DFromFile<U_INT2,INT4>(aNameImage,Pt2di(cmin,lmin),orthoSz));
+            std_unique_ptr<TIm2D<U_INT2,INT4> > cropImg (createTIm2DFromFile<U_INT2,INT4>(aNameImage,Pt2di(cmin,lmin),Sz));
             Tiff_Im out(nomVignetteResult.c_str(), cropImg->sz(),GenIm::u_int2,Tiff_Im::No_Compr,Tiff_Im::BlackIsZero);
             ELISE_COPY(cropImg->_the_im.all_pts(),cropImg->_the_im.in(),out.out());
         }
