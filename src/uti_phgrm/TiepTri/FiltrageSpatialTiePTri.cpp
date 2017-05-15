4b4be2e53bc20d437a5de84a27d772c70e8e3dc7
nguyengiang41 <nguyengiang41@gmail.com>
2017-05-15 19:17:18 +0200

    Commentaire Filtrage Spatial pour Giang

--- src/uti_phgrm/TiepTri/FiltrageSpatialTiePTri.cpp
+++ src/uti_phgrm/TiepTri/FiltrageSpatialTiePTri.cpp
@@ -60,12 +60,14 @@ class cTpP_HeapCompare
 
         bool operator () (cResulMultiImRechCorrel *  & aR1,cResulMultiImRechCorrel *  & aR2)
         {
-              return aR1->Score() > aR2->Score();
+              return aR1->Score() > aR2->Score();   // compare score correl global
         }
+        // est ce que objet 1 est meuilleur que 2
 };
 
 class cFuncPtOfRMICPtr
-{
+{   // argument du qauad tri
+    // comment à partir un objet, je recuper sa pt2D
       public :
          Pt2dr operator () (cResulMultiImRechCorrel * aRMIRC) {return Pt2dr(aRMIRC->PtMast());}
 };
@@ -110,23 +112,45 @@ if (0) // (MPD__MM())
 
    std::vector<cResulMultiImRechCorrel *>  aResult;
 
-   static tQtTiepT * aQdt = 0;
+   static tQtTiepT * aQdt = 0;  // le quad-tri
    static cFuncPtOfRMICPtr  aFctr;
    if (aQdt==0)
    {
-       Pt2dr aSz= Pt2dr(mMasIm->Tif().sz());
+       Pt2dr aSz= Pt2dr(mMasIm->Tif().sz());    // taille d'espace à recuperer les objets = taille d'image
        Pt2dr aRab(10,10);
        aQdt = new tQtTiepT(aFctr,Box2dr(-aRab,aSz+aRab),10,20.0);
+       /*
+        * Creer un nouveau Quad-tri.
+
+        * Defini un quad tri par typedef ElQT<cResulMultiImRechCorrel *,Pt2dr,cFuncPtOfRMICPtr> tQtTiepT;
+        * Ca veut dire il accept les objets type cResulMultiImRechCorrel *,
+        * recherche par Pt2dr, avec le method de recuperer Pt2dr à partir de cResulMultiImRechCorrel * est definie dans cFuncPtOfRMICPtr
+        * Creer un nouveau tQtTiepT :
+        *   .) foncteur = cFuncPtOfRMICPtr  aFctr => method de recuperer Pt2dr à partir de pointer cResulMultiImRechCorrel *
+        *   .) Box2dr(-aRab,aSz+aRab) => une region = taille d'image + rab 10 pxl (pour rassurer?), qui contient tout mes objet
+        *   .) NbObjMax = 10 => une seuile de bas pour commencer à decouper l'espace objet (en 4)
+        *   .) SzMin = 20.0 => ?
+
+       */
+
    }
 
 
    cTpP_HeapCompare aCmp;
    ElHeap<cResulMultiImRechCorrel *,cTpP_HeapCompare,cTpP_HeapParam> aHeap(aCmp);
+   /* == Definir un structure donne type Heap ==
+    * ElHeap<cResulMultiImRechCorrel *,cTpP_HeapCompare,cTpP_HeapParam> aHeap(aCmp);
+    *  .) Type objet cResulMultiImRechCorrel *
+    *  .) cTpP_HeapCompare => comment heap evaluer objet
+    *  .) cTpP_HeapParam => access au heap index dans l'objet
+   */
    for (int aK=0; aK <int(aVIn.size()) ; aK++)
    {
+       // calcul score global de correlation sur tout les coup image
+       // 1 pt Mas correl sur plsr pt 2nd => calcul 1 score glob, mis a jour le score glob dans cResulMultiImRechCorrel aussi
        aVIn[aK]->CalculScoreAgreg(EpsilAggr,PowAggreg,aSign);  // Epsilon, power
-       aHeap.push(aVIn[aK]);
-       aQdt->insert(aVIn[aK]);
+       aHeap.push(aVIn[aK]);    // mets l'objet dans heap
+       aQdt->insert(aVIn[aK]);  // mets l'objet dans le Quad-Tri
    }
 
    cResulMultiImRechCorrel * aRM_1;
@@ -135,15 +159,21 @@ if (0) // (MPD__MM())
    Video_Win *  aW = mMasIm->W();
    while (aHeap.pop(aRM_1))
    {
+       // pop à partir un heap => recuper la "meuilleur" point et l'enleve dans heap
+       // aRM_1 contient le point avec Score() meuilleur
        if (aW)
        {
            aW->draw_circle_loc(aFctr(aRM_1),aSeuilDist,aW->pdisc()(P8COL::cyan));
+           // dessine un cercle sur Img Master, au pt master, rayon TT_DefSeuilDensiteResul = 50
        }
        aResult.push_back(aRM_1);
        const std::vector<int> &  aVI_1 = aRM_1->VIndex();
        int aNbI_1 = aVI_1.size();
        const std::vector<cResulRechCorrel > & aVC_1 = aRM_1->VRRC() ;
-
+       /*
+        * aVI_1 = vector<int> contient index de tout les pt correl dans aRM_1
+        * aVC_1 = vector<cResulRechCorrel > contient tout les pt correl (mPt, mScore)
+        */
        // Mets a jour le score fonction du numero
        for (int aK=0 ; aK<aNbI_1 ; aK++)
        {
@@ -151,21 +181,28 @@ if (0) // (MPD__MM())
        }
 
        std::set<cResulMultiImRechCorrel *> aSet;
+       // recuper tout les pts dans aSeuilDist (TT_DefSeuilDensiteResul = 50pxl) distance (region à filtrer)
        aQdt->RVoisins(aSet,aFctr(aRM_1),aSeuilDist);
        for (std::set<cResulMultiImRechCorrel *>::iterator itS=aSet.begin(); itS!=aSet.end() ; itS++)
-       {
+       {   //== parcourir tout les point dans region à filtrer ==
            cResulMultiImRechCorrel * aRM_2 = *itS;
            if (aRM_1==aRM_2)
            {
               aQdt->remove(aRM_2);
+              // enleve le point dans le Quad-Tri (on a pop out de heap, mais il exist encore dans le Quad-Tri)
            }
            else
            {
               const std::vector<int> &  aVI_2 = aRM_2->VIndex();
               int aNbI_2 = aVI_2.size();
               const std::vector<cResulRechCorrel > & aVC_2 = aRM_2->VRRC() ;
+              /*
+               * aVI_2 = vector<int> contient index de tout les pt correl dans aRM_2
+               * aVC_2 = vector<cResulRechCorrel > contient tout les pt correl (mPt, mScore)
+               */
 
-              double aDist = euclid(aFctr(aRM_1)-aFctr(aRM_2));
+              // === formule pour decider si on enleve un point ===
+              double aDist = euclid(aFctr(aRM_1)-aFctr(aRM_2)); // distance euclid entre 2 point master
               double aRabCorrel = (1-(aDist/aSeuilDist)) * aGainCorrel;
 
               int aNbS0 = aRM_2->NbSel();
@@ -173,8 +210,15 @@ if (0) // (MPD__MM())
               {
                   int aKIm = aVI_2[aK];
                   if (aVC_2[aK].mCorrel < (aVCorrel[aKIm]+aRabCorrel))
+                  /*
+                   * Consider 2 point master aRM_1 & aRM_2
+                   * On veut filtrer spatial autour de point aRM_1 avec 1 rayon donné
+                   * On recuper tout les pts aRM_2, puis on regarde score correl du aRM_2 < aRM_1 + rab correl
+                   * Si oui, on de-selection aRM_2
+                   * Ca veut dire meme si aRM_2 est une score plus grand que aRM_1, mais la grandeur est pas assez grand, on jete aussi
+                   */
                   {
-                      aRM_2->SetSelec(aK,false);
+                      aRM_2->SetSelec(aK,false); // déseletioner un pt
                   }
               }
               int aNbSelEnd = aRM_2->NbSel();
@@ -190,7 +234,7 @@ if (0) // (MPD__MM())
                   }
                   else
                   {
-                     aHeap.MAJ(aRM_2);
+                     aHeap.MAJ(aRM_2);  // mis à jour pour ne pas cassé la structure de heap
                   }
                   // if (aRM_2->NbSel()
               }
