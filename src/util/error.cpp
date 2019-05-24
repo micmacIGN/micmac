67baa7c69ddf4078163bd5bce00ee444fd0664f3
Jon-Pierre Stoermer <stoermerjp@gmail.com>
2019-05-24 11:05:19 -0600

    set ExitOnBrkp true

--- src/util/error.cpp
+++ src/util/error.cpp
@@ -63,7 +63,7 @@ void BasicErrorHandler()
 int TheIntFuckingReturnValue=1234567;
 char * TheCharPtrFuckingReturnValue=0;
 
-bool TheExitOnBrkp  = false;
+bool TheExitOnBrkp  = true;
 // bool TheExitOnBrkp  = false;
 bool TheExitOnNan   = false;
 bool TheExitOnWarn  = false;
@@ -87,7 +87,7 @@ void throwError(std::string err)
 
 int GetCharOnBrkp()
 {
-std::cout << "TTTTTTTtttttttttttttttt\n"; getchar();
+//std::cout << "TTTTTTTtttttttttttttttt\n"; getchar();
 
 
    BasicErrorHandler();
@@ -586,7 +586,7 @@ void  cMajickChek::Add(cSetEqFormelles & aSetEq)
 
 /*Footer-MicMac-eLiSe-25/06/2007
 
-Ce logiciel est un programme informatique servant �  la mise en
+Ce logiciel est un programme informatique servant �  la mise en
 correspondances d'images pour la reconstruction du relief.
 
 Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
@@ -602,17 +602,17 @@ seule une responsabilité restreinte pèse sur l'auteur du programme,  le
 titulaire des droits patrimoniaux et les concédants successifs.
 
 A cet égard  l'attention de l'utilisateur est attirée sur les risques
-associés au chargement,  �  l'utilisation,  �  la modification et/ou au
-développement et �  la reproduction du logiciel par l'utilisateur étant
-donné sa spécificité de logiciel libre, qui peut le rendre complexe �
-manipuler et qui le réserve donc �  des développeurs et des professionnels
+associés au chargement,  �  l'utilisation,  �  la modification et/ou au
+développement et �  la reproduction du logiciel par l'utilisateur étant
+donné sa spécificité de logiciel libre, qui peut le rendre complexe �
+manipuler et qui le réserve donc �  des développeurs et des professionnels
 avertis possédant  des  connaissances  informatiques approfondies.  Les
-utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
-logiciel �  leurs besoins dans des conditions permettant d'assurer la
+utilisateurs sont donc invités �  charger  et  tester  l'adéquation  du
+logiciel �  leurs besoins dans des conditions permettant d'assurer la
 sécurité de leurs systèmes et ou de leurs données et, plus généralement,
-�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+�  l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
 
-Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
+Le fait que vous puissiez accéder �  cet en-tête signifie que vous avez
 pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
 termes.
 Footer-MicMac-eLiSe-25/06/2007*/
