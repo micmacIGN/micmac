a51e3abd597160cc4b3c6469eded626cd729dcf5
ewelina rupnik <erupnik@gmail.com>
2019-06-07 11:01:16 +0200

    Modif TestLib NO_R2A and HomProfPx

--- src/uti_phgrm/CPP_HomolFromProfEtPx.cpp
+++ src/uti_phgrm/CPP_HomolFromProfEtPx.cpp
@@ -51,12 +51,13 @@ class cAppliHomProfPx
 
     private:
         std::string mDir;
+        std::string mNameIm1;
         std::string mNameIm2;
         std::string mNameCoordIm1;
         std::string mOri;
         std::string mNuageName;
         std::string mPxName;
-        std::string mOut;
+        std::string mSH;
         cInterfChantierNameManipulateur* mICNM;
 
         cElNuage3DMaille * mNuage;
@@ -83,7 +84,7 @@ class cAppliHomProfPx
 
 cAppliHomProfPx::cAppliHomProfPx(int argc,char ** argv) :
     mDir("./"),
-    mOut("HomProfPx.txt"),
+    mSH(""),
     mNuage     (0),
     mImCorrel  (1,1),
     mImProf    (1,1),
@@ -101,12 +102,13 @@ cAppliHomProfPx::cAppliHomProfPx(int argc,char ** argv) :
     ElInitArgMain
     (
         argc,argv,
-        LArgMain()  << EAMC(mNameIm2,"Name of the \"second\" image of the pair", eSAM_IsPatFile)
+        LArgMain()  << EAMC(mNameIm1,"Name of the \"first\" image of the pair", eSAM_IsPatFile)
+                    << EAMC(mNameIm2,"Name of the \"second\" image of the pair", eSAM_IsPatFile)
                     << EAMC(mOri,"Input orientation directory", eSAM_IsExistFile)
                     << EAMC(mNuageName,"Path to the NuageImProf file", eSAM_IsExistFile)
                     << EAMC(mPxName,"Path to the Px2 file", eSAM_IsExistFile)
                     << EAMC(mNameCoordIm1,"Input file with pixel positions in the \"first\" image pair (DicoAppuisFlottant format)", eSAM_NoInit),
-        LArgMain()  << EAM(mOut,"Out",true,"Output file with correspondences in the image pair", eSAM_NoInit)
+        LArgMain()  << EAM(mSH,"SH",true,"Output homol post-fix", eSAM_NoInit)
                     << EAM(mZoomRatio,"ZoomR", true, "Zoom Ratio",eSAM_IsPowerOf2)
                     << EAM(mZoomFinal,"ZoomF", true, "Zoom Final",eSAM_IsPowerOf2)
                     << EAM(mNumFinal,"NumF", true, "Num Final",eSAM_IsPowerOf2)
@@ -181,6 +183,7 @@ void cAppliHomProfPx::ExportHom()
 {
 
     ElPackHomologue aPHom;
+    ElPackHomologue aPHomSym;
 
     std::list<cOneAppuisDAF>::iterator itA=aD.OneAppuisDAF().begin();
     for( ; itA!=aD.OneAppuisDAF().end(); itA++ )
@@ -202,15 +205,35 @@ void cAppliHomProfPx::ExportHom()
          
             ElCplePtsHomologues aCple(aPtIm1Full,aPtIm2Cor);
             aPHom.Cple_Add(aCple);
+            aPHomSym.Cple_Add(ElCplePtsHomologues(aPtIm2Cor,aPtIm1Full));
         }
         else
             std::cout << "not visible : " << aPtIm1Full << "\n"; 
 
     }
    
-    aPHom.StdPutInFile(mOut); 
+    std::string aHomFile = mICNM->Assoc1To2("NKS-Assoc-CplIm2Hom@"+ mSH+"@txt",mNameIm1,mNameIm2,true);
+    std::string aHomSymFile = mICNM->Assoc1To2("NKS-Assoc-CplIm2Hom@"+ mSH+"@txt",mNameIm2,mNameIm1,true);
 
-    std::cout << "Saved to: " << mOut << "\n";
+    std::cout << "Saved to: \n";
+    if (! ELISE_fp::exist_file(aHomFile))
+    {
+        aPHom.StdPutInFile(aHomFile);
+        std::cout << aHomFile << " \n";
+    }
+    else
+        std::cout << aHomFile << " already exists; nothing has been saved." << "\n";
+
+    if (! ELISE_fp::exist_file(aHomSymFile))
+    {
+        aPHomSym.StdPutInFile(aHomSymFile);
+        std::cout << aHomSymFile << "\n";
+    }
+    else
+        std::cout << aHomSymFile << " already exists; nothing has been saved." << "\n";
+
+
+
 
 
 }
