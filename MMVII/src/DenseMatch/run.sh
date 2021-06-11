d49e0ecbb9e64ca37b96150fa370a0f290ebe5cc
ewelina rupnik <erupnik@gmail.com>
2021-06-11 09:25:14 +0200

    MMVII DenseMatch add disp norm + readme

--- MMVII/src/DenseMatch/run.sh
+++ MMVII/src/DenseMatch/run.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
-# change the path to your trained model
+######### SET-UP
+# path to your trained model
 TrainedModelFile="/home/er/Documents/d_development/stereo_matching/Vaih/models/finetune_PSMnet.tar"
 
-EnvName=psmnet_env
+# disparity scale of the trained model
+DispScale=256.0
+########
 
+EnvName=psmnet_env
 mm3dBin=$(which MMVII)
 DenseMDir=${mm3dBin::(-9)}"src/DenseMatch/"
-
 CodeDir=${DenseMDir}"PSMNet/"
 
 # enter virtual env
 source ${DenseMDir}python_env/${EnvName}/bin/activate
 
-# run programm
+# run image matching
 python ${CodeDir}Test_img.py --loadmodel ${TrainedModelFile} "$@"
+
+# de-normalise disparities
+mv ${6} ${6}_unnorm.tif
+mm3d Nikrup "/ ${6}_unnorm.tif ${DispScale}" ${6}
+
 
 # quit virtual env
 deactivate
-
