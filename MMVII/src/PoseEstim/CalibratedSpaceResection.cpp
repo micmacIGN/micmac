efcb576815400e14081ca8404a542ca569b9966b
deseilligny <marc.pierrot-deseilligny@ign.fr>
2022-11-29 22:40:25 +0100

    Resection : polynome done=> ToTest...

--- MMVII/src/PoseEstim/CalibratedSpaceResection.cpp
+++ MMVII/src/PoseEstim/CalibratedSpaceResection.cpp
@@ -1,4 +1,5 @@
 #include "MMVII_Ptxd.h"
+#include "cMMVII_Appli.h"
 
 
 /**
@@ -8,62 +9,8 @@
 
  */
 
-/*
-      3 direction   A,B,C   we assume ||A|| = ||B|| = ||C|| = 1
-      We parametrize 3 point on the bundle by 2 parameters b & c:
-           PA  = A  (arbitrarily we fix on this bundle)
-	   PB  = B(1+b)
-	   PC  = C(1+c)
-      This parametrization is made to be more numerically stable when 
-     the bundle are close to each others (b & c small)
-
-     We know  GA,GB,GC  the ground point
-
-     (1) eliminate b
-
-     |PA-PB|^2    |GA-GB|^2
-      -----   =  ---------  = rA
-     |PA-PC|^2    |GA-GC|^2
-
-
-
-    ((1+b)B-A)^2 = ra ((1+c)C-A)^2
-     (bB + AB) ^2 = ra (cC + AC) 
-     b^2 + 2AB.B b  + (AB^2 -ra (c^2+ 2AC.C +AC^2)) =0
-     b^2 + 2AB.B b + P(c) =0 
-
-     b =  - AB.B +E SQRT(AB.B -P(c)^2)   E in {-1,+1}
-     b =  B1 + E SQRT(B1-P(c)^2) = B1 +E S(Q(c))
-
-     (2) resolve C
-     |PC-PB|^2    |GC-GB|^2
-      -----   =  ---------  = rc
-     |PA-PC|^2    |GC-GA|^2
-
-
-     ((1+c)C - (1+b)B)^2 = ((1+c)C -A)^2 rc
-     (BC + cC -bB) ^2 = rc (AC + cC) ^2
-     rc (AC + cC) ^2 = (BC +cC - (B1 + E *  S(Q)) B)^2 =   ((BC-B1B) +c C - E S(Q) B) ^2
-
-     rc (AC + cC) ^2  - (BC-B1B)^2  - B2 Q = 2 E (BC-B1B).B S(Q)
-       
-
-
-*/
-
 namespace MMVII
 {
-
-	/*
-struct  cPair3D3D
-{
-     public :
-          cPair2D3D(const cPt2dr &,const cPt3dr &);
-          cPt2dr mP2;
-          cPt3dr mP3;
-};
-*/
-
 
 template <class Type>  class cElemSpaceResection
 {
@@ -86,14 +33,14 @@ template <class Type>  class cElemSpaceResection
 	   );
 	   
 	   // copy bundles, are unitary
-	   tP3  bA;
-	   tP3  bB;
-	   tP3  bC;
+	   tP3  A;
+	   tP3  B;
+	   tP3  C;
 
-	   tP3  bVAB;      ///<  Vector  A -> B
-	   tP3  bVAC;      ///<  Vector  A -> C
-	   Type bDistAB2;  ///<  ||A-B||^2 
-	   Type bABsB;     ///<  (A->B).B
+	   tP3  AB;      ///<  Vector  A -> B
+	   tP3  AC;      ///<  Vector  A -> C
+	   tP3  BC;      ///<  Vector  A -> C
+	   Type abb;     ///<  (A->B).B
 
 
 	   // copy of ground point coordinates, local precision
@@ -121,14 +68,14 @@ template <class Type>
        const tPt3dr & aPGroundB,
        const tPt3dr & aPGroundC
    ) :
-        bA     (VUnit(ToPt(aDirBundlA))),
-        bB     (VUnit(ToPt(aDirBundlB))),
-        bC     (VUnit(ToPt(aDirBundlC))),
+        A     (VUnit(ToPt(aDirBundlA))),
+        B     (VUnit(ToPt(aDirBundlB))),
+        C     (VUnit(ToPt(aDirBundlC))),
 
-        bVAB      (bB - bA),
-        bVAC      (bC - bA),
-	bDistAB2  (SqN2(bVAB)),
-	bABsB     (Scal(bVAB,bB)),
+        AB      (B - A),
+        AC      (C - A),
+        BC      (C - B),
+	abb   (Scal(AB,B)),
 
         gA (ToPt(aPGroundA)),
         gB (ToPt(aPGroundB)),
@@ -165,8 +112,8 @@ template <class Type>
 
      b^2 + 2AB.B b  + (AB^2 -rABC(cC + AC)^2  ) =0
    # P(c) =  (AB^2 -rABC (cC + AC)^2)
-     b^2 + 2AB.B b + P(c) =0   => 
-
+     b^2 + 2AB.B b + P(c) =0   =>    
+     2nd degre equation in b 
      b =  - AB.B +E SQRT(AB.B -P(c))   E in {-1,+1}
    # Q(c) = AB.B -P(c)
      b =  - AB.B + E S(Q(c))
@@ -174,12 +121,14 @@ template <class Type>
 
 
      // P(c) =  (AB^2 -rABC (cC + AC)^2)
-    cPolynom<Type> aPc =  tPol::D0(bDistAB2) -  PolSqN(bVAC,bC) *rABC ;
+    tPol  aPol_AC_C =  PolSqN(AC,C);
+    cPolynom<Type> aPc =  tPol::D0(SqN2(AB)) -  aPol_AC_C *rABC ;
     // Q(c) = AB.B -P(c)^2
-    cPolynom<Type> aQc =  tPol::D0(bABsB) - aPc;
+    cPolynom<Type> aQc =  tPol::D0(abb) - aPc;
 
   //  Now we can eliminate b using :   b =  - AB.B + E S(Q(c))   E in {-1,1} 
 /* ======================== (2) resolve c =====================
+    2nd conservation  of ratio
      |PC-PB|^2    |GC-GB|^2
       -----   =  ---------  = rCBA
      |PA-PC|^2    |GC-GA|^2
@@ -187,18 +136,32 @@ template <class Type>
 
      ((1+c)C - (1+b)B)^2 = ((1+c)C -A)^2 rCBA
      (BC + cC -bB) ^2 = rCBA (AC + cC) ^2
-     rCBA (AC + cC) ^2 = (BC +cC - (B1 + E *  S(Q)) B)^2 =   ((BC-B1B) +c C - E S(Q) B) ^2
+     rCBA (AC + cC) ^2 = (BC +cC - (-AB.B + E *  S(Q)) B)^2 =   ((BC + AB.B B +c C)  - E S(Q) B) ^2
 
-     rc (AC + cC) ^2  - (BC-B1B)^2  - B2 Q = 2 E (BC-B1B).B S(Q)
+     rCBA (AC + cC) ^2 = (BC + AB.B B +c C)^2 -2 (BC + AB.B B +c C) .B E S(Q) + Q B^2
+     B^2 = 1
+     rCBA (AC + cC) ^2 - (BC + AB.B B +c C)^2  - Q  =  -2 (BC.B + AB.B  +c C.B) E S(Q)
+
+                      
+                       R(c) = -2E L(c) S(Q)
+		       R^2(c) = 4 L(c)^2 Q(c)
+
 */
        
+    tPol  aRc =   aPol_AC_C *rCBA  -  aQc  -  PolSqN(BC +  abb*B  ,C);
+    tPol  aLc ({Scal(BC,C)+abb,Scal(B,C)});
 
+    tPol aSolver = Square(aRc) - aQc * Square(aLc) * 4;
+
+    StdOut()  << "DDDD " << aSolver.Degree()  << "\n";
+	    /*
     for (Type E : {-1.0,1.0})
     {
         FakeUseIt(E);
     }
 
     FakeUseIt(aQc);
+    */
 }
 
 
