deef9120b274593d2a7078a8b14a93e9c1bdec79
deseilligny <marc.pierrot-deseilligny@ign.fr>
2023-06-06 14:59:25 +0200

    iiii

--- MMVII/src/Geoms/TilesIndexGeom.cpp
+++ MMVII/src/Geoms/TilesIndexGeom.cpp
@@ -11,7 +11,7 @@ template <const int Dim> class cTilingIndex
        public :
            typedef cPtxd<int,Dim>        tIPt;
            typedef cPtxd<tREAL8,Dim>     tRPt;
-           typedef cTplBox<int,Dim>      tIBox;
+           typedef cPixBox<Dim>          tIBox;
            typedef cTplBox<tREAL8,Dim>   tRBox;
 
 	   cTilingIndex(const tRBox &,bool WithBoxOut, int aNbCase);
@@ -19,15 +19,17 @@ template <const int Dim> class cTilingIndex
         private :
 
 	   static tREAL8  ComputeStep(const tRBox &, int aNbCase);
+	   void AssertInside(const tRPt &) const;
 	  
-	   tIPt  Index(const tRPt &) const;
-
+	   // tIPt  Index(const tRPt &) const;
+	   tRBox    mRBoxIn;
+	   bool     mOkOut;
+	   int      mNbCase;
 
 	   tREAL8   mStep;
 	   cPt2di   mSzI;
 	   tIBox    mIBoxIn;
 	   /*
-	   tRBox    mRBoxIn;
 
 	   tRPt     mRP0;
 	   tRPt     mMulSz;
@@ -49,15 +51,19 @@ template <const int Dim> tREAL8  cTilingIndex<Dim>::ComputeStep(const tRBox & aB
 }
 
 template <const int Dim>  cTilingIndex<Dim>::cTilingIndex(const tRBox & aBox,bool OkOut, int aNbCase) :
+	mRBoxIn  (aBox),
+	mOkOut   (OkOut),
+        mNbCase  (aNbCase),
 	mStep    (ComputeStep(aBox,aNbCase)),
 	mSzI     (Pt_round_up(aBox.Sz()/mStep)),
         mIBoxIn  (  tIBox(tIPt::PCste(0),mSzI+tIPt::PCste(2)))
 {
-   StdOut()  <<  " -- SSSS=" << mStep << " " << mSzI << "\n";
+    StdOut()  <<  " -- SSSS=" << mStep << " " << mSzI << "\n";
 
     StdOut()  <<  mIBoxIn.P0()  << mIBoxIn.P1() << "\n";
     StdOut()  <<  mIBoxIn.Proj(cPt2di(1,1))   << mIBoxIn.Proj(cPt2di(-3,-3))  << mIBoxIn.Proj(cPt2di(10,10)) << "\n";
 }
+
 
 /*
 class cTilingIndex<1>;
@@ -71,6 +77,7 @@ void Bench_SpatialIndex(cParamExeBench & aParam)
 
 
      cTilingIndex<2>  aTI(cBox2dr(cPt2dr(0,0),cPt2dr(2,2)),true,5);
+     FakeUseIt(aTI);
 
      aParam.EndBench();
 }
