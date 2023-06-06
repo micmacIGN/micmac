a9565a304d6b8ba1e510909d645b88f5a34060a8
deseilligny <marc.pierrot-deseilligny@ign.fr>
2023-06-06 17:38:58 +0200

    IN tiling

--- MMVII/src/Geoms/TilesIndexGeom.cpp
+++ MMVII/src/Geoms/TilesIndexGeom.cpp
@@ -6,6 +6,12 @@
 namespace MMVII
 {
 
+/* **************************************** */
+/*                                          */
+/*              cTilingIndex                */
+/*                                          */
+/* **************************************** */
+
 template <const int Dim> class cTilingIndex
 {
        public :
@@ -29,13 +35,8 @@ template <const int Dim> class cTilingIndex
 	   int      mNbCase;
 
 	   tREAL8   mStep;
-	   cPt2di   mSzI;
+	   tIPt     mSzI;
 	   tIBox    mIBoxIn;
-	   /*
-
-	   tRPt     mRP0;
-	   tRPt     mMulSz;
-	   */
 };
 
 template <const int Dim> tREAL8  cTilingIndex<Dim>::ComputeStep(const tRBox & aBox, int aNbCase)
@@ -62,25 +63,28 @@ template <const int Dim>  cTilingIndex<Dim>::cTilingIndex(const tRBox & aBox,boo
 {
     StdOut()  <<  " -- SSSS=" << mStep << " " << mSzI << "\n";
     StdOut()  <<  mIBoxIn.P0()  << mIBoxIn.P1() << "\n";
-    StdOut()  <<  mIBoxIn.Proj(cPt2di(1,1))   << mIBoxIn.Proj(cPt2di(-3,-3))  << mIBoxIn.Proj(cPt2di(10,10)) << "\n";
-}
-
-
+    // StdOut()  <<  mIBoxIn.Proj(cPt2di(1,1))   << mIBoxIn.Proj(cPt2di(-3,-3))  << mIBoxIn.Proj(cPt2di(10,10)) << "\n";
 /*
-class cTilingIndex<1>;
-class cTilingIndex<3>;
 */
-template  class cTilingIndex<2>;
-
-void Bench_SpatialIndex(cParamExeBench & aParam)
-{
-     if (! aParam.NewBench("SpatialIndex")) return;
-
-     cTilingIndex<2>  aTI(cBox2dr(cPt2dr(0,0),cPt2dr(2,2)),true,5);
-     FakeUseIt(aTI);
-
-     aParam.EndBench();
 }
+
+template <const int Dim> void cTilingIndex<Dim>::AssertInside(const tRPt & aPt) const
+{
+    MMVII_INTERNAL_ASSERT_tiny(mOkOut || mRBoxIn.Inside(aPt),"cTilingIndex point out");
+}
+
+template <const int Dim>  typename cTilingIndex<Dim>::tIPt  cTilingIndex<Dim>::Index(const tRPt & aPt) const
+{
+     return  mIBoxIn.Proj( Pt_round_down((aPt-mRBoxIn.P0())/mStep) + tIPt::PCste(1)   );
+}
+
+
+template  class cTilingIndex<1>;
+template  class cTilingIndex<2>;
+template  class cTilingIndex<3>;
+
+
+
 /*
 
 void Bench_SpatialIndex(cParamExeBench & aParam)
@@ -104,7 +108,57 @@ template <const int Dim>  typename  cTilingIndex<Dim>::tIPt cTilingIndex<Dim>::I
 }
 
  */
+/* **************************************** */
+/*                                          */
+/*              cTilingIndex                */
+/*                                          */
+/* **************************************** */
 
+class cTestSpatialIndex
+{
+    public :
+        static constexpr int Dim = 2;
+        typedef cPt2dr  tPrimGeom;
+
+	const tPrimGeom & PrimGeom() const {return mPt;}
+         
+    private :
+	cPt2dr  mPt;
+};
+
+
+template <class Type>  class  cTiling : public cTilingIndex<Type::Dim>
+{
+     public :
+           typedef cTilingIndex<Type::Dim> tTI;
+           typedef typename tTI::tRBox     tRBox;
+
+
+	   cTiling(const tRBox &,bool WithBoxOut, int aNbCase);
+     private :
+};
+
+template <class Type> 
+    cTiling<Type>::cTiling(const tRBox & aBox,bool WithBoxOut, int aNbCase)  :
+	   tTI(aBox,WithBoxOut,aNbCase)
+{
+}
+
+
+template class cTiling<cTestSpatialIndex>;
+
+
+void Bench_SpatialIndex(cParamExeBench & aParam)
+{
+     if (! aParam.NewBench("SpatialIndex")) return;
+
+     cTilingIndex<2>  aTI(cBox2dr(cPt2dr(0,0),cPt2dr(2,2)),true,5);
+     FakeUseIt(aTI);
+
+
+
+     aParam.EndBench();
+}
 
 
 
