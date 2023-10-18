1cd3458159c9f1c1a5c25937bc0dc75e1c8afda9
deseilligny <marc.pierrot-deseilligny@ign.fr>
2023-10-18 03:05:31 +0200

    Reengenring mat ess

---
+++ MMVII/include/MMVII_PoseRel.h
@@ -0,0 +1,106 @@
+#ifndef  _MMVII_POSE_REL_H_
+#define  _MMVII_POSE_REL_H_
+
+#include "MMVII_PCSens.h"
+
+namespace MMVII
+{
+
+class cSetHomogCpleDir;  // store tie point as pairs of homologous directions
+class cMatEssential;     // class for representing a matt ess, double rep :  vector 9 + matrix 3x3
+class cCamSimul ;        //  Class for simulating position of set camera having visibility domain in common
+
+// Vect being "a-plat" representation of matrix M, put in Vect the constraint P1 M P2
+void SetVectMatEss(cDenseVect<tREAL8> & aVect,const cPt3dr& aP1,const cPt3dr& aP2);
+// transformate linear rep in a matrix
+cDenseMatrix<tREAL8> Vect2MatEss(const cDenseVect<tREAL8> & aSol);
+//  Add in Sys all the constraint relative to direction of Sys
+void  MatEssAddEquations(const cSetHomogCpleDir & aSetD,cLinearOverCstrSys<tREAL8> & aSys);
+
+
+/**   Class for storing homologous point as unitary direction of bundles (one made
+ *     its no longer needed to access to the internal calibration + the coordonates are bounded).
+ *
+ *     Optionnaly , for normalization, the point can be turned from a rotation
+ */
+
+class cSetHomogCpleDir
+{
+     public :
+        typedef cRotation3D<tREAL8> tRot;
+
+        ///  Create from image homologue + internal calibration
+        cSetHomogCpleDir(const cSetHomogCpleIm &,const cPerspCamIntrCalib &,const cPerspCamIntrCalib &);
+
+        /// make both normalization so that bundles are +- centered onK
+        void NormalizeRot();
+
+        const std::vector<cPt3dr>& VDir1() const; ///< Accessor
+        const std::vector<cPt3dr>& VDir2() const; ///< Accessor
+        void Show() const;
+
+
+        /// 4 bench :  Randomize the rotation 
+        void RandomizeRot();
+        ///  4 bench : Put in current data an outlayer, in Dir1 or Dir2 (randomly), at random position
+        void GenerateRandomOutLayer(double aAmpl);
+
+     private :
+        /// make one normalization so that bundles are +- centered onK
+        void  NormalizeRot(tRot&,std::vector<cPt3dr> &);
+        /// Transormate bundle and accuumlate to memorize transformation
+        void  AddRot(const tRot&,tRot&,std::vector<cPt3dr> &);
+
+        std::vector<cPt3dr>  mVDir1;
+        std::vector<cPt3dr>  mVDir2;
+
+        tRot                 mR1ToInit;
+        tRot                 mR2ToInit;
+};
+
+/** Class to represent the result of essential matrix computation,
+ * essentially 3x3 matrix and an eq
+ */
+
+class cMatEssential
+{
+    public :
+        typedef cDenseMatrix<tREAL8> tMat;
+        typedef cIsometry3D<tREAL8>  tPose;
+
+        /** Constructor use a set of 3D homologous dir, a solver (L1/L2) , and the number, in [0,8] of the variable
+            that is arbirtrarily fixed */
+        cMatEssential(const cSetHomogCpleDir &,cLinearOverCstrSys<tREAL8> & aSys,int aKFix);
+
+        ///  Sigma attenuates big error  E*S / (E+S)  => ~E in 0  , bound to S at infty, if S<=0  return E
+        tREAL8  Cost(const  cPt3dr & aP1,const  cPt3dr &aP2,const tREAL8 & aSigma) const;
+
+        /// No more than average of Cost
+        tREAL8  AvgCost(const  cSetHomogCpleDir &,const tREAL8 & aSigma) const;
+
+        /// No more than
+        tREAL8  KthCost(const  cSetHomogCpleDir &,tREAL8 aProp) const;
+
+        void Show(const cSetHomogCpleDir &) const;
+
+        cMatEssential(const  tMat & aMat);
+
+         tPose ComputePose(const cSetHomogCpleDir & aHoms,tPose * aRes= nullptr) const;
+
+    private :
+        tMat mMat; /// The Ess  matrix itself
+};
+
+/**  For essential matrix as the equations are purely linear,  it is necessary to add
+ *  some arbitray constraint to make the system solvable. 
+ *
+ *  => Make an estimation of this direction
+ */
+
+int   MatEss_GetKMax(const cSetHomogCpleDir & aSetD,tREAL8 aWeightStab,bool Show=false);
+
+
+
+};
+#endif // _MMVII_POSE_REL_H_
+
---
+++ MMVII/src/Sensors/cCamSimul.cpp
@@ -0,0 +1,230 @@
+#include "MMVII_PoseRel.h"
+#include "MMVII_Tpl_Images.h"
+
+
+/* We have the image formula w/o distorsion:
+
+
+*/
+
+namespace MMVII
+{
+
+bool BUGME = false;
+
+/* ************************************** */
+/*                                        */
+/*            cCamSimul                   */
+/*                                        */
+/* ************************************** */
+
+
+cCamSimul::cCamSimul() :
+   mCenterGround (10.0,5.0,20.0),
+   mProfMin      (10.0),
+   mProfMax      (20.0),
+   mBsHMin       (0.1),
+   mBsHMax       (0.5),
+   mRandInterK   (0.1)
+{
+}
+
+cCamSimul::~cCamSimul()
+{
+    DeleteAllAndClear(mListCam);
+    DeleteAllAndClear(mListCalib);
+}
+
+bool cCamSimul::ValidateCenter(const cPt3dr & aP2) const
+{ 
+    if (mListCam.empty()) return true;
+
+    tREAL8 aTetaMin = 1e10;
+    cPt3dr aV20 = aP2 - mCenterGround;
+    for (const auto & aPtr : mListCam)
+    {
+         cPt3dr aV10 = aPtr->Center() - mCenterGround;
+	 UpdateMin(aTetaMin,AbsAngleTrnk(aV10,aV20));
+    }
+    return  (aTetaMin>mBsHMin) && (aTetaMin<mBsHMax);
+}
+
+cPt3dr  cCamSimul::GenAnyCenter(bool SubVert) const
+{
+    if (SubVert)
+       return    mCenterGround 
+	       + cPt3dr(RandUnif_C()/mProfMax,RandUnif_C()/mProfMax,1.0) * RandInInterval(mProfMin,mProfMax);
+
+    return mCenterGround + cPt3dr::PRandUnit() * RandInInterval(mProfMin,mProfMax);
+}
+
+
+cPt3dr  cCamSimul::GenValideCenter(bool SubVert) const
+{
+   cPt3dr aRes = GenAnyCenter(SubVert);
+   while (! ValidateCenter(aRes))
+          aRes = GenAnyCenter(SubVert);
+   return aRes;
+}
+
+
+void cCamSimul::AddCam(cPerspCamIntrCalib * aPC,bool SubVert)
+{
+      cPt3dr aNewC = GenValideCenter(SubVert);
+
+      cPt3dr aK = VUnit(mCenterGround - aNewC);
+      cPt3dr aI = cPt3dr::PRandUnitNonAligned(aK,1e-2);
+      cPt3dr aJ = VUnit(aK ^aI);
+      aI = aJ ^aK;
+
+
+      cRotation3D<tREAL8> aRot(M3x3FromCol(aI,aJ,aK),false);
+
+      aNewC += cPt3dr::PRandC() * mRandInterK;
+      cIsometry3D<tREAL8> aPose(aNewC,aRot);
+
+      mListCam.push_back(new cSensorCamPC("Test",aPose,aPC));
+
+}
+
+void cCamSimul::AddCam(eProjPC aProj,bool SubVert)
+{
+    cPerspCamIntrCalib * aCalib = cPerspCamIntrCalib::RandomCalib(aProj,1);
+
+    mListCalib.push_back(aCalib);
+    AddCam(aCalib,SubVert);
+}
+
+cCamSimul * cCamSimul::Alloc2VIewTerrestrial(eProjPC aProj1,eProjPC aProj2,bool SubVert)
+{
+   cCamSimul * aRes = new cCamSimul();
+
+   aRes->AddCam(aProj1,SubVert);
+   aRes->AddCam(aProj2,SubVert);
+
+   return aRes;
+}
+
+void cCamSimul::TestCam(cSensorCamPC * aCam) const
+{
+	StdOut() << "CC " << aCam->Center()  << " CG=" << mCenterGround << std::endl;
+
+cPt3dr aV = aCam->Center() - mCenterGround;
+
+StdOut()  << " I " << Cos(aV,aCam->AxeI())
+          << " J " << Cos(aV,aCam->AxeI())
+          << " K " << Cos(aV,aCam->AxeK())
+	  << " \n";
+
+	StdOut() << "Vis " <<  aCam->IsVisible(mCenterGround) << std::endl;
+}
+
+void cCamSimul::BenchMatEss
+     (
+        cTimerSegm * aTS,
+        bool         PerfInter
+     )
+{
+    static int aCpt=0;
+    /// cLinearOverCstrSys<tREAL8> *  aSysL1 = AllocL1_Barrodale<tREAL8>(9);
+    // cLinearOverCstrSys<tREAL8> *  aSysL1 = new cLeasSqtAA<tREAL8>(9);
+    cLeasSqtAA<tREAL8> aSysL2(9);
+
+    static int aCptPbL1 = 0;
+
+    for (int aK1=0 ; aK1<(int)eProjPC::eNbVals ; aK1++)
+    {
+        for (int aK2=0 ; aK2<(int)eProjPC::eNbVals ; aK2++)
+        {
+            cAutoTimerSegm aTSSim(aTS,"CreateSimul");
+            aCpt++;
+            cCamSimul * aCamSim = cCamSimul::Alloc2VIewTerrestrial(eProjPC(aK1),eProjPC(aK2),false);
+
+            // we want to test robustness in perfect degenerate & close to degenertae
+            if (PerfInter)
+               aCamSim->mRandInterK = 0.0;
+
+            // Generate 2 cams 
+	    cSensorCamPC * aCam1 = aCamSim->mListCam.at(0);
+	    cSensorCamPC * aCam2 = aCamSim->mListCam.at(1);
+
+            // generate  perfect homologous point
+	    cSetHomogCpleIm aSetH;
+	    size_t aNbPts = 40;
+	    for (size_t aKP=0 ; aKP<aNbPts ; aKP++)
+	    {
+		 aSetH.Add(aCam1->RandomVisibleCple(*aCam2));
+	    }
+
+            // Make 3D direction of points
+	    cSetHomogCpleDir aSetD (aSetH,*(aCam1->InternalCalib()),*(aCam2->InternalCalib()));
+
+            cAutoTimerSegm aTSGetMax(aTS,"GetMaxK");
+            int aKMax =  MatEss_GetKMax(aSetD,1e-6);
+
+            // These point where axe k almost intersect, the z1z2 term of mat ess is probably small
+            // and must not be KMax
+            MMVII_INTERNAL_ASSERT_bench(aKMax!=8,"cComputeMatEssential::GetKMax");
+            
+            // Now test that residual is ~ 0 on these perfect points
+            cAutoTimerSegm aTSL2(aTS,"L2");
+            cMatEssential aMatEL2(aSetD,aSysL2,aKMax);
+
+            {
+                cIsometry3D<tREAL8>  aPRel =  aCam1->RelativePose(*aCam2);
+                // StdOut() << " PROJS  " << E2Str(eProjPC(aK1)) << " " << E2Str(eProjPC(aK2))  << std::endl;
+                aMatEL2.ComputePose(aSetD,&aPRel);
+            }
+            MMVII_INTERNAL_ASSERT_bench(aMatEL2.AvgCost(aSetD,1.0)<1e-5,"Avg cost ");
+
+            cAutoTimerSegm aTSL1(aTS,"L1");
+            cLinearOverCstrSys<tREAL8> *  aSysL1 = AllocL1_Barrodale<tREAL8>(9);
+            cMatEssential aMatEL1(aSetD,*aSysL1,aKMax);
+            MMVII_INTERNAL_ASSERT_bench(aMatEL1.AvgCost(aSetD,1.0)<1e-5,"Avg cost ");
+
+
+            for (int aK=0 ; aK<4 ; aK++)
+                aSetD.GenerateRandomOutLayer(0.1);
+
+            cMatEssential aMatNoise(aSetD,*aSysL1,aKMax);
+
+            delete aSysL1;
+	    
+            if (0)
+            {
+	        StdOut() << "Cpt=" << aCpt 
+		     << " Cost95= "  << aMatNoise.KthCost(aSetD,0.95)  
+		     << " Cost80= "  << aMatNoise.KthCost(aSetD,0.70)  
+		     << " KMax= "  << aKMax
+		     // << " Disp= "  << aSetD.Disp()
+		    << "\n"; 
+                MMVII_INTERNAL_ASSERT_bench(aMatNoise.KthCost(aSetD,0.70) <1e-5,"Kth cost ");
+                MMVII_INTERNAL_ASSERT_bench(aMatNoise.KthCost(aSetD,0.95) >1e-2,"Kth cost ");
+            }
+
+	    // We test if the residual at 70% is almost 0 (with 4/40 outlayers)
+            if (aMatNoise.KthCost(aSetD,0.70)>1e-5)
+               aCptPbL1++;
+
+
+	    if (BUGME) 
+	    {
+
+		StdOut() << "***************************************************" << std::endl;
+                getchar();
+	    }
+
+
+            delete aCamSim;
+        }
+    }
+    StdOut() << "CPT " << aCptPbL1  << " " << aCpt << std::endl;
+
+}
+
+
+}; // MMVII
+
+
+
+
