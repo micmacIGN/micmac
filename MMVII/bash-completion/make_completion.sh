c3a5ab3760430e1e8a95851a3eeff7d1c8602d6e
Christophe Meynard <Christophe.Meynard@ign.fr>
2023-10-30 16:45:34 +0100

    MMVII: bash_completion, improve argument completion (based on semantics)

--- MMVII/bash-completion/make_completion.sh
+++ MMVII/bash-completion/make_completion.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sed -e'/< *mmvii-completion.py)"/a\EOT\n)"' mmvii-completion.template | sed -e'/< *mmvii-completion.py)"/rmmvii-completion.py' -e's/< *mmvii-completion.py)".*/<<'"'"EOT"'"'/'   >  mmvii-completion
+VARNAME='_MMVII_COMPLETION_PYTHON'
+REPLACE='${'${VARNAME}'})"'
+
+sed -e'/< *'${REPLACE}'/a\EOT\n)"' mmvii-completion.template | sed -e'/< *'${REPLACE}'/rmmvii-completion.py' -e's/< *'${REPLACE}'.*/<<'"'"EOT"'"'/'  -e '/'${VARNAME}'/d' >  mmvii-completion
 
--- MMVII/bash-completion/mmvii-completion.py
+++ MMVII/bash-completion/mmvii-completion.py
@@ -9,6 +9,7 @@ debug=0
 cur_word=sys.argv[1]
 comp_cword=int(sys.argv[2])
 comp_line=os.getenv('COMP_LINE')
+screen_columns=int(os.getenv('COLUMNS','80'))
 
 
 if debug>1:
@@ -18,68 +19,116 @@ if debug>1:
   print (f'COMP_LINE=<{comp_line}>',file=sys.stderr)
 
 
-def printFilter(word,words,option=None) -> None:
+def parseList(s) -> list:
+    if not s:
+        return [None,None]
+    return [int(x) if x!='' else None for x in s[1:-1].split(',')]
+
+def print_line(s) -> None:
+    if not s:
+        return
+    spaces_needed = screen_columns - len(s) % screen_columns - 1
+    print (s + ' ' * spaces_needed)
+    print(' ')
+
+def printMsgExit(s,code=0) -> None:
+    print_line(s)
+    print('Options:-o nosort')
+    sys.exit(code)
+
+    
+
+def printFilter(word,words,option='') -> None:
     word_lower = word.lower()
-    matches=[w for w in words if w.lower().startswith(word_lower)]
+    matches=[w for w in sorted(words) if w.lower().startswith(word_lower)]
     if len(matches) == 1:
         print(re.sub(r'^=','',matches[0]))
     else:
         for w in matches:
             print(re.sub(r'^=','',word + w[len(word):]))
-    if option:
-        print(option)
+    print (f"Options:-o nosort {option}")
 
-def printMsgExit(str,code=0) -> None:
-    print(str)
-    print(' ')
-    print('Options:-o nosort')
-    sys.exit(code)
-
-def printSpecHelp(spec, value) -> None:
+def printSpecHelp(all_specs, spec, value) -> None:
     atype = spec['type']
     msg_type = atype.replace('std::','')
     semantic = spec.get('semantic')
-    allowed = None
     allowed = spec.get('allowed')
+    vrange = parseList(spec.get('range'))
+    vsize = parseList(spec.get('vsize'))
     vector = re.search(r'vector<(.*)>',msg_type)
     if vector:
         msg_type='[' + vector.group(1) + ',...]'
     elif atype == 'bool':
         allowed=['True','1','False','0']
-        
-    if not value:
-        msg = f'>Expect <{msg_type}>: {spec["comment"]}'
-        if allowed:
-            msg += ' (Value one of [' + ','.join(allowed) + '])'
-        if semantic and debug>1:
-            msg += ' SEMANTIC:{' + ','.join(semantic) + '}'
+
+    msg=None
+    msg = f'>Expect <{msg_type}>: {spec["comment"]}'
+    if allowed:
+        msg += ' (Value one of [' + ','.join(allowed) + '])'
+    if semantic and debug>0:
+        msg += ' SEMANTIC:{' + ','.join(semantic) + '}'
+
+    if vector and vsize[0] and vsize[0]>1:
+        if not value:
+            printFilter (value,'[','-o nospace')
+            return
         printMsgExit(msg)
 
+    if not value:
+        print_line(msg)
+
+    if vrange[0] and vrange[1]:
+        if vrange[1] - vrange[0] < 20:
+            printFilter(value,[str(i) for i in range(vrange[0],vrange[1]+1)])
+        return
+        
+        
     if allowed:
         printFilter(value,allowed)
         return
-    if msg_type == 'string':
-        print(f'File:{value}')
+        
+    if msg_type != 'string':
+        printMsgExit('')
+    if not semantic:
+        printMsgExit('')
+
+    dir_types = all_specs["config"]["eTa2007DirTypes"]
+    dir_type = set(semantic).intersection(dir_types)
+    if len(dir_type) == 1:
+        dir_type = list(dir_type)[0]
+        sub_dir = all_specs['config']['MMVIIDirPhp'] +  dir_type
+        print(f"File:pushd {sub_dir} >/dev/null 2>&1 && compgen -d -- '{value}'  && popd  >/dev/null 2>&1")
         return
 
-def getApplets() -> dict:
+    file_types = all_specs["config"]["eTa2007FileTypes"]
+    file_types += ('MFP','FDP','I','Out','OptEx')
+    file_type = set(semantic).intersection(file_types)
+    if len(file_type) > 0:
+        print(f"File:compgen -f -- '{value}'")
+    if "DP" in semantic:
+        print(f"File:compgen -d -- '{value}'")
+
+def getAllSpecs() -> dict:
     try:
         result=subprocess.run(['MMVII','GenArgsSpec'],stdout=subprocess.PIPE,stderr=subprocess.DEVNULL,text=True)
     except:
         printMsgExit('>ERROR: MMVII not found.',1)
     try:
-        specs=json.loads(result.stdout)
-        applets=specs['applets']
+        all_specs=json.loads(result.stdout)
     except:
         printMsgExit(">ERROR: Can't get args specification from MMVII.",1)
-    return applets
+    return all_specs
 
 def commandNames(applets) -> None:
     app_names=(a['name'] for a in applets)
     printFilter(cur_word,app_names)
 
 def main() -> int:
-    applets=getApplets()
+    all_specs=getAllSpecs()
+    try:
+        applets=all_specs['applets']
+    except:
+        printMsgExit(">ERROR: Can't get args specification from MMVII.",1)
     
     # 1st argument: MMVII command name
     if comp_cword == 1:
@@ -96,7 +145,7 @@ def main() -> int:
     # Nth first arguments are mandatory
     if comp_cword < len(applet['mandatory'])+2:
         spec = applet['mandatory'][comp_cword-2]
-        printSpecHelp(spec, cur_word)
+        printSpecHelp(all_specs, spec, cur_word)
         return 0
 
     # Others are optionals
@@ -106,14 +155,14 @@ def main() -> int:
         normal=sorted((a['name']+'=' for a in applet['optional'] if a['level'] == 'normal'),key=str.lower)
         tuning=sorted((a['name']+'=' for a in applet['optional'] if a['level'] == 'tuning'),key=str.lower)
         common=sorted((a['name']+'=' for a in applet['optional'] if a['level'] == 'global'),key=str.lower)
-        printFilter(cur_word,normal+tuning+common,'Options:-o nospace -o nosort')
+        printFilter(cur_word,normal+tuning+common,'-o nospace')
         return 0
     # got '='
     arg=arg_split.group(1)
     specs = [ s for s in applet.get('optional') if s['name'].lower() == arg.lower() ] 
     if len(specs) != 1:
         return 0
-    printSpecHelp(specs[0], arg_split.group(3))
+    printSpecHelp(all_specs, specs[0], arg_split.group(3))
     return 0
 
 
