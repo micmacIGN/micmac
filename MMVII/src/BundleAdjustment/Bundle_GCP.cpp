e9171d2ef43bdd40a6b1cbbdde7297615c0efedf
deseilligny <marc.pierrot-deseilligny@ign.fr>
2023-12-13 09:53:40 +0100

    Pt3dr UK ->  refactor

--- MMVII/src/BundleAdjustment/Bundle_GCP.cpp
+++ MMVII/src/BundleAdjustment/Bundle_GCP.cpp
@@ -158,17 +158,63 @@ cPt3dr_UK::cPt3dr_UK(const cPt3dr & aPt) :
 {
 }
 
-void cPt3dr_UK::PutUknowsInSetInterval()
-{
-    mSetInterv->AddOneInterv(mPt);
-}
-
-const cPt3dr & cPt3dr_UK::Pt() const {return mPt;}
-
 cPt3dr_UK::~cPt3dr_UK()
 {
         OUK_Reset();
 }
+
+cPt3dr * cPt3dr_UK::AdrPt() { return & mPt; }
+
+    /* ---------------------------------------- */
+    /*            cBasePt3dr_UK                 */
+    /* ---------------------------------------- */
+
+void cBasePt3dr_UK::PutUknowsInSetInterval()
+{
+    mSetInterv->AddOneInterv(*AdrPt());
+}
+
+const cPt3dr & cBasePt3dr_UK::Pt() const 
+{
+    return *const_cast<cBasePt3dr_UK*>(this)->AdrPt(); 
+}
+
+cBasePt3dr_UK::cBasePt3dr_UK() {}
+
+
+/*
+void cPt3dr_UK::PutUknowsInSetInterval()
+{
+    mSetInterv->AddOneInterv(mPt);
+}
+const cPt3dr & cPt3dr_UK::Pt() const {return mPt;}
+*/
+
+class cCenterPose_UK : public cBasePt3dr_UK,
+                       public cMemCheck
+{
+    public :
+         cCenterPose_UK(tPoseR & aPose) : mPose (&aPose) {}
+         cPt3dr * AdrPt() override {return &(mPose->Tr());}
+    private :
+         tPoseR * mPose;
+};
+
+void TestCompile_cCenterPose_UK()
+{
+    tPoseR * aPose = nullptr;
+    cResolSysNonLinear<tREAL8> *  aR8_Sys = nullptr;
+    std::vector<int> aVIndGlob;
+
+    cCenterPose_UK aCPUK(*aPose);
+    aCPUK.PushIndexes(aVIndGlob);
+    aR8_Sys->AddEqFixCurVar(aCPUK,aCPUK.Pt(),1.0);
+
+   //  Can also use temporary 
+    cCenterPose_UK(*aPose).PushIndexes(aVIndGlob);
+    aR8_Sys->AddEqFixCurVar(cCenterPose_UK(*aPose),aPose->Tr(),1.0);
+}
+
 
 
 };
