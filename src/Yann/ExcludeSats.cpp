73ef9ce3df7b2c9d42c845100506dad7489ac3f7
Charles Villard <charles.villard@epita.fr>
2024-02-08 11:31:53 +0100

    edit: ExcludeStats: fixed ingored attribute warning as error

--- src/Yann/ExcludeSats.cpp
+++ src/Yann/ExcludeSats.cpp
@@ -223,7 +223,9 @@ std::string execCmdOutput(const char* cmd) {
 
 		std::array<char, 128> buffer;
    		std::string result;
-    	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+        const auto deleter = [] (FILE* f) { pclose(f); };
+    	std::unique_ptr<FILE, decltype(deleter)> pipe(popen(cmd, "r"), deleter);
+    	//std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
 	    if (!pipe) {
         	throw std::runtime_error("popen() failed!");
     	}
