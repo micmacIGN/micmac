4c270f51da3e0e79d3f3e7b2faebd5efec086136
Christophe Meynard <Christophe.Meynard@ign.fr>
2024-02-19 12:11:04 +0100

    MMVII: fix missing template specialization declaration

--- MMVII/include/MMVII_Stringifier.h
+++ MMVII/include/MMVII_Stringifier.h
@@ -119,6 +119,10 @@ template <>  const std::string cStrIO<eTypeSerial>::msNameType;
 template <>  const std::string cStrIO<eTyCodeTarget>::msNameType;
 template <>  const std::string cStrIO<eSysCoGeo>::msNameType;
 template <>  const std::string cStrIO<eMTDIm>::msNameType;
+
+template <>  const std::string cStrIO<eTypeSensor>::msNameType;
+template <>  const std::string cStrIO<eFormatSensor>::msNameType;
+
 #endif
 
 /** These functions offer an"easy" interface to cStrIO, however I think
