89b41abe31227b2843512f0f0cf12ecb5b3e427e
Christophe Meynard <Christophe.Meynard@ign.fr>
2024-07-30 15:23:09 +0200

    Revert "Included radiometry factor in new form equation, added possibility to use multi-scale approach, initialise values of unknowns with values obtained in previous iteration along with  hard and soft freezing unknowns. Added option to display last translations and radiometry unknown values. Added command and equation to specifically compute radiometric deformations between images and another for translations."
    This reverts commit c74aefe94fcf497ee72e806284dfa484efb0cb31.

--- MMVII/src/Appli/cSpecMMVII_Appli.cpp
+++ MMVII/src/Appli/cSpecMMVII_Appli.cpp
@@ -239,12 +239,6 @@ std::vector<cSpecMMVII_Appli *> & cSpecMMVII_Appli::InternVecAll()
         TheVecAll.push_back(&TheSpec_TestProj);
         TheVecAll.push_back(&TheSpec_ChSysCo);
         TheVecAll.push_back(&TheSpec_CreateCalib);
-        TheVecAll.push_back(&TheSpec_RandomGeneratedDelaunay);
-        TheVecAll.push_back(&TheSpec_ComputeTriangleDeformation);
-        TheVecAll.push_back(&TheSpec_ComputeTriangleDeformationTrRad);
-        TheVecAll.push_back(&TheSpec_ComputeTriangleDeformationTranslation);
-        TheVecAll.push_back(&TheSpec_ComputeTriangleDeformationRadiometry);
-        TheVecAll.push_back(&TheSpec_ComputeTriangleDeformationRad);
         TheVecAll.push_back(&TheSpec_ImportTiePMul);
         TheVecAll.push_back(&TheSpec_ImportMesImGCP);
         TheVecAll.push_back(&TheSpecImportExtSens);
