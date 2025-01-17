1f05824090c94ffa43000d5a465b997ee491d829
jmmuller <jean-michael.muller@ign.fr>
2025-01-17 12:17:08 +0100

    update MMVII-UseCaseDataSet/Aerien-Sat

--- MMVII/MMVII-UseCaseDataSet/Aerien-Sat/Cmd3-Mixt.sh
+++ MMVII/MMVII-UseCaseDataSet/Aerien-Sat/Cmd3-Mixt.sh
@@ -9,7 +9,7 @@ MMVII TiePConvert AllIm.xml MMV1 Mixt  UpSByIm=ScaleImage.xml
 #  To TEST
 #  MMVII OriBundleAdj  AllIm.xml RTLD0  Test  TPDir=Mixt TiePWeight=[1,1]
 
-MMVII OriBundleAdj  AllIm.xml RTLD0  Test   AddGCPW=[[RTLSat,1,1],[AerRTL,0.1,0.1]]   AddTieP=[[Mixt,1,1],[ORGI,1,1],[Vexcell,1,1]] RefOri=[RTLD0,0.5] PPFzCal='[pbK].*'
+MMVII OriBundleAdj  AllIm.xml RTLD0  Test   GCP2D=[[ORGI,1],[AerRTL,0.1]] GCP3D=[[RTLSat,1],[AerRTL,0.1]] TPDir=Mixt TiePWeight=[1,1]  AddTieP=[[ORGI,1,1],[Vexcell,1,1]] RefOri=[RTLD0,0.5] PPFzCal='[pbK].*'
 
-MMVII OriBundleAdj  AllIm.xml RTLD0  Test   AddGCPW=[[RTLSat,1,1],[AerRTL,0.1,0.1]]   AddTieP=[[Mixt,1,1],[ORGI,1,1],[Vexcell,1,1]] RefOri=[RTLD0,0.5] PPFzCal='.*'
+MMVII OriBundleAdj  AllIm.xml RTLD0  Test   GCP2D=[[ORGI,1],[AerRTL,0.1]] GCP3D=[[RTLSat,1],[AerRTL,0.1]] TPDir=Mixt TiePWeight=[1,1]  AddTieP=[[ORGI,1,1],[Vexcell,1,1]] RefOri=[RTLD0,0.5] PPFzCal='.*'
 
