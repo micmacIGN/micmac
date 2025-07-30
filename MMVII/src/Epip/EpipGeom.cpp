22690604a83580c7debf3521d77aa07a0dc20b91
Christophe Meynard <Christophe.Meynard@ign.fr>
2025-07-30 16:57:14 +0200

    Epip

--- MMVII/src/Epip/EpipGeom.cpp
+++ MMVII/src/Epip/EpipGeom.cpp
@@ -41,8 +41,9 @@ private :
     std::string  mImName2;
     cSensorImage *mSens1;
     cSensorImage *mSens2;
-    int mDeltaXY;
-    int mDeltaZ;
+    int mDegre;
+    int mNbXY;
+    int mNbZ;
     int mMinZ;
     int mMaxZ;
 };
@@ -53,8 +54,9 @@ cAppli_EpipGeom::cAppli_EpipGeom (
     )
     : cMMVII_Appli  (aVArgs,aSpec)
     , mPhProj       (*this)
-    , mDeltaXY(100)
-    , mDeltaZ(100)
+    , mDegre(5)
+    , mNbXY(100)
+    , mNbZ(3)
     , mMinZ(100)
     , mMaxZ(1000)
 {
@@ -74,8 +76,9 @@ cCollecSpecArg2007 & cAppli_EpipGeom::ArgOpt(cCollecSpecArg2007 & anArgOpt)
 {
     
     return anArgOpt
-           << AOpt2007(mDeltaXY,"deltaXY","XY step",{eTA2007::HDV})
-           << AOpt2007(mDeltaZ,"deltaXY","XY step",{eTA2007::HDV})
+           << AOpt2007(mDegre,"Degre","Poly degre",{eTA2007::HDV})
+           << AOpt2007(mNbXY,"XYSteps","Nb XY steps",{eTA2007::HDV})
+           << AOpt2007(mNbZ,"ZSteps","Nb Z steps",{eTA2007::HDV})
            << AOpt2007(mMinZ,"minZ","Z start",{eTA2007::HDV})
            << AOpt2007(mMaxZ,"maxZ","Z stopt",{eTA2007::HDV})
         ;
@@ -89,18 +92,25 @@ std::tuple<cPt2dr, cPt2dr, cHCompatList> cAppli_EpipGeom::GenerateData(
     cSensorImage *aSensor2
     )
 {
+    // TODOCM: Determiner min & max Z (stenopee, RPC)
+    // TODOCM: X & Y pas trop pres des bords ?
+    
+    
     cHCompatList HCompatList;
     auto c1 = cPt2dr(0,0);
     auto d2  = cPt2dr(0,0);
     int t = 0;
     unsigned n = 0;
-    for (int x = aRect1.P0().x(); x < aRect1.P1().x(); x += mDeltaXY) {
-        for (int y = aRect1.P0().y(); y < aRect1.P1().y(); y += mDeltaXY) {
-            for (int z = mMinZ; z < mMaxZ; z += mDeltaZ) {
+    int deltaZ = (mMaxZ - mMinZ) / mNbZ;
+    int deltaX = aRect1.Sz().x() / mNbXY;
+    int deltaY = aRect1.Sz().y() / mNbXY;
+    for (int x = aRect1.P0().x(); x < aRect1.P1().x(); x += deltaX) {
+        for (int y = aRect1.P0().y(); y < aRect1.P1().y(); y += deltaY) {
+            for (int z = mMinZ; z < mMaxZ; z += deltaZ) {
                 t++;
                 auto p1 = cPt2dr(x,y);
                 auto p1_z = cPt3dr(x,y,z);
-                auto p1p_z = cPt3dr(x,y,z+mDeltaZ);
+                auto p1p_z = cPt3dr(x,y,z+deltaZ);
                 auto p2 = aSensor2->Ground2Image(aSensor1->ImageAndZ2Ground(p1_z));
                 if (! aRect2.InsideBL(p2))
                     continue;
@@ -119,6 +129,7 @@ std::tuple<cPt2dr, cPt2dr, cHCompatList> cAppli_EpipGeom::GenerateData(
     d2 = d2 / double(n);
     
     std::cout << n << "/" << t << std::endl;
+
     return {c1,d2,std::move(HCompatList)};
 }
 
@@ -139,33 +150,93 @@ int cAppli_EpipGeom::Exe()
     StdOut() << x.P1() << "," << x.P2() << std::endl;
     StdOut() << mSens1->Ground2Image(x.P2()) << std::endl;
     
-    auto [c1,d2,List1] = GenerateData(mImRect1, mSens1, mImRect2, mSens2);
-    auto [c2,d1,List2] = GenerateData(mImRect2, mSens2, mImRect1, mSens1);
+    auto [centre1,dir2,List1] = GenerateData(mImRect1, mSens1, mImRect2, mSens2);
+    auto [centre2,dir1,List2] = GenerateData(mImRect2, mSens2, mImRect1, mSens1);
     
-    
-    std::cout <<  "List p1,p2: " << std::endl;
-    for (const auto& pair : List1)
-        std::cout << pair.p1 << " -> " << pair.p2 << std::endl;
-    
-    // TODO: Check d1 and d2 /= 0
+    // TODOCM: Check almost epip
+    // if ((dir2.x+dir1.x) <0)
+    // {
+    //     dir1 = -dir1;
+    //     dir2 = -dir2;
+    // }
+   
+    // TODOCM: Check d1 and d2 /= 0
     // pair.p1 from Im1 and pair.p2 from Im2
     for (auto& pair : List1) {
-        pair.p1 = (pair.p1 - c1) / d1;
-        pair.p2 = (pair.p2 - c2) / d2;
+        pair.p1 = (pair.p1 - centre1) / dir1;
+        pair.p2 = (pair.p2 - centre2) / dir2;
     }
     
     // pair.p1 from Im2 and pair.p2 from Im1
     for (auto& pair : List2) {
-        pair.p1 = (pair.p1 - c2) / d2;
-        pair.p2 = (pair.p2 - c1) / d1;
+        pair.p1 = (pair.p1 - centre2) / dir2;
+        pair.p2 = (pair.p2 - centre1) / dir1;
     }
     
-    
+/*    
     std::cout <<  "Norm List p1,p2: " << std::endl;
     for (const auto& pair : List1)
         std::cout << pair.p1 << " -> " << pair.p2 << std::endl;
+  */
+    
+    /* V1 Calculus */
+    /* v1(x,y) = y + S(i=1->d, S(j=0->d-i; C(i,j) * x^i * y^j ) )
+     * x = p1.x(); y = p1.y(); v1(x,y) = p2.y()
+     */
+    int nbVar1 = (mDegre+1) * (mDegre+2) / 2  - (mDegre + 1);
+    cDenseVect<tREAL8> v1(nbVar1);
+    cLeasSqtAA<tREAL8> leastSq1(nbVar1);
+    cDenseVect<tREAL8> coeffs1(nbVar1);
+    
+    for (auto& pair : List1) {
+        int n = 0;
+        tREAL8 x = pair.p1.x();
+        for (int i=1; i<=mDegre; i++) {
+            tREAL8 y = 1;
+            for (int j=0; j<=mDegre-i; j++) {
+                coeffs1(n) = x*y;
+                y *= pair.p1.y();
+                n++;
+            }
+            x *= pair.p1.x();
+        }
+        leastSq1.PublicAddObservation(1,coeffs1,pair.p2.y() - pair.p1.y());
+    }
+    v1 = leastSq1.PublicSolve();
+    printf("V1 var %lf\n",leastSq1.VarCurSol());
+    
+    /* V2 Calculus */
+    /* v2(x,y) = S(i=0->d; S(j->d-i; C(i,j) * x^i * y^j ) )
+     * x = p1.x(); y = p1.y(); v2(x,y) = p2.y()
+     */
+    int nbVar2 = (mDegre+1) * (mDegre+2) / 2 ;
+    cDenseVect<tREAL8> v2(nbVar2);
+    cLeasSqtAA<tREAL8> leastSq2(nbVar2);
+    cDenseVect<tREAL8> coeffs2(nbVar2);
+    
+    for (auto& pair : List2) {
+        int n = 0;
+        tREAL8 x = 1;
+        for (int i=0; i<=mDegre; i++) {
+            tREAL8 y = 1;
+            for (int j=0; j<=mDegre-i; j++) {
+                coeffs2(n) = x*y;
+                y *= pair.p1.y();
+                n++;
+            }
+            x *= pair.p1.x();
+        }
+        leastSq2.PublicAddObservation(1,coeffs2,pair.p2.y());
+    }
+    v2 = leastSq2.PublicSolve();
+    printf("V2 var %lf\n",leastSq2.VarCurSol());
     
     
+    // TODOCM: Calcul fonctions inverses W1 W2
+    
+    // TODOCM: Utilisation de  v1 O R et de R-1 O W1
+    
+    // TODOCM: jeu test !!
     return EXIT_SUCCESS;
 }
 
