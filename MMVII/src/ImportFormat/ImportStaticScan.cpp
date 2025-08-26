eee57294330ac88bbf3ea4519c4e3cffa529d5b3
jmmuller <jean-michael.muller@ign.fr>
2025-08-26 18:15:25 +0200

    continue cAppli_ImportStaticScan

--- MMVII/src/ImportFormat/ImportStaticScan.cpp
+++ MMVII/src/ImportFormat/ImportStaticScan.cpp
@@ -13,37 +13,39 @@
 
 namespace MMVII
 {
-   /* ********************************************************** */
-   /*                                                            */
-   /*                 cAppli_ImportStaticScan                    */
-   /*                                                            */
-   /* ********************************************************** */
+/* ********************************************************** */
+/*                                                            */
+/*                 cAppli_ImportStaticScan                    */
+/*                                                            */
+/* ********************************************************** */
 
 class cAppli_ImportStaticScan : public cMMVII_Appli
 {
-     public :
-        cAppli_ImportStaticScan(const std::vector<std::string> & aVArgs,const cSpecMMVII_Appli & aSpec);
-        int Exe() override;
-        cCollecSpecArg2007 & ArgObl(cCollecSpecArg2007 & anArgObl) override ;
-        cCollecSpecArg2007 & ArgOpt(cCollecSpecArg2007 & anArgOpt) override ;
+public :
+    cAppli_ImportStaticScan(const std::vector<std::string> & aVArgs,const cSpecMMVII_Appli & aSpec);
+    int Exe() override;
+    cCollecSpecArg2007 & ArgObl(cCollecSpecArg2007 & anArgObl) override ;
+    cCollecSpecArg2007 & ArgOpt(cCollecSpecArg2007 & anArgOpt) override ;
 
-        std::vector<std::string>  Samples() const override;
-     private :
+    std::vector<std::string>  Samples() const override;
+private :
 
 
-	// Mandatory Arg
-	std::string              mNameFile;
-	std::string              mStationName;
+    // Mandatory Arg
+    std::string              mNameFile;
+    std::string              mStationName;
     std::string              mScanName;
 
-	// Optional Arg
-	tREAL8                   mAngTolerancy;
+    // Optional Arg
+    tREAL8                   mAngTolerancy;
+    std::string              mTransfoIJK;
 
 };
 
 cAppli_ImportStaticScan::cAppli_ImportStaticScan(const std::vector<std::string> & aVArgs,const cSpecMMVII_Appli & aSpec) :
-   cMMVII_Appli    (aVArgs,aSpec),
-   mAngTolerancy   (1e-6)
+    cMMVII_Appli    (aVArgs,aSpec),
+    mAngTolerancy   (1e-6),
+    mTransfoIJK         ("ijk")
 {
 }
 
@@ -53,20 +55,30 @@ cCollecSpecArg2007 & cAppli_ImportStaticScan::ArgObl(cCollecSpecArg2007 & anArgO
            <<  Arg2007(mNameFile ,"Name of Input File",{eTA2007::FileAny})
            <<  Arg2007(mStationName ,"Station name",{eTA2007::Topo}) // TODO: change type to future station
            <<  Arg2007(mScanName ,"Scan name",{eTA2007::Topo}) // TODO: change type to future scan
-           ;
+        ;
 }
 
 cCollecSpecArg2007 & cAppli_ImportStaticScan::ArgOpt(cCollecSpecArg2007 & anArgOpt)
 {
     return    anArgOpt
            << AOpt2007(mAngTolerancy,"AngTol","Angle tolerancy",{eTA2007::HDV})
-    ;
+           << AOpt2007(mTransfoIJK,"Transfo","Transfo to have primariy rotation axis as Z and X as theta origin",{{eTA2007::HDV}})
+        ;
 }
 
+cPt3dr cart2spher(const cPt3dr & aPtCart) // returns theta phi dist
+{
+    // atan2($3,sqrt($1*$1+$2*$2))
+    tREAL8 theta =  atan2(aPtCart.y(),aPtCart.x());
+    tREAL8 dist = Norm2(aPtCart);
+    tREAL8 distxy = sqrt(aPtCart.BigX2()+aPtCart.BigY2());
+    tREAL8 phi =  atan2(aPtCart.z(),distxy);
+    return {theta, phi, dist};
+}
 
 int cAppli_ImportStaticScan::Exe()
 {
-    cTriangulation3D<float> aTriangulation3D(mNameFile);
+    cTriangulation3D<tREAL8> aTriangulation3D(mNameFile);
 
     StdOut() << "Got " <<aTriangulation3D.NbPts() <<" points.\n";
     if (aTriangulation3D.HasPtAttribute())
@@ -84,33 +96,44 @@ int cAppli_ImportStaticScan::Exe()
     }
     StdOut() << "...\n";
 
+    cRotation3D<tREAL8>  aRotFrame = cRotation3D<tREAL8>::RotFromCanonicalAxes(mTransfoIJK);
+    // check theta-phi :
+
+    StdOut() << "Spherical sample:\n";
+    for (size_t i=0; (i<10)&&(i<aTriangulation3D.VPts().size()); ++i)
+    {
+        StdOut() << cart2spher(aRotFrame.Value(aTriangulation3D.KthPts(i)));
+        StdOut() << "\n";
+    }
+    StdOut() << "...\n";
+
     return EXIT_SUCCESS;
 }
 
 std::vector<std::string>  cAppli_ImportStaticScan::Samples() const
 {
-   return 
-   {
+    return
+        {
 
-   };
+        };
 }
 
 
 tMMVII_UnikPApli Alloc_ImportStaticScan(const std::vector<std::string> & aVArgs,const cSpecMMVII_Appli & aSpec)
 {
-   return tMMVII_UnikPApli(new cAppli_ImportStaticScan(aVArgs,aSpec));
+    return tMMVII_UnikPApli(new cAppli_ImportStaticScan(aVArgs,aSpec));
 }
 
 cSpecMMVII_Appli  TheSpec_ImportStaticScan
-(
-     "ImportStaticScan",
-      Alloc_ImportStaticScan,
-      "Import static scan cloud point into instrument raster geometry",
-      {eApF::Cloud},
-      {eApDT::Ply},
-      {eApDT::MMVIICloud},
-      __FILE__
-);
+    (
+        "ImportStaticScan",
+        Alloc_ImportStaticScan,
+        "Import static scan cloud point into instrument raster geometry",
+        {eApF::Cloud},
+        {eApDT::Ply},
+        {eApDT::MMVIICloud},
+        __FILE__
+        );
 
 }; // MMVII
 
