872038d0589c620fd3d0d0c21907cf57b0f7ee60
Christophe Meynard <Christophe.Meynard@ign.fr>
2026-02-27 17:37:31 +0100

    epip class

--- MMVII/src/Epip/cEpipolar.cpp
+++ MMVII/src/Epip/cEpipolar.cpp
@@ -34,7 +34,7 @@ cSetHomogCpleIm GenRandomHomolSet(const cSensorImage *aSensor1,const cSensorImag
     return aSetHomogCpleIm;
 }
 
-
+#if 0
 void GenTraverseHomolSet(const cSensorImage *aSensor1,const cSensorImage *aSensor2, int aNbXY, cSetHomogCpleIm& aSetHomogCpleIm)
 {
     auto aBoxIm1 = aSensor1->PixelDomain().Box();
@@ -48,81 +48,127 @@ void GenTraverseHomolSet(const cSensorImage *aSensor1,const cSensorImage *aSenso
     
     for (int x = aXMin ; x < aXMax; x += deltaX) {
         for (int y = aYMin; y < aYMax; y += deltaY) {
-            AddHomolSetIfVisible(cPt2dr(x,y)
+            AddHomolSetIfVisible(cPt2dr(x,y));
         }
-
-    return aSetHomogCpleIm;
-}
-} // namespce anonymous
-
-static std::pair<std::optional<cPt2dr>,cPt2dr> toto(const cSegmentCompiled<double,3>& aBundle, double anAbscissa1, double anAbscissa2, const cSensorImage *aSensor)
-{
-    auto p2g = aBundle.PtOfAbscissa(anAbscissa1);
-    auto p2gp = aBundle.PtOfAbscissa(anAbscissa2);
-    if (! aSensor->IsVisible(p2g) || !aSensor->IsVisible(p2gp))
-        return {std::nullopt,{}};
-    auto p2 = aSensor->Ground2Image(p2g);
-    auto p2p = aSensor->Ground2Image(p2gp);
-    return {p2, p2p};
-}
-
-
-template<typename T>
-std::tuple<cPt2dr, cPt2dr,cPt2dr, cPt2dr, cSetHomogCpleIm> cEpipolarCouple<T>::GenerateData(
-    const cSensorImage *aSensor1,
-    const cSensorImage *aSensor2,
-    const cSetHomogCpleIm& aSetHomogCpleIm
-    
-    )
-{
+    }
+    cSetHomogCpleIm aSetHomogCpleIm;
     auto c1 = cPt2dr(0,0);
-    auto c2 = cPt2dr(0,0);
-    auto d1 = cPt2dr(0,0);
-    auto d2 = cPt2dr(0,0);
-    double aZStep = 0.1;
+    auto d2  = cPt2dr(0,0);
 
-    int n1 = 0;
-    int n2 = 0;
-    cSetHomogCpleIm aList;
-    for (const auto& aHPair : aSetHomogCpleIm.SetH()) {
-        auto aPtInter = aSensor1->PInterBundle(aHPair,*aSensor2);
-        auto aBundle1 = cSegmentCompiled(aSensor1->Image2Bundle(aHPair.mP1));
-        auto aBundle2 = cSegmentCompiled(aSensor2->Image2Bundle(aHPair.mP2));
-        auto anAbscissa1 = aBundle1.Abscissa(aPtInter);
-        auto anAbscissa2 = aBundle2.Abscissa(aPtInter);
+    auto C1 = cPt2dr(1000,750);
+    auto C2 = cPt2dr(1000,750);
 
-        for (double aKz = 0.9; aKz <= 1.1; aKz += aZStep)
-        {
-            auto [p2,p2p] = toto(aBundle1, anAbscissa1 * aKz, anAbscissa1 *(aKz+aZStep), aSensor2);
-            if (p2)
-            {
-                aList.Add(cHomogCpleIm(aHPair.mP1,*p2));
-                d2 += VUnit(p2p - *p2);
-                c1 += aHPair.mP1;
-                c2 += aHPair.mP2;
-                n2++;
+    auto inter_g = aSensor1->PInterBundle(cHomogCpleIm(C1,C2),*aSensor2);
+    aZ = inter_g.z();
+    auto aMinZ = aZ * .8;
+    auto aMaxZ = aZ * 1.2;
 
-            }
-            auto [p1,p1p] = toto(aBundle2, anAbscissa2 * aKz, anAbscissa2 *(aKz+aZStep), aSensor1);
-            if (p1)
-            {
-                aList.Add(cHomogCpleIm(*p1,aHPair.mP2));
-                d1 += VUnit(p1p - *p1);
-                c1 += aHPair.mP1;
-                c2 += aHPair.mP2;
-                n1++;
+    int t = 0;
+    unsigned n = 0;
+    double deltaZ = (aMaxZ - aMinZ) / mNbZ;
+    int deltaX = aRect1.Sz().x() / mNbXY;
+    int deltaY = aRect1.Sz().y() / mNbXY;
 
+    for (int x = aRect1.P0().x(); x < aRect1.P1().x(); x += deltaX) {
+        for (int y = aRect1.P0().y(); y < aRect1.P1().y(); y += deltaY) {
+            auto p1 = cPt2dr(x,y);
+            auto bundle = aSensor1->Image2Bundle(p1);
+            auto vect = VUnit(bundle.P2() - bundle.P1());
+            for (double z = aMinZ; z < aMaxZ; z += deltaZ) {
+                t++;
+                auto p1g = bundle.P1() + vect * z;
+                auto p2 = aSensor2->Ground2Image(p1g);
+                if (! aRect2.InsideBL(p2))
+                    continue;
+                auto p1pg = bundle.P1() + vect * (z + deltaZ);
+                auto p2p = aSensor2->Ground2Image(p1pg);
+                if (! aRect2.InsideBL(p2p))
+                    continue;
+                n ++;
+                aSetHomogCpleIm.Add(cHomogCpleIm(p1,p2));
+                c1 += p1;
+                d2 += VUnit(p2p - p2);
             }
         }
     }
-    c1 = c1 / double(n1+n2);
-    c2 = c2 / double(n1+n2);
+    c1 = c1 / double(n);
+    d2 = d2 / double(n);
 
-    d1 = VUnit(d1 / double(n1));
-    d2 = VUnit(d2 / double(n2));
 
-    return std::tuple(c1,c2,d1,d2,aList);
+    return aSetHomogCpleIm;
 }
+#endif
+
+struct GenDataCtxt
+{
+    enum class eBundleFrom{FIRST,SECOND};
+    
+    void Generate(
+        const cSensorImage *aSensor1,
+        const cSensorImage *aSensor2,
+        const cSetHomogCpleIm& aSetHomogCpleIm
+    )
+    {
+        double aZStep = 0.1;
+        
+        for (const auto& aHPair : aSetHomogCpleIm.SetH()) {
+            auto aPtInter = aSensor1->PInterBundle(aHPair,*aSensor2);
+            auto aBundle1 = cSegmentCompiled(aSensor1->Image2Bundle(aHPair.mP1));
+            auto aBundle2 = cSegmentCompiled(aSensor2->Image2Bundle(aHPair.mP2));
+            double aDist1 = aBundle1.Abscissa(aPtInter);
+            double aDist2 = aBundle2.Abscissa(aPtInter);
+            
+            for (double aKz = 0.9; aKz <= 1.1; aKz += aZStep)
+            {
+                AddCouple(aBundle1, aSensor2, aHPair, aDist1 * aKz, aDist1 * (aKz+aZStep), eBundleFrom::FIRST);
+                AddCouple(aBundle2, aSensor1, aHPair, aDist2 * aKz, aDist2 * (aKz+aZStep), eBundleFrom::SECOND);
+            }
+        }
+        Normalize();
+    }
+
+    void AddCouple(const tSegComp3dr& aBundle, const cSensorImage *aSensor, const cHomogCpleIm& aHPair, double aDist1, double aDist2, eBundleFrom aBundleFrom)
+    {
+        auto p2g = aBundle.PtOfAbscissa(aDist1);
+        auto p2gp = aBundle.PtOfAbscissa(aDist2);
+        if (! aSensor->IsVisible(p2g) || !aSensor->IsVisible(p2gp))
+            return;
+        auto p2 = aSensor->Ground2Image(p2g);
+        auto p2p = aSensor->Ground2Image(p2gp);
+        c1 += aHPair.mP1;
+        c2 += aHPair.mP2;
+        if (aBundleFrom == eBundleFrom::FIRST) {
+            homogSet.Add(cHomogCpleIm(aHPair.mP1,p2));
+            d2 += VUnit(p2p - p2);
+            n2++;
+        } else {
+            homogSet.Add(cHomogCpleIm(p2,aHPair.mP2));
+            d1 += VUnit(p2p - p2);
+            n1++;
+        }
+    }
+
+    void Normalize()
+    {
+        c1 = c1 / double(n1+n2);
+        c2 = c2 / double(n1+n2);
+        d1 = VUnit(d1 / double(n1));
+        d2 = VUnit(d2 / double(n2));
+    }
+
+   
+    cSetHomogCpleIm homogSet;
+    cPt2dr c1{0,0};
+    cPt2dr c2{0,0};
+    cPt2dr d1{0,0};
+    cPt2dr d2{0,0};
+    int n1;
+    int n2;
+};
+
+
+} // namespace anonymous
+
 
 
 template<typename T>
@@ -132,7 +178,10 @@ cEpipolarCouple<T> cEpipolarCouple<T>::FromSensors(
     int aDegree
     )
 {
-    auto [aCentre1, aCentre2, aDir1, aDir2, aListH] = GenerateData(aSensor1, aSensor2);
+    auto aSetHomol = GenRandomHomolSet(aSensor1, aSensor2, 10000);
+    GenDataCtxt aCtxt;
+    aCtxt.Generate(aSensor1, aSensor2, aSetHomol);
+   
 
     auto anEpipolarCouple = std::make_unique<cEpipolarCouple<T>(aCentre1,aDir1,aCentre2,aDir2,aDegree));
             
